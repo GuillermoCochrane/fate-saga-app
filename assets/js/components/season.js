486203befb039740208375e3e0d8d452dad7a7cd
GuillermoCochrane <guilleac81@gmail.com>
2025-09-20 02:26:49 -0300

    Módulo de componente season corregido por cambios de nombres de estilos

--- assets/js/components/season.js
+++ assets/js/components/season.js
@@ -3,31 +3,31 @@
 import { createElement, createDetails, createLabel, createCheckbox } from '../utilities/dom.js';
 import { episodeCreator } from './episode.js';
 
-// Crear elemento de anime con sus episodios
+// Crear elemento de la temporada con sus episodios
 export function seasonContainerCreator(season, sagaName, handleToggleCheckbox) {
     //Creacion de los elementos
-    const $animeDiv = createElement('div', 'anime-item');
-    const $details = createDetails('anime-details', season.opened); //creamos el elemento details, abierto cuando corresponda
+    const $seasonDiv = createElement('div', 'season-item');
+    const $details = createDetails('season-details', season.opened); //creamos el elemento details, abierto cuando corresponda
     const $summary = seasonSummaryCreator(sagaName, season, handleToggleCheckbox);  //creamos el summary de la temporada
     const $seasonContainer = seasonCreator(season, sagaName, handleToggleCheckbox); //creamos el contenedor de la temporada
     
     $details.appendChild($summary);         //agregamos el summary al details
     $details.appendChild($seasonContainer); //Agregamos el contenedor de la temporada al details
-    $animeDiv.appendChild($details);        //agregamos el details al contenedor general de la teporada
+    $seasonDiv.appendChild($details);        //agregamos el details al contenedor general de la temporada
     /*
         hasta aca tendriamos (
-        <div class="anime-item">
-            <details class="anime-details">
+        <div class="season-item">
+            <details class="season-details">
                 --- con todo lo de summary ---
                 <div class="checklist">
                     <div class="episode-list">
-                        --- todos los episodios del anime ---
+                        --- todos los episodios de la temporada ---
                     </div>
                 </div>
             </details>
         </div>
     */
-    return $animeDiv; //devuelve el contendeor de la temporada completo, con todos sus elementos hijos
+    return $seasonDiv; //devuelve el contendeor de la temporada completo, con todos sus elementos hijos
 }
 
 // Crear el contenedor de la temporada
@@ -50,35 +50,35 @@ export function seasonCreator(season, sagaName, handleToggleCheckbox) {
 
 // Crear el summary de la temporada
 export function seasonSummaryCreator(sagaName, season, handleToggleCheckbox) {
-    const $summary = createElement('summary', 'anime-summary');
+    const $summary = createElement('summary', 'season-summary');
     const checkboxID = `main-${season.id}`; //id del checkbox
     const checkBoxEventHandler = () => handleToggleCheckbox(season.id, sagaName); //funcion que se ejecuta al cambiar el estado del checkbox
     //const checkBoxEventHandler = () => toggleSeasonComplete(season.id, sagaName, checklistdata); //funcion que se ejecuta al cambiar el estado del checkbox
-    // Checkbox para el anime completo
-    const $animeCheckbox = createCheckbox(checkboxID, season.completed, checkBoxEventHandler);
+    // Checkbox para la temporada completa
+    const $seasonCheckbox = createCheckbox(checkboxID, season.completed, checkBoxEventHandler);
 
-    const $label = createLabel(season.label, checkboxID, 'anime-label');              //label del checkbox 
-    const $formatSpan = createElement('span', 'anime-format', `${season.format}`);    //span con el formato del anime
-    const $titleContainer = createElement('div', 'anime-title-container');          //contenedor para el label y el formato
+    const $label = createLabel(season.label, checkboxID, 'season-label');              //label del checkbox 
+    const $formatSpan = createElement('span', 'season-format', `${season.format}`);    //span con el formato de la temporada
+    const $titleContainer = createElement('div', 'season-title-container');          //contenedor para el label y el formato
     
     // Agregamos el label y el span al contenedor de título
     $titleContainer.appendChild($label);
     $titleContainer.appendChild($formatSpan);
-    /*  <div class="anime-title-container">
-            <label for="main-1" class="anime-label">Titulo del anime</label>
-            <span class="anime-format">Formato del anime</span>
+    /*  <div class="season-title-container">
+            <label for="main-1" class="season-label">Titulo de la temporada</label>
+            <span class="season-format">Formato de la temporada</span>
         </div> 
     */
     // Agregamos el checkbox y el contenedor de título al summary
-    $summary.appendChild($animeCheckbox);
+    $summary.appendChild($seasonCheckbox);
     $summary.appendChild($titleContainer);
     /* 
         hasta aca tendriamos (1)
-        <summary class="anime-summary">
+        <summary class="season-summary">
             <input type="checkbox" id="main-1">
-            <div class="anime-title-container">
-                <label for="main-1" class="anime-label">Titulo del anime</label>
-                <span class="anime-format">Formato del anime</span>
+            <div class="season-title-container">
+                <label for="main-1" class="season-label">Titulo de la temporada</label>
+                <span class="season-format">Formato de la temporada</span>
             </div>
         </summary>
     */    
