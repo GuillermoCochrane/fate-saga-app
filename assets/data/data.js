c1135c180c29e938895982fae6faf7fc7cb20195
GuillermoCochrane <guilleac81@gmail.com>
2026-01-17 02:52:51 -0300

    Agregadas entradas de Fate/Zero S2 especial 2

--- assets/data/data.js
+++ assets/data/data.js
@@ -313,6 +313,34 @@ export const fateChecklistData = {
             }
           ]
         },
+        {
+          id: "fate-zero-remix",
+          label: "Fate/Zero Remix",
+          format: "Especial",
+          note: "Dos especiales recapitulativos de los primeros 13 episodios",
+          completed: false,
+          opened: false,
+          episodes: [
+            {
+              id: "fz-rm-1",
+              number: 1,
+              label: "Fate/Zero Remix I",
+              romaji: "Fate/Zero Remix I",
+              jp: "Fate/Zero Remix I",
+              es: "Fate/Zero Remix I",
+              completed: false
+            },
+            {
+              id: "fz-rm-2",
+              number: 2,
+              label: "Fate/Zero Remix II", 
+              romaji: "Fate/Zero Remix II",
+              jp: "Fate/Zero Remix II",
+              es: "Fate/Zero Remix II",
+              completed: false
+            }
+          ]
+        },
       ]
     },
     {
