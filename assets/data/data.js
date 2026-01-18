be630f75c468e0e60bd41761acdd07513d24d8eb
GuillermoCochrane <guilleac81@gmail.com>
2026-01-18 04:55:45 -0300

    Modificadas entradas de Lord El-Melloi II's Case Files: Rail Zeppelin Grace Note S!

--- assets/data/data.js
+++ assets/data/data.js
@@ -1124,7 +1124,7 @@ export const fateChecklistData = {
         },
         {
           id: "elmelloi-anime",
-          label: "Lord El-Melloi II Sei no Jikenbo: Rail Zeppelin Grace Note",
+          label: "Lord El-Melloi II's Case Files: Rail Zeppelin Grace Note",
           format: "Anime",
           completed: false,
           opened: false,
@@ -1132,82 +1132,121 @@ export const fateChecklistData = {
             {
               id: "elmelloi-1",
               number: 1,
-              label: "El caso del asesinato en el compartimento B4 (Parte 1)",
-              completed: false,
+              label: "Babylon, The Condemned, and the Memories of the King",
+              romaji: "Babylon to Keishisha to Ou no Kioku",
+              jp: "バビロンと刑死者と王の記憶",
+              es: "Babilonia, el ejecutor y los recuerdos del rey",
+              completed: false
             },
             {
               id: "elmelloi-2",
               number: 2,
-              label: "El caso del asesinato en el compartimento B4 (Parte 2)",
-              completed: false,
+              label: "The Seven Stars and the Eternal Cage",
+              romaji: "Nanatsu no Hoshi to Eien no Ori",
+              jp: "七つの星と永遠の檻",
+              es: "Siete cuerpos celestes y una celda eterna",
+              completed: false
             },
             {
               id: "elmelloi-3",
               number: 3,
-              label: "El caso del asesinato en el compartimento B4 (Parte 3)",
-              completed: false,
+              label: "Thunder and the Underground Labyrinth",
+              romaji: "Raimei to Chika Meikyuu",
+              jp: "雷鳴と地下迷宮",
+              es: "Truenos y el laberinto subterráneo",
+              completed: false
             },
             {
               id: "elmelloi-4",
               number: 4,
-              label: "El caso del asesinato en el compartimento B4 (Parte 4)",
-              completed: false,
+              label: "A Workshop, a Grave, and a Necromancer",
+              romaji: "Koubou to Dzuka to Shiryou Majutsushi",
+              jp: "工房と塚と死霊魔術師",
+              es: "El taller, la tumba y el nigromante",
+              completed: false
             },
             {
               id: "elmelloi-5",
               number: 5,
-              label: "El caso del asesinato en el compartimento B4 (Parte 5)",
-              completed: false,
+              label: "The Lance that Shines to the End of the World and the Fairy Eyes",
+              romaji: "Saihate no Yari to Yousei Me",
+              jp: "最果ての槍と妖精眼",
+              es: "La lanza del fin del mundo y el Ojo Hado",
+              completed: false
             },
             {
               id: "elmelloi-6",
               number: 6,
-              label: "El caso del asesinato en el compartimento B4 (Parte 6)",
-              completed: false,
+              label: "A Girl, a Department Store, and a Gift",
+              romaji: "Shoujo to Department Store to Present",
+              jp: "少女とデパートとプレゼント",
+              es: "Chicas, centro comercial y regalos",
+              completed: false
             },
             {
               id: "elmelloi-7",
               number: 7,
-              label: "El caso del asesinato en el compartimento B4 (Parte 7)",
-              completed: false,
+              label: "Rail Zeppelin (1/6) – A Train Whistle of Departure and the First Murder",
+              romaji: "Magan Shuushuu Ressha 1/6 Tabidachi no Kiteki to Daiichi no Satsujin",
+              jp: "魔眼蒐集列車1/6 旅立ちの汽笛と第一の殺人",
+              es: "El silbato del inicio del viaje y el primer asesinato",
+              completed: false
             },
             {
               id: "elmelloi-8",
               number: 8,
-              label: "El caso del asesinato en el compartimento B4 (Parte 8)",
-              completed: false,
+              label: "Rail Zeppelin 2/6 Gordius Wheel and the Memory of the King of Conquerors",
+              romaji: "Magan Shuushuu Ressha 2/6 Gordeiasu Houin to Seifuku Ou no Kioku",
+              jp: "魔眼蒐集列車2/6 神威の車輪と征服王の記憶",
+              es: "Gordius Wheel y los recuerdos del rey conquistador",
+              completed: false
             },
             {
               id: "elmelloi-9",
               number: 9,
-              label: "El caso del asesinato en el compartimento B4 (Parte 9)",
-              completed: false,
+              label: "Rail Zeppelin 3/6 A Sibyl, Decision, and Child of Einnashe",
+              romaji: "Magan Shuushuu Ressha 3/6 Miko to Ketsui to Fukairin no Ko",
+              jp: "魔眼蒐集列車 3/6 巫女と決意と腑海林の仔",
+              es: "Sacerdotisas, determinación y el Hijo de Einnashe",
+              completed: false
             },
             {
               id: "elmelloi-10",
               number: 10,
-              label: "El caso del asesinato en el compartimento B4 (Parte 10)",
-              completed: false,
+              label: "Rail Zeppelin 4/6 Mystic Eyes of Transience and an Awakening Detective",
+              romaji: "Magan Shuushuu Ressha 4/6 Houei no Magan to Mezameru Tantei",
+              jp: "魔眼蒐集列車 4/6 泡影の魔眼と目覚める探偵",
+              es: "El Ojo Místico Transitorio y el despertar del detective",
+              completed: false
             },
             {
               id: "elmelloi-11",
               number: 11,
-              label: "El caso del asesinato en el compartimento B4 (Parte 11)",
-              completed: false,
+              label: "Rail Zeppelin 5/6 The Residual Image and Auctions",
+              romaji: "Magan Shuushuu Ressha 5/6 Zanzou to Auction",
+              jp: "魔眼蒐集列車 5/6 残像とオークション",
+              es: "Reflejo y subasta",
+              completed: false
             },
             {
               id: "elmelloi-12",
               number: 12,
-              label: "El caso del asesinato en el compartimento B4 (Parte 12)",
-              completed: false,
+              label: "Rail Zeppelin 6/6 Lightning and Shooting Star",
+              romaji: "Magan Shuushuu Ressha 6/6 Raikou to Ryuusei",
+              jp: "魔眼蒐集列車 6/6 雷光と流星",
+              es: "Rayos y estrellas fugaces",
+              completed: false
             },
             {
               id: "elmelloi-13",
               number: 13,
-              label: "El caso del asesinato en el compartimento B4 (Parte 13)",
-              completed: false,
-            },
-          ],
+              label: "The Clock Tower, Usual Days, and the First Step Forward to the Future",
+              romaji: "Tokeitou to Nichijou to Mirai e no Daiippo",
+              jp: "時計塔と日常と未来への第一歩",
+              es: "Episodio final",
+              completed: false
+            }
+          ]
         },
         {
           id: "elmelloi-special",
