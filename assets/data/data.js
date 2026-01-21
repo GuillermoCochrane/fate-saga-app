0c40b765eefc96ce42a85de0a86b3d287183b9fa
GuillermoCochrane <guilleac81@gmail.com>
2026-01-21 03:20:45 -0300

    Agregada entradas de Otros: Especial -Fate/Apocrypha Recaps

--- assets/data/data.js
+++ assets/data/data.js
@@ -3514,6 +3514,33 @@ export const fateChecklistData = {
           ]
         },
         {
+          id: "fate-apocrypha-recaps",
+          label: "Fate/Apocrypha Recaps",
+          format: "Especial",
+          completed: false,
+          opened: false,
+          episodes: [
+            {
+              id: "apoc-recap-1",
+              number: 1,
+              label: "The Great Holy Grail War - Turmoil Chapter",
+              romaji: "Seihai Taisen Douran-hen",
+              jp: "Fate/Apocrypha 聖杯大戦動乱編",
+              es: "La Gran Guerra del Santo Grial - Arco de la Batalla",
+              completed: false
+            },
+            {
+              id: "apoc-recap-2",
+              number: 2,
+              label: "The Great Holy Grail War - Commencement Chapter",
+              romaji: "Seihai Taisen Kaimaku-hen",
+              jp: "Fate/Apocrypha 聖杯大戦開幕編",
+              es: "La Gran Guerra del Santo Grial - El Comienzo",
+              completed: false
+            }
+          ]
+        },
+        {
           id: "fate-prototype",
           label: "Fate/Prototype",
           format: "OVA",
