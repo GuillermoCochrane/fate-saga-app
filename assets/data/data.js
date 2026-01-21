994d12173a012ca4aafaebfc8595f3763bbe9554
GuillermoCochrane <guilleac81@gmail.com>
2026-01-21 03:18:58 -0300

    Agregada entradas de Otros: Fate/Extra Last Encore: Illustrias Tendousetsu

--- assets/data/data.js
+++ assets/data/data.js
@@ -3631,6 +3631,43 @@ export const fateChecklistData = {
           ]
         },
         {
+          id: "fate-extra-2",
+          label: "Fate/Extra Last Encore: Illustrias Tendousetsu",
+          format: "Anime",
+          note: "También conocida como 'Geocentrismo ilustre' - Continuación de Fate/Extra Last Encore",
+          completed: false,
+          opened: false,
+          episodes: [
+            {
+              id: "extra-ill-1",
+              number: 1,
+              label: "Resurrected Sword of Victory: Excalibur Galatine",
+              romaji: "Tenrin Suru Shouri no Ken: Excalibur Galatine",
+              jp: "転輪する勝利の剣 エクスカリバー・ガラティーン",
+              es: "La Espada de la Victoria Resucitada: Excalibur Galatine",
+              completed: false
+            },
+            {
+              id: "extra-ill-2",
+              number: 2,
+              label: "Turner of the Wheel: Chakra Vartin",
+              romaji: "Tenrinjouou: Chakra Vartin",
+              jp: "天輪聖王 チャクラ・ヴァルティン",
+              es: "El que Gira la Rueda: Chakra Vartin",
+              completed: false
+            },
+            {
+              id: "extra-ill-3",
+              number: 3,
+              label: "The Applauding Rose: Olympia Plaudere",
+              romaji: "Kassai no Bara: Olympia Plaudere",
+              jp: "喝采の薔薇 オリンピア・プラウデーレ",
+              es: "La Rosa que Aplaude: Olympia Plaudere",
+              completed: false
+            }
+          ]
+        },
+        {
           id: "carnival-phantasm",
           label: "Carnival Phantasm",
           format: "Serie OVA [Parodia esencial del Nasuverse]",
