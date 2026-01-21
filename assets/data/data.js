ee0c35ab0d4884567b4f75e1bdef127ff7f56894
GuillermoCochrane <guilleac81@gmail.com>
2026-01-21 03:16:42 -0300

    Actualizada entradas de Otros: Fate/Extra Last Encore

--- assets/data/data.js
+++ assets/data/data.js
@@ -3541,82 +3541,94 @@ export const fateChecklistData = {
             {
               id: "extra-1",
               number: 1,
-              label: "El castillo de la muerte (Parte 1)",
-              completed: false,
+              label: "Praeteritus Limbus Vorago",
+              romaji: "Ima wa Furuki Hengoku no Soko: Preteritus Rinbus Woraago",
+              jp: "今は旧き辺獄の底 ―プレテリトゥス・リンブス・ヴォラーゴ",
+              es: "Praeteritus Limbus Vorago",
+              completed: false
             },
             {
               id: "extra-2",
               number: 2,
-              label: "El castillo de la muerte (Parte 2)",
-              completed: false,
+              label: "Dead Face",
+              romaji: "Shisou: Dead Face",
+              jp: "死相 デッドフェイス",
+              es: "El rostro de la muerte",
+              completed: false
             },
             {
               id: "extra-3",
               number: 3,
-              label: "El castillo de la muerte (Parte 3)",
-              completed: false,
+              label: "Golden Wild Hunt",
+              romaji: "Ougon Shishi to Arashi no Yoru: Golden Wild Hunt",
+              jp: "黄金鹿と嵐の夜 ゴールデン・ワイルドハント",
+              es: "La cacería salvaje dorada",
+              completed: false
             },
             {
               id: "extra-4",
               number: 4,
-              label: "El castillo de la muerte (Parte 4)",
-              completed: false,
+              label: "No Face May King",
+              romaji: "Kao no Nai Ou: No Face Mei King",
+              jp: "顔の無い王 ―ノフェイス・メイキング",
+              es: "El camuflaje mágico",
+              completed: false
             },
             {
               id: "extra-5",
               number: 5,
-              label: "El castillo de la muerte (Parte 5)",
-              completed: false,
+              label: "Yew Bow",
+              romaji: "Inori no Yumi: Ii Bow",
+              jp: "祈りの弓 イー・バウ",
+              es: "El arco de tejo",
+              completed: false
             },
             {
               id: "extra-6",
               number: 6,
-              label: "El castillo de la muerte (Parte 6)",
-              completed: false,
+              label: "The Queen's Glass Game",
+              romaji: "Eikyuukikan Shoujo Teikoku: Queens Glass Game",
+              jp: "永久機関・少女帝国 ―クイーンズ・グラスゲーム―",
+              es: "El juego de cristal de la reina",
+              completed: false
             },
             {
               id: "extra-7",
               number: 7,
-              label: "El castillo de la muerte (Parte 7)",
-              completed: false,
+              label: "Nursery Rhyme",
+              romaji: "Dareka no Tame no Monogatari: Nursery Rhyme",
+              jp: "誰かの為の物語 ―ナーサリー・ライム―",
+              es: "La canción de cuna",
+              completed: false
             },
             {
               id: "extra-8",
               number: 8,
-              label: "El castillo de la muerte (Parte 8)",
-              completed: false,
+              label: "Bajiquan",
+              romaji: "Munida: Dead End",
+              jp: "无二打 ―デッド・エンド―",
+              es: "Bajiquan",
+              completed: false
             },
             {
               id: "extra-9",
               number: 9,
-              label: "El castillo de la muerte (Parte 9)",
-              completed: false,
+              label: "Aestus Domus Aurea",
+              romaji: "Maneki Tou Ougon Gekijou: Aestus Domus Aurea",
+              jp: "招き蕩う黄金劇場 -アエストゥス・ドムス・アウレア-",
+              es: "Aestus Domus Aurea",
+              completed: false
             },
             {
               id: "extra-10",
               number: 10,
-              label: "El castillo de la muerte (Parte 10)",
-              completed: false,
-            },
-            {
-              id: "extra-11",
-              number: 11,
-              label: "El castillo de la muerte (Parte 11)",
-              completed: false,
-            },
-            {
-              id: "extra-12",
-              number: 12,
-              label: "El castillo de la muerte (Parte 12)",
-              completed: false,
-            },
-            {
-              id: "extra-13",
-              number: 13,
-              label: "El castillo de la muerte (Parte 13)",
-              completed: false,
-            },
-          ],
+              label: "Unlimited/Raise Dead",
+              romaji: "Mugen no Zangai: Unlimited/Raise Dead",
+              jp: "無限の残骸 -アンリミテッド／レイズ・デッド-",
+              es: "Los restos de la batalla infinita",
+              completed: false
+            }
+          ]
         },
         {
           id: "carnival-phantasm",
