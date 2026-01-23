6fea3370fce374aa0da7a476939daf376dd951c9
GuillermoCochrane <guilleac81@gmail.com>
2026-01-23 01:13:03 -0300

    Agregada entradas de Fate/kaleid liner Prisma☆Illya - Licht - Namae no Nai Shoujo: ONA - Mini Anime

--- assets/data/data.js
+++ assets/data/data.js
@@ -3466,6 +3466,42 @@ export const fateChecklistData = {
           ]
         },
         {
+          id: "prisma-movie-licht-mini",
+          label: "Fate/kaleid liner Prisma☆Illya Movie: Licht - Namae no Nai Shoujo Mini Anime",
+          format: "ONA",
+          completed: false,
+          opened: false,
+          episodes: [
+            {
+              id: "prisma-licht-mini-1",
+              number: 1,
+              label: "Fate/kaleid liner Prisma Illya: Licht Nameless Girl Mini Anime Episode 1",
+              romaji: "Fate/kaleid liner Prisma Illya: Licht Nameless Girl Mini Anime Episode 1",
+              jp: "Fate/kaleid liner プリズマ☆イリヤミニアニメ 第1話",
+              es: "Fate/kaleid liner Prisma Illya: Licht - La Chica sin Nombre Mini Anime Episodio 1",
+              completed: false
+            },
+            {
+              id: "prisma-licht-mini-2",
+              number: 2,
+              label: "Fate/kaleid liner Prisma Illya: Licht Nameless Girl Mini Anime Episode 2",
+              romaji: "Fate/kaleid liner Prisma Illya: Licht Nameless Girl Mini Anime Episode 2",
+              jp: "Fate/kaleid liner プリズマ☆イリヤミニアニメ 第2話",
+              es: "Fate/kaleid liner Prisma Illya: Licht - La Chica sin Nombre Mini Anime Episodio 2",
+              completed: false
+            },
+            {
+              id: "prisma-licht-mini-3",
+              number: 3,
+              label: "Fate/kaleid liner Prisma Illya: Licht Nameless Girl Mini Anime Episode 3",
+              romaji: "Fate/kaleid liner Prisma Illya: Licht Nameless Girl Mini Anime Episode 3",
+              jp: "Fate/kaleid liner プリズマ☆イリヤミニアニメ 第3話",
+              es: "Fate/kaleid liner Prisma Illya: Licht - La Chica sin Nombre Mini Anime Episodio 3",
+              completed: false
+            }
+          ]
+        },
+        {
           id: "prisma-phantasm",
           label: "Fate/Kaleid Liner Prisma Illya: Prisma★Phantasm",
           format: "OVA",
