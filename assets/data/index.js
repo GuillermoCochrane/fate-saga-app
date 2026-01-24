b07208e22764c2804fd7ab623a8e99fddc0343d3
GuillermoCochrane <guilleac81@gmail.com>
2026-01-24 02:40:39 -0300

    Agregado indice de franquicias

---
+++ assets/data/index.js
@@ -0,0 +1,9 @@
+import { fateChecklistData } from './data.js';
+import { asistenceChecklistData } from "./assistance.js";
+import { fateChecklist } from './fate.js';
+
+export const checklistData = {
+    fate: fateChecklistData,
+    asistence: asistenceChecklistData,
+    fate2: fateChecklist
+};
