acb895855969a894b049b60450153f00781c2d10
GuillermoCochrane <guilleac81@gmail.com>
2026-01-27 03:39:45 -0300

    Agregado renderFranchiseSelector en selectorHandler. que renderiza las opciones de select del template

---
+++ assets/js/handlers/selectorHandler.js
@@ -0,0 +1,21 @@
+// handlers/franchiseSelectorHandler.js
+import { $ } from '../utilities/dom.js';
+import { createOption } from '../utilities/dom.js';
+import { checklistData } from '../../data/index.js';
+
+export function renderFranchiseSelector() {
+    const $select = $('#franchise-select');
+    if (!$select) return;
+    
+    // Limpiar select
+    $select.innerHTML = '';
+    
+    // Option por defecto
+    $select.appendChild(createOption('', 'Selecciona una franquicia', true, true));
+    
+    // Opciones de cada franquicia
+    for (const key in checklistData) {
+      const $option = createOption(key, checklistData[key].franchise);
+      $select.appendChild($option);
+    }
+}
