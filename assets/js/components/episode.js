c0d63a82caf298f1bccd3202548112c125ef318c
GuillermoCochrane <guilleac81@gmail.com>
2026-01-29 03:32:42 -0300

    episodeCreator refactorizado, agregandole dataset para uso futuro y opciones de accesibilidad, y mejorando la semántica

--- assets/js/components/episode.js
+++ assets/js/components/episode.js
@@ -1,40 +1,45 @@
 //? funcion de creación de componente episode
-
 import { createElement, createCheckbox, createLabel } from '../utilities/dom.js';
 
 // Crear elemento de episodio individual
-export function episodeCreator(episode, seasonID, sagaName, handleToggleCheckbox) {
+export function episodeCreator(episode, seasonID, sagaName, handleToggleCheckbox, parentSagaID) {
     //Creamos el contendor con la clase check-item
-    const $episodeDiv = createElement('div', 'check-item');
+    const $episodeArticle = createElement('article', 'check-item');
 
     //Creamos el checkbox, dejándolo marcado según corresponda
     const checkboxID = `${episode.id}`;
-    // handleToggleCheckbox(seasonID, sagaName, isEpisode = false, episodeID = null) 
     const checkBoxEventHandler = () => handleToggleCheckbox(seasonID, sagaName , true, episode.id);
-    const $checkbox = createCheckbox(checkboxID, episode.completed, checkBoxEventHandler);
 
-    const episodeTag =  `<strong>Episodio ${episode.number}:</strong>`
-    const $episodeNumber = createElement('span', 'episode-number', episodeTag, true); //Marcador del episodio en negrita
+    const ariaLabel = `Episodio ${episode.number}: ${episode.label}` 
+    const $checkbox = createCheckbox(checkboxID, episode.completed, checkBoxEventHandler, null, parentSagaID, ariaLabel);
+    const $episode =  createElement('span', null, "Episodio ");
+    const $episodeTag = createElement('strong', null); //Marcador del episodio en negrita
+    $episodeTag.appendChild($episode);
+    $episodeTag.appendChild(document.createTextNode(`${episode.number}:`));
+
+    // const episodeTag =  `<strong>Episodio ${episode.number}:</strong>`
+    const $episodeNumber = createElement('aside', 'episode-number');
+    $episodeNumber.appendChild($episodeTag);
 
     const $label = createLabel('', checkboxID, 'episode-label');      //creamos el label
     $label.appendChild($episodeNumber);                               //agregamos el marcador del episodio al label
     $label.appendChild(document.createTextNode(` ${episode.label}`)); //agregamos el nombre del episodio al label
 
 
-    $episodeDiv.appendChild($checkbox);  //agregamos el checkbox al contenedor de episodio
-    $episodeDiv.appendChild($label);     //agregamos el label al contenedor de episodio
+    $episodeArticle.appendChild($checkbox);  //agregamos el checkbox al contenedor de episodio
+    $episodeArticle.appendChild($label);     //agregamos el label al contenedor de episodio
     /*
         Estructura HTML resultante:
-        <div class="check-item">
+        <article class="check-item">
             <input type="checkbox" id="main-1">
             <label for="main-1" class="episode-label">
-                <span class="episode-number">
+                <aside class="episode-number">
                     <strong>Episodio 1:</strong> 
-                </span>
+                </aside>
                 Título del episodio
             </label>
-        </div>
+        </article>
      */
     
-    return $episodeDiv; //Devuelve el contendor del episodio
+    return $episodeArticle; //Devuelve el contendor del episodio
 }
