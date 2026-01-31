96701fbb78425c9c985d3500c324e00336c6e6da
GuillermoCochrane <guilleac81@gmail.com>
2026-01-31 18:44:41 -0300

    Agregada El Increíble Hulk a la fase 1 del MCU

---
+++ assets/data/mcu/phaseOne/incrediblehulk2008.js
@@ -0,0 +1,15 @@
+import { hulk_2008_1 } from "./incrediblehulk2008/episodes.js";
+
+export const incrediblehulk2008 = {
+  id: "ihm",
+  label: "The Incredible Hulk",
+  format: "Película",
+  release: 2008,
+  order: 2,
+  year_in_movie: 2008,
+  completed: false,
+  opened: false,
+  episodes: [
+    hulk_2008_1
+  ],
+};
---
+++ assets/data/mcu/phaseOne/incrediblehulk2008/episodes.js
@@ -0,0 +1,9 @@
+export const hulk_2008_1 = {
+  id: "tih",
+  number: 1,
+  completed: false,
+  label: {
+    en: "The Incredible Hulk",
+    es:  "El Increíble Hulk",
+  },
+};
