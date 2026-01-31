ed38a40de6774c3cd6b3b69dc700766ea45066cb
GuillermoCochrane <guilleac81@gmail.com>
2026-01-31 18:43:57 -0300

    Agregada iron man a la phase 1 del MCU

---
+++ assets/data/mcu/phaseOne/ironman2008.js
@@ -0,0 +1,15 @@
+import { im_2008_1 } from "./ironman2008/episodes.js";
+
+export const ironman2008 = {
+  id: "imm",
+  label: "Iron Man",
+  format: "Película",
+  release: 2008,
+  order: 1,
+  year_in_movie: 2008,
+  completed: false,
+  opened: false,
+  episodes: [
+    im_2008_1
+  ],
+};
---
+++ assets/data/mcu/phaseOne/ironman2008/episodes.js
@@ -0,0 +1,10 @@
+export const im_2008_1 = {
+  id: "im1",
+  number: 1,
+  completed: false,
+  show_sublabel: false,
+  label: {
+    en: "Iron Man",
+    es: "Iron Man: El hombre de acero",
+  },
+};
