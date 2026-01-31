d0bfb16844c435dd7e68a478533f20bd696ebcd7
GuillermoCochrane <guilleac81@gmail.com>
2026-01-31 18:41:48 -0300

    Agregada nueva franquicia, disponible en el selector

--- assets/data/index.js
+++ assets/data/index.js
@@ -1,8 +1,10 @@
 import { fateChecklistData } from './data.js';
 import { asistenceChecklistData } from "./assistance.js";
 import { fateChecklist } from './fate.js';
+import { mcuChecklist } from './mcu.js';
 
 export const checklistData = {
+    mcu: mcuChecklist,
     fate: fateChecklistData,
     assistence: asistenceChecklistData,
     fate2: fateChecklist
---
+++ assets/data/mcu.js
@@ -0,0 +1,11 @@
+import { phaseOne } from "./mcu/phaseOne.js";
+import { phaseTwo } from "./mcu/phaseTwo.js";
+
+export const mcuChecklist = {
+  franchiseName: "Marvel Cinematic Universe",
+  franchise: "mcu",
+  sagas: [ 
+    phaseOne, 
+    phaseTwo, 
+  ],
+};
