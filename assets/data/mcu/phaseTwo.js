c7e829edf4d1bc54537121bf5115240aa7167ad6
GuillermoCochrane <guilleac81@gmail.com>
2026-01-31 18:42:24 -0300

    Agregada fase 2 del mcu

---
+++ assets/data/mcu/phaseTwo.js
@@ -0,0 +1,12 @@
+import { ironman32013 } from "./phaseTwo/ironman32013.js";
+import { thordarkworld2013 } from "./phaseTwo/thordarkworld2013.js";
+
+export const phaseTwo = {
+  saga: "Phase Two",
+  id: "ph2",
+  opened: false,
+  seasons: [
+    ironman32013,
+    thordarkworld2013,
+  ],
+};
