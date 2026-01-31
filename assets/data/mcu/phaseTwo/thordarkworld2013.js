f859076d0ed0051f866a6c76d1be03f9efe6d95d
GuillermoCochrane <guilleac81@gmail.com>
2026-01-31 18:47:06 -0300

    Agregada Thor: The Dark World a la fase 2 del MCU

---
+++ assets/data/mcu/phaseTwo/thordarkworld2013.js
@@ -0,0 +1,15 @@
+import { thor2_2013_1 } from "./thordarkworld2013/episodes.js";
+
+export const thordarkworld2013 = {
+  id: "twm",
+  label: "Thor: The Dark World",
+  format: "Película",
+  release: 2013,
+  order: 8,
+  year_in_movie: 2013,
+  completed: false,
+  opened: false,
+  episodes: [
+    thor2_2013_1
+  ],
+};
---
+++ assets/data/mcu/phaseTwo/thordarkworld2013/episodes.js
@@ -0,0 +1,10 @@
+export const thor2_2013_1 = {
+  id: "tdw",
+  number: 1,
+  completed: false,
+  label: {
+    en: "Thor: The Dark World",
+    es: "Thor: El mundo oscuro",
+  },
+};
+
