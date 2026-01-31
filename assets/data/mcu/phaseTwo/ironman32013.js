98d1997b77542b51e617af31f6521263a41b9784
GuillermoCochrane <guilleac81@gmail.com>
2026-01-31 18:46:07 -0300

    Agregada Iron Man 3 a la fase 2 del MCU

---
+++ assets/data/mcu/phaseTwo/ironman32013.js
@@ -0,0 +1,15 @@
+import { im3_2013_1 } from "./ironman32013/episodes.js";
+
+export const ironman32013 = {
+  id: "i3m",
+  label: "Iron Man 3",
+  format: "Película",
+  release: 2013,
+  order: 7,
+  year_in_movie: 2013,
+  completed: false,
+  opened: false,
+  episodes: [
+    im3_2013_1
+  ],
+};
---
+++ assets/data/mcu/phaseTwo/ironman32013/episodes.js
@@ -0,0 +1,10 @@
+export const im3_2013_1 = {
+  id: "im3",
+  number: 1,
+  completed: false,
+  label: {
+    en: "Iron Man 3",
+    es: "Iron Man 3",
+  },
+};
+
