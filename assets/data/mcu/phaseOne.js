d75b4afdef7ef8a0e7f14b8471d311926fe39ffd
GuillermoCochrane <guilleac81@gmail.com>
2026-01-31 18:42:13 -0300

    Agregada fase 1 del mcu

---
+++ assets/data/mcu/phaseOne.js
@@ -0,0 +1,12 @@
+import { ironman2008 } from "./phaseOne/ironman2008.js";
+import { incrediblehulk2008 } from "./phaseOne/incrediblehulk2008.js";
+
+export const phaseOne = {
+  saga: "Phase One",
+  id: "pho",
+  opened: false,
+  seasons: [
+    ironman2008,
+    incrediblehulk2008,
+  ],
+};
