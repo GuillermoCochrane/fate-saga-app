f5074470ca7ef134619decf232e2c9e190588ec6
GuillermoCochrane <guilleac81@gmail.com>
2026-02-01 18:01:31 -0300

    Agregada Marvel: Agents of S.H.I.E.L.D. a la fase 2 del MCU

---
+++ assets/data/mcu/phaseTwo/agentsshield2013.js
@@ -0,0 +1,16 @@
+import { aos_s1e1 } from "./agentsshield2013/episodes.js";
+
+export const agentsshield2013 = {
+  id: "ass",
+  label: "Marvel: Agents of S.H.I.E.L.D.",
+  format: "Serie",
+  season: 1,
+  release: 2013,
+  order: 10,
+  phase_order: 1,
+  completed: false,
+  opened: false,
+  episodes: [
+    aos_s1e1
+  ],
+};
---
+++ assets/data/mcu/phaseTwo/agentsshield2013/episodes.js
@@ -0,0 +1,9 @@
+export const aos_s1e1 = {
+  id: "as1",
+  number: 1,
+  completed: false,
+  label: {
+    en: "Pilot",
+    es: "Piloto",
+  },
+};
