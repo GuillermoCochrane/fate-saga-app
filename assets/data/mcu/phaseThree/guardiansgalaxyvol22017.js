a61d924bb57d74ccf2c3883a25565e1b6d97956c
GuillermoCochrane <guilleac81@gmail.com>
2026-02-02 03:42:29 -0300

    Agregada Guardians of the Galaxy Vol. 2 a la fase 3 del MCU

---
+++ assets/data/mcu/phaseThree/guardiansgalaxyvol22017.js
@@ -0,0 +1,15 @@
+import { gotg2_2017_1 } from "./guardiansgalaxyvol22017/episodes.js";
+
+export const guardiansgalaxyvol22017 = {
+  id: "gm2",
+  label: "Guardians of the Galaxy Vol. 2",
+  format: "Película",
+  release: 2017,
+  order: 26,
+  phase_order: 5,
+  completed: false,
+  opened: false,
+  episodes: [
+    gotg2_2017_1
+  ],
+};
---
+++ assets/data/mcu/phaseThree/guardiansgalaxyvol22017/episodes.js
@@ -0,0 +1,9 @@
+export const gotg2_2017_1 = {
+  id: "gg2",
+  number: 1,
+  completed: false,
+  label: {
+    en: "Guardians of the Galaxy Vol. 2",
+    es: "Guardianes de la Galaxia Vol. 2",
+  },
+};
