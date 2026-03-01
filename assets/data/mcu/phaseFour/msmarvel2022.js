5f19d7473c91d4e7d617ff2ac4263dbd14faf2e4
GuillermoCochrane <guilleac81@gmail.com>
2026-02-28 22:02:53 -0300

    Agregada Ms. Marvel a la fase 4 del MCU

---
+++ assets/data/mcu/phaseFour/msmarvel2022.js
@@ -0,0 +1,17 @@
+import { msm_e1 } from "./msmarvel2022/episodes.js";
+
+export const msmarvel2022 = {
+  id: "msmS1",
+  label: "Ms. Marvel",
+  format: "Serie",
+  release: 2022,
+  order: 42,
+  phase: 4,
+  phase_order: 12,
+  year_in_timeline: 2022,
+  completed: false,
+  opened: false,
+  episodes: [
+    msm_e1
+  ],
+};
---
+++ assets/data/mcu/phaseFour/msmarvel2022/episodes.js
@@ -0,0 +1,9 @@
+export const msm_e1 = {
+  id: "msm",
+  number: 1,
+  completed: false,
+  label: {
+    en: "Generation Why",
+    es: "Generación Porque",
+  },
+};
