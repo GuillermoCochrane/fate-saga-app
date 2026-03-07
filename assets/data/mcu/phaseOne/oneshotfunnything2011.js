548a87534fc3056c77c8debd93d770afdb080c4a
Guille <cochraneguillermo@gmail.com>
2026-03-07 02:03:34 -0300

    Marvel One-Shot: A Funny Thing Happened on the Way to Thor's Hammer auditada y corregida

--- assets/data/mcu/phaseOne/oneshotfunnything2011.js
+++ assets/data/mcu/phaseOne/oneshotfunnything2011.js
@@ -4,11 +4,16 @@ export const oneshotfunnything2011 = {
   id: "oftE1",
   label: "Marvel One-Shot: A Funny Thing Happened on the Way to Thor's Hammer",
   format: "Especial",
-  release: 2011,
-  order: 7,
+  release: {
+    year: 2011,
+    month: 10,
+    day: 25
+  },
   phase: 1,
-  phase_order: 7,
-  year_in_timeline: 2011,
+  year_in_timeline: {
+    start: 2010,
+    end: 2010
+  },
   completed: false,
   opened: false,
   episodes: [
--- assets/data/mcu/phaseOne/oneshotfunnything2011/episodes.js
+++ assets/data/mcu/phaseOne/oneshotfunnything2011/episodes.js
@@ -4,7 +4,6 @@ export const funny_thing_2011_1 = {
   completed: false,
   label: {
     en: "A Funny Thing Happened on the Way to Thor's Hammer",
-    es: "Algo Loco Pasó en Camino al Martillo de Thor",
+    es: "Algo curioso pasó camino al martillo de Thor",
   },
 };
-
