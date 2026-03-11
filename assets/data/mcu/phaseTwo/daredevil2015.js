484fcd2ca5ca24b6aa8715cd50cbe266858d260f
Guille <cochraneguillermo@gmail.com>
2026-03-11 04:40:35 -0300

    Daredevil (S1) auditada y corregida

--- assets/data/mcu/phaseTwo/daredevil2015.js
+++ assets/data/mcu/phaseTwo/daredevil2015.js
@@ -1,18 +1,27 @@
-import { dd_s1e1 } from "./daredevil2015/episodes.js";
+import { 
+  dd_s1e1, dd_s1e2, dd_s1e3, dd_s1e4, dd_s1e5, dd_s1e6, dd_s1e7,
+  dd_s1e8, dd_s1e9, dd_s1e10, dd_s1e11, dd_s1e12, dd_s1e13
+} from "./daredevil2015/episodes.js";
 
 export const daredevil2015 = {
   id: "drdS1",
-  label: "Daredevil",
+  label: "Daredevil (S1)",
   format: "Serie",
   season: 1,
-  release: 2015,
-  order: 20,
+  release: {
+    year: 2015,
+    month: 4,
+    day: 10
+  },
   phase: 2,
-  phase_order: 11,
-  year_in_timeline: 2015,
+  year_in_timeline: {
+    start: 2015,
+    end: 2015
+  },
   completed: false,
   opened: false,
   episodes: [
-    dd_s1e1
+    dd_s1e1, dd_s1e2, dd_s1e3, dd_s1e4, dd_s1e5, dd_s1e6, dd_s1e7,
+    dd_s1e8, dd_s1e9, dd_s1e10, dd_s1e11, dd_s1e12, dd_s1e13
   ],
 };
--- assets/data/mcu/phaseTwo/daredevil2015/episodes.js
+++ assets/data/mcu/phaseTwo/daredevil2015/episodes.js
@@ -1,9 +1,129 @@
 export const dd_s1e1 = {
-  id: "dd1",
+  id: "dd1e1",
   number: 1,
   completed: false,
   label: {
     en: "Into the Ring",
-    es: "En el Ring",
+    es: "En el ring",
+  },
+};
+
+export const dd_s1e2 = {
+  id: "dd1e2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Cut Man",
+    es: "Hombre vendado",
+  },
+};
+
+export const dd_s1e3 = {
+  id: "dd1e3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Rabbit in a Snowstorm",
+    es: "Conejo en una tormenta de nieve",
+  },
+};
+
+export const dd_s1e4 = {
+  id: "dd1e4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "In the Blood",
+    es: "En la sangre",
+  },
+};
+
+export const dd_s1e5 = {
+  id: "dd1e5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "World on Fire",
+    es: "Mundo en llamas",
+  },
+};
+
+export const dd_s1e6 = {
+  id: "dd1e6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Condemned",
+    es: "Condenado",
+  },
+};
+
+export const dd_s1e7 = {
+  id: "dd1e7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Stick",
+    es: "Stick",
+  },
+};
+
+export const dd_s1e8 = {
+  id: "dd1e8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Shadows in the Glass",
+    es: "Sombras en el cristal",
+  },
+};
+
+export const dd_s1e9 = {
+  id: "dd1e9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Speak of the Devil",
+    es: "Hablando del diablo",
+  },
+};
+
+export const dd_s1e10 = {
+  id: "dd1e10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "Nelson v. Murdock",
+    es: "Nelson contra Murdock",
+  },
+};
+
+export const dd_s1e11 = {
+  id: "dd1e11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Path of the Righteous",
+    es: "El camino del justo",
+  },
+};
+
+export const dd_s1e12 = {
+  id: "dd1e12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Ones We Leave Behind",
+    es: "Los que dejamos atrás",
+  },
+};
+
+export const dd_s1e13 = {
+  id: "dd1e13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "Daredevil",
+    es: "Daredevil",
   },
 };
