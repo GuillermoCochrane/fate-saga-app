ff95d1cc899ee6515baaa4f389d7f5d8c3ac1a4e
Guille <cochraneguillermo@gmail.com>
2026-03-11 04:03:38 -0300

    Marvel: Agents of S.H.I.E.L.D. (S1) auditada y corregida

--- assets/data/mcu/phaseTwo/agentsshieldS1.js
+++ assets/data/mcu/phaseTwo/agentsshieldS1.js
@@ -1,18 +1,29 @@
-import { aos_s1e1 } from "./agentsshieldS1/episodes.js";
+import { 
+  aos_s1e1, aos_s1e2, aos_s1e3, aos_s1e4, aos_s1e5, aos_s1e6, aos_s1e7, aos_s1e8,
+  aos_s1e9, aos_s1e10, aos_s1e11, aos_s1e12, aos_s1e13, aos_s1e14, aos_s1e15, aos_s1e16,
+  aos_s1e17, aos_s1e18, aos_s1e19, aos_s1e20, aos_s1e21, aos_s1e22
+} from "./agentsshieldS1/episodes.js";
 
 export const agentsshieldS1 = {
   id: "aosS1",
   label: "Marvel: Agents of S.H.I.E.L.D. (S1)",
   format: "Serie",
   season: 1,
-  release: 2013,
-  order: 10,
+  release: {
+    year: 2013,
+    month: 9,
+    day: 24
+  },
   phase: 2,
-  phase_order: 1,
-  year_in_timeline: 2013,
+  year_in_timeline: {
+    start: 2013,
+    end: 2014
+  },
   completed: false,
   opened: false,
   episodes: [
-    aos_s1e1
+    aos_s1e1, aos_s1e2, aos_s1e3, aos_s1e4, aos_s1e5, aos_s1e6, aos_s1e7, aos_s1e8,
+    aos_s1e9, aos_s1e10, aos_s1e11, aos_s1e12, aos_s1e13, aos_s1e14, aos_s1e15, aos_s1e16,
+    aos_s1e17, aos_s1e18, aos_s1e19, aos_s1e20, aos_s1e21, aos_s1e22
   ],
 };
--- assets/data/mcu/phaseTwo/agentsshieldS1/episodes.js
+++ assets/data/mcu/phaseTwo/agentsshieldS1/episodes.js
@@ -1,9 +1,219 @@
 export const aos_s1e1 = {
-  id: "as1",
+  id: "as1e1",
   number: 1,
   completed: false,
   label: {
     en: "Pilot",
     es: "Piloto",
+  },
+};
+
+export const aos_s1e2 = {
+  id: "as1e2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "0-8-4",
+    es: "0-8-4",
+  },
+};
+
+export const aos_s1e3 = {
+  id: "as1e3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Asset",
+    es: "El activo",
+  },
+};
+
+export const aos_s1e4 = {
+  id: "as1e4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Eye Spy",
+    es: "Ojo espía",
+  },
+};
+
+export const aos_s1e5 = {
+  id: "as1e5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Girl in the Flower Dress",
+    es: "La chica del vestido de flores",
+  },
+};
+
+export const aos_s1e6 = {
+  id: "as1e6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "FZZT",
+    es: "FZZT",
+  },
+};
+
+export const aos_s1e7 = {
+  id: "as1e7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The Hub",
+    es: "El Hub",
+  },
+};
+
+export const aos_s1e8 = {
+  id: "as1e8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Well",
+    es: "El pozo",
+  },
+};
+
+export const aos_s1e9 = {
+  id: "as1e9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Repairs",
+    es: "Reparaciones",
+  },
+};
+
+export const aos_s1e10 = {
+  id: "as1e10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Bridge",
+    es: "El puente",
+  },
+};
+
+export const aos_s1e11 = {
+  id: "as1e11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Magical Place",
+    es: "El lugar mágico",
+  },
+};
+
+export const aos_s1e12 = {
+  id: "as1e12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "Seeds",
+    es: "Semillas",
+  },
+};
+
+export const aos_s1e13 = {
+  id: "as1e13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "T.R.A.C.K.S.",
+    es: "T.R.A.C.K.S.",
+  },
+};
+
+export const aos_s1e14 = {
+  id: "as1e14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "T.A.H.I.T.I.",
+    es: "T.A.H.I.T.I.",
+  },
+};
+
+export const aos_s1e15 = {
+  id: "as1e15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "Yes Men",
+    es: "Lacayos",
+  },
+};
+
+export const aos_s1e16 = {
+  id: "as1e16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "End of the Beginning",
+    es: "El fin del principio",
+  },
+};
+
+export const aos_s1e17 = {
+  id: "as1e17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "Turn, Turn, Turn",
+    es: "Gira, gira, gira",
+  },
+};
+
+export const aos_s1e18 = {
+  id: "as1e18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "Providence",
+    es: "Providence",
+  },
+};
+
+export const aos_s1e19 = {
+  id: "as1e19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "The Only Light in the Darkness",
+    es: "La única luz en la oscuridad",
+  },
+};
+
+export const aos_s1e20 = {
+  id: "as1e20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "Nothing Personal",
+    es: "Nada personal",
+  },
+};
+
+export const aos_s1e21 = {
+  id: "as1e21",
+  number: 21,
+  completed: false,
+  label: {
+    en: "Ragtag",
+    es: "Popurrí",
+  },
+};
+
+export const aos_s1e22 = {
+  id: "as1e22",
+  number: 22,
+  completed: false,
+  label: {
+    en: "Beginning of the End",
+    es: "El principio del fin",
   },
 };
