2a22beb1f351fa2a567335e742dd6ffe89786494
Guille <cochraneguillermo@gmail.com>
2026-03-11 04:43:58 -0300

    Jessica Jones (S1) auditada y corregida

--- assets/data/mcu/phaseTwo/jessicajones2015.js
+++ assets/data/mcu/phaseTwo/jessicajones2015.js
@@ -1,18 +1,27 @@
-import { jj_s1e1 } from "./jessicajones2015/episodes.js";
+import { 
+  jj_s1e1, jj_s1e2, jj_s1e3, jj_s1e4, jj_s1e5, jj_s1e6, jj_s1e7,
+  jj_s1e8, jj_s1e9, jj_s1e10, jj_s1e11, jj_s1e12, jj_s1e13
+} from "./jessicajones2015/episodes.js";
 
 export const jessicajones2015 = {
   id: "jsjS1",
-  label: "Jessica Jones",
+  label: "Jessica Jones (S1)",
   format: "Serie",
   season: 1,
-  release: 2015,
-  order: 21,
+  release: {
+    year: 2015,
+    month: 11,
+    day: 20
+  },
   phase: 2,
-  phase_order: 12,
-  year_in_timeline: 2015,
+  year_in_timeline: {
+    start: 2015,
+    end: 2015
+  },
   completed: false,
   opened: false,
   episodes: [
-    jj_s1e1
+    jj_s1e1, jj_s1e2, jj_s1e3, jj_s1e4, jj_s1e5, jj_s1e6, jj_s1e7,
+    jj_s1e8, jj_s1e9, jj_s1e10, jj_s1e11, jj_s1e12, jj_s1e13
   ],
 };
--- assets/data/mcu/phaseTwo/jessicajones2015/episodes.js
+++ assets/data/mcu/phaseTwo/jessicajones2015/episodes.js
@@ -1,9 +1,129 @@
 export const jj_s1e1 = {
-  id: "jj1",
+  id: "jj1e1",
   number: 1,
   completed: false,
   label: {
     en: "AKA Ladies Night",
     es: "AKA Noche de señoritas",
+  },
+};
+
+export const jj_s1e2 = {
+  id: "jj1e2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "AKA Crush Syndrome",
+    es: "AKA Síndrome de aplastamiento",
+  },
+};
+
+export const jj_s1e3 = {
+  id: "jj1e3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "AKA It's Called Whiskey",
+    es: "AKA Se llama whisky",
+  },
+};
+
+export const jj_s1e4 = {
+  id: "jj1e4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "AKA 99 Friends",
+    es: "AKA 99 amigos",
+  },
+};
+
+export const jj_s1e5 = {
+  id: "jj1e5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "AKA The Sandwich Saved Me",
+    es: "AKA El sándwich me salvó",
+  },
+};
+
+export const jj_s1e6 = {
+  id: "jj1e6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "AKA You're a Winner!",
+    es: "AKA ¡Eres un ganador!",
+  },
+};
+
+export const jj_s1e7 = {
+  id: "jj1e7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "AKA Top Shelf Perverts",
+    es: "AKA Pervertidos de primera",
+  },
+};
+
+export const jj_s1e8 = {
+  id: "jj1e8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "AKA WWJD?",
+    es: "AKA ¿QJH?",
+  },
+};
+
+export const jj_s1e9 = {
+  id: "jj1e9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "AKA Sin Bin",
+    es: "AKA El rincón de pensar",
+  },
+};
+
+export const jj_s1e10 = {
+  id: "jj1e10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "AKA 1,000 Cuts",
+    es: "AKA 1,000 cortes",
+  },
+};
+
+export const jj_s1e11 = {
+  id: "jj1e11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "AKA I've Got the Blues",
+    es: "AKA Tengo el blues",
+  },
+};
+
+export const jj_s1e12 = {
+  id: "jj1e12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "AKA Take a Bloody Number",
+    es: "AKA Toma un maldito número",
+  },
+};
+
+export const jj_s1e13 = {
+  id: "jj1e13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "AKA Smile",
+    es: "AKA Sonríe",
   },
 };
