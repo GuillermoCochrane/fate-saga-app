25578aa99e48fa0f1047a37c96a788c5aea1ae29
Guille <cochraneguillermo@gmail.com>
2026-03-11 03:53:58 -0300

    Jessica Jones (S3) a la fase 3 del MCU

---
+++ assets/data/mcu/phaseThree/jessicajonesS3.js
@@ -0,0 +1,18 @@
+import { jj_s3e1 } from "./jessicajonesS3/episodes.js";
+
+export const jessicajonesS3 = {
+  id: "jsjS3",
+  label: "Jessica Jones (S3)",
+  format: "Serie",
+  season: 3,
+  release: 2015,
+  order: 21,
+  phase: 3,
+  phase_order: 12,
+  year_in_timeline: 2015,
+  completed: false,
+  opened: false,
+  episodes: [
+    jj_s3e1
+  ],
+};
