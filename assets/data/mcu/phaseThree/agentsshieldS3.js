798a9fa94f542e23e3026c17c186a6ed9a3637d5
Guille <cochraneguillermo@gmail.com>
2026-03-12 16:19:10 -0300

    Marvel: Agents of S.H.I.E.L.D. (S3) auditada y corregida

--- assets/data/mcu/phaseThree/agentsshieldS3.js
+++ assets/data/mcu/phaseThree/agentsshieldS3.js
@@ -1,18 +1,29 @@
-import { aos_s3e1 } from "./agentsshieldS3/episodes.js";
+import { 
+  aos_s3e1, aos_s3e2, aos_s3e3, aos_s3e4, aos_s3e5, aos_s3e6, aos_s3e7, aos_s3e8,
+  aos_s3e9, aos_s3e10, aos_s3e11, aos_s3e12, aos_s3e13, aos_s3e14, aos_s3e15, aos_s3e16,
+  aos_s3e17, aos_s3e18, aos_s3e19, aos_s3e20, aos_s3e21, aos_s3e22
+} from "./agentsshieldS3/episodes.js";
 
 export const agentsshieldS3 = {
-  id: "aosS1",
+  id: "aosS3",
   label: "Marvel: Agents of S.H.I.E.L.D. (S3)",
   format: "Serie",
-  season: 1,
-  release: 2013,
-  order: 10,
-  phase: 2,
-  phase_order: 1,
-  year_in_timeline: 2013,
+  season: 3,
+  release: {
+    year: 2015,
+    month: 9,
+    day: 29
+  },
+  phase: 3,
+  year_in_timeline: {
+    start: 2015,
+    end: 2016
+  },
   completed: false,
   opened: false,
   episodes: [
-    aos_s3e1
+    aos_s3e1, aos_s3e2, aos_s3e3, aos_s3e4, aos_s3e5, aos_s3e6, aos_s3e7, aos_s3e8,
+    aos_s3e9, aos_s3e10, aos_s3e11, aos_s3e12, aos_s3e13, aos_s3e14, aos_s3e15, aos_s3e16,
+    aos_s3e17, aos_s3e18, aos_s3e19, aos_s3e20, aos_s3e21, aos_s3e22
   ],
 };
--- assets/data/mcu/phaseThree/agentsshieldS3/episodes.js
+++ assets/data/mcu/phaseThree/agentsshieldS3/episodes.js
@@ -1,9 +1,219 @@
 export const aos_s3e1 = {
-  id: "aosS3E1",
+  id: "as3e1",
   number: 1,
   completed: false,
   label: {
-    en: "Pilot",
-    es: "Piloto",
+    en: "Laws of Nature",
+    es: "Leyes de la naturaleza",
+  },
+};
+
+export const aos_s3e2 = {
+  id: "as3e2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Purpose in the Machine",
+    es: "Propósito en la máquina",
+  },
+};
+
+export const aos_s3e3 = {
+  id: "as3e3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "A Wanted (Inhu)man",
+    es: "Un (In)humano buscado",
+  },
+};
+
+export const aos_s3e4 = {
+  id: "as3e4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Devils You Know",
+    es: "Diablos conocidos",
+  },
+};
+
+export const aos_s3e5 = {
+  id: "as3e5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "4,722 Hours",
+    es: "4,722 horas",
+  },
+};
+
+export const aos_s3e6 = {
+  id: "as3e6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Among Us Hide...",
+    es: "Entre nosotros se esconde...",
+  },
+};
+
+export const aos_s3e7 = {
+  id: "as3e7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Chaos Theory",
+    es: "Teoría del caos",
+  },
+};
+
+export const aos_s3e8 = {
+  id: "as3e8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Many Heads, One Tale",
+    es: "Muchas cabezas, un cuento",
+  },
+};
+
+export const aos_s3e9 = {
+  id: "as3e9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Closure",
+    es: "Cierre",
+  },
+};
+
+export const aos_s3e10 = {
+  id: "as3e10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "Maveth",
+    es: "Maveth",
+  },
+};
+
+export const aos_s3e11 = {
+  id: "as3e11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "Bouncing Back",
+    es: "Recuperándose",
+  },
+};
+
+export const aos_s3e12 = {
+  id: "as3e12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Inside Man",
+    es: "El infiltrado",
+  },
+};
+
+export const aos_s3e13 = {
+  id: "as3e13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "Parting Shot",
+    es: "Disparo de despedida",
+  },
+};
+
+export const aos_s3e14 = {
+  id: "as3e14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "Watchdogs",
+    es: "Watchdogs",
+  },
+};
+
+export const aos_s3e15 = {
+  id: "as3e15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "Spacetime",
+    es: "Espacio-tiempo",
+  },
+};
+
+export const aos_s3e16 = {
+  id: "as3e16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "Paradise Lost",
+    es: "Paraíso perdido",
+  },
+};
+
+export const aos_s3e17 = {
+  id: "as3e17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "The Team",
+    es: "El equipo",
+  },
+};
+
+export const aos_s3e18 = {
+  id: "as3e18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "The Singularity",
+    es: "La singularidad",
+  },
+};
+
+export const aos_s3e19 = {
+  id: "as3e19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "Failed Experiments",
+    es: "Experimentos fallidos",
+  },
+};
+
+export const aos_s3e20 = {
+  id: "as3e20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "Emancipation",
+    es: "Emancipación",
+  },
+};
+
+export const aos_s3e21 = {
+  id: "as3e21",
+  number: 21,
+  completed: false,
+  label: {
+    en: "Absolution",
+    es: "Absolución",
+  },
+};
+
+export const aos_s3e22 = {
+  id: "as3e22",
+  number: 22,
+  completed: false,
+  label: {
+    en: "Ascension",
+    es: "Ascensión",
   },
 };
