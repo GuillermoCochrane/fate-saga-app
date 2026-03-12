891d8d97d31d9278776dac0602ed60a7b6e4c549
Guille <cochraneguillermo@gmail.com>
2026-03-12 16:15:24 -0300

    Daredevil (S2) auditada y corregida

--- assets/data/mcu/phaseThree/daredevilS2.js
+++ assets/data/mcu/phaseThree/daredevilS2.js
@@ -1,18 +1,27 @@
-import { dd_s2e1 } from "./daredevilS2/episodes.js";
+import { 
+  dd_s2e1, dd_s2e2, dd_s2e3, dd_s2e4, dd_s2e5, dd_s2e6, dd_s2e7,
+  dd_s2e8, dd_s2e9, dd_s2e10, dd_s2e11, dd_s2e12, dd_s2e13
+} from "./daredevilS2/episodes.js";
 
 export const daredevilS2 = {
   id: "drdS2",
   label: "Daredevil (S2)",
   format: "Serie",
-  season: 1,
-  release: 2015,
-  order: 20,
-  phase: 2,
-  phase_order: 11,
-  year_in_timeline: 2015,
+  season: 2,
+  release: {
+    year: 2016,
+    month: 3,
+    day: 18
+  },
+  phase: 3,
+  year_in_timeline: {
+    start: 2016,
+    end: 2016
+  },
   completed: false,
   opened: false,
   episodes: [
-    dd_s2e1
+    dd_s2e1, dd_s2e2, dd_s2e3, dd_s2e4, dd_s2e5, dd_s2e6, dd_s2e7,
+    dd_s2e8, dd_s2e9, dd_s2e10, dd_s2e11, dd_s2e12, dd_s2e13
   ],
 };
--- assets/data/mcu/phaseThree/daredevilS2/episodes.js
+++ assets/data/mcu/phaseThree/daredevilS2/episodes.js
@@ -1,9 +1,129 @@
 export const dd_s2e1 = {
-  id: "ddS2E1",
+  id: "dd2e1",
   number: 1,
   completed: false,
   label: {
-    en: "Pilot",
-    es: "Piloto",
+    en: "Bang",
+    es: "Bang",
+  },
+};
+
+export const dd_s2e2 = {
+  id: "dd2e2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Dogs to a Gunfight",
+    es: "Perros a un tiroteo",
+  },
+};
+
+export const dd_s2e3 = {
+  id: "dd2e3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "New York's Finest",
+    es: "Lo mejor de Nueva York",
+  },
+};
+
+export const dd_s2e4 = {
+  id: "dd2e4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Penny and Dime",
+    es: "Penny and Dime",
+  },
+};
+
+export const dd_s2e5 = {
+  id: "dd2e5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Kinbaku",
+    es: "Kinbaku",
+  },
+};
+
+export const dd_s2e6 = {
+  id: "dd2e6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Regrets Only",
+    es: "Solo pesares",
+  },
+};
+
+export const dd_s2e7 = {
+  id: "dd2e7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Semper Fidelis",
+    es: "Siempre fiel",
+  },
+};
+
+export const dd_s2e8 = {
+  id: "dd2e8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Guilty as Sin",
+    es: "Culpable como el pecado",
+  },
+};
+
+export const dd_s2e9 = {
+  id: "dd2e9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Seven Minutes in Heaven",
+    es: "Siete minutos en el cielo",
+  },
+};
+
+export const dd_s2e10 = {
+  id: "dd2e10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Man in the Box",
+    es: "El hombre en la caja",
+  },
+};
+
+export const dd_s2e11 = {
+  id: "dd2e11",
+  number: 11,
+  completed: false,
+  label: {
+    en: ".380",
+    es: ".380",
+  },
+};
+
+export const dd_s2e12 = {
+  id: "dd2e12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Dark at the End of the Tunnel",
+    es: "La oscuridad al final del túnel",
+  },
+};
+
+export const dd_s2e13 = {
+  id: "dd2e13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "A Cold Day in Hell's Kitchen",
+    es: "Un día frío en el infierno de la cocina",
   },
 };
