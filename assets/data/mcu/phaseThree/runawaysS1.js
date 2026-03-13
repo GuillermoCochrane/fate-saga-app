25ee7647bbca0e98fb0bd083001325d7c23fe6b2
Guille <cochraneguillermo@gmail.com>
2026-03-13 16:31:13 -0300

    Marvel:Runaways (S1) auditada y corregida

--- assets/data/mcu/phaseThree/runawaysS1.js
+++ assets/data/mcu/phaseThree/runawaysS1.js
@@ -1,18 +1,27 @@
-import { runaways_s1e1 } from "./runawaysS1/episodes.js";
+import { 
+  runaways_s1e1, runaways_s1e2, runaways_s1e3, runaways_s1e4, runaways_s1e5,
+  runaways_s1e6, runaways_s1e7, runaways_s1e8, runaways_s1e9, runaways_s1e10
+} from "./runawaysS1/episodes.js";
 
 export const runawaysS1 = {
   id: "mraS1",
-  label: "Marvel:Runaways (S1)",
+  label: "Marvel's Runaways (S1)",
   format: "Serie",
   season: 1,
-  release: 2017,
-  order: 29,
+  release: {
+    year: 2017,
+    month: 11,
+    day: 21
+  },
   phase: 3,
-  phase_order: 8,
-  year_in_timeline: 2017,
+  year_in_timeline: {
+    start: 2017,
+    end: 2018
+  },
   completed: false,
   opened: false,
   episodes: [
-    runaways_s1e1
+    runaways_s1e1, runaways_s1e2, runaways_s1e3, runaways_s1e4, runaways_s1e5,
+    runaways_s1e6, runaways_s1e7, runaways_s1e8, runaways_s1e9, runaways_s1e10
   ],
 };
--- assets/data/mcu/phaseThree/runawaysS1/episodes.js
+++ assets/data/mcu/phaseThree/runawaysS1/episodes.js
@@ -1,9 +1,99 @@
 export const runaways_s1e1 = {
-  id: "mrS1E1",
+  id: "mr1e1",
   number: 1,
   completed: false,
   label: {
     en: "Reunion",
     es: "Reunión",
+  },
+};
+
+export const runaways_s1e2 = {
+  id: "mr1e2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Rewind",
+    es: "Rebobinar",
+  },
+};
+
+export const runaways_s1e3 = {
+  id: "mr1e3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Destiny",
+    es: "Destino",
+  },
+};
+
+export const runaways_s1e4 = {
+  id: "mr1e4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Fifteen",
+    es: "Quince",
+  },
+};
+
+export const runaways_s1e5 = {
+  id: "mr1e5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Kingdom",
+    es: "Reino",
+  },
+};
+
+export const runaways_s1e6 = {
+  id: "mr1e6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Metamorphosis",
+    es: "Metamorfosis",
+  },
+};
+
+export const runaways_s1e7 = {
+  id: "mr1e7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Refraction",
+    es: "Refracción",
+  },
+};
+
+export const runaways_s1e8 = {
+  id: "mr1e8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Tsunami",
+    es: "Tsunami",
+  },
+};
+
+export const runaways_s1e9 = {
+  id: "mr1e9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Doomsday",
+    es: "Día del juicio",
+  },
+};
+
+export const runaways_s1e10 = {
+  id: "mr1e10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "Hostile",
+    es: "Hostil",
   },
 };
