6d18a50813b400e8d96b1bc70bec47e849d0c462
Guille <cochraneguillermo@gmail.com>
2026-03-15 05:57:29 -0300

    Marvel's Agents of S.H.I.E.L.D. (S4) auditada y corregida

--- assets/data/mcu/phaseThree/agentsshieldS4.js
+++ assets/data/mcu/phaseThree/agentsshieldS4.js
@@ -1,18 +1,29 @@
-import { aos_s4e1 } from "./agentsshieldS4/episodes.js";
+import { 
+  aos_s4e1, aos_s4e2, aos_s4e3, aos_s4e4, aos_s4e5, aos_s4e6, aos_s4e7, aos_s4e8,
+  aos_s4e9, aos_s4e10, aos_s4e11, aos_s4e12, aos_s4e13, aos_s4e14, aos_s4e15, aos_s4e16,
+  aos_s4e17, aos_s4e18, aos_s4e19, aos_s4e20, aos_s4e21, aos_s4e22
+} from "./agentsshieldS4/episodes.js";
 
 export const agentsshieldS4 = {
-  id: "aosS1",
-  label: "Marvel: Agents of S.H.I.E.L.D. (S3)",
+  id: "aosS4",
+  label: "Marvel's Agents of S.H.I.E.L.D. (S4)",
   format: "Serie",
-  season: 1,
-  release: 2013,
-  order: 10,
-  phase: 2,
-  phase_order: 1,
-  year_in_timeline: 2013,
+  season: 4,
+  release: {
+    year: 2016,
+    month: 9,
+    day: 20
+  },
+  phase: 3,
+  year_in_timeline: {
+    start: 2016,
+    end: 2017
+  },
   completed: false,
   opened: false,
   episodes: [
-    aos_s4e1
+    aos_s4e1, aos_s4e2, aos_s4e3, aos_s4e4, aos_s4e5, aos_s4e6, aos_s4e7, aos_s4e8,
+    aos_s4e9, aos_s4e10, aos_s4e11, aos_s4e12, aos_s4e13, aos_s4e14, aos_s4e15, aos_s4e16,
+    aos_s4e17, aos_s4e18, aos_s4e19, aos_s4e20, aos_s4e21, aos_s4e22
   ],
 };
--- assets/data/mcu/phaseThree/agentsshieldS4/episodes.js
+++ assets/data/mcu/phaseThree/agentsshieldS4/episodes.js
@@ -1,9 +1,219 @@
 export const aos_s4e1 = {
-  id: "aosS4E1",
+  id: "as4e1",
   number: 1,
   completed: false,
   label: {
-    en: "Pilot",
-    es: "Piloto",
+    en: "The Ghost",
+    es: "El fantasma",
+  },
+};
+
+export const aos_s4e2 = {
+  id: "as4e2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Meet the New Boss",
+    es: "Conoce al nuevo jefe",
+  },
+};
+
+export const aos_s4e3 = {
+  id: "as4e3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Uprising",
+    es: "Levantamiento",
+  },
+};
+
+export const aos_s4e4 = {
+  id: "as4e4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Let Me Stand Next to Your Fire",
+    es: "Déjame estar junto a tu fuego",
+  },
+};
+
+export const aos_s4e5 = {
+  id: "as4e5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Lockup",
+    es: "Encierro",
+  },
+};
+
+export const aos_s4e6 = {
+  id: "as4e6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Good Samaritan",
+    es: "El buen samaritano",
+  },
+};
+
+export const aos_s4e7 = {
+  id: "as4e7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Deals with Our Devils",
+    es: "Tratos con nuestros demonios",
+  },
+};
+
+export const aos_s4e8 = {
+  id: "as4e8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Laws of Inferno Dynamics",
+    es: "Las leyes de la dinámica del infierno",
+  },
+};
+
+export const aos_s4e9 = {
+  id: "as4e9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Broken Promises",
+    es: "Promesas rotas",
+  },
+};
+
+export const aos_s4e10 = {
+  id: "as4e10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Patriot",
+    es: "El patriota",
+  },
+};
+
+export const aos_s4e11 = {
+  id: "as4e11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "Wake Up",
+    es: "Despierta",
+  },
+};
+
+export const aos_s4e12 = {
+  id: "as4e12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "Hot Potato Soup",
+    es: "Sopa de papa caliente",
+  },
+};
+
+export const aos_s4e13 = {
+  id: "as4e13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "BOOM",
+    es: "BOOM",
+  },
+};
+
+export const aos_s4e14 = {
+  id: "as4e14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "The Man Behind the Shield",
+    es: "El hombre detrás del escudo",
+  },
+};
+
+export const aos_s4e15 = {
+  id: "as4e15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "Self Control",
+    es: "Autocontrol",
+  },
+};
+
+export const aos_s4e16 = {
+  id: "as4e16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "What If...",
+    es: "¿Qué pasaría si...?",
+  },
+};
+
+export const aos_s4e17 = {
+  id: "as4e17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "Identity and Change",
+    es: "Identidad y cambio",
+  },
+};
+
+export const aos_s4e18 = {
+  id: "as4e18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "No Regrets",
+    es: "Sin arrepentimientos",
+  },
+};
+
+export const aos_s4e19 = {
+  id: "as4e19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "All the Madame's Men",
+    es: "Todos los hombres de Madame",
+  },
+};
+
+export const aos_s4e20 = {
+  id: "as4e20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "Farewell, Cruel World!",
+    es: "¡Adiós, mundo cruel!",
+  },
+};
+
+export const aos_s4e21 = {
+  id: "as4e21",
+  number: 21,
+  completed: false,
+  label: {
+    en: "The Return",
+    es: "El regreso",
+  },
+};
+
+export const aos_s4e22 = {
+  id: "as4e22",
+  number: 22,
+  completed: false,
+  label: {
+    en: "World's End",
+    es: "El fin del mundo",
   },
 };
