48eb4af5fe2705954411ebf9d184e7dac4a9834e
Guille <cochraneguillermo@gmail.com>
2026-03-15 05:59:30 -0300

    Marvel's Agents of S.H.I.E.L.D. (S5) auditada y corregida

--- assets/data/mcu/phaseThree/agentsshieldS5.js
+++ assets/data/mcu/phaseThree/agentsshieldS5.js
@@ -1,18 +1,29 @@
-import { aos_s5e1 } from "./agentsshieldS5/episodes.js";
+import { 
+  aos_s5e1, aos_s5e2, aos_s5e3, aos_s5e4, aos_s5e5, aos_s5e6, aos_s5e7, aos_s5e8,
+  aos_s5e9, aos_s5e10, aos_s5e11, aos_s5e12, aos_s5e13, aos_s5e14, aos_s5e15, aos_s5e16,
+  aos_s5e17, aos_s5e18, aos_s5e19, aos_s5e20, aos_s5e21, aos_s5e22
+} from "./agentsshieldS5/episodes.js";
 
 export const agentsshieldS5 = {
-  id: "aosS1",
-  label: "Marvel: Agents of S.H.I.E.L.D. (S5)",
+  id: "aosS5",
+  label: "Marvel's Agents of S.H.I.E.L.D. (S5)",
   format: "Serie",
-  season: 1,
-  release: 2013,
-  order: 10,
-  phase: 2,
-  phase_order: 1,
-  year_in_timeline: 2013,
+  season: 5,
+  release: {
+    year: 2017,
+    month: 12,
+    day: 1
+  },
+  phase: 3,
+  year_in_timeline: {
+    start: 2017,
+    end: 2018
+  },
   completed: false,
   opened: false,
   episodes: [
-    aos_s5e1
+    aos_s5e1, aos_s5e2, aos_s5e3, aos_s5e4, aos_s5e5, aos_s5e6, aos_s5e7, aos_s5e8,
+    aos_s5e9, aos_s5e10, aos_s5e11, aos_s5e12, aos_s5e13, aos_s5e14, aos_s5e15, aos_s5e16,
+    aos_s5e17, aos_s5e18, aos_s5e19, aos_s5e20, aos_s5e21, aos_s5e22
   ],
 };
--- assets/data/mcu/phaseThree/agentsshieldS5/episodes.js
+++ assets/data/mcu/phaseThree/agentsshieldS5/episodes.js
@@ -1,9 +1,219 @@
 export const aos_s5e1 = {
-  id: "aosS5E1",
+  id: "as5e1",
   number: 1,
   completed: false,
   label: {
-    en: "Pilot",
-    es: "Piloto",
+    en: "Orientation (Part 1)",
+    es: "Orientación (Parte 1)",
+  },
+};
+
+export const aos_s5e2 = {
+  id: "as5e2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Orientation (Part 2)",
+    es: "Orientación (Parte 2)",
+  },
+};
+
+export const aos_s5e3 = {
+  id: "as5e3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "A Life Spent",
+    es: "Una vida gastada",
+  },
+};
+
+export const aos_s5e4 = {
+  id: "as5e4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "A Life Earned",
+    es: "Una vida ganada",
+  },
+};
+
+export const aos_s5e5 = {
+  id: "as5e5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Rewind",
+    es: "Rebobinar",
+  },
+};
+
+export const aos_s5e6 = {
+  id: "as5e6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Fun & Games",
+    es: "Diversión y juegos",
+  },
+};
+
+export const aos_s5e7 = {
+  id: "as5e7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Together or Not at All",
+    es: "Juntos o no",
+  },
+};
+
+export const aos_s5e8 = {
+  id: "as5e8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Last Day",
+    es: "El último día",
+  },
+};
+
+export const aos_s5e9 = {
+  id: "as5e9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Best Laid Plans",
+    es: "Los mejores planes",
+  },
+};
+
+export const aos_s5e10 = {
+  id: "as5e10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "Past Life",
+    es: "Vida pasada",
+  },
+};
+
+export const aos_s5e11 = {
+  id: "as5e11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "All the Comforts of Home",
+    es: "Todas las comodidades del hogar",
+  },
+};
+
+export const aos_s5e12 = {
+  id: "as5e12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Real Deal",
+    es: "El verdadero trato",
+  },
+};
+
+export const aos_s5e13 = {
+  id: "as5e13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "Principia",
+    es: "Principia",
+  },
+};
+
+export const aos_s5e14 = {
+  id: "as5e14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "The Devil Complex",
+    es: "El complejo del diablo",
+  },
+};
+
+export const aos_s5e15 = {
+  id: "as5e15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "Rise and Shine",
+    es: "Levantarse y brillar",
+  },
+};
+
+export const aos_s5e16 = {
+  id: "as5e16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "Inside Voices",
+    es: "Voces internas",
+  },
+};
+
+export const aos_s5e17 = {
+  id: "as5e17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "The Honeymoon",
+    es: "La luna de miel",
+  },
+};
+
+export const aos_s5e18 = {
+  id: "as5e18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "All Roads Lead...",
+    es: "Todos los caminos llevan...",
+  },
+};
+
+export const aos_s5e19 = {
+  id: "as5e19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "Option Two",
+    es: "Opción dos",
+  },
+};
+
+export const aos_s5e20 = {
+  id: "as5e20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "The One Who Will Save Us All",
+    es: "El que nos salvará a todos",
+  },
+};
+
+export const aos_s5e21 = {
+  id: "as5e21",
+  number: 21,
+  completed: false,
+  label: {
+    en: "The Force of Gravity",
+    es: "La fuerza de la gravedad",
+  },
+};
+
+export const aos_s5e22 = {
+  id: "as5e22",
+  number: 22,
+  completed: false,
+  label: {
+    en: "The End",
+    es: "El fin",
   },
 };
