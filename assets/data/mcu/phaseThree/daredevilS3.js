f6f476867ede2c9119e9113fe11d4c7beddd4e29
Guille <cochraneguillermo@gmail.com>
2026-03-15 06:09:15 -0300

    Daredevil (S3) auditada y corregida

--- assets/data/mcu/phaseThree/daredevilS3.js
+++ assets/data/mcu/phaseThree/daredevilS3.js
@@ -1,18 +1,27 @@
-import { dd_s3e1 } from "./daredevilS3/episodes.js";
+import { 
+  ddS3E1, ddS3E2, ddS3E3, ddS3E4, ddS3E5, ddS3E6, ddS3E7,
+  ddS3E8, ddS3E9, ddS3E10, ddS3E11, ddS3E12, ddS3E13
+} from "./daredevilS3/episodes.js";
 
 export const daredevilS3 = {
   id: "drdS3",
   label: "Daredevil (S3)",
   format: "Serie",
-  season: 1,
-  release: 2015,
-  order: 20,
+  season: 3,
+  release: {
+    year: 2018,
+    month: 10,
+    day: 19
+  },
   phase: 3,
-  phase_order: 11,
-  year_in_timeline: 2015,
+  year_in_timeline: {
+    start: 2018,
+    end: 2018
+  },
   completed: false,
   opened: false,
   episodes: [
-    dd_s3e1
+    ddS3E1, ddS3E2, ddS3E3, ddS3E4, ddS3E5, ddS3E6, ddS3E7,
+    ddS3E8, ddS3E9, ddS3E10, ddS3E11, ddS3E12, ddS3E13
   ],
 };
--- assets/data/mcu/phaseThree/daredevilS3/episodes.js
+++ assets/data/mcu/phaseThree/daredevilS3/episodes.js
@@ -1,9 +1,129 @@
-export const dd_s3e1 = {
+export const ddS3E1 = {
   id: "ddS3E1",
   number: 1,
   completed: false,
   label: {
-    en: "Pilot",
-    es: "Piloto",
+    en: "Resurrection",
+    es: "Resurrección",
+  },
+};
+
+export const ddS3E2 = {
+  id: "ddS3E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Please",
+    es: "Por favor",
+  },
+};
+
+export const ddS3E3 = {
+  id: "ddS3E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "No Good Deed",
+    es: "Ninguna buena acción",
+  },
+};
+
+export const ddS3E4 = {
+  id: "ddS3E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Blindsided",
+    es: "Cegado",
+  },
+};
+
+export const ddS3E5 = {
+  id: "ddS3E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Perfect Game",
+    es: "El juego perfecto",
+  },
+};
+
+export const ddS3E6 = {
+  id: "ddS3E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Devil You Know",
+    es: "El diablo que conoces",
+  },
+};
+
+export const ddS3E7 = {
+  id: "ddS3E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Aftermath",
+    es: "Secuelas",
+  },
+};
+
+export const ddS3E8 = {
+  id: "ddS3E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Upstairs/Downstairs",
+    es: "Arriba/Abajo",
+  },
+};
+
+export const ddS3E9 = {
+  id: "ddS3E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Revelations",
+    es: "Revelaciones",
+  },
+};
+
+export const ddS3E10 = {
+  id: "ddS3E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "Karen",
+    es: "Karen",
+  },
+};
+
+export const ddS3E11 = {
+  id: "ddS3E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "Reunion",
+    es: "Reunión",
+  },
+};
+
+export const ddS3E12 = {
+  id: "ddS3E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "One Last Shot",
+    es: "Un último tiro",
+  },
+};
+
+export const ddS3E13 = {
+  id: "ddS3E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "A New Napkin",
+    es: "Una servilleta nueva",
   },
 };
