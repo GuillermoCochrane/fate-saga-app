e3cf0f156d50054242758e9b84a247fa7235c2d3
Guille <cochraneguillermo@gmail.com>
2026-03-15 06:07:13 -0300

    Jessica Jones (S2) auditada y corregida

--- assets/data/mcu/phaseThree/jessicajonesS2.js
+++ assets/data/mcu/phaseThree/jessicajonesS2.js
@@ -1,18 +1,27 @@
-import { jj_s2e1 } from "./jessicajonesS2/episodes.js";
+import { 
+  jj_s2e1, jj_s2e2, jj_s2e3, jj_s2e4, jj_s2e5, jj_s2e6, jj_s2e7,
+  jj_s2e8, jj_s2e9, jj_s2e10, jj_s2e11, jj_s2e12, jj_s2e13
+} from "./jessicajonesS2/episodes.js";
 
 export const jessicajonesS2 = {
   id: "jsjS2",
   label: "Jessica Jones (S2)",
   format: "Serie",
   season: 2,
-  release: 2015,
-  order: 21,
+  release: {
+    year: 2018,
+    month: 3,
+    day: 8
+  },
   phase: 3,
-  phase_order: 12,
-  year_in_timeline: 2015,
+  year_in_timeline: {
+    start: 2018,
+    end: 2018
+  },
   completed: false,
   opened: false,
   episodes: [
-    jj_s2e1
+    jj_s2e1, jj_s2e2, jj_s2e3, jj_s2e4, jj_s2e5, jj_s2e6, jj_s2e7,
+    jj_s2e8, jj_s2e9, jj_s2e10, jj_s2e11, jj_s2e12, jj_s2e13
   ],
 };
--- assets/data/mcu/phaseThree/jessicajonesS2/episodes.js
+++ assets/data/mcu/phaseThree/jessicajonesS2/episodes.js
@@ -1,9 +1,129 @@
 export const jj_s2e1 = {
-  id: "jjS2E1",
+  id: "jj2e1",
   number: 1,
   completed: false,
   label: {
-    en: "Pilot",
-    es: "Piloto",
+    en: "AKA Start at the Beginning",
+    es: "AKA Empezar por el principio",
+  },
+};
+
+export const jj_s2e2 = {
+  id: "jj2e2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "AKA Freak Accident",
+    es: "AKA Accidente extraño",
+  },
+};
+
+export const jj_s2e3 = {
+  id: "jj2e3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "AKA Sole Survivor",
+    es: "AKA Único sobreviviente",
+  },
+};
+
+export const jj_s2e4 = {
+  id: "jj2e4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "AKA God Help the Hobo",
+    es: "AKA Dios ayude al vagabundo",
+  },
+};
+
+export const jj_s2e5 = {
+  id: "jj2e5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "AKA The Octopus",
+    es: "AKA El pulpo",
+  },
+};
+
+export const jj_s2e6 = {
+  id: "jj2e6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "AKA Facetime",
+    es: "AKA Videollamada",
+  },
+};
+
+export const jj_s2e7 = {
+  id: "jj2e7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "AKA I Want Your Cray Cray",
+    es: "AKA Quiero tu locura",
+  },
+};
+
+export const jj_s2e8 = {
+  id: "jj2e8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "AKA Ain't We Got Fun",
+    es: "AKA ¿Acaso no nos divertimos?",
+  },
+};
+
+export const jj_s2e9 = {
+  id: "jj2e9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "AKA Shark in the Bathtub",
+    es: "AKA Tiburón en la bañera",
+  },
+};
+
+export const jj_s2e10 = {
+  id: "jj2e10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "AKA Pork Chop",
+    es: "AKA Chuleta de cerdo",
+  },
+};
+
+export const jj_s2e11 = {
+  id: "jj2e11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "AKA Three Lives and Counting",
+    es: "AKA Tres vidas y contando",
+  },
+};
+
+export const jj_s2e12 = {
+  id: "jj2e12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "AKA Pray for My Patsy",
+    es: "AKA Reza por mi Patsy",
+  },
+};
+
+export const jj_s2e13 = {
+  id: "jj2e13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "AKA Playland",
+    es: "AKA Playland",
   },
 };
