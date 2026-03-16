2aab07f5f90f624583e7e1685038a2e5e2127b40
Guille <cochraneguillermo@gmail.com>
2026-03-16 15:20:51 -0300

    The Falcon and The Winter Soldier auditada y corregida

--- assets/data/mcu/phaseFour/falconwintersoldier2021.js
+++ assets/data/mcu/phaseFour/falconwintersoldier2021.js
@@ -1,18 +1,25 @@
-import { tfatws_e1 } from "./falconwintersoldier2021/episodes.js";
+import { 
+  fwE1, fwE2, fwE3, fwE4, fwE5, fwE6
+} from "./falconwintersoldier2021/episodes.js";
 
 export const falconwintersoldier2021 = {
   id: "fwsS1",
   label: "The Falcon and The Winter Soldier",
   format: "Serie",
   season: 1,
-  release: 2021,
-  order: 32,
+  release: {
+    year: 2021,
+    month: 3,
+    day: 19
+  },
   phase: 4,
-  phase_order: 2,
-  year_in_timeline: 2021,
+  year_in_timeline: {
+    start: 2024,
+    end: 2024
+  },
   completed: false,
   opened: false,
   episodes: [
-    tfatws_e1
+    fwE1, fwE2, fwE3, fwE4, fwE5, fwE6
   ],
 };
--- assets/data/mcu/phaseFour/falconwintersoldier2021/episodes.js
+++ assets/data/mcu/phaseFour/falconwintersoldier2021/episodes.js
@@ -1,5 +1,5 @@
-export const tfatws_e1 = {
-  id: "fw1",
+export const fwE1 = {
+  id: "fwE1",
   number: 1,
   completed: false,
   label: {
@@ -8,3 +8,52 @@ export const tfatws_e1 = {
   },
 };
 
+export const fwE2 = {
+  id: "fwE2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Star-Spangled Man",
+    es: "El hombre de las estrellas",
+  },
+};
+
+export const fwE3 = {
+  id: "fwE3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Power Broker",
+    es: "El Power Broker",
+  },
+};
+
+export const fwE4 = {
+  id: "fwE4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Whole World Is Watching",
+    es: "El mundo entero está mirando",
+  },
+};
+
+export const fwE5 = {
+  id: "fwE5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Truth",
+    es: "Verdad",
+  },
+};
+
+export const fwE6 = {
+  id: "fwE6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "One World, One People",
+    es: "Un mundo, un pueblo",
+  },
+};
