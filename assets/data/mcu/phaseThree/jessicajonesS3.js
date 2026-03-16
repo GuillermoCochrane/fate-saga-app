218f4e0606cd485bc8f4bee0778e4aa51ad3ac18
Guille <cochraneguillermo@gmail.com>
2026-03-16 14:56:49 -0300

    Jessica Jones (S3) auditada y corregida

--- assets/data/mcu/phaseThree/jessicajonesS3.js
+++ assets/data/mcu/phaseThree/jessicajonesS3.js
@@ -1,18 +1,27 @@
-import { jj_s3e1 } from "./jessicajonesS3/episodes.js";
+import { 
+  jjS3E1, jjS3E2, jjS3E3, jjS3E4, jjS3E5, jjS3E6, jjS3E7,
+  jjS3E8, jjS3E9, jjS3E10, jjS3E11, jjS3E12, jjS3E13
+} from "./jessicajonesS3/episodes.js";
 
 export const jessicajonesS3 = {
   id: "jsjS3",
   label: "Jessica Jones (S3)",
   format: "Serie",
   season: 3,
-  release: 2015,
-  order: 21,
+  release: {
+    year: 2019,
+    month: 6,
+    day: 14
+  },
   phase: 3,
-  phase_order: 12,
-  year_in_timeline: 2015,
+  year_in_timeline: {
+    start: 2019,
+    end: 2019
+  },
   completed: false,
   opened: false,
   episodes: [
-    jj_s3e1
+    jjS3E1, jjS3E2, jjS3E3, jjS3E4, jjS3E5, jjS3E6, jjS3E7,
+    jjS3E8, jjS3E9, jjS3E10, jjS3E11, jjS3E12, jjS3E13
   ],
 };
--- assets/data/mcu/phaseThree/jessicajonesS3/episodes.js
+++ assets/data/mcu/phaseThree/jessicajonesS3/episodes.js
@@ -1,9 +1,129 @@
-export const jj_s3e1 = {
+export const jjS3E1 = {
   id: "jjS3E1",
   number: 1,
   completed: false,
   label: {
-    en: "Pilot",
-    es: "Piloto",
+    en: "AKA The Perfect Burger",
+    es: "AKA La hamburguesa perfecta",
+  },
+};
+
+export const jjS3E2 = {
+  id: "jjS3E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "AKA You're Welcome",
+    es: "AKA De nada",
+  },
+};
+
+export const jjS3E3 = {
+  id: "jjS3E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "AKA I Have No Spleen",
+    es: "AKA No tengo bazo",
+  },
+};
+
+export const jjS3E4 = {
+  id: "jjS3E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "AKA Customer Service is Standing By",
+    es: "AKA Servicio al cliente en espera",
+  },
+};
+
+export const jjS3E5 = {
+  id: "jjS3E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "AKA I Wish",
+    es: "AKA Ojalá",
+  },
+};
+
+export const jjS3E6 = {
+  id: "jjS3E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "AKA Sorry Face",
+    es: "AKA Cara de perdón",
+  },
+};
+
+export const jjS3E7 = {
+  id: "jjS3E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "AKA The Double Half-Wappinger",
+    es: "AKA El doble Half-Wappinger",
+  },
+};
+
+export const jjS3E8 = {
+  id: "jjS3E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "AKA Camera in the Kitchen",
+    es: "AKA Cámara en la cocina",
+  },
+};
+
+export const jjS3E9 = {
+  id: "jjS3E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "AKA I Did Something Today",
+    es: "AKA Hoy hice algo",
+  },
+};
+
+export const jjS3E10 = {
+  id: "jjS3E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "AKA Hero Pants",
+    es: "AKA Pantalones de héroe",
+  },
+};
+
+export const jjS3E11 = {
+  id: "jjS3E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "AKA Hell Cat",
+    es: "AKA Hellcat",
+  },
+};
+
+export const jjS3E12 = {
+  id: "jjS3E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "AKA A Lotta Worms",
+    es: "AKA Muchos gusanos",
+  },
+};
+
+export const jjS3E13 = {
+  id: "jjS3E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "AKA Everything",
+    es: "AKA Todo",
   },
 };
