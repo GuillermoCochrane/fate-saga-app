4078d0429eaea0c377849aa45ccc5f84032bfd5f
Guille <cochraneguillermo@gmail.com>
2026-03-19 18:01:55 -0300

    Fase 5 del MCU ordenada por fecha de emision

--- assets/data/mcu/phaseFive.js
+++ assets/data/mcu/phaseFive.js
@@ -1,17 +1,19 @@
 // 2022
-import { iamgroot2022 } from "./phaseFive/iamgroot2022.js";
+import { iamgrootS1 } from "./phaseFive/iamgrootS1.js";
 
 // 2023
 import { antmanquantumania2023 } from "./phaseFive/antmanquantumania2023.js";
 import { guardiansgalaxyvol32023 } from "./phaseFive/guardiansgalaxyvol32023.js";
 import { secretinvasion2023 } from "./phaseFive/secretinvasion2023.js";
-import { whatifseason22023 } from "./phaseFive/whatifseason22023.js";
+import { iamgrootS2 } from "./phaseFive/iamgrootS2.js";
 import { themarvels2023 } from "./phaseFive/themarvels2023.js";
+import { whatifseason22023 } from "./phaseFive/whatifseason22023.js";
 
 // 2024
 import { echo2024 } from "./phaseFive/echo2024.js";
 import { deadpoolwolverine2024 } from "./phaseFive/deadpoolwolverine2024.js";
 import { agathaallalong2024 } from "./phaseFive/agathaallalong2024.js";
+import { whatifS3 } from "./phaseFive/whatifS3.js";
 
 // 2025
 import { captainamericabravenewworld2025 } from "./phaseFive/captainamericabravenewworld2025.js";
@@ -22,15 +24,17 @@ export const phaseFive = {
   id: "ph5",
   opened: false,
   seasons: [
-    iamgroot2022,
+    iamgrootS1,
     antmanquantumania2023,
     guardiansgalaxyvol32023,
     secretinvasion2023,
-    whatifseason22023,
+    iamgrootS2,
     themarvels2023,
+    whatifseason22023,
     echo2024,
     deadpoolwolverine2024,
     agathaallalong2024,
+    whatifS3,
     captainamericabravenewworld2025,
     thunderbolts2025,
   ],
