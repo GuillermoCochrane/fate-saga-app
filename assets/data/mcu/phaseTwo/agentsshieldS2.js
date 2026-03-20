716bd06a5a13ecbf2b574daabc0c46174b5f2e6d
Guille <cochraneguillermo@gmail.com>
2026-03-20 17:13:35 -0300

    Season corregida  en Marvel: Agents of S.H.I.E.L.D. (S2)

--- assets/data/mcu/phaseTwo/agentsshieldS2.js
+++ assets/data/mcu/phaseTwo/agentsshieldS2.js
@@ -2,9 +2,9 @@ import { aos_s2e1 } from "./agentsshieldS2/episodes.js";
 
 export const agentsshieldS2 = {
   id: "aosS2",
-  label: "Marvel: Agents of S.H.I.E.L.D. S2",
+  label: "Marvel: Agents of S.H.I.E.L.D. (S2)",
   format: "Serie",
-  season: 1,
+  season: 2,
   release: 2013,
   order: 10,
   phase: 2,
