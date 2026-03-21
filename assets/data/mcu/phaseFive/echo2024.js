9f7b365ac9ce9f7f3cd58812a65d4c95dd9e862c
Guille <cochraneguillermo@gmail.com>
2026-03-21 18:30:29 -0300

    Echo auditada y corregida

--- assets/data/mcu/phaseFive/echo2024.js
+++ assets/data/mcu/phaseFive/echo2024.js
@@ -1,18 +1,25 @@
-import { echo_e1 } from "./echo2024/episodes.js";
+import { 
+  ecE1, ecE2, ecE3, ecE4, ecE5
+} from "./echo2024/episodes.js";
 
 export const echo2024 = {
   id: "echS1",
   label: "Echo",
   format: "Serie",
   season: 1,
-  release: 2024,
-  order: 54,
+  release: {
+    year: 2024,
+    month: 1,
+    day: 9
+  },
   phase: 5,
-  phase_order: 7,
-  year_in_timeline: 2024,
+  year_in_timeline: {
+    start: 2025,
+    end: 2025
+  },
   completed: false,
   opened: false,
   episodes: [
-    echo_e1
+    ecE1, ecE2, ecE3, ecE4, ecE5
   ],
 };
--- assets/data/mcu/phaseFive/echo2024/episodes.js
+++ assets/data/mcu/phaseFive/echo2024/episodes.js
@@ -1,9 +1,49 @@
-export const echo_e1 = {
-  id: "echS1E1",
+export const ecE1 = {
+  id: "ecE1",
   number: 1,
   completed: false,
   label: {
     en: "Chafa",
     es: "Chafa",
+  },
+};
+
+export const ecE2 = {
+  id: "ecE2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Lowak",
+    es: "Lowak",
+  },
+};
+
+export const ecE3 = {
+  id: "ecE3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Tuklo",
+    es: "Tuklo",
+  },
+};
+
+export const ecE4 = {
+  id: "ecE4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Taloa",
+    es: "Taloa",
+  },
+};
+
+export const ecE5 = {
+  id: "ecE5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Maya",
+    es: "Maya",
   },
 };
