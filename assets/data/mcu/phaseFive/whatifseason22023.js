e2ff78f854ab124fec73995deb12f01efcf839c9
Guille <cochraneguillermo@gmail.com>
2026-03-21 18:28:21 -0300

    What If...? (S2 auditada y corregida

--- assets/data/mcu/phaseFive/whatifseason22023.js
+++ assets/data/mcu/phaseFive/whatifseason22023.js
@@ -1,18 +1,25 @@
-import { wi_s2e1 } from "./whatifseason22023/episodes.js";
+import { 
+  wiS2E1, wiS2E2, wiS2E3, wiS2E4, wiS2E5, wiS2E6, wiS2E7, wiS2E8, wiS2E9
+} from "./whatifseason22023/episodes.js";
 
 export const whatifseason22023 = {
   id: "wtiS2",
-  label: "What If...?",
+  label: "What If...? (S2)",
   format: "Serie",
   season: 2,
-  release: 2023,
-  order: 52,
+  release: {
+    year: 2023,
+    month: 12,
+    day: 22
+  },
   phase: 5,
-  phase_order: 5,
-  year_in_timeline: 2023,
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
   completed: false,
   opened: false,
   episodes: [
-    wi_s2e1
+    wiS2E1, wiS2E2, wiS2E3, wiS2E4, wiS2E5, wiS2E6, wiS2E7, wiS2E8, wiS2E9
   ],
 };
--- assets/data/mcu/phaseFive/whatifseason22023/episodes.js
+++ assets/data/mcu/phaseFive/whatifseason22023/episodes.js
@@ -1,9 +1,89 @@
-export const wi_s2e1 = {
+export const wiS2E1 = {
   id: "wiS2E1",
   number: 1,
   completed: false,
   label: {
     en: "What If... Nebula Joined the Nova Corps?",
-    es: "¿Y si Nebula se hubiese unido a la Nova Corps?",
+    es: "¿Qué pasaría si... Nebula se hubiera unido al Cuerpo Nova?",
+  },
+};
+
+export const wiS2E2 = {
+  id: "wiS2E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "What If... Peter Quill Attacked Earth's Mightiest Heroes?",
+    es: "¿Qué pasaría si... Peter Quill atacara a los héroes más poderosos de la Tierra?",
+  },
+};
+
+export const wiS2E3 = {
+  id: "wiS2E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "What If... Happy Hogan Saved Christmas?",
+    es: "¿Qué pasaría si... Happy Hogan salvara la Navidad?",
+  },
+};
+
+export const wiS2E4 = {
+  id: "wiS2E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "What If... Iron Man Crashed Into the Grandmaster?",
+    es: "¿Qué pasaría si... Iron Man se estrellara contra el Gran Maestro?",
+  },
+};
+
+export const wiS2E5 = {
+  id: "wiS2E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "What If... Captain Carter Fought the Hydra Stomper?",
+    es: "¿Qué pasaría si... la Capitana Carter se enfrentara al Hydra Stomper?",
+  },
+};
+
+export const wiS2E6 = {
+  id: "wiS2E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "What If... Kahhori Reshaped the World?",
+    es: "¿Qué pasaría si... Kahhori remodelara el mundo?",
+  },
+};
+
+export const wiS2E7 = {
+  id: "wiS2E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "What If... Hela Found the Ten Rings?",
+    es: "¿Qué pasaría si... Hela encontrara los Diez Anillos?",
+  },
+};
+
+export const wiS2E8 = {
+  id: "wiS2E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "What If... The Avengers Assembled in 1602?",
+    es: "¿Qué pasaría si... Los Vengadores se reunieran en 1602?",
+  },
+};
+
+export const wiS2E9 = {
+  id: "wiS2E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "What If... Strange Supreme Intervened?",
+    es: "¿Qué pasaría si... el Supremo Strange interviniera?",
   },
 };
