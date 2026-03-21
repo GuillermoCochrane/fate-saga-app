85fc8021531aba56c9ad6daf1a4e86c0eb1f0347
Guille <cochraneguillermo@gmail.com>
2026-03-21 18:33:11 -0300

    Agatha All Along auditada y corregida

--- assets/data/mcu/phaseFive/agathaallalong2024.js
+++ assets/data/mcu/phaseFive/agathaallalong2024.js
@@ -1,18 +1,25 @@
-import { agatha_e1 } from "./agathaallalong2024/episodes.js";
+import { 
+  aaE1, aaE2, aaE3, aaE4, aaE5, aaE6, aaE7, aaE8, aaE9
+} from "./agathaallalong2024/episodes.js";
 
 export const agathaallalong2024 = {
   id: "aaaS1",
   label: "Agatha All Along",
   format: "Serie",
   season: 1,
-  release: 2024,
-  order: 56,
+  release: {
+    year: 2024,
+    month: 9,
+    day: 18
+  },
   phase: 5,
-  phase_order: 9, 
-  year_in_timeline: 2024,
+  year_in_timeline: {
+    start: 2026,
+    end: 2026
+  },
   completed: false,
   opened: false,
   episodes: [
-    agatha_e1
+    aaE1, aaE2, aaE3, aaE4, aaE5, aaE6, aaE7, aaE8, aaE9
   ],
 };
--- assets/data/mcu/phaseFive/agathaallalong2024/episodes.js
+++ assets/data/mcu/phaseFive/agathaallalong2024/episodes.js
@@ -1,5 +1,5 @@
-export const agatha_e1 = {
-  id: "aaaS1E1",
+export const aaE1 = {
+  id: "aaE1",
   number: 1,
   completed: false,
   label: {
@@ -8,3 +8,82 @@ export const agatha_e1 = {
   },
 };
 
+export const aaE2 = {
+  id: "aaE2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Circle Sewn with Fate / Unlock Thy Hidden Gate",
+    es: "Círculo cosido con el destino / Abre tu puerta escondida",
+  },
+};
+
+export const aaE3 = {
+  id: "aaE3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Through Many Miles / Of Tricks and Trials",
+    es: "A través de muchas millas / De trucos y pruebas",
+  },
+};
+
+export const aaE4 = {
+  id: "aaE4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "If I Can't Reach You / Let My Song Teach You",
+    es: "Si no puedo alcanzarte / Que mi canción te enseñe",
+  },
+};
+
+export const aaE5 = {
+  id: "aaE5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Darkest Hour / Wake Thy Power",
+    es: "Hora más oscura / Despierta tu poder",
+  },
+};
+
+export const aaE6 = {
+  id: "aaE6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Familiar by Thy Side",
+    es: "Familiar a tu lado",
+  },
+};
+
+export const aaE7 = {
+  id: "aaE7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Death's Hand in Mine",
+    es: "La mano de la muerte en la mía",
+  },
+};
+
+export const aaE8 = {
+  id: "aaE8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Follow Me My Friend / To Glory at the End",
+    es: "Sígueme, amigo mío / Hacia la gloria al final",
+  },
+};
+
+export const aaE9 = {
+  id: "aaE9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Maiden Mother Crone",
+    es: "Doncella Madre Ancestra",
+  },
+};
