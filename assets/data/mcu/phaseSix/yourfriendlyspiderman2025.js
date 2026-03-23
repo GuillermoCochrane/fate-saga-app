7f64b3882fd3493f52383f922b24de88f62bece6
Guille <cochraneguillermo@gmail.com>
2026-03-22 22:31:28 -0300

    Your Friendly Neighborhood Spider-Man (S1) auditada y corregida

--- assets/data/mcu/phaseSix/yourfriendlyspiderman2025.js
+++ assets/data/mcu/phaseSix/yourfriendlyspiderman2025.js
@@ -1,18 +1,27 @@
-import { yfnS1E1 } from "./yourfriendlyspiderman2025/episodes.js";
+import { 
+  yfnS1E1, yfnS1E2, yfnS1E3, yfnS1E4, yfnS1E5,
+  yfnS1E6, yfnS1E7, yfnS1E8, yfnS1E9, yfnS1E10
+} from "./yourfriendlyspiderman2025/episodes.js";
 
 export const yourfriendlyspiderman2025 = {
   id: "yfnS1",
-  label: "Your Friendly Neighborhood Spider-Man",
+  label: "Your Friendly Neighborhood Spider-Man (S1)",
   format: "Serie",
   season: 1,
-  release: 2025,
-  order: 59,
+  release: {
+    year: 2025,
+    month: 1,
+    day: 29
+  },
   phase: 6,
-  phase_order: 1,
-  year_in_timeline: 2025,
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
   completed: false,
   opened: false,
   episodes: [
-    yfnS1E1
+    yfnS1E1, yfnS1E2, yfnS1E3, yfnS1E4, yfnS1E5,
+    yfnS1E6, yfnS1E7, yfnS1E8, yfnS1E9, yfnS1E10
   ],
 };
--- assets/data/mcu/phaseSix/yourfriendlyspiderman2025/episodes.js
+++ assets/data/mcu/phaseSix/yourfriendlyspiderman2025/episodes.js
@@ -3,7 +3,97 @@ export const yfnS1E1 = {
   number: 1,
   completed: false,
   label: {
-    en: "Pilot",
-    es: "Piloto",
+    en: "Amazing Fantasy",
+    es: "Fantasía asombrosa",
+  },
+};
+
+export const yfnS1E2 = {
+  id: "yfnS1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Parker Luck",
+    es: "La suerte de Parker",
+  },
+};
+
+export const yfnS1E3 = {
+  id: "yfnS1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Secret Identity Crisis",
+    es: "Crisis de identidad secreta",
+  },
+};
+
+export const yfnS1E4 = {
+  id: "yfnS1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Hitting the Big Time",
+    es: "Llegando a la cima",
+  },
+};
+
+export const yfnS1E5 = {
+  id: "yfnS1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Unicorn Unleashed!",
+    es: "¡El Unicornio desatado!",
+  },
+};
+
+export const yfnS1E6 = {
+  id: "yfnS1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Duel with the Devil",
+    es: "Duelo con el diablo",
+  },
+};
+
+export const yfnS1E7 = {
+  id: "yfnS1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Scorpion Rising",
+    es: "El Escorpión se levanta",
+  },
+};
+
+export const yfnS1E8 = {
+  id: "yfnS1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Tangled Web",
+    es: "Telaraña enredada",
+  },
+};
+
+export const yfnS1E9 = {
+  id: "yfnS1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Hero or Menace",
+    es: "¿Héroe o amenaza?",
+  },
+};
+
+export const yfnS1E10 = {
+  id: "yfnS1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "If This Be My Destiny...",
+    es: "Si este es mi destino...",
   },
 };
