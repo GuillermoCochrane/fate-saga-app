b7ea431651a23a7e318f3de4a421503961189079
Guille <cochraneguillermo@gmail.com>
2026-03-23 05:15:19 -0300

    Daredevil: Born Again (S2) completada con placeholders por falta de datos

--- assets/data/mcu/phaseSix/daredevilbornagainS2.js
+++ assets/data/mcu/phaseSix/daredevilbornagainS2.js
@@ -1,18 +1,25 @@
-import { dbaS2E1 } from "./daredevilbornagainS2/episodes.js";
+import { 
+  dbaS2E1, dbaS2E2, dbaS2E3, dbaS2E4, dbaS2E5, dbaS2E6, dbaS2E7, dbaS2E8
+} from "./daredevilbornagainS2/episodes.js";
 
 export const daredevilbornagainS2 = {
   id: "dbaS2",
   label: "Daredevil: Born Again (S2)",
   format: "Serie",
   season: 2,
-  release: 2025,
-  order: 66,
+  release: {
+    year: 2026,
+    month: 3,
+    day: 24
+  },
   phase: 6,
-  phase_order: 8,
-  year_in_timeline: 2025,
+  year_in_timeline: {
+    start: 2026,
+    end: 2026
+  },
   completed: false,
   opened: false,
   episodes: [
-    dbaS2E1
+    dbaS2E1, dbaS2E2, dbaS2E3, dbaS2E4, dbaS2E5, dbaS2E6, dbaS2E7, dbaS2E8
   ],
 };
--- assets/data/mcu/phaseSix/daredevilbornagainS2/episodes.js
+++ assets/data/mcu/phaseSix/daredevilbornagainS2/episodes.js
@@ -3,7 +3,77 @@ export const dbaS2E1 = {
   number: 1,
   completed: false,
   label: {
-    en: "Pilot",
-    es: "Piloto",
+    en: "Episode 1",
+    es: "Episodio 1",
+  },
+};
+
+export const dbaS2E2 = {
+  id: "dbaS2E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Episode 2",
+    es: "Episodio 2",
+  },
+};
+
+export const dbaS2E3 = {
+  id: "dbaS2E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Episode 3",
+    es: "Episodio 3",
+  },
+};
+
+export const dbaS2E4 = {
+  id: "dbaS2E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Episode 4",
+    es: "Episodio 4",
+  },
+};
+
+export const dbaS2E5 = {
+  id: "dbaS2E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Episode 5",
+    es: "Episodio 5",
+  },
+};
+
+export const dbaS2E6 = {
+  id: "dbaS2E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Episode 6",
+    es: "Episodio 6",
+  },
+};
+
+export const dbaS2E7 = {
+  id: "dbaS2E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Episode 7",
+    es: "Episodio 7",
+  },
+};
+
+export const dbaS2E8 = {
+  id: "dbaS2E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Episode 8",
+    es: "Episodio 8",
   },
 };
