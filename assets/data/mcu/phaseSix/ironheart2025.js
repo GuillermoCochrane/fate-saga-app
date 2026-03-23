f8a7c85ffdf3a4cacb6659901768ebab9c997013
Guille <cochraneguillermo@gmail.com>
2026-03-22 22:35:09 -0300

    Ironheart auditada y corregida

--- assets/data/mcu/phaseSix/ironheart2025.js
+++ assets/data/mcu/phaseSix/ironheart2025.js
@@ -1,18 +1,25 @@
-import { irhS1E1 } from "./ironheart2025/episodes.js";
+import { 
+  irhE1, irhE2, irhE3, irhE4, irhE5, irhE6
+} from "./ironheart2025/episodes.js";
 
 export const ironheart2025 = {
   id: "irhS1",
   label: "Ironheart",
   format: "Serie",
   season: 1,
-  release: 2025,
-  order: 63,
+  release: {
+    year: 2025,
+    month: 6,
+    day: 24
+  },
   phase: 6,
-  phase_order: 5,
-  year_in_timeline: 2025,
+  year_in_timeline: {
+    start: 2025,
+    end: 2025
+  },
   completed: false,
   opened: false,
   episodes: [
-    irhS1E1
+    irhE1, irhE2, irhE3, irhE4, irhE5, irhE6
   ],
 };
--- assets/data/mcu/phaseSix/ironheart2025/episodes.js
+++ assets/data/mcu/phaseSix/ironheart2025/episodes.js
@@ -1,9 +1,59 @@
-export const irhS1E1 = {
-  id: "irhS1E1",
+export const irhE1 = {
+  id: "irhE1",
   number: 1,
   completed: false,
   label: {
-    en: "Pilot",
-    es: "Piloto",
+    en: "Take Me Home",
+    es: "Portame a casa",
+  },
+};
+
+export const irhE2 = {
+  id: "irhE2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Will the Real Natalie Please Stand Up?",
+    es: "¿La verdadera Natalie puede levantarse?",
+  },
+};
+
+export const irhE3 = {
+  id: "irhE3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "We in Danger, Girl",
+    es: "Estamos en peligro, chica",
+  },
+};
+
+export const irhE4 = {
+  id: "irhE4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Bad Magic",
+    es: "Magia peligrosa",
+  },
+};
+
+export const irhE5 = {
+  id: "irhE5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Karma's a Glitch",
+    es: "El karma es un glitch",
+  },
+};
+
+export const irhE6 = {
+  id: "irhE6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Past Is the Past",
+    es: "El pasado es el pasado",
   },
 };
