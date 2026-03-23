c70ba250ac2aea784780f23ac1df6e0e064b3bd4
Guille <cochraneguillermo@gmail.com>
2026-03-22 22:28:10 -0300

    Orquestador de la fase 6 auditado y corregido

--- assets/data/mcu/phaseSix.js
+++ assets/data/mcu/phaseSix.js
@@ -1,16 +1,16 @@
 // 2025
 import { yourfriendlyspiderman2025 } from "./phaseSix/yourfriendlyspiderman2025.js";
-import { daredevilbornagain2025 } from "./phaseSix/daredevilbornagain2025.js";
-import { eyesofwakanda2025 } from "./phaseSix/eyesofwakanda2025.js";
-import { fantasticfour2025 } from "./phaseSix/fantasticfour2025.js";
+import { daredevilbornagainS1 } from "./phaseSix/daredevilbornagainS1.js";
 import { ironheart2025 } from "./phaseSix/ironheart2025.js";
+import { fantasticfour2025 } from "./phaseSix/fantasticfour2025.js";
+import { eyesofwakanda2025 } from "./phaseSix/eyesofwakanda2025.js";
 import { marvelzombies2025 } from "./phaseSix/marvelzombies2025.js";
 
 // 2026
 import { wonderman2026 } from "./phaseSix/wonderman2026.js";
-import { daredevilbornagainS22025 } from "./phaseSix/daredevilbornagainS22025.js";
-import { avengersdoomsday2026 } from "./phaseSix/avengersdoomsday2026.js";
+import { daredevilbornagainS2 } from "./phaseSix/daredevilbornagainS2.js";
 import { spidermanbrandnewday2026 } from "./phaseSix/spidermanbrandnewday2026.js";
+import { avengersdoomsday2026 } from "./phaseSix/avengersdoomsday2026.js";
 import { visionquest2026 } from "./phaseSix/visionquest2026.js";
 
 // 2027
@@ -18,18 +18,19 @@ import { avengerssecretwars2027 } from "./phaseSix/avengerssecretwars2027.js";
 
 export const phaseSix = {
   saga: "Phase Six",
+  id: "ph6",
   opened: false,
   seasons: [
     yourfriendlyspiderman2025,
-    daredevilbornagain2025,
-    eyesofwakanda2025,
-    fantasticfour2025,
+    daredevilbornagainS1,
     ironheart2025,
+    fantasticfour2025,
+    eyesofwakanda2025,
     marvelzombies2025,
     wonderman2026,
-    daredevilbornagainS22025,
-    avengersdoomsday2026,
+    daredevilbornagainS2,
     spidermanbrandnewday2026,
+    avengersdoomsday2026,
     visionquest2026,
     avengerssecretwars2027,
   ],
