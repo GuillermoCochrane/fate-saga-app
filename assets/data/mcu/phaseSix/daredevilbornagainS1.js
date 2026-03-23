89687a37abaf8882cf2072da1118640b58c9f4ae
Guille <cochraneguillermo@gmail.com>
2026-03-22 22:33:26 -0300

    Daredevil: Born Again (S1) auditada y corregida

--- assets/data/mcu/phaseSix/daredevilbornagainS1.js
+++ assets/data/mcu/phaseSix/daredevilbornagainS1.js
@@ -1,18 +1,26 @@
-import { dbaS1E1 } from "./daredevilbornagainS1/episodes.js";
+import { 
+  dbaS1E1, dbaS1E2, dbaS1E3, dbaS1E4, dbaS1E5, dbaS1E6, dbaS1E7, dbaS1E8, dbaS1E9
+} from "./daredevilbornagainS1/episodes.js";
 
 export const daredevilbornagainS1 = {
   id: "dbaS1",
   label: "Daredevil: Born Again (S1)",
   format: "Serie",
   season: 1,
-  release: 2025,
-  order: 60,
+  release: {
+    year: 2025,
+    month: 3,
+    day: 4
+  },
   phase: 6,
-  phase_order: 2,
-  year_in_timeline: 2025,
+  year_in_timeline: {
+    start: 2025,
+    end: 2025
+  },
   completed: false,
   opened: false,
   episodes: [
-    dbaS1E1
+    dbaS1E1, dbaS1E2, dbaS1E3, dbaS1E4, dbaS1E5,
+    dbaS1E6, dbaS1E7, dbaS1E8, dbaS1E9
   ],
 };
--- assets/data/mcu/phaseSix/daredevilbornagainS1/episodes.js
+++ assets/data/mcu/phaseSix/daredevilbornagainS1/episodes.js
@@ -3,7 +3,87 @@ export const dbaS1E1 = {
   number: 1,
   completed: false,
   label: {
-    en: "Pilot",
-    es: "Piloto",
+    en: "Heaven's Half Hour",
+    es: "La media hora del cielo",
+  },
+};
+
+export const dbaS1E2 = {
+  id: "dbaS1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Optics",
+    es: "Óptica",
+  },
+};
+
+export const dbaS1E3 = {
+  id: "dbaS1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Hollow of His Hand",
+    es: "El hueco de su mano",
+  },
+};
+
+export const dbaS1E4 = {
+  id: "dbaS1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Sic Semper Systema",
+    es: "Sic Semper Systema",
+  },
+};
+
+export const dbaS1E5 = {
+  id: "dbaS1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "With Interest",
+    es: "Con interés",
+  },
+};
+
+export const dbaS1E6 = {
+  id: "dbaS1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Excessive Force",
+    es: "Fuerza excesiva",
+  },
+};
+
+export const dbaS1E7 = {
+  id: "dbaS1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Art for Art's Sake",
+    es: "El arte por el arte",
+  },
+};
+
+export const dbaS1E8 = {
+  id: "dbaS1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Isle of Joy",
+    es: "Isla de la alegría",
+  },
+};
+
+export const dbaS1E9 = {
+  id: "dbaS1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Straight to Hell",
+    es: "Directo al infierno",
   },
 };
