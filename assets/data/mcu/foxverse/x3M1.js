d5caaed603fb1176d4cd993ef6f926f04923bc6a
Guille <cochraneguillermo@gmail.com>
2026-03-24 04:52:38 -0300

    X-Men: The Last Stand del FoxVerse auditada y corregida

--- assets/data/mcu/foxverse/x3M1.js
+++ assets/data/mcu/foxverse/x3M1.js
@@ -3,6 +3,18 @@ import { x3 } from "./x3M1/episodes.js";
 export const x3M1 = {
   id: "x3M1",
   label: "X-Men: The Last Stand",
+  format: "Película",
+  release: {
+    year: 2006,
+    month: 5,
+    day: 26
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
     x3
   ],
--- assets/data/mcu/foxverse/x3M1/episodes.js
+++ assets/data/mcu/foxverse/x3M1/episodes.js
@@ -1,7 +1,9 @@
 export const x3 = {
   id: "x3",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"X-Men: The Last Stand"},
+    en: "X-Men: The Last Stand",
+    es: "X-Men: La batalla final",
   },
 };
-
