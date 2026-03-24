12ca17b9bfe369a60fcb41c53948fe725c5162ae
Guille <cochraneguillermo@gmail.com>
2026-03-24 04:54:57 -0300

    Fantastic Four: Rise of the Silver Surfer, del FoxVerse, auditada y corregida

--- assets/data/mcu/foxverse/ff2M1.js
+++ assets/data/mcu/foxverse/ff2M1.js
@@ -3,6 +3,18 @@ import { ff2 } from "./ff2M1/episodes.js";
 export const ff2M1 = {
   id: "ff2M1",
   label: "Fantastic Four: Rise of the Silver Surfer",
+  format: "Película",
+  release: {
+    year: 2007,
+    month: 6,
+    day: 15
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
     ff2
   ],
--- assets/data/mcu/foxverse/ff2M1/episodes.js
+++ assets/data/mcu/foxverse/ff2M1/episodes.js
@@ -1,7 +1,9 @@
 export const ff2 = {
   id: "ff2",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Fantastic Four: Rise of the Silver Surfer"},
+    en: "Fantastic Four: Rise of the Silver Surfer",
+    es: "Los 4 Fantásticos y Silver Surfer",
   },
 };
-
