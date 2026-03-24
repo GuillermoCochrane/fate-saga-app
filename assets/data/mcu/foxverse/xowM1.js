88dd283934b2afc87f0872ac87afea9d17d6eae9
Guille <cochraneguillermo@gmail.com>
2026-03-24 04:56:30 -0300

    X-Men Origins: Wolverine, del FoxVerse, auditada y corregida

--- assets/data/mcu/foxverse/xowM1.js
+++ assets/data/mcu/foxverse/xowM1.js
@@ -3,6 +3,18 @@ import { xow } from "./xowM1/episodes.js";
 export const xowM1 = {
   id: "xowM1",
   label: "X-Men Origins: Wolverine",
+  format: "Película",
+  release: {
+    year: 2009,
+    month: 5,
+    day: 1
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
     xow
   ],
--- assets/data/mcu/foxverse/xowM1/episodes.js
+++ assets/data/mcu/foxverse/xowM1/episodes.js
@@ -1,7 +1,9 @@
 export const xow = {
   id: "xow",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"X-Men Origins: Wolverine"},
+    en: "X-Men Origins: Wolverine",
+    es: "X-Men Orígenes: Wolverine",
   },
 };
-
