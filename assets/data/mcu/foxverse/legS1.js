3434c11722342a0de0e336ffecf797a424aaa1ff
Guille <cochraneguillermo@gmail.com>
2026-03-26 17:03:17 -0300

    Legion (S1), del FoxVerse, auditada y corregida

--- assets/data/mcu/foxverse/legS1.js
+++ assets/data/mcu/foxverse/legS1.js
@@ -1,9 +1,26 @@
-import {  } from "./legS1/episodes.js";
+import { 
+  legS1E1, legS1E2, legS1E3, legS1E4, legS1E5,
+  legS1E6, legS1E7, legS1E8
+} from "./legS1/episodes.js";
 
 export const legS1 = {
   id: "legS1",
   label: "Legion (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 2017,
+    month: 2,
+    day: 8
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    
+    legS1E1, legS1E2, legS1E3, legS1E4, legS1E5,
+    legS1E6, legS1E7, legS1E8
   ],
 };
---
+++ assets/data/mcu/foxverse/legS1/episodes.js
@@ -0,0 +1,79 @@
+export const legS1E1 = {
+  id: "legS1E1",
+  number: 1,
+  completed: false,
+  label: {
+    en: "Chapter 1",
+    es: "Capítulo 1",
+  },
+};
+
+export const legS1E2 = {
+  id: "legS1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Chapter 2",
+    es: "Capítulo 2",
+  },
+};
+
+export const legS1E3 = {
+  id: "legS1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Chapter 3",
+    es: "Capítulo 3",
+  },
+};
+
+export const legS1E4 = {
+  id: "legS1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Chapter 4",
+    es: "Capítulo 4",
+  },
+};
+
+export const legS1E5 = {
+  id: "legS1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Chapter 5",
+    es: "Capítulo 5",
+  },
+};
+
+export const legS1E6 = {
+  id: "legS1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Chapter 6",
+    es: "Capítulo 6",
+  },
+};
+
+export const legS1E7 = {
+  id: "legS1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Chapter 7",
+    es: "Capítulo 7",
+  },
+};
+
+export const legS1E8 = {
+  id: "legS1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Chapter 8",
+    es: "Capítulo 8",
+  },
+};
