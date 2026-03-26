889c0b26d4b98ef3366995cb07329f7d85e858f4
Guille <cochraneguillermo@gmail.com>
2026-03-26 17:22:06 -0300

    Legion (S3), del FoxVerse, auditada y corregida

--- assets/data/mcu/foxverse/legS3.js
+++ assets/data/mcu/foxverse/legS3.js
@@ -1,9 +1,26 @@
-import {  } from "./legS3/episodes.js";
+import { 
+  legS3E1, legS3E2, legS3E3, legS3E4, legS3E5,
+  legS3E6, legS3E7, legS3E8
+} from "./legS3/episodes.js";
 
 export const legS3 = {
   id: "legS3",
   label: "Legion (S3)",
+  format: "Serie",
+  season: 3,
+  release: {
+    year: 2019,
+    month: 6,
+    day: 24
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    
+    legS3E1, legS3E2, legS3E3, legS3E4, legS3E5,
+    legS3E6, legS3E7, legS3E8
   ],
 };
---
+++ assets/data/mcu/foxverse/legS3/episodes.js
@@ -0,0 +1,79 @@
+export const legS3E1 = {
+  id: "legS3E1",
+  number: 1,
+  completed: false,
+  label: {
+    en: "Chapter 20",
+    es: "Capítulo 20",
+  },
+};
+
+export const legS3E2 = {
+  id: "legS3E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Chapter 21",
+    es: "Capítulo 21",
+  },
+};
+
+export const legS3E3 = {
+  id: "legS3E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Chapter 22",
+    es: "Capítulo 22",
+  },
+};
+
+export const legS3E4 = {
+  id: "legS3E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Chapter 23",
+    es: "Capítulo 23",
+  },
+};
+
+export const legS3E5 = {
+  id: "legS3E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Chapter 24",
+    es: "Capítulo 24",
+  },
+};
+
+export const legS3E6 = {
+  id: "legS3E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Chapter 25",
+    es: "Capítulo 25",
+  },
+};
+
+export const legS3E7 = {
+  id: "legS3E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Chapter 26",
+    es: "Capítulo 26",
+  },
+};
+
+export const legS3E8 = {
+  id: "legS3E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Chapter 27",
+    es: "Capítulo 27",
+  },
+};
