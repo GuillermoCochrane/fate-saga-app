4819c282df5bd5bb311bd3d10f5e2f2ee69f8072
Guille <cochraneguillermo@gmail.com>
2026-03-26 17:08:23 -0300

    The Gifted (S1), del FoxVerse, auditada y corregida

--- assets/data/mcu/foxverse/gftS1.js
+++ assets/data/mcu/foxverse/gftS1.js
@@ -1,9 +1,28 @@
-import {  } from "./gftS1/episodes.js";
+import { 
+  gftS1E1, gftS1E2, gftS1E3, gftS1E4, gftS1E5,
+  gftS1E6, gftS1E7, gftS1E8, gftS1E9, gftS1E10,
+  gftS1E11, gftS1E12, gftS1E13
+} from "./gftS1/episodes.js";
 
 export const gftS1 = {
   id: "gftS1",
   label: "The Gifted (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 2017,
+    month: 10,
+    day: 2
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    
+    gftS1E1, gftS1E2, gftS1E3, gftS1E4, gftS1E5,
+    gftS1E6, gftS1E7, gftS1E8, gftS1E9, gftS1E10,
+    gftS1E11, gftS1E12, gftS1E13
   ],
 };
---
+++ assets/data/mcu/foxverse/gftS1/episodes.js
@@ -0,0 +1,129 @@
+export const gftS1E1 = {
+  id: "gftS1E1",
+  number: 1,
+  completed: false,
+  label: {
+    en: "eXposed",
+    es: "eXposed",
+  },
+};
+
+export const gftS1E2 = {
+  id: "gftS1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "rX",
+    es: "rX",
+  },
+};
+
+export const gftS1E3 = {
+  id: "gftS1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "eXodus",
+    es: "eXodus",
+  },
+};
+
+export const gftS1E4 = {
+  id: "gftS1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "eXit strategy",
+    es: "eXit strategy",
+  },
+};
+
+export const gftS1E5 = {
+  id: "gftS1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "boXed in",
+    es: "boXed in",
+  },
+};
+
+export const gftS1E6 = {
+  id: "gftS1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "got your siX",
+    es: "got your siX",
+  },
+};
+
+export const gftS1E7 = {
+  id: "gftS1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "eXtreme measures",
+    es: "eXtreme measures",
+  },
+};
+
+export const gftS1E8 = {
+  id: "gftS1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "threat of eXtinction",
+    es: "threat of eXtinction",
+  },
+};
+
+export const gftS1E9 = {
+  id: "gftS1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "outfoX",
+    es: "outfoX",
+  },
+};
+
+export const gftS1E10 = {
+  id: "gftS1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "eXploited",
+    es: "eXploited",
+  },
+};
+
+export const gftS1E11 = {
+  id: "gftS1E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "3 X 1",
+    es: "3 X 1",
+  },
+};
+
+export const gftS1E12 = {
+  id: "gftS1E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "eXtraction",
+    es: "eXtraction",
+  },
+};
+
+export const gftS1E13 = {
+  id: "gftS1E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "X-roads",
+    es: "X-roads",
+  },
+};
