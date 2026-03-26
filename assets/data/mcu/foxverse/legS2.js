591400362d25b59f11fe4f2fd5d57de6b487f7ad
Guille <cochraneguillermo@gmail.com>
2026-03-26 17:12:59 -0300

    Legion (S2), del FoxVerse, auditada y corregida

--- assets/data/mcu/foxverse/legS2.js
+++ assets/data/mcu/foxverse/legS2.js
@@ -1,9 +1,28 @@
-import {  } from "./legS2/episodes.js";
+import { 
+  legS2E1, legS2E2, legS2E3, legS2E4, legS2E5,
+  legS2E6, legS2E7, legS2E8, legS2E9, legS2E10,
+  legS2E11
+} from "./legS2/episodes.js";
 
 export const legS2 = {
   id: "legS2",
   label: "Legion (S2)",
+  format: "Serie",
+  season: 2,
+  release: {
+    year: 2018,
+    month: 4,
+    day: 3
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    
+    legS2E1, legS2E2, legS2E3, legS2E4, legS2E5,
+    legS2E6, legS2E7, legS2E8, legS2E9, legS2E10,
+    legS2E11
   ],
 };
---
+++ assets/data/mcu/foxverse/legS2/episodes.js
@@ -0,0 +1,109 @@
+export const legS2E1 = {
+  id: "legS2E1",
+  number: 1,
+  completed: false,
+  label: {
+    en: "Chapter 9",
+    es: "Capítulo 9",
+  },
+};
+
+export const legS2E2 = {
+  id: "legS2E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Chapter 10",
+    es: "Capítulo 10",
+  },
+};
+
+export const legS2E3 = {
+  id: "legS2E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Chapter 11",
+    es: "Capítulo 11",
+  },
+};
+
+export const legS2E4 = {
+  id: "legS2E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Chapter 12",
+    es: "Capítulo 12",
+  },
+};
+
+export const legS2E5 = {
+  id: "legS2E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Chapter 13",
+    es: "Capítulo 13",
+  },
+};
+
+export const legS2E6 = {
+  id: "legS2E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Chapter 14",
+    es: "Capítulo 14",
+  },
+};
+
+export const legS2E7 = {
+  id: "legS2E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Chapter 15",
+    es: "Capítulo 15",
+  },
+};
+
+export const legS2E8 = {
+  id: "legS2E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Chapter 16",
+    es: "Capítulo 16",
+  },
+};
+
+export const legS2E9 = {
+  id: "legS2E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Chapter 17",
+    es: "Capítulo 17",
+  },
+};
+
+export const legS2E10 = {
+  id: "legS2E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "Chapter 18",
+    es: "Capítulo 18",
+  },
+};
+
+export const legS2E11 = {
+  id: "legS2E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "Chapter 19",
+    es: "Capítulo 19",
+  },
+};
