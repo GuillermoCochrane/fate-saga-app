f797acc244b9b185886a1c6fdb6a9b304a1c9335
Guille <cochraneguillermo@gmail.com>
2026-03-26 17:17:32 -0300

    The Gifted (S1), del FoxVerse, auditada y corregida

--- assets/data/mcu/foxverse/gftS2.js
+++ assets/data/mcu/foxverse/gftS2.js
@@ -1,9 +1,30 @@
-import {  } from "./gftS2/episodes.js";
+import { 
+  gftS2E1, gftS2E2, gftS2E3, gftS2E4, gftS2E5,
+  gftS2E6, gftS2E7, gftS2E8, gftS2E9, gftS2E10,
+  gftS2E11, gftS2E12, gftS2E13, gftS2E14, gftS2E15,
+  gftS2E16
+} from "./gftS2/episodes.js";
 
 export const gftS2 = {
   id: "gftS2",
   label: "The Gifted (S2)",
+  format: "Serie",
+  season: 2,
+  release: {
+    year: 2018,
+    month: 9,
+    day: 25
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    
+    gftS2E1, gftS2E2, gftS2E3, gftS2E4, gftS2E5,
+    gftS2E6, gftS2E7, gftS2E8, gftS2E9, gftS2E10,
+    gftS2E11, gftS2E12, gftS2E13, gftS2E14, gftS2E15,
+    gftS2E16
   ],
 };
---
+++ assets/data/mcu/foxverse/gftS2/episodes.js
@@ -0,0 +1,159 @@
+export const gftS2E1 = {
+  id: "gftS2E1",
+  number: 1,
+  completed: false,
+  label: {
+    en: "eMergence",
+    es: "eMergence",
+  },
+};
+
+export const gftS2E2 = {
+  id: "gftS2E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "unMoored",
+    es: "unMoored",
+  },
+};
+
+export const gftS2E3 = {
+  id: "gftS2E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "coMplications",
+    es: "coMplications",
+  },
+};
+
+export const gftS2E4 = {
+  id: "gftS2E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "outMatched",
+    es: "outMatched",
+  },
+};
+
+export const gftS2E5 = {
+  id: "gftS2E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "afterMath",
+    es: "afterMath",
+  },
+};
+
+export const gftS2E6 = {
+  id: "gftS2E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "iMprint",
+    es: "iMprint",
+  },
+};
+
+export const gftS2E7 = {
+  id: "gftS2E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "no Mercy",
+    es: "no Mercy",
+  },
+};
+
+export const gftS2E8 = {
+  id: "gftS2E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "the dreaM",
+    es: "the dreaM",
+  },
+};
+
+export const gftS2E9 = {
+  id: "gftS2E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "gaMe changer",
+    es: "gaMe changer",
+  },
+};
+
+export const gftS2E10 = {
+  id: "gftS2E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "eneMy of my eneMy",
+    es: "eneMy of my eneMy",
+  },
+};
+
+export const gftS2E11 = {
+  id: "gftS2E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "meMento",
+    es: "meMento",
+  },
+};
+
+export const gftS2E12 = {
+  id: "gftS2E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "hoMe",
+    es: "hoMe",
+  },
+};
+
+export const gftS2E13 = {
+  id: "gftS2E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "teMpted",
+    es: "teMpted",
+  },
+};
+
+export const gftS2E14 = {
+  id: "gftS2E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "calaMity",
+    es: "calaMity",
+  },
+};
+
+export const gftS2E15 = {
+  id: "gftS2E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "Monsters",
+    es: "Monsters",
+  },
+};
+
+export const gftS2E16 = {
+  id: "gftS2E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "oMens",
+    es: "oMens",
+  },
+};
