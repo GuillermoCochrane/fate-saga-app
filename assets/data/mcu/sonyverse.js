f3b693986ac01387559d72d994689affa724efd2
Guille <cochraneguillermo@gmail.com>
2026-03-27 22:42:21 -0300

    orquestador organizado por fecha de emision

--- assets/data/mcu/sonyverse.js
+++ assets/data/mcu/sonyverse.js
@@ -1,19 +1,42 @@
+// 2002
 import { sm1M1 } from "./sonyverse/sm1M1.js";
+
+// 2004
 import { sm2M1 } from "./sonyverse/sm2M1.js";
+
+// 2007
 import { sm3M1 } from "./sonyverse/sm3M1.js";
+
+// 2008-2009 (The Spectacular Spider-Man)
+import { sspS1 } from "./sonyverse/sspS1.js";
+import { sspS2 } from "./sonyverse/sspS2.js";
+
+// 2012
 import { tasm1M1 } from "./sonyverse/tasm1M1.js";
+
+// 2014
 import { tasm2M1 } from "./sonyverse/tasm2M1.js";
+
+// 2018
 import { venom1M1 } from "./sonyverse/venom1M1.js";
+import { sv1M1 } from "./sonyverse/sv1M1.js";
+
+// 2021
 import { venom2M1 } from "./sonyverse/venom2M1.js";
+
+// 2022
 import { morbiusM1 } from "./sonyverse/morbiusM1.js";
+
+// 2023
+import { sv2M1 } from "./sonyverse/sv2M1.js";
+
+// 2024
 import { madamewebM1 } from "./sonyverse/madamewebM1.js";
 import { venom3M1 } from "./sonyverse/venom3M1.js";
 import { kravenM1 } from "./sonyverse/kravenM1.js";
-import { sv1M1 } from "./sonyverse/sv1M1.js";
-import { sv2M1 } from "./sonyverse/sv2M1.js";
+
+// TBA
 import { sv3M1 } from "./sonyverse/sv3M1.js";
-import { sspS1 } from "./sonyverse/sspS1.js";
-import { sspS2 } from "./sonyverse/sspS2.js";
 
 export const sonyverse = {
   saga: "Sonyverse",
@@ -23,18 +46,18 @@ export const sonyverse = {
     sm1M1,
     sm2M1,
     sm3M1,
+    sspS1,
+    sspS2,
     tasm1M1,
     tasm2M1,
     venom1M1,
+    sv1M1,
     venom2M1,
     morbiusM1,
+    sv2M1,
     madamewebM1,
     venom3M1,
     kravenM1,
-    sv1M1,
-    sv2M1,
     sv3M1,
-    sspS1,
-    sspS2
   ],
 };
