92f1fd68c67969af296e5e9b9038dcf2e93f1fa5
Guille <cochraneguillermo@gmail.com>
2026-03-27 22:07:10 -0300

    The New Mutants, del FoxVerse, auditada y corregida

--- assets/data/mcu/foxverse/nmM1.js
+++ assets/data/mcu/foxverse/nmM1.js
@@ -3,6 +3,18 @@ import { nm } from "./nmM1/episodes.js";
 export const nmM1 = {
   id: "nmM1",
   label: "The New Mutants",
+  format: "Película",
+  release: {
+    year: 2020,
+    month: 8,
+    day: 28
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
     nm
   ],
--- assets/data/mcu/foxverse/nmM1/episodes.js
+++ assets/data/mcu/foxverse/nmM1/episodes.js
@@ -1,7 +1,9 @@
 export const nm = {
   id: "nm",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The New Mutants"},
+    en: "The New Mutants",
+    es: "The New Mutants",
   },
 };
-
