80cb6ebf737f0a7d9e20a69dd195c171664e65e4
Guille <cochraneguillermo@gmail.com>
2026-03-27 22:46:06 -0300

    Spider-Man 3, del SonyVerse, auditada y corregida

--- assets/data/mcu/sonyverse/sm3M1.js
+++ assets/data/mcu/sonyverse/sm3M1.js
@@ -3,6 +3,18 @@ import { sm3 } from "./sm3M1/episodes.js";
 export const sm3M1 = {
   id: "sm3M1",
   label: "Spider-Man 3",
+  format: "Película",
+  release: {
+    year: 2007,
+    month: 5,
+    day: 4
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
     sm3
   ],
--- assets/data/mcu/sonyverse/sm3M1/episodes.js
+++ assets/data/mcu/sonyverse/sm3M1/episodes.js
@@ -1,7 +1,9 @@
 export const sm3 = {
   id: "sm3",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Spider-Man 3"},
+    en: "Spider-Man 3",
+    es: "Spider-Man 3",
   },
 };
-
