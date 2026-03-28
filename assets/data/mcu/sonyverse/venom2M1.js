f7a08bbec8cf3f4774632f340252f023717b136e
Guille <cochraneguillermo@gmail.com>
2026-03-28 15:22:40 -0300

    Venom: Let There Be Carnage, del SonyVerse, auditada y corregida

--- assets/data/mcu/sonyverse/venom2M1.js
+++ assets/data/mcu/sonyverse/venom2M1.js
@@ -3,6 +3,18 @@ import { venom2 } from "./venom2M1/episodes.js";
 export const venom2M1 = {
   id: "venom2M1",
   label: "Venom: Let There Be Carnage",
+  format: "Película",
+  release: {
+    year: 2021,
+    month: 10,
+    day: 1
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
     venom2
   ],
--- assets/data/mcu/sonyverse/venom2M1/episodes.js
+++ assets/data/mcu/sonyverse/venom2M1/episodes.js
@@ -1,7 +1,9 @@
 export const venom2 = {
   id: "venom2",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Venom: Let There Be Carnage"},
+    en: "Venom: Let There Be Carnage",
+    es: "Venom: Habrá Matanza",
   },
 };
-
