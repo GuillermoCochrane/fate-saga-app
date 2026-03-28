7fe462c00567a1c0b2a7a076ae01fe9b7e35943b
Guille <cochraneguillermo@gmail.com>
2026-03-28 15:17:10 -0300

    The Amazing Spider-Man 2, del SonyVerse, auditada y corregida

--- assets/data/mcu/sonyverse/tasm2M1.js
+++ assets/data/mcu/sonyverse/tasm2M1.js
@@ -3,6 +3,18 @@ import { tasm2 } from "./tasm2M1/episodes.js";
 export const tasm2M1 = {
   id: "tasm2M1",
   label: "The Amazing Spider-Man 2",
+  format: "Película",
+  release: {
+    year: 2014,
+    month: 5,
+    day: 2
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
     tasm2
   ],
--- assets/data/mcu/sonyverse/tasm2M1/episodes.js
+++ assets/data/mcu/sonyverse/tasm2M1/episodes.js
@@ -1,7 +1,9 @@
 export const tasm2 = {
   id: "tasm2",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Amazing Spider-Man 2"},
+    en: "The Amazing Spider-Man 2",
+    es: "The Amazing Spider-Man 2",
   },
 };
-
