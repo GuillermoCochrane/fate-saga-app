ea8ffb39645a40143275eace1f798f07a0a39eb9
Guille <cochraneguillermo@gmail.com>
2026-03-28 15:15:19 -0300

    The Amazing Spider-Man, del SonyVerse, auditada y corregida

--- assets/data/mcu/sonyverse/tasm1M1.js
+++ assets/data/mcu/sonyverse/tasm1M1.js
@@ -3,6 +3,18 @@ import { tasm1 } from "./tasm1M1/episodes.js";
 export const tasm1M1 = {
   id: "tasm1M1",
   label: "The Amazing Spider-Man",
+  format: "Película",
+  release: {
+    year: 2012,
+    month: 7,
+    day: 3
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
     tasm1
   ],
--- assets/data/mcu/sonyverse/tasm1M1/episodes.js
+++ assets/data/mcu/sonyverse/tasm1M1/episodes.js
@@ -1,7 +1,9 @@
 export const tasm1 = {
   id: "tasm1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Amazing Spider-Man"},
+    en: "The Amazing Spider-Man",
+    es: "The Amazing Spider-Man",
   },
 };
-
