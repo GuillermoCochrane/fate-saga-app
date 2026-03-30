36f89b03e2e95b2e7f134933d9b545f4f43f82cf
Guille <cochraneguillermo@gmail.com>
2026-03-30 05:11:20 -0300

    The Amazing Spider-Man (S1) auditada y corregida

--- assets/data/mcu/marvelKnightsLegacy/spidey77S1.js
+++ assets/data/mcu/marvelKnightsLegacy/spidey77S1.js
@@ -1,9 +1,23 @@
-import { spidey77E1 } from "./spidey77S1/episodes.js";
+import { spidey77E1, spidey77E2, spidey77E3, spidey77E4, spidey77E5, spidey77E6, spidey77E7, spidey77E8, spidey77E9 } from "./spidey77S1/episodes.js";
 
 export const spidey77S1 = {
   id: "spidey77S1",
   label: "The Amazing Spider-Man (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 1977,
+    month: 9,
+    day: 10
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    spidey77E1
+    spidey77E1, spidey77E2, spidey77E3, spidey77E4, spidey77E5,
+    spidey77E6, spidey77E7, spidey77E8, spidey77E9
   ],
 };
--- assets/data/mcu/marvelKnightsLegacy/spidey77S1/episodes.js
+++ assets/data/mcu/marvelKnightsLegacy/spidey77S1/episodes.js
@@ -1,7 +1,89 @@
 export const spidey77E1 = {
   id: "spidey77E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Deadly Dust"},
+    en: "Spider-Man (Pilot)",
+    es: "Spider-Man (Piloto)"
   },
 };
 
+export const spidey77E2 = {
+  id: "spidey77E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Deadly Dust (Part 1)",
+    es: "El polvo mortal (Parte 1)"
+  },
+};
+
+export const spidey77E3 = {
+  id: "spidey77E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Deadly Dust (Part 2)",
+    es: "El polvo mortal (Parte 2)"
+  },
+};
+
+export const spidey77E4 = {
+  id: "spidey77E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Curse of Rava",
+    es: "La maldición de Rava"
+  },
+};
+
+export const spidey77E5 = {
+  id: "spidey77E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Night of the Clones",
+    es: "La noche de los clones"
+  },
+};
+
+export const spidey77E6 = {
+  id: "spidey77E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Escort to Danger",
+    es: "Escolta hacia el peligro"
+  },
+};
+
+export const spidey77E7 = {
+  id: "spidey77E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The Captive Tower",
+    es: "La torre cautiva"
+  },
+};
+
+export const spidey77E8 = {
+  id: "spidey77E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Con Caper",
+    es: "El timo"
+  },
+};
+
+export const spidey77E9 = {
+  id: "spidey77E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The Kirkwood Haunting",
+    es: "La posesión de Kirkwood"
+  },
+};
