c8272744233f585df754acab5e9a867aa0cb60cc
Guille <cochraneguillermo@gmail.com>
2026-03-30 05:00:47 -0300

    Spider-Man (1977 TV Movie) auditada y corregida

--- assets/data/mcu/marvelKnightsLegacy/spidey77TV.js
+++ assets/data/mcu/marvelKnightsLegacy/spidey77TV.js
@@ -3,7 +3,17 @@ import { spidey77TVE1 } from "./spidey77TV/episodes.js";
 export const spidey77TV = {
   id: "spidey77TV",
   label: "Spider-Man (1977 TV Movie)",
-  episodes: [
-    spidey77TVE1
-  ],
+  format: "Película",
+  release: {
+    year: 1977,
+    month: 9,
+    day: 14,
+  },
+  year_in_timeline: {
+    start: null,
+    end: null,
+  },
+  completed: false,
+  opened: false,
+  episodes: [spidey77TVE1],
 };
--- assets/data/mcu/marvelKnightsLegacy/spidey77TV/episodes.js
+++ assets/data/mcu/marvelKnightsLegacy/spidey77TV/episodes.js
@@ -1,7 +1,9 @@
 export const spidey77TVE1 = {
   id: "spidey77TVE1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Spider-Man"},
+    en: "Spider-Man",
+    es: "Spider-Man"
   },
 };
-
