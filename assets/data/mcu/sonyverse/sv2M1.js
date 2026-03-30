bbf4e675ff45c1c20de9f67b6ee3a1c006e88537
Guille <cochraneguillermo@gmail.com>
2026-03-29 21:02:37 -0300

    Spider-Man: Across the Spider-Verse, del SonyVerse, auditada y corregida

--- assets/data/mcu/sonyverse/sv2M1.js
+++ assets/data/mcu/sonyverse/sv2M1.js
@@ -3,6 +3,18 @@ import { sv2 } from "./sv2M1/episodes.js";
 export const sv2M1 = {
   id: "sv2M1",
   label: "Spider-Man: Across the Spider-Verse",
+  format: "Película",
+  release: {
+    year: 2023,
+    month: 6,
+    day: 2
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
     sv2
   ],
--- assets/data/mcu/sonyverse/sv2M1/episodes.js
+++ assets/data/mcu/sonyverse/sv2M1/episodes.js
@@ -1,7 +1,9 @@
 export const sv2 = {
-  id: "sv2",
+  id: "sv2", // Ojo que no colisione con el ID de la season si tu script no los separa
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Spider-Man: Across the Spider-Verse"},
-  },
+    en: "Spider-Man: Across the Spider-Verse",
+    es: "Spider-Man: A través del Spider-Verso"
+  }
 };
-
