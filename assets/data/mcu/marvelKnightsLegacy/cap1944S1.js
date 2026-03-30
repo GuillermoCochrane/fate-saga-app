25bca63c3c59e1e8cbb211edcbae2bf4d6dc4f1d
Guille <cochraneguillermo@gmail.com>
2026-03-30 03:47:53 -0300

    Captain America (1944 Serial) auditada y corregida

--- assets/data/mcu/marvelKnightsLegacy/cap1944S1.js
+++ assets/data/mcu/marvelKnightsLegacy/cap1944S1.js
@@ -1,9 +1,28 @@
-import { cap1944E1 } from "./cap1944S1/episodes.js";
+import { 
+  cap1944E1, cap1944E2, cap1944E3, cap1944E4, cap1944E5,
+  cap1944E6, cap1944E7, cap1944E8, cap1944E9, cap1944E10,
+  cap1944E11, cap1944E12, cap1944E13, cap1944E14, cap1944E15
+} from "./cap1944S1/episodes.js";
 
 export const cap1944S1 = {
   id: "cap1944S1",
   label: "Captain America (1944 Serial)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 1944,
+    month: 2,
+    day: 5
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    cap1944E1
+    cap1944E1, cap1944E2, cap1944E3, cap1944E4, cap1944E5,
+    cap1944E6, cap1944E7, cap1944E8, cap1944E9, cap1944E10,
+    cap1944E11, cap1944E12, cap1944E13, cap1944E14, cap1944E15
   ],
 };
--- assets/data/mcu/marvelKnightsLegacy/cap1944S1/episodes.js
+++ assets/data/mcu/marvelKnightsLegacy/cap1944S1/episodes.js
@@ -1,7 +1,149 @@
 export const cap1944E1 = {
   id: "cap1944E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Purple Death"},
+    en: "The Purple Death",
+    es: "La muerte púrpura",
   },
 };
 
+export const cap1944E2 = {
+  id: "cap1944E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Mechanical Executioner",
+    es: "El verdugo mecánico",
+  },
+};
+
+export const cap1944E3 = {
+  id: "cap1944E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Scarlet Scarab",
+    es: "El escarabajo escarlata",
+  },
+};
+
+export const cap1944E4 = {
+  id: "cap1944E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Dead Man's Chest",
+    es: "El cofre del muerto",
+  },
+};
+
+export const cap1944E5 = {
+  id: "cap1944E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Iron Claw",
+    es: "La garra de hierro",
+  },
+};
+
+export const cap1944E6 = {
+  id: "cap1944E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Phantoms Attack",
+    es: "Ataque de los fantasmas",
+  },
+};
+
+export const cap1944E7 = {
+  id: "cap1944E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The Bridge of Peril",
+    es: "El puente peligroso",
+  },
+};
+
+export const cap1944E8 = {
+  id: "cap1944E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Destroyer Strikes",
+    es: "El destructor ataca",
+  },
+};
+
+export const cap1944E9 = {
+  id: "cap1944E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The Mysterious Doctor",
+    es: "El misterioso doctor",
+  },
+};
+
+export const cap1944E10 = {
+  id: "cap1944E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Trap",
+    es: "La trampa",
+  },
+};
+
+export const cap1944E11 = {
+  id: "cap1944E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Whirlwind",
+    es: "El torbellino",
+  },
+};
+
+export const cap1944E12 = {
+  id: "cap1944E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Spider's Web",
+    es: "La telaraña",
+  },
+};
+
+export const cap1944E13 = {
+  id: "cap1944E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "The Deadly Sacrifice",
+    es: "El sacrificio mortal",
+  },
+};
+
+export const cap1944E14 = {
+  id: "cap1944E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "The Unmasking of the Scourge",
+    es: "Desenmascarando al azote",
+  },
+};
+
+export const cap1944E15 = {
+  id: "cap1944E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "The Valley of the Vanishing",
+    es: "El valle de los desaparecidos",
+  },
+};
