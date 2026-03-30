4b009262099daaf13b3b8c8e1691d72f1e53d520
Guille <cochraneguillermo@gmail.com>
2026-03-29 21:05:59 -0300

    Venom: The Last Dance, del SonyVerse, auditada y corregida

--- assets/data/mcu/sonyverse/venom3M1.js
+++ assets/data/mcu/sonyverse/venom3M1.js
@@ -3,6 +3,18 @@ import { venom3 } from "./venom3M1/episodes.js";
 export const venom3M1 = {
   id: "venom3M1",
   label: "Venom: The Last Dance",
+  format: "Película",
+  release: {
+    year: 2024,
+    month: 10,
+    day: 25
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
     venom3
   ],
--- assets/data/mcu/sonyverse/venom3M1/episodes.js
+++ assets/data/mcu/sonyverse/venom3M1/episodes.js
@@ -1,7 +1,9 @@
 export const venom3 = {
-  id: "venom3",
+  id: "vnm3", 
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Venom: The Last Dance"},
-  },
+    en: "Venom: The Last Dance",
+    es: "Venom: El Último Baile"
+  }
 };
-
