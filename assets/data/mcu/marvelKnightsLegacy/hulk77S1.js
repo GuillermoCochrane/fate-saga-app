90773b6258f83183c8df06958a310c2918acf2e1
Guille <cochraneguillermo@gmail.com>
2026-03-30 05:18:58 -0300

    The Incredible Hulk (S1) auditada y corregida

--- assets/data/mcu/marvelKnightsLegacy/hulk77S1.js
+++ assets/data/mcu/marvelKnightsLegacy/hulk77S1.js
@@ -1,9 +1,23 @@
-import { hulk77S1E1 } from "./hulk77S1/episodes.js";
+import { hkS1E1, hkS1E2, hkS1E3, hkS1E4, hkS1E5, hkS1E6, hkS1E7, hkS1E8, hkS1E9, hkS1E10, hkS1E11 } from "./hkS1/episodes.js";
 
-export const hulk77S1 = {
-  id: "hulk77S1",
+export const hkS1 = {
+  id: "hkS1",
   label: "The Incredible Hulk (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 1977,
+    month: 11,
+    day: 4
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    hulk77S1E1
+    hkS1E1, hkS1E2, hkS1E3, hkS1E4, hkS1E5,
+    hkS1E6, hkS1E7, hkS1E8, hkS1E9, hkS1E10, hkS1E11
   ],
 };
--- assets/data/mcu/marvelKnightsLegacy/hulk77S1/episodes.js
+++ assets/data/mcu/marvelKnightsLegacy/hulk77S1/episodes.js
@@ -1,7 +1,109 @@
-export const hulk77S1E1 = {
-  id: "hulk77S1E1",
+export const hkS1E1 = {
+  id: "hkS1E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Incredible Hulk"},
+    en: "The Incredible Hulk (Pilot)",
+    es: "El increíble Hulk (Piloto)"
   },
 };
 
+export const hkS1E2 = {
+  id: "hkS1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Death in the Family",
+    es: "Muerte en la familia"
+  },
+};
+
+export const hkS1E3 = {
+  id: "hkS1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Final Round",
+    es: "La ronda final"
+  },
+};
+
+export const hkS1E4 = {
+  id: "hkS1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Beast Within",
+    es: "La bestia interior"
+  },
+};
+
+export const hkS1E5 = {
+  id: "hkS1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Of Guilt, Models and Murder",
+    es: "Culpa, modelos y asesinato"
+  },
+};
+
+export const hkS1E6 = {
+  id: "hkS1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Terror in Times Square",
+    es: "Terror en Times Square"
+  },
+};
+
+export const hkS1E7 = {
+  id: "hkS1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "747",
+    es: "747"
+  },
+};
+
+export const hkS1E8 = {
+  id: "hkS1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Hulk Breaks Las Vegas",
+    es: "Hulk irrumpe en Las Vegas"
+  },
+};
+
+export const hkS1E9 = {
+  id: "hkS1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Never Give a Trucker an Even Break",
+    es: "Nunca le des una oportunidad a un camionero"
+  },
+};
+
+export const hkS1E10 = {
+  id: "hkS1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "Life and Death",
+    es: "Vida y muerte"
+  },
+};
+
+export const hkS1E11 = {
+  id: "hkS1E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "Earthquakes Happen",
+    es: "Los terremotos suceden"
+  },
+};
