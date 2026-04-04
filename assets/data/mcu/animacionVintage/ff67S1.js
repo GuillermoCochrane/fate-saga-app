7f0037eb6c8e24193a232fefc7f00b8d5793aaa9
Guille <cochraneguillermo@gmail.com>
2026-04-04 18:06:18 -0300

    Fantastic Four (1967) (S1) auditada y corregida

--- assets/data/mcu/animacionVintage/ff67S1.js
+++ assets/data/mcu/animacionVintage/ff67S1.js
@@ -1,9 +1,23 @@
-import { ff67S1E1 } from "./ff67S1/episodes.js";
+import { ff67S1E1, ff67S1E2, ff67S1E3, ff67S1E4, ff67S1E5, ff67S1E6, ff67S1E7, ff67S1E8, ff67S1E9, ff67S1E10, ff67S1E11, ff67S1E12, ff67S1E13, ff67S1E14, ff67S1E15, ff67S1E16, ff67S1E17, ff67S1E18, ff67S1E19, ff67S1E20 } from "./ff67S1/episodes.js";
 
 export const ff67S1 = {
   id: "ff67S1",
   label: "Fantastic Four (1967) (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 1967,
+    month: 9,
+    day: 9
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    ff67S1E1
+    ff67S1E1, ff67S1E2, ff67S1E3, ff67S1E4, ff67S1E5, ff67S1E6, ff67S1E7, ff67S1E8, ff67S1E9, ff67S1E10,
+    ff67S1E11, ff67S1E12, ff67S1E13, ff67S1E14, ff67S1E15, ff67S1E16, ff67S1E17, ff67S1E18, ff67S1E19, ff67S1E20
   ],
 };
--- assets/data/mcu/animacionVintage/ff67S1/episodes.js
+++ assets/data/mcu/animacionVintage/ff67S1/episodes.js
@@ -1,7 +1,199 @@
 export const ff67S1E1 = {
   id: "ff67S1E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Klaws"},
+    en: "Klaws",
+    es: "Klaws"
   },
 };
 
+export const ff67S1E2 = {
+  id: "ff67S1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Menace of Magneto",
+    es: "La amenaza de Magneto"
+  },
+};
+
+export const ff67S1E3 = {
+  id: "ff67S1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Diabolical Doctor Doom",
+    es: "El diabólico Doctor Doom"
+  },
+};
+
+export const ff67S1E4 = {
+  id: "ff67S1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Way of the Mole Man",
+    es: "El camino del Hombre Topo"
+  },
+};
+
+export const ff67S1E5 = {
+  id: "ff67S1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Terrible Tribunal",
+    es: "El terrible tribunal"
+  },
+};
+
+export const ff67S1E6 = {
+  id: "ff67S1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Return of Doctor Doom",
+    es: "El regreso de Doctor Doom"
+  },
+};
+
+export const ff67S1E7 = {
+  id: "ff67S1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The Frightful Four",
+    es: "Los Cuatro Terribles"
+  },
+};
+
+export const ff67S1E8 = {
+  id: "ff67S1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Micro World of Doctor Doom",
+    es: "El micro mundo de Doctor Doom"
+  },
+};
+
+export const ff67S1E9 = {
+  id: "ff67S1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The Rampage of the Mole Man",
+    es: "El ataque del Hombre Topo"
+  },
+};
+
+export const ff67S1E10 = {
+  id: "ff67S1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Return of the Mole Man",
+    es: "El regreso del Hombre Topo"
+  },
+};
+
+export const ff67S1E11 = {
+  id: "ff67S1E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Red Ghost",
+    es: "El Fantasma Rojo"
+  },
+};
+
+export const ff67S1E12 = {
+  id: "ff67S1E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Menace of the Mole Men",
+    es: "La amenaza de los Hombres Topo"
+  },
+};
+
+export const ff67S1E13 = {
+  id: "ff67S1E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "The Deadly Doctor Doom",
+    es: "El mortal Doctor Doom"
+  },
+};
+
+export const ff67S1E14 = {
+  id: "ff67S1E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "The Fantastic Four Meet the Hulk",
+    es: "Los 4 Fantásticos se encuentran con Hulk"
+  },
+};
+
+export const ff67S1E15 = {
+  id: "ff67S1E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "The Fantastic Four Meet Spider-Man",
+    es: "Los 4 Fantásticos se encuentran con Spider-Man"
+  },
+};
+
+export const ff67S1E16 = {
+  id: "ff67S1E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "The Fantastic Four Meet the X-Men",
+    es: "Los 4 Fantásticos se encuentran con los X-Men"
+  },
+};
+
+export const ff67S1E17 = {
+  id: "ff67S1E17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "The Fantastic Four Meet Doctor Doom",
+    es: "Los 4 Fantásticos se encuentran con Doctor Doom"
+  },
+};
+
+export const ff67S1E18 = {
+  id: "ff67S1E18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "The Fantastic Four Meet the Silver Surfer",
+    es: "Los 4 Fantásticos se encuentran con Silver Surfer"
+  },
+};
+
+export const ff67S1E19 = {
+  id: "ff67S1E19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "The Fantastic Four Meet the Sub-Mariner",
+    es: "Los 4 Fantásticos se encuentran con Sub-Mariner"
+  },
+};
+
+export const ff67S1E20 = {
+  id: "ff67S1E20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "The Fantastic Four Meet the Inhumans",
+    es: "Los 4 Fantásticos se encuentran con los Inhumanos"
+  },
+};
