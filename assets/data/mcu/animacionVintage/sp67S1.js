9c765d247d4606d6eaf12a749a874c95ba31ad10
Guille <cochraneguillermo@gmail.com>
2026-04-04 18:08:53 -0300

    Spider-Man (1967) (S1) auditada y corregida

--- assets/data/mcu/animacionVintage/sp67S1.js
+++ assets/data/mcu/animacionVintage/sp67S1.js
@@ -1,9 +1,23 @@
-import { sp67S1E1 } from "./sp67S1/episodes.js";
+import { sp67S1E1, sp67S1E2, sp67S1E3, sp67S1E4, sp67S1E5, sp67S1E6, sp67S1E7, sp67S1E8, sp67S1E9, sp67S1E10, sp67S1E11, sp67S1E12, sp67S1E13, sp67S1E14, sp67S1E15, sp67S1E16, sp67S1E17, sp67S1E18, sp67S1E19, sp67S1E20 } from "./sp67S1/episodes.js";
 
 export const sp67S1 = {
   id: "sp67S1",
   label: "Spider-Man (1967) (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 1967,
+    month: 9,
+    day: 9
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    sp67S1E1
+    sp67S1E1, sp67S1E2, sp67S1E3, sp67S1E4, sp67S1E5, sp67S1E6, sp67S1E7, sp67S1E8, sp67S1E9, sp67S1E10,
+    sp67S1E11, sp67S1E12, sp67S1E13, sp67S1E14, sp67S1E15, sp67S1E16, sp67S1E17, sp67S1E18, sp67S1E19, sp67S1E20
   ],
 };
--- assets/data/mcu/animacionVintage/sp67S1/episodes.js
+++ assets/data/mcu/animacionVintage/sp67S1/episodes.js
@@ -1,7 +1,199 @@
 export const sp67S1E1 = {
   id: "sp67S1E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Power of Dr. Octopus"},
+    en: "The Power of Dr. Octopus",
+    es: "El poder del Dr. Octopus"
   },
 };
 
+export const sp67S1E2 = {
+  id: "sp67S1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Sub-Zero for Spidey",
+    es: "Bajo cero para Spidey"
+  },
+};
+
+export const sp67S1E3 = {
+  id: "sp67S1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Where Crawls the Lizard",
+    es: "Donde se arrastra el Lagarto"
+  },
+};
+
+export const sp67S1E4 = {
+  id: "sp67S1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Origin of Spider-Man",
+    es: "El origen de Spider-Man"
+  },
+};
+
+export const sp67S1E5 = {
+  id: "sp67S1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "King Pinned",
+    es: "El Rey Pin"
+  },
+};
+
+export const sp67S1E6 = {
+  id: "sp67S1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Swing City",
+    es: "Balanceándose en la ciudad"
+  },
+};
+
+export const sp67S1E7 = {
+  id: "sp67S1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Criminals in the Clouds",
+    es: "Criminales en las nubes"
+  },
+};
+
+export const sp67S1E8 = {
+  id: "sp67S1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Menace from the Bottom of the World",
+    es: "Amenaza desde el fondo del mundo"
+  },
+};
+
+export const sp67S1E9 = {
+  id: "sp67S1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Diamond Dust",
+    es: "Polvo de diamante"
+  },
+};
+
+export const sp67S1E10 = {
+  id: "sp67S1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Coming of the Scorcher",
+    es: "La llegada del Scorcher"
+  },
+};
+
+export const sp67S1E11 = {
+  id: "sp67S1E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Big Brain",
+    es: "El gran cerebro"
+  },
+};
+
+export const sp67S1E12 = {
+  id: "sp67S1E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Peril of Parafino",
+    es: "El peligro de Parafino"
+  },
+};
+
+export const sp67S1E13 = {
+  id: "sp67S1E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "The Incredible Shrinking Spy",
+    es: "El espía increíblemente encogido"
+  },
+};
+
+export const sp67S1E14 = {
+  id: "sp67S1E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "The Golden Rhino",
+    es: "El Rhino dorado"
+  },
+};
+
+export const sp67S1E15 = {
+  id: "sp67S1E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "Blueprint for Crime",
+    es: "Plan para el crimen"
+  },
+};
+
+export const sp67S1E16 = {
+  id: "sp67S1E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "The Sinister Prime Minister",
+    es: "El siniestro primer ministro"
+  },
+};
+
+export const sp67S1E17 = {
+  id: "sp67S1E17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "The Trickster",
+    es: "El tramposo"
+  },
+};
+
+export const sp67S1E18 = {
+  id: "sp67S1E18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "The Night of the Villains",
+    es: "La noche de los villanos"
+  },
+};
+
+export const sp67S1E19 = {
+  id: "sp67S1E19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "Here Comes Trubble",
+    es: "Aquí viene Trubble"
+  },
+};
+
+export const sp67S1E20 = {
+  id: "sp67S1E20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "Spider-Man Vs. the Giant",
+    es: "Spider-Man contra el gigante"
+  },
+};
