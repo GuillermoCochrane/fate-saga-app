3e5b54450f5fd05055cc10c3bcadc6ba5df65ab9
Guille <cochraneguillermo@gmail.com>
2026-04-04 18:11:22 -0300

    Spider-Man (1967) (S3) auditada y corregida

--- assets/data/mcu/animacionVintage/sp67S3.js
+++ assets/data/mcu/animacionVintage/sp67S3.js
@@ -1,9 +1,23 @@
-import { sp67S3E1 } from "./sp67S3/episodes.js";
+import { sp67S3E1, sp67S3E2, sp67S3E3, sp67S3E4, sp67S3E5, sp67S3E6, sp67S3E7, sp67S3E8, sp67S3E9, sp67S3E10, sp67S3E11, sp67S3E12, sp67S3E13 } from "./sp67S3/episodes.js";
 
 export const sp67S3 = {
   id: "sp67S3",
   label: "Spider-Man (1967) (S3)",
+  format: "Serie",
+  season: 3,
+  release: {
+    year: 1969,
+    month: 6,
+    day: 14
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    sp67S3E1
+    sp67S3E1, sp67S3E2, sp67S3E3, sp67S3E4, sp67S3E5, sp67S3E6, sp67S3E7, sp67S3E8,
+    sp67S3E9, sp67S3E10, sp67S3E11, sp67S3E12, sp67S3E13
   ],
 };
--- assets/data/mcu/animacionVintage/sp67S3/episodes.js
+++ assets/data/mcu/animacionVintage/sp67S3/episodes.js
@@ -1,7 +1,129 @@
 export const sp67S3E1 = {
   id: "sp67S3E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Vanishing Doctor Vespasian"},
+    en: "The Vanishing Doctor Vespasian",
+    es: "El desaparecido Doctor Vespasian"
   },
 };
 
+export const sp67S3E2 = {
+  id: "sp67S3E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Scourge of the Scarf",
+    es: "El azote de la bufanda"
+  },
+};
+
+export const sp67S3E3 = {
+  id: "sp67S3E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Devious Dr. Dumpty",
+    es: "El astuto Dr. Dumpty"
+  },
+};
+
+export const sp67S3E4 = {
+  id: "sp67S3E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Sandman Is Coming",
+    es: "Sandman se acerca"
+  },
+};
+
+export const sp67S3E5 = {
+  id: "sp67S3E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Coat of Arms",
+    es: "El escudo de armas"
+  },
+};
+
+export const sp67S3E6 = {
+  id: "sp67S3E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Invisible Man",
+    es: "El hombre invisible"
+  },
+};
+
+export const sp67S3E7 = {
+  id: "sp67S3E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The Deadly Dust",
+    es: "El polvo mortal"
+  },
+};
+
+export const sp67S3E8 = {
+  id: "sp67S3E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Evil, Evil Empire",
+    es: "El malvado imperio"
+  },
+};
+
+export const sp67S3E9 = {
+  id: "sp67S3E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The Revenge of the Green Goblin",
+    es: "La venganza del Duende Verde"
+  },
+};
+
+export const sp67S3E10 = {
+  id: "sp67S3E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Fantastic Mr. Fabulous",
+    es: "El fantástico Sr. Fabuloso"
+  },
+};
+
+export const sp67S3E11 = {
+  id: "sp67S3E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Winged Thing",
+    es: "La cosa alada"
+  },
+};
+
+export const sp67S3E12 = {
+  id: "sp67S3E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Golden Fleece",
+    es: "El vellocino de oro"
+  },
+};
+
+export const sp67S3E13 = {
+  id: "sp67S3E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "The Dark of the Moon",
+    es: "La oscuridad de la luna"
+  },
+};
