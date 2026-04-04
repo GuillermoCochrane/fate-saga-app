3684b0c8a39776d74f939efc9a9992e1f9c6f830
Guille <cochraneguillermo@gmail.com>
2026-04-04 18:13:31 -0300

    Fred and Barney Meet the Thing (1979) (S1) auditada y corregida

--- assets/data/mcu/animacionVintage/fbmtt79S1.js
+++ assets/data/mcu/animacionVintage/fbmtt79S1.js
@@ -1,9 +1,23 @@
-import { fbmtt79S1E1 } from "./fbmtt79S1/episodes.js";
+import { fbmtt79S1E1, fbmtt79S1E2, fbmtt79S1E3, fbmtt79S1E4, fbmtt79S1E5, fbmtt79S1E6, fbmtt79S1E7, fbmtt79S1E8, fbmtt79S1E9, fbmtt79S1E10, fbmtt79S1E11, fbmtt79S1E12, fbmtt79S1E13 } from "./fbmtt79S1/episodes.js";
 
 export const fbmtt79S1 = {
   id: "fbmtt79S1",
   label: "Fred and Barney Meet the Thing (1979) (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 1979,
+    month: 12,
+    day: 8
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    fbmtt79S1E1
+    fbmtt79S1E1, fbmtt79S1E2, fbmtt79S1E3, fbmtt79S1E4, fbmtt79S1E5, fbmtt79S1E6, fbmtt79S1E7,
+    fbmtt79S1E8, fbmtt79S1E9, fbmtt79S1E10, fbmtt79S1E11, fbmtt79S1E12, fbmtt79S1E13
   ],
 };
--- assets/data/mcu/animacionVintage/fbmtt79S1/episodes.js
+++ assets/data/mcu/animacionVintage/fbmtt79S1/episodes.js
@@ -1,7 +1,129 @@
 export const fbmtt79S1E1 = {
   id: "fbmtt79S1E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Rockhound"},
+    en: "The Rockhound",
+    es: "El cazador de rocas"
   },
 };
 
+export const fbmtt79S1E2 = {
+  id: "fbmtt79S1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Thing's 30th Century",
+    es: "El siglo 30 de la Cosa"
+  },
+};
+
+export const fbmtt79S1E3 = {
+  id: "fbmtt79S1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Thing's New Friend",
+    es: "El nuevo amigo de la Cosa"
+  },
+};
+
+export const fbmtt79S1E4 = {
+  id: "fbmtt79S1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Thing and the Bounty Hunter",
+    es: "La Cosa y el cazarrecompensas"
+  },
+};
+
+export const fbmtt79S1E5 = {
+  id: "fbmtt79S1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Thing and the Grand Prix",
+    es: "La Cosa y el Gran Premio"
+  },
+};
+
+export const fbmtt79S1E6 = {
+  id: "fbmtt79S1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Thing and the Computer Whiz",
+    es: "La Cosa y el genio de la computadora"
+  },
+};
+
+export const fbmtt79S1E7 = {
+  id: "fbmtt79S1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The Thing and the Roman Empire",
+    es: "La Cosa y el Imperio Romano"
+  },
+};
+
+export const fbmtt79S1E8 = {
+  id: "fbmtt79S1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Thing and the London Thief",
+    es: "La Cosa y el ladrón de Londres"
+  },
+};
+
+export const fbmtt79S1E9 = {
+  id: "fbmtt79S1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The Thing and the Kidnapper",
+    es: "La Cosa y el secuestrador"
+  },
+};
+
+export const fbmtt79S1E10 = {
+  id: "fbmtt79S1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Thing and the Bank Robbers",
+    es: "La Cosa y los ladrones de bancos"
+  },
+};
+
+export const fbmtt79S1E11 = {
+  id: "fbmtt79S1E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Thing and the Airline Pilot",
+    es: "La Cosa y el piloto de aerolínea"
+  },
+};
+
+export const fbmtt79S1E12 = {
+  id: "fbmtt79S1E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Thing and the Mafia",
+    es: "La Cosa y la mafia"
+  },
+};
+
+export const fbmtt79S1E13 = {
+  id: "fbmtt79S1E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "The Thing and the Treasure Map",
+    es: "La Cosa y el mapa del tesoro"
+  },
+};
