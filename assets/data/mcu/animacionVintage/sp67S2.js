a8528d46568a3b60d6407e5089806933d7cf0602
Guille <cochraneguillermo@gmail.com>
2026-04-04 18:10:17 -0300

    Spider-Man (1967) (S2) auditada y corregida

--- assets/data/mcu/animacionVintage/sp67S2.js
+++ assets/data/mcu/animacionVintage/sp67S2.js
@@ -1,9 +1,23 @@
-import { sp67S2E1 } from "./sp67S2/episodes.js";
+import { sp67S2E1, sp67S2E2, sp67S2E3, sp67S2E4, sp67S2E5, sp67S2E6, sp67S2E7, sp67S2E8, sp67S2E9, sp67S2E10, sp67S2E11, sp67S2E12, sp67S2E13, sp67S2E14, sp67S2E15, sp67S2E16, sp67S2E17, sp67S2E18, sp67S2E19 } from "./sp67S2/episodes.js";
 
 export const sp67S2 = {
   id: "sp67S2",
   label: "Spider-Man (1967) (S2)",
+  format: "Serie",
+  season: 2,
+  release: {
+    year: 1968,
+    month: 9,
+    day: 14
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    sp67S2E1
+    sp67S2E1, sp67S2E2, sp67S2E3, sp67S2E4, sp67S2E5, sp67S2E6, sp67S2E7, sp67S2E8, sp67S2E9, sp67S2E10,
+    sp67S2E11, sp67S2E12, sp67S2E13, sp67S2E14, sp67S2E15, sp67S2E16, sp67S2E17, sp67S2E18, sp67S2E19
   ],
 };
--- assets/data/mcu/animacionVintage/sp67S2/episodes.js
+++ assets/data/mcu/animacionVintage/sp67S2/episodes.js
@@ -1,7 +1,189 @@
 export const sp67S2E1 = {
   id: "sp67S2E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Origin of Spider-Man"},
+    en: "The Phantom from the Depths of Time",
+    es: "El fantasma desde las profundidades del tiempo"
   },
 };
 
+export const sp67S2E2 = {
+  id: "sp67S2E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Evil Sorcerer",
+    es: "El hechicero malvado"
+  },
+};
+
+export const sp67S2E3 = {
+  id: "sp67S2E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Vulture's Prey",
+    es: "La presa del Buitre"
+  },
+};
+
+export const sp67S2E4 = {
+  id: "sp67S2E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Dark Terrors of the Museum",
+    es: "Los terrores oscuros del museo"
+  },
+};
+
+export const sp67S2E5 = {
+  id: "sp67S2E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Madness of Mysterio",
+    es: "La locura de Mysterio"
+  },
+};
+
+export const sp67S2E6 = {
+  id: "sp67S2E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Return of the Flying Dutchman",
+    es: "El regreso del Holandés Errante"
+  },
+};
+
+export const sp67S2E7 = {
+  id: "sp67S2E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The Fantastic Fakir",
+    es: "El fakir fantástico"
+  },
+};
+
+export const sp67S2E8 = {
+  id: "sp67S2E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Birth of Micro Man",
+    es: "El nacimiento de Micro Man"
+  },
+};
+
+export const sp67S2E9 = {
+  id: "sp67S2E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The Master of the Vicious Vampire",
+    es: "El maestro del vampiro vicioso"
+  },
+};
+
+export const sp67S2E10 = {
+  id: "sp67S2E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Skeleton in the Closet",
+    es: "El esqueleto en el armario"
+  },
+};
+
+export const sp67S2E11 = {
+  id: "sp67S2E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Coming of the Iceman",
+    es: "La llegada del Hombre de Hielo"
+  },
+};
+
+export const sp67S2E12 = {
+  id: "sp67S2E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Wrath of the Sub-Mariner",
+    es: "La ira del Sub-Mariner"
+  },
+};
+
+export const sp67S2E13 = {
+  id: "sp67S2E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "The Birth of the Super Hero",
+    es: "El nacimiento del superhéroe"
+  },
+};
+
+export const sp67S2E14 = {
+  id: "sp67S2E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "The Monster from the Murky Swamp",
+    es: "El monstruo del pantano turbio"
+  },
+};
+
+export const sp67S2E15 = {
+  id: "sp67S2E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "The Web of the Wizard",
+    es: "La telaraña del hechicero"
+  },
+};
+
+export const sp67S2E16 = {
+  id: "sp67S2E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "The Seventh Vampire",
+    es: "El séptimo vampiro"
+  },
+};
+
+export const sp67S2E17 = {
+  id: "sp67S2E17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "The Madness of Mysterio (Part 2)",
+    es: "La locura de Mysterio (Parte 2)"
+  },
+};
+
+export const sp67S2E18 = {
+  id: "sp67S2E18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "The Return of the Vulture",
+    es: "El regreso del Buitre"
+  },
+};
+
+export const sp67S2E19 = {
+  id: "sp67S2E19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "The Final Battle",
+    es: "La batalla final"
+  },
+};
