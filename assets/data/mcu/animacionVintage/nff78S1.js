2cc8180a3533687ad2bacee32fc0b1f42b4fbc67
Guille <cochraneguillermo@gmail.com>
2026-04-04 18:12:26 -0300

    The New Fantastic Four (1978) (S1) auditada y corregida

--- assets/data/mcu/animacionVintage/nff78S1.js
+++ assets/data/mcu/animacionVintage/nff78S1.js
@@ -1,9 +1,23 @@
-import { nff78S1E1 } from "./nff78S1/episodes.js";
+import { nff78S1E1, nff78S1E2, nff78S1E3, nff78S1E4, nff78S1E5, nff78S1E6, nff78S1E7, nff78S1E8, nff78S1E9, nff78S1E10, nff78S1E11, nff78S1E12, nff78S1E13 } from "./nff78S1/episodes.js";
 
 export const nff78S1 = {
   id: "nff78S1",
   label: "The New Fantastic Four (1978) (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 1978,
+    month: 9,
+    day: 9
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    nff78S1E1
+    nff78S1E1, nff78S1E2, nff78S1E3, nff78S1E4, nff78S1E5, nff78S1E6, nff78S1E7,
+    nff78S1E8, nff78S1E9, nff78S1E10, nff78S1E11, nff78S1E12, nff78S1E13
   ],
 };
--- assets/data/mcu/animacionVintage/nff78S1/episodes.js
+++ assets/data/mcu/animacionVintage/nff78S1/episodes.js
@@ -1,7 +1,129 @@
 export const nff78S1E1 = {
   id: "nff78S1E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Mole Man"},
+    en: "The Mole Man",
+    es: "El Hombre Topo"
   },
 };
 
+export const nff78S1E2 = {
+  id: "nff78S1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Skyscraper from the Sea",
+    es: "El rascacielos del mar"
+  },
+};
+
+export const nff78S1E3 = {
+  id: "nff78S1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Fantastic Four Meet the Incredible Hulk",
+    es: "Los 4 Fantásticos se encuentran con el increíble Hulk"
+  },
+};
+
+export const nff78S1E4 = {
+  id: "nff78S1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Doctor Doom",
+    es: "Doctor Doom"
+  },
+};
+
+export const nff78S1E5 = {
+  id: "nff78S1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Frightful Four",
+    es: "Los Cuatro Terribles"
+  },
+};
+
+export const nff78S1E6 = {
+  id: "nff78S1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Incredible Shrinking Fantastic Four",
+    es: "Los 4 Fantásticos increíblemente encogidos"
+  },
+};
+
+export const nff78S1E7 = {
+  id: "nff78S1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The Menace of the Mole Men",
+    es: "La amenaza de los Hombres Topo"
+  },
+};
+
+export const nff78S1E8 = {
+  id: "nff78S1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Fantastic Four Meet the Hulk II",
+    es: "Los 4 Fantásticos se encuentran con Hulk II"
+  },
+};
+
+export const nff78S1E9 = {
+  id: "nff78S1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The Fantastic Four Meet the Sub-Mariner",
+    es: "Los 4 Fantásticos se encuentran con el Sub-Mariner"
+  },
+};
+
+export const nff78S1E10 = {
+  id: "nff78S1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Fantastic Four Meet Doctor Doom",
+    es: "Los 4 Fantásticos se encuentran con Doctor Doom"
+  },
+};
+
+export const nff78S1E11 = {
+  id: "nff78S1E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Fantastic Four Meet the Silver Surfer",
+    es: "Los 4 Fantásticos se encuentran con Silver Surfer"
+  },
+};
+
+export const nff78S1E12 = {
+  id: "nff78S1E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Fantastic Four Meet the X-Men",
+    es: "Los 4 Fantásticos se encuentran con los X-Men"
+  },
+};
+
+export const nff78S1E13 = {
+  id: "nff78S1E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "The Fantastic Four Meet Spider-Man",
+    es: "Los 4 Fantásticos se encuentran con Spider-Man"
+  },
+};
