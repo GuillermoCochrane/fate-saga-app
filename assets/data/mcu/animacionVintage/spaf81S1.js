e375aa39ade5afebb7738e0f681d51ef8ff46274
Guille <cochraneguillermo@gmail.com>
2026-04-05 19:02:39 -0300

    Spider-Man and His Amazing Friends (1981) (S1) auditada y corregida

--- assets/data/mcu/animacionVintage/spaf81S1.js
+++ assets/data/mcu/animacionVintage/spaf81S1.js
@@ -1,9 +1,23 @@
-import { spaf81S1E1 } from "./spaf81S1/episodes.js";
+import { spaf81S1E1, spaf81S1E2, spaf81S1E3, spaf81S1E4, spaf81S1E5, spaf81S1E6, spaf81S1E7, spaf81S1E8, spaf81S1E9, spaf81S1E10, spaf81S1E11, spaf81S1E12, spaf81S1E13 } from "./spaf81S1/episodes.js";
 
 export const spaf81S1 = {
   id: "spaf81S1",
   label: "Spider-Man and His Amazing Friends (1981) (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 1981,
+    month: 9,
+    day: 12
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    spaf81S1E1
+    spaf81S1E1, spaf81S1E2, spaf81S1E3, spaf81S1E4, spaf81S1E5, spaf81S1E6, spaf81S1E7,
+    spaf81S1E8, spaf81S1E9, spaf81S1E10, spaf81S1E11, spaf81S1E12, spaf81S1E13
   ],
 };
--- assets/data/mcu/animacionVintage/spaf81S1/episodes.js
+++ assets/data/mcu/animacionVintage/spaf81S1/episodes.js
@@ -1,7 +1,129 @@
 export const spaf81S1E1 = {
   id: "spaf81S1E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Triumph of the Green Goblin"},
+    en: "Triumph of the Green Goblin",
+    es: "Triunfo del Duende Verde"
   },
 };
 
+export const spaf81S1E2 = {
+  id: "spaf81S1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Crime of All Centuries",
+    es: "El crimen de todos los siglos"
+  },
+};
+
+export const spaf81S1E3 = {
+  id: "spaf81S1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Fantastic Mr. Fabulous",
+    es: "El fantástico Sr. Fabuloso"
+  },
+};
+
+export const spaf81S1E4 = {
+  id: "spaf81S1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Vulture's Prey",
+    es: "La presa del Buitre"
+  },
+};
+
+export const spaf81S1E5 = {
+  id: "spaf81S1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Return of the Kingpin",
+    es: "El regreso del Kingpin"
+  },
+};
+
+export const spaf81S1E6 = {
+  id: "spaf81S1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Under the Wizard's Spell",
+    es: "Bajo el hechizo del hechicero"
+  },
+};
+
+export const spaf81S1E7 = {
+  id: "spaf81S1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The Sinister Spider-Man",
+    es: "El siniestro Spider-Man"
+  },
+};
+
+export const spaf81S1E8 = {
+  id: "spaf81S1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Hunter and the Hunted",
+    es: "El cazador y el cazado"
+  },
+};
+
+export const spaf81S1E9 = {
+  id: "spaf81S1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The Incredible Shrinking Spider-Man",
+    es: "El increíble Spider-Man encogido"
+  },
+};
+
+export const spaf81S1E10 = {
+  id: "spaf81S1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Unfathomable Professor Fix",
+    es: "El insondable profesor Fix"
+  },
+};
+
+export const spaf81S1E11 = {
+  id: "spaf81S1E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Capture of Captain America",
+    es: "La captura del Capitán América"
+  },
+};
+
+export const spaf81S1E12 = {
+  id: "spaf81S1E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Doom Report",
+    es: "El informe Doom"
+  },
+};
+
+export const spaf81S1E13 = {
+  id: "spaf81S1E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "The Spider-Man and the Dragon",
+    es: "Spider-Man y el dragón"
+  },
+};
