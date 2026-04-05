cdd3ba95f244d1b0c1890ae9a53dba0daad3118b
Guille <cochraneguillermo@gmail.com>
2026-04-05 19:05:01 -0300

    Spider-Man and His Amazing Friends (1981) (S2) auditada y corregida

--- assets/data/mcu/animacionVintage/spaf81S2.js
+++ assets/data/mcu/animacionVintage/spaf81S2.js
@@ -1,9 +1,22 @@
-import { spaf81S2E1 } from "./spaf81S2/episodes.js";
+import { spaf81S2E1, spaf81S2E2, spaf81S2E3 } from "./spaf81S2/episodes.js";
 
 export const spaf81S2 = {
   id: "spaf81S2",
   label: "Spider-Man and His Amazing Friends (1981) (S2)",
+  format: "Serie",
+  season: 2,
+  release: {
+    year: 1982,
+    month: 9,
+    day: 18
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    spaf81S2E1
+    spaf81S2E1, spaf81S2E2, spaf81S2E3
   ],
 };
--- assets/data/mcu/animacionVintage/spaf81S2/episodes.js
+++ assets/data/mcu/animacionVintage/spaf81S2/episodes.js
@@ -1,7 +1,29 @@
 export const spaf81S2E1 = {
   id: "spaf81S2E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Crime of All Centuries"},
+    en: "The Origin of Iceman",
+    es: "El origen de Iceman"
   },
 };
 
+export const spaf81S2E2 = {
+  id: "spaf81S2E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Origin of Firestar",
+    es: "El origen de Firestar"
+  },
+};
+
+export const spaf81S2E3 = {
+  id: "spaf81S2E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Origin of Spider-Man",
+    es: "El origen de Spider-Man"
+  },
+};
