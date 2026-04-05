6ba31980f7fe48f0e74b4a2a267a57cb0694af29
Guille <cochraneguillermo@gmail.com>
2026-04-05 19:08:09 -0300

    The Incredible Hulk (1982) (S1) auditada y corregida

--- assets/data/mcu/animacionVintage/hk82S1.js
+++ assets/data/mcu/animacionVintage/hk82S1.js
@@ -1,9 +1,23 @@
-import { hk82S1E1 } from "./hk82S1/episodes.js";
+import { hk82S1E1, hk82S1E2, hk82S1E3, hk82S1E4, hk82S1E5, hk82S1E6, hk82S1E7, hk82S1E8, hk82S1E9, hk82S1E10, hk82S1E11, hk82S1E12, hk82S1E13 } from "./hk82S1/episodes.js";
 
 export const hk82S1 = {
   id: "hk82S1",
   label: "The Incredible Hulk (1982) (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 1982,
+    month: 9,
+    day: 18
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    hk82S1E1
+    hk82S1E1, hk82S1E2, hk82S1E3, hk82S1E4, hk82S1E5, hk82S1E6, hk82S1E7,
+    hk82S1E8, hk82S1E9, hk82S1E10, hk82S1E11, hk82S1E12, hk82S1E13
   ],
 };
--- assets/data/mcu/animacionVintage/hk82S1/episodes.js
+++ assets/data/mcu/animacionVintage/hk82S1/episodes.js
@@ -1,7 +1,129 @@
 export const hk82S1E1 = {
   id: "hk82S1E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Hulk Meets the Thing"},
+    en: "The Hulk Meets the Thing",
+    es: "Hulk se encuentra con la Cosa"
   },
 };
 
+export const hk82S1E2 = {
+  id: "hk82S1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Hulk Meets the Sub-Mariner",
+    es: "Hulk se encuentra con el Sub-Mariner"
+  },
+};
+
+export const hk82S1E3 = {
+  id: "hk82S1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Hulk Meets Doctor Strange",
+    es: "Hulk se encuentra con Doctor Strange"
+  },
+};
+
+export const hk82S1E4 = {
+  id: "hk82S1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Hulk Meets the Leader",
+    es: "Hulk se encuentra con el Líder"
+  },
+};
+
+export const hk82S1E5 = {
+  id: "hk82S1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Hulk Meets the Abomination",
+    es: "Hulk se encuentra con el Abominación"
+  },
+};
+
+export const hk82S1E6 = {
+  id: "hk82S1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Hulk Meets the Absorbing Man",
+    es: "Hulk se encuentra con el Hombre Absorbente"
+  },
+};
+
+export const hk82S1E7 = {
+  id: "hk82S1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The Hulk Meets the Enchantress",
+    es: "Hulk se encuentra con la Encantadora"
+  },
+};
+
+export const hk82S1E8 = {
+  id: "hk82S1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Hulk Meets the Executioner",
+    es: "Hulk se encuentra con el Verdugo"
+  },
+};
+
+export const hk82S1E9 = {
+  id: "hk82S1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The Hulk Meets the Wrecker",
+    es: "Hulk se encuentra con el Destructor"
+  },
+};
+
+export const hk82S1E10 = {
+  id: "hk82S1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Hulk Meets the Piledriver",
+    es: "Hulk se encuentra con el Martinete"
+  },
+};
+
+export const hk82S1E11 = {
+  id: "hk82S1E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Hulk Meets the Thunderball",
+    es: "Hulk se encuentra con el Thunderball"
+  },
+};
+
+export const hk82S1E12 = {
+  id: "hk82S1E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Hulk Meets the Bulldozer",
+    es: "Hulk se encuentra con el Bulldozer"
+  },
+};
+
+export const hk82S1E13 = {
+  id: "hk82S1E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "The Hulk Meets the Red Skull",
+    es: "Hulk se encuentra con la Calavera Roja"
+  },
+};
