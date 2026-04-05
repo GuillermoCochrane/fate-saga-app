e3885e4a5db3647021470d4a95c24addfdae182a
Guille <cochraneguillermo@gmail.com>
2026-04-05 19:06:27 -0300

    Spider-Man and His Amazing Friends (1981) (S3) auditada y corregida

--- assets/data/mcu/animacionVintage/spaf81S3.js
+++ assets/data/mcu/animacionVintage/spaf81S3.js
@@ -1,9 +1,22 @@
-import { spaf81S3E1 } from "./spaf81S3/episodes.js";
+import { spaf81S3E1, spaf81S3E2, spaf81S3E3, spaf81S3E4, spaf81S3E5, spaf81S3E6, spaf81S3E7, spaf81S3E8 } from "./spaf81S3/episodes.js";
 
 export const spaf81S3 = {
   id: "spaf81S3",
   label: "Spider-Man and His Amazing Friends (1981) (S3)",
+  format: "Serie",
+  season: 3,
+  release: {
+    year: 1983,
+    month: 9,
+    day: 17
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    spaf81S3E1
+    spaf81S3E1, spaf81S3E2, spaf81S3E3, spaf81S3E4, spaf81S3E5, spaf81S3E6, spaf81S3E7, spaf81S3E8
   ],
 };
--- assets/data/mcu/animacionVintage/spaf81S3/episodes.js
+++ assets/data/mcu/animacionVintage/spaf81S3/episodes.js
@@ -1,7 +1,79 @@
 export const spaf81S3E1 = {
   id: "spaf81S3E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The X-Men Adventure"},
+    en: "The X-Men Adventure",
+    es: "La aventura de los X-Men"
   },
 };
 
+export const spaf81S3E2 = {
+  id: "spaf81S3E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Avengers Adventure",
+    es: "La aventura de los Vengadores"
+  },
+};
+
+export const spaf81S3E3 = {
+  id: "spaf81S3E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Fantastic Four Adventure",
+    es: "La aventura de los 4 Fantásticos"
+  },
+};
+
+export const spaf81S3E4 = {
+  id: "spaf81S3E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Doctor Doom Adventure",
+    es: "La aventura de Doctor Doom"
+  },
+};
+
+export const spaf81S3E5 = {
+  id: "spaf81S3E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Green Goblin Adventure",
+    es: "La aventura del Duende Verde"
+  },
+};
+
+export const spaf81S3E6 = {
+  id: "spaf81S3E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Kingpin Adventure",
+    es: "La aventura del Kingpin"
+  },
+};
+
+export const spaf81S3E7 = {
+  id: "spaf81S3E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The Kraven Adventure",
+    es: "La aventura de Kraven"
+  },
+};
+
+export const spaf81S3E8 = {
+  id: "spaf81S3E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Mysterio Adventure",
+    es: "La aventura de Mysterio"
+  },
+};
