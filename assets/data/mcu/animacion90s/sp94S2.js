f4f3e89761f5ca30a41bf7a4a0a2b2b37736bc7e
Guille <cochraneguillermo@gmail.com>
2026-04-06 17:18:15 -0300

    Spider-Man: The Animated Series (1994) (S2) auditada y corregida

--- assets/data/mcu/animacion90s/sp94S2.js
+++ assets/data/mcu/animacion90s/sp94S2.js
@@ -1,9 +1,23 @@
-import { sp94S2E1 } from "./sp94S2/episodes.js";
+import { sp94S2E1, sp94S2E2, sp94S2E3, sp94S2E4, sp94S2E5, sp94S2E6, sp94S2E7, sp94S2E8, sp94S2E9, sp94S2E10, sp94S2E11, sp94S2E12, sp94S2E13, sp94S2E14 } from "./sp94S2/episodes.js";
 
 export const sp94S2 = {
   id: "sp94S2",
   label: "Spider-Man: The Animated Series (1994) (S2)",
+  format: "Serie",
+  season: 2,
+  release: {
+    year: 1995,
+    month: 9,
+    day: 9
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    sp94S2E1
+    sp94S2E1, sp94S2E2, sp94S2E3, sp94S2E4, sp94S2E5, sp94S2E6, sp94S2E7, sp94S2E8,
+    sp94S2E9, sp94S2E10, sp94S2E11, sp94S2E12, sp94S2E13, sp94S2E14
   ],
 };
--- assets/data/mcu/animacion90s/sp94S2/episodes.js
+++ assets/data/mcu/animacion90s/sp94S2/episodes.js
@@ -1,7 +1,139 @@
 export const sp94S2E1 = {
   id: "sp94S2E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Insidious Six"},
+    en: "The Insidious Six",
+    es: "Los Insidiosos Seis"
   },
 };
 
+export const sp94S2E2 = {
+  id: "sp94S2E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Battle of the Insidious Six",
+    es: "Batalla de los Insidiosos Seis"
+  },
+};
+
+export const sp94S2E3 = {
+  id: "sp94S2E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Hydro-Man",
+    es: "Hydro-Man"
+  },
+};
+
+export const sp94S2E4 = {
+  id: "sp94S2E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Mutant Agenda",
+    es: "La agenda mutante"
+  },
+};
+
+export const sp94S2E5 = {
+  id: "sp94S2E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Mutants' Revenge",
+    es: "La venganza de los mutantes"
+  },
+};
+
+export const sp94S2E6 = {
+  id: "sp94S2E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Morbius (Part 1)",
+    es: "Morbius (Parte 1)"
+  },
+};
+
+export const sp94S2E7 = {
+  id: "sp94S2E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Morbius (Part 2)",
+    es: "Morbius (Parte 2)"
+  },
+};
+
+export const sp94S2E8 = {
+  id: "sp94S2E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Enter the Punisher",
+    es: "Entra el Castigador"
+  },
+};
+
+export const sp94S2E9 = {
+  id: "sp94S2E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Duel of the Hunters",
+    es: "Duelo de cazadores"
+  },
+};
+
+export const sp94S2E10 = {
+  id: "sp94S2E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "Blade the Vampire Hunter",
+    es: "Blade, el cazavampiros"
+  },
+};
+
+export const sp94S2E11 = {
+  id: "sp94S2E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Immortal Vampire",
+    es: "El vampiro inmortal"
+  },
+};
+
+export const sp94S2E12 = {
+  id: "sp94S2E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "Tablet of Time (Part 1)",
+    es: "La tablilla del tiempo (Parte 1)"
+  },
+};
+
+export const sp94S2E13 = {
+  id: "sp94S2E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "Tablet of Time (Part 2)",
+    es: "La tablilla del tiempo (Parte 2)"
+  },
+};
+
+export const sp94S2E14 = {
+  id: "sp94S2E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "Tablet of Time (Part 3)",
+    es: "La tablilla del tiempo (Parte 3)"
+  },
+};
