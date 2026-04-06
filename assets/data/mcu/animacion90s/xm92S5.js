b0ceccaa282c12d4843ab760df0057b673fe2532
Guille <cochraneguillermo@gmail.com>
2026-04-06 17:13:50 -0300

    X-Men: The Animated Series (1992) (S5) auditada y corregida

--- assets/data/mcu/animacion90s/xm92S5.js
+++ assets/data/mcu/animacion90s/xm92S5.js
@@ -1,9 +1,23 @@
-import { xm92S5E1 } from "./xm92S5/episodes.js";
+import { xm92S5E1, xm92S5E2, xm92S5E3, xm92S5E4, xm92S5E5, xm92S5E6, xm92S5E7, xm92S5E8, xm92S5E9, xm92S5E10 } from "./xm92S5/episodes.js";
 
 export const xm92S5 = {
   id: "xm92S5",
   label: "X-Men: The Animated Series (1992) (S5)",
+  format: "Serie",
+  season: 5,
+  release: {
+    year: 1996,
+    month: 9,
+    day: 7
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    xm92S5E1
+    xm92S5E1, xm92S5E2, xm92S5E3, xm92S5E4, xm92S5E5,
+    xm92S5E6, xm92S5E7, xm92S5E8, xm92S5E9, xm92S5E10
   ],
 };
--- assets/data/mcu/animacion90s/xm92S5/episodes.js
+++ assets/data/mcu/animacion90s/xm92S5/episodes.js
@@ -1,7 +1,99 @@
 export const xm92S5E1 = {
   id: "xm92S5E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Phalanx Covenant (Part 1)"},
+    en: "The Phalanx Covenant (Part 1): Creed",
+    es: "El pacto Phalanx (Parte 1): Credo"
   },
 };
 
+export const xm92S5E2 = {
+  id: "xm92S5E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Phalanx Covenant (Part 2): The Hunt for Jubilee",
+    es: "El pacto Phalanx (Parte 2): La búsqueda de Jubilee"
+  },
+};
+
+export const xm92S5E3 = {
+  id: "xm92S5E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Phalanx Covenant (Part 3): Annihilation",
+    es: "El pacto Phalanx (Parte 3): Aniquilación"
+  },
+};
+
+export const xm92S5E4 = {
+  id: "xm92S5E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Storm Front (Part 1)",
+    es: "Frente tormentoso (Parte 1)"
+  },
+};
+
+export const xm92S5E5 = {
+  id: "xm92S5E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Storm Front (Part 2)",
+    es: "Frente tormentoso (Parte 2)"
+  },
+};
+
+export const xm92S5E6 = {
+  id: "xm92S5E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Fifth Horseman",
+    es: "El quinto jinete"
+  },
+};
+
+export const xm92S5E7 = {
+  id: "xm92S5E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Jubilee's Fairytale Theatre",
+    es: "El teatro de cuentos de Jubilee"
+  },
+};
+
+export const xm92S5E8 = {
+  id: "xm92S5E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Old Soldiers",
+    es: "Viejos soldados"
+  },
+};
+
+export const xm92S5E9 = {
+  id: "xm92S5E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Descent",
+    es: "Descenso"
+  },
+};
+
+export const xm92S5E10 = {
+  id: "xm92S5E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "Graduation Day",
+    es: "Día de graduación"
+  },
+};
