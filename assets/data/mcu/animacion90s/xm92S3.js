fe465611dbe9bfa8f7c0161df4cc9ceed4ce1102
Guille <cochraneguillermo@gmail.com>
2026-04-06 17:11:25 -0300

    X-Men: The Animated Series (1992) (S3) auditada y corregida

--- assets/data/mcu/animacion90s/xm92S3.js
+++ assets/data/mcu/animacion90s/xm92S3.js
@@ -1,9 +1,25 @@
-import { xm92S3E1 } from "./xm92S3/episodes.js";
+import { xm92S3E1, xm92S3E2, xm92S3E3, xm92S3E4, xm92S3E5, xm92S3E6, xm92S3E7, xm92S3E8, xm92S3E9, xm92S3E10, xm92S3E11, xm92S3E12, xm92S3E13, xm92S3E14, xm92S3E15, xm92S3E16, xm92S3E17, xm92S3E18, xm92S3E19, xm92S3E20, xm92S3E21, xm92S3E22, xm92S3E23, xm92S3E24, xm92S3E25, xm92S3E26 } from "./xm92S3/episodes.js";
 
 export const xm92S3 = {
   id: "xm92S3",
   label: "X-Men: The Animated Series (1992) (S3)",
+  format: "Serie",
+  season: 3,
+  release: {
+    year: 1994,
+    month: 7,
+    day: 29
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    xm92S3E1
+    xm92S3E1, xm92S3E2, xm92S3E3, xm92S3E4, xm92S3E5, xm92S3E6, xm92S3E7, xm92S3E8,
+    xm92S3E9, xm92S3E10, xm92S3E11, xm92S3E12, xm92S3E13, xm92S3E14, xm92S3E15, xm92S3E16,
+    xm92S3E17, xm92S3E18, xm92S3E19, xm92S3E20, xm92S3E21, xm92S3E22, xm92S3E23, xm92S3E24,
+    xm92S3E25, xm92S3E26
   ],
 };
--- assets/data/mcu/animacion90s/xm92S3/episodes.js
+++ assets/data/mcu/animacion90s/xm92S3/episodes.js
@@ -1,7 +1,259 @@
 export const xm92S3E1 = {
   id: "xm92S3E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Phoenix Saga (Part 1)"},
+    en: "The Phoenix Saga (Part 1): Sacrifice",
+    es: "La saga de Fénix (Parte 1): Sacrificio"
   },
 };
 
+export const xm92S3E2 = {
+  id: "xm92S3E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Phoenix Saga (Part 2): The Dark Shroud",
+    es: "La saga de Fénix (Parte 2): El manto oscuro"
+  },
+};
+
+export const xm92S3E3 = {
+  id: "xm92S3E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Phoenix Saga (Part 3): The Cry of the Banshee",
+    es: "La saga de Fénix (Parte 3): El grito de Banshee"
+  },
+};
+
+export const xm92S3E4 = {
+  id: "xm92S3E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Phoenix Saga (Part 4): The Starjammers",
+    es: "La saga de Fénix (Parte 4): Los Starjammers"
+  },
+};
+
+export const xm92S3E5 = {
+  id: "xm92S3E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Phoenix Saga (Part 5): Child of Light",
+    es: "La saga de Fénix (Parte 5): Hija de la luz"
+  },
+};
+
+export const xm92S3E6 = {
+  id: "xm92S3E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "No Mutant Is an Island",
+    es: "Ningún mutante es una isla"
+  },
+};
+
+export const xm92S3E7 = {
+  id: "xm92S3E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Longshot",
+    es: "Longshot"
+  },
+};
+
+export const xm92S3E8 = {
+  id: "xm92S3E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Dark Phoenix Saga (Part 1): Dazzled",
+    es: "La saga de Fénix Oscura (Parte 1): Deslumbrada"
+  },
+};
+
+export const xm92S3E9 = {
+  id: "xm92S3E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The Dark Phoenix Saga (Part 2): The Inner Circle",
+    es: "La saga de Fénix Oscura (Parte 2): El círculo íntimo"
+  },
+};
+
+export const xm92S3E10 = {
+  id: "xm92S3E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Dark Phoenix Saga (Part 3): The Fate of the Phoenix",
+    es: "La saga de Fénix Oscura (Parte 3): El destino de Fénix"
+  },
+};
+
+export const xm92S3E11 = {
+  id: "xm92S3E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "Cold Comfort",
+    es: "Frío consuelo"
+  },
+};
+
+export const xm92S3E12 = {
+  id: "xm92S3E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "Obsession",
+    es: "Ob sesión"
+  },
+};
+
+export const xm92S3E13 = {
+  id: "xm92S3E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "The Juggernaut Returns",
+    es: "El regreso de Juggernaut"
+  },
+};
+
+export const xm92S3E14 = {
+  id: "xm92S3E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "Nightcrawler",
+    es: "Nightcrawler"
+  },
+};
+
+export const xm92S3E15 = {
+  id: "xm92S3E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "Weapon X, Lies and Videotape",
+    es: "Arma X, mentiras y video"
+  },
+};
+
+export const xm92S3E16 = {
+  id: "xm92S3E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "One Man's Worth (Part 1)",
+    es: "El valor de un hombre (Parte 1)"
+  },
+};
+
+export const xm92S3E17 = {
+  id: "xm92S3E17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "One Man's Worth (Part 2)",
+    es: "El valor de un hombre (Parte 2)"
+  },
+};
+
+export const xm92S3E18 = {
+  id: "xm92S3E18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "Courage",
+    es: "Coraje"
+  },
+};
+
+export const xm92S3E19 = {
+  id: "xm92S3E19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "Proteus (Part 1)",
+    es: "Proteus (Parte 1)"
+  },
+};
+
+export const xm92S3E20 = {
+  id: "xm92S3E20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "Proteus (Part 2)",
+    es: "Proteus (Parte 2)"
+  },
+};
+
+export const xm92S3E21 = {
+  id: "xm92S3E21",
+  number: 21,
+  completed: false,
+  label: {
+    en: "Family Ties",
+    es: "Lazos familiares"
+  },
+};
+
+export const xm92S3E22 = {
+  id: "xm92S3E22",
+  number: 22,
+  completed: false,
+  label: {
+    en: "Bloodlines",
+    es: "Líneas de sangre"
+  },
+};
+
+export const xm92S3E23 = {
+  id: "xm92S3E23",
+  number: 23,
+  completed: false,
+  label: {
+    en: "Xavier Remembers",
+    es: "Xavier recuerda"
+  },
+};
+
+export const xm92S3E24 = {
+  id: "xm92S3E24",
+  number: 24,
+  completed: false,
+  label: {
+    en: "The Lotus and the Steel",
+    es: "El loto y el acero"
+  },
+};
+
+export const xm92S3E25 = {
+  id: "xm92S3E25",
+  number: 25,
+  completed: false,
+  label: {
+    en: "Love in Vain",
+    es: "Amor en vano"
+  },
+};
+
+export const xm92S3E26 = {
+  id: "xm92S3E26",
+  number: 26,
+  completed: false,
+  label: {
+    en: "Secrets, Not Long Buried",
+    es: "Secretos no enterrados"
+  },
+};
