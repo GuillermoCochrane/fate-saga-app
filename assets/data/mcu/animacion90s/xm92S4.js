d2992a078a42b457a38b71dc1a8c6506d2ae315d
Guille <cochraneguillermo@gmail.com>
2026-04-06 17:12:35 -0300

    X-Men: The Animated Series (1992) (S4) auditada y corregida

--- assets/data/mcu/animacion90s/xm92S4.js
+++ assets/data/mcu/animacion90s/xm92S4.js
@@ -1,9 +1,24 @@
-import { xm92S4E1 } from "./xm92S4/episodes.js";
+import { xm92S4E1, xm92S4E2, xm92S4E3, xm92S4E4, xm92S4E5, xm92S4E6, xm92S4E7, xm92S4E8, xm92S4E9, xm92S4E10, xm92S4E11, xm92S4E12, xm92S4E13, xm92S4E14, xm92S4E15, xm92S4E16, xm92S4E17, xm92S4E18, xm92S4E19, xm92S4E20, xm92S4E21 } from "./xm92S4/episodes.js";
 
 export const xm92S4 = {
   id: "xm92S4",
   label: "X-Men: The Animated Series (1992) (S4)",
+  format: "Serie",
+  season: 4,
+  release: {
+    year: 1995,
+    month: 9,
+    day: 9
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    xm92S4E1
+    xm92S4E1, xm92S4E2, xm92S4E3, xm92S4E4, xm92S4E5, xm92S4E6, xm92S4E7, xm92S4E8,
+    xm92S4E9, xm92S4E10, xm92S4E11, xm92S4E12, xm92S4E13, xm92S4E14, xm92S4E15, xm92S4E16,
+    xm92S4E17, xm92S4E18, xm92S4E19, xm92S4E20, xm92S4E21
   ],
 };
--- assets/data/mcu/animacion90s/xm92S4/episodes.js
+++ assets/data/mcu/animacion90s/xm92S4/episodes.js
@@ -1,7 +1,209 @@
 export const xm92S4E1 = {
   id: "xm92S4E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Juggernaut Returns"},
+    en: "The Juggernaut Returns",
+    es: "El regreso de Juggernaut"
   },
 };
 
+export const xm92S4E2 = {
+  id: "xm92S4E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Nightcrawler",
+    es: "Nightcrawler"
+  },
+};
+
+export const xm92S4E3 = {
+  id: "xm92S4E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Weapon X, Lies and Videotape",
+    es: "Arma X, mentiras y video"
+  },
+};
+
+export const xm92S4E4 = {
+  id: "xm92S4E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "One Man's Worth (Part 1)",
+    es: "El valor de un hombre (Parte 1)"
+  },
+};
+
+export const xm92S4E5 = {
+  id: "xm92S4E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "One Man's Worth (Part 2)",
+    es: "El valor de un hombre (Parte 2)"
+  },
+};
+
+export const xm92S4E6 = {
+  id: "xm92S4E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Courage",
+    es: "Coraje"
+  },
+};
+
+export const xm92S4E7 = {
+  id: "xm92S4E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Proteus (Part 1)",
+    es: "Proteus (Parte 1)"
+  },
+};
+
+export const xm92S4E8 = {
+  id: "xm92S4E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Proteus (Part 2)",
+    es: "Proteus (Parte 2)"
+  },
+};
+
+export const xm92S4E9 = {
+  id: "xm92S4E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Family Ties",
+    es: "Lazos familiares"
+  },
+};
+
+export const xm92S4E10 = {
+  id: "xm92S4E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "Bloodlines",
+    es: "Líneas de sangre"
+  },
+};
+
+export const xm92S4E11 = {
+  id: "xm92S4E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "Xavier Remembers",
+    es: "Xavier recuerda"
+  },
+};
+
+export const xm92S4E12 = {
+  id: "xm92S4E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Lotus and the Steel",
+    es: "El loto y el acero"
+  },
+};
+
+export const xm92S4E13 = {
+  id: "xm92S4E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "Love in Vain",
+    es: "Amor en vano"
+  },
+};
+
+export const xm92S4E14 = {
+  id: "xm92S4E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "Secrets, Not Long Buried",
+    es: "Secretos no enterrados"
+  },
+};
+
+export const xm92S4E15 = {
+  id: "xm92S4E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "Beyond Good and Evil (Part 1): The End of Time",
+    es: "Más allá del bien y del mal (Parte 1): El fin del tiempo"
+  },
+};
+
+export const xm92S4E16 = {
+  id: "xm92S4E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "Beyond Good and Evil (Part 2): Promise of Apocalypse",
+    es: "Más allá del bien y del mal (Parte 2): La promesa del Apocalipsis"
+  },
+};
+
+export const xm92S4E17 = {
+  id: "xm92S4E17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "Beyond Good and Evil (Part 3): The Lazarus Chamber",
+    es: "Más allá del bien y del mal (Parte 3): La cámara de Lázaro"
+  },
+};
+
+export const xm92S4E18 = {
+  id: "xm92S4E18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "Beyond Good and Evil (Part 4): End and Beginning",
+    es: "Más allá del bien y del mal (Parte 4): Fin y principio"
+  },
+};
+
+export const xm92S4E19 = {
+  id: "xm92S4E19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "The Phalanx Covenant (Part 1): Creed",
+    es: "El pacto Phalanx (Parte 1): Credo"
+  },
+};
+
+export const xm92S4E20 = {
+  id: "xm92S4E20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "The Phalanx Covenant (Part 2): The Hunt for Jubilee",
+    es: "El pacto Phalanx (Parte 2): La búsqueda de Jubilee"
+  },
+};
+
+export const xm92S4E21 = {
+  id: "xm92S4E21",
+  number: 21,
+  completed: false,
+  label: {
+    en: "The Phalanx Covenant (Part 3): Annihilation",
+    es: "El pacto Phalanx (Parte 3): Aniquilación"
+  },
+};
