0e7c71d28802ae64901986d45c4acc96d0a17880
Guille <cochraneguillermo@gmail.com>
2026-04-06 17:09:00 -0300

    X-Men: The Animated Series (1992) (S1) auditada y corregida

--- assets/data/mcu/animacion90s/xm92S1.js
+++ assets/data/mcu/animacion90s/xm92S1.js
@@ -1,9 +1,23 @@
-import { xm92S1E1 } from "./xm92S1/episodes.js";
+import { xm92S1E1, xm92S1E2, xm92S1E3, xm92S1E4, xm92S1E5, xm92S1E6, xm92S1E7, xm92S1E8, xm92S1E9, xm92S1E10, xm92S1E11, xm92S1E12, xm92S1E13 } from "./xm92S1/episodes.js";
 
 export const xm92S1 = {
   id: "xm92S1",
   label: "X-Men: The Animated Series (1992) (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 1992,
+    month: 10,
+    day: 31
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    xm92S1E1
+    xm92S1E1, xm92S1E2, xm92S1E3, xm92S1E4, xm92S1E5, xm92S1E6, xm92S1E7,
+    xm92S1E8, xm92S1E9, xm92S1E10, xm92S1E11, xm92S1E12, xm92S1E13
   ],
 };
--- assets/data/mcu/animacion90s/xm92S1/episodes.js
+++ assets/data/mcu/animacion90s/xm92S1/episodes.js
@@ -1,7 +1,129 @@
 export const xm92S1E1 = {
   id: "xm92S1E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Night of the Sentinels (Part 1)"},
+    en: "Night of the Sentinels (Part 1)",
+    es: "La noche de los centinelas (Parte 1)"
   },
 };
 
+export const xm92S1E2 = {
+  id: "xm92S1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Night of the Sentinels (Part 2)",
+    es: "La noche de los centinelas (Parte 2)"
+  },
+};
+
+export const xm92S1E3 = {
+  id: "xm92S1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Enter Magneto",
+    es: "Entra Magneto"
+  },
+};
+
+export const xm92S1E4 = {
+  id: "xm92S1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Deadly Reunions",
+    es: "Reuniones mortales"
+  },
+};
+
+export const xm92S1E5 = {
+  id: "xm92S1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Captive Hearts",
+    es: "Corazones cautivos"
+  },
+};
+
+export const xm92S1E6 = {
+  id: "xm92S1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Cold Vengeance",
+    es: "Venganza fría"
+  },
+};
+
+export const xm92S1E7 = {
+  id: "xm92S1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Slave Island",
+    es: "Isla esclava"
+  },
+};
+
+export const xm92S1E8 = {
+  id: "xm92S1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Unstoppable Juggernaut",
+    es: "El Juggernaut imparable"
+  },
+};
+
+export const xm92S1E9 = {
+  id: "xm92S1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The Cure",
+    es: "La cura"
+  },
+};
+
+export const xm92S1E10 = {
+  id: "xm92S1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "Come the Apocalypse",
+    es: "Llega el Apocalipsis"
+  },
+};
+
+export const xm92S1E11 = {
+  id: "xm92S1E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "Days of Future Past (Part 1)",
+    es: "Días del futuro pasado (Parte 1)"
+  },
+};
+
+export const xm92S1E12 = {
+  id: "xm92S1E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "Days of Future Past (Part 2)",
+    es: "Días del futuro pasado (Parte 2)"
+  },
+};
+
+export const xm92S1E13 = {
+  id: "xm92S1E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "The Final Decision",
+    es: "La decisión final"
+  },
+};
