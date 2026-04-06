2eeb52e432d37465c56d2625534bdc88d590c509
Guille <cochraneguillermo@gmail.com>
2026-04-06 17:17:17 -0300

    Spider-Man: The Animated Series (1994) (S1) auditada y corregida

--- assets/data/mcu/animacion90s/sp94S1.js
+++ assets/data/mcu/animacion90s/sp94S1.js
@@ -1,9 +1,23 @@
-import { sp94S1E1 } from "./sp94S1/episodes.js";
+import { sp94S1E1, sp94S1E2, sp94S1E3, sp94S1E4, sp94S1E5, sp94S1E6, sp94S1E7, sp94S1E8, sp94S1E9, sp94S1E10, sp94S1E11, sp94S1E12, sp94S1E13 } from "./sp94S1/episodes.js";
 
 export const sp94S1 = {
   id: "sp94S1",
   label: "Spider-Man: The Animated Series (1994) (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 1994,
+    month: 11,
+    day: 19
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    sp94S1E1
+    sp94S1E1, sp94S1E2, sp94S1E3, sp94S1E4, sp94S1E5, sp94S1E6, sp94S1E7,
+    sp94S1E8, sp94S1E9, sp94S1E10, sp94S1E11, sp94S1E12, sp94S1E13
   ],
 };
--- assets/data/mcu/animacion90s/sp94S1/episodes.js
+++ assets/data/mcu/animacion90s/sp94S1/episodes.js
@@ -1,7 +1,129 @@
 export const sp94S1E1 = {
   id: "sp94S1E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Night of the Lizard"},
+    en: "Night of the Lizard",
+    es: "La noche del Lagarto"
   },
 };
 
+export const sp94S1E2 = {
+  id: "sp94S1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Spider Slayer",
+    es: "El Mataraneas"
+  },
+};
+
+export const sp94S1E3 = {
+  id: "sp94S1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Return of the Spider Slayer",
+    es: "El regreso del Mataraneas"
+  },
+};
+
+export const sp94S1E4 = {
+  id: "sp94S1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Doctor Octopus: Armed and Dangerous",
+    es: "Doctor Octopus: Armado y peligroso"
+  },
+};
+
+export const sp94S1E5 = {
+  id: "sp94S1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Menace of Mysterio",
+    es: "La amenaza de Mysterio"
+  },
+};
+
+export const sp94S1E6 = {
+  id: "sp94S1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Sting of the Scorpion",
+    es: "El aguijón del Escorpión"
+  },
+};
+
+export const sp94S1E7 = {
+  id: "sp94S1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Kraven the Hunter",
+    es: "Kraven el Cazador"
+  },
+};
+
+export const sp94S1E8 = {
+  id: "sp94S1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Alien Costume (Part 1)",
+    es: "El traje alienígena (Parte 1)"
+  },
+};
+
+export const sp94S1E9 = {
+  id: "sp94S1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The Alien Costume (Part 2)",
+    es: "El traje alienígena (Parte 2)"
+  },
+};
+
+export const sp94S1E10 = {
+  id: "sp94S1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Alien Costume (Part 3)",
+    es: "El traje alienígena (Parte 3)"
+  },
+};
+
+export const sp94S1E11 = {
+  id: "sp94S1E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Hobgoblin (Part 1)",
+    es: "El Hobgoblin (Parte 1)"
+  },
+};
+
+export const sp94S1E12 = {
+  id: "sp94S1E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Hobgoblin (Part 2)",
+    es: "El Hobgoblin (Parte 2)"
+  },
+};
+
+export const sp94S1E13 = {
+  id: "sp94S1E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "Day of the Chameleon",
+    es: "El día del Camaleón"
+  },
+};
