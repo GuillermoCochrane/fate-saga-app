708b89e6495633278fabb6a727af2f2d416d68f4
Guille <cochraneguillermo@gmail.com>
2026-04-06 17:09:53 -0300

    X-Men: The Animated Series (1992) (S2) auditada y corregida

--- assets/data/mcu/animacion90s/xm92S2.js
+++ assets/data/mcu/animacion90s/xm92S2.js
@@ -1,9 +1,23 @@
-import { xm92S2E1 } from "./xm92S2/episodes.js";
+import { xm92S2E1, xm92S2E2, xm92S2E3, xm92S2E4, xm92S2E5, xm92S2E6, xm92S2E7, xm92S2E8, xm92S2E9, xm92S2E10, xm92S2E11, xm92S2E12, xm92S2E13 } from "./xm92S2/episodes.js";
 
 export const xm92S2 = {
   id: "xm92S2",
   label: "X-Men: The Animated Series (1992) (S2)",
+  format: "Serie",
+  season: 2,
+  release: {
+    year: 1993,
+    month: 10,
+    day: 23
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    xm92S2E1
+    xm92S2E1, xm92S2E2, xm92S2E3, xm92S2E4, xm92S2E5, xm92S2E6, xm92S2E7,
+    xm92S2E8, xm92S2E9, xm92S2E10, xm92S2E11, xm92S2E12, xm92S2E13
   ],
 };
--- assets/data/mcu/animacion90s/xm92S2/episodes.js
+++ assets/data/mcu/animacion90s/xm92S2/episodes.js
@@ -1,7 +1,129 @@
 export const xm92S2E1 = {
   id: "xm92S2E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Till Death Do Us Part (Part 1)"},
+    en: "Till Death Do Us Part (Part 1)",
+    es: "Hasta que la muerte nos separe (Parte 1)"
   },
 };
 
+export const xm92S2E2 = {
+  id: "xm92S2E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Till Death Do Us Part (Part 2)",
+    es: "Hasta que la muerte nos separe (Parte 2)"
+  },
+};
+
+export const xm92S2E3 = {
+  id: "xm92S2E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Whatever It Takes",
+    es: "Lo que sea necesario"
+  },
+};
+
+export const xm92S2E4 = {
+  id: "xm92S2E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Red Dawn",
+    es: "Amanecer rojo"
+  },
+};
+
+export const xm92S2E5 = {
+  id: "xm92S2E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Repo Man",
+    es: "Hombre de la reposición"
+  },
+};
+
+export const xm92S2E6 = {
+  id: "xm92S2E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "X-Ternally Yours",
+    es: "X-Ternalmente tuyo"
+  },
+};
+
+export const xm92S2E7 = {
+  id: "xm92S2E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Time Fugitives (Part 1)",
+    es: "Fugitivos del tiempo (Parte 1)"
+  },
+};
+
+export const xm92S2E8 = {
+  id: "xm92S2E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Time Fugitives (Part 2)",
+    es: "Fugitivos del tiempo (Parte 2)"
+  },
+};
+
+export const xm92S2E9 = {
+  id: "xm92S2E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "A Rogue's Tale",
+    es: "La historia de Rogue"
+  },
+};
+
+export const xm92S2E10 = {
+  id: "xm92S2E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "Beauty & the Beast",
+    es: "La bella y la bestia"
+  },
+};
+
+export const xm92S2E11 = {
+  id: "xm92S2E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "Mojovision",
+    es: "Mojovisión"
+  },
+};
+
+export const xm92S2E12 = {
+  id: "xm92S2E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "Reunion (Part 1)",
+    es: "Reunión (Parte 1)"
+  },
+};
+
+export const xm92S2E13 = {
+  id: "xm92S2E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "Reunion (Part 2)",
+    es: "Reunión (Parte 2)"
+  },
+};
