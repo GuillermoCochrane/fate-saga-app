da7ba9517c12481a58459248d1d9353f6dad45bf
Guille <cochraneguillermo@gmail.com>
2026-04-07 15:54:23 -0300

    Spider-Man: The Animated Series (1994) (S3) auditada y corregida

--- assets/data/mcu/animacion90s/sp94S3.js
+++ assets/data/mcu/animacion90s/sp94S3.js
@@ -1,9 +1,23 @@
-import { sp94S3E1 } from "./sp94S3/episodes.js";
+import { sp94S3E1, sp94S3E2, sp94S3E3, sp94S3E4, sp94S3E5, sp94S3E6, sp94S3E7, sp94S3E8, sp94S3E9, sp94S3E10, sp94S3E11, sp94S3E12, sp94S3E13, sp94S3E14 } from "./sp94S3/episodes.js";
 
 export const sp94S3 = {
   id: "sp94S3",
   label: "Spider-Man: The Animated Series (1994) (S3)",
+  format: "Serie",
+  season: 3,
+  release: {
+    year: 1996,
+    month: 4,
+    day: 27
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    sp94S3E1
+    sp94S3E1, sp94S3E2, sp94S3E3, sp94S3E4, sp94S3E5, sp94S3E6, sp94S3E7, sp94S3E8,
+    sp94S3E9, sp94S3E10, sp94S3E11, sp94S3E12, sp94S3E13, sp94S3E14
   ],
 };
--- assets/data/mcu/animacion90s/sp94S3/episodes.js
+++ assets/data/mcu/animacion90s/sp94S3/episodes.js
@@ -1,7 +1,139 @@
 export const sp94S3E1 = {
   id: "sp94S3E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Doctor Strange"},
+    en: "Doctor Strange",
+    es: "Doctor Strange"
   },
 };
 
+export const sp94S3E2 = {
+  id: "sp94S3E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Make a Wish",
+    es: "Pide un deseo"
+  },
+};
+
+export const sp94S3E3 = {
+  id: "sp94S3E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Attack of the Octobot",
+    es: "Ataque del Octobot"
+  },
+};
+
+export const sp94S3E4 = {
+  id: "sp94S3E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Enter the Green Goblin",
+    es: "Entra el Duende Verde"
+  },
+};
+
+export const sp94S3E5 = {
+  id: "sp94S3E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Rocket Racer",
+    es: "Rocket Racer"
+  },
+};
+
+export const sp94S3E6 = {
+  id: "sp94S3E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Framed",
+    es: "Enmarcado"
+  },
+};
+
+export const sp94S3E7 = {
+  id: "sp94S3E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The Man Without Fear",
+    es: "El hombre sin miedo"
+  },
+};
+
+export const sp94S3E8 = {
+  id: "sp94S3E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Ultimate Slayer",
+    es: "El Mataraneas definitivo"
+  },
+};
+
+export const sp94S3E9 = {
+  id: "sp94S3E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Tombstone",
+    es: "Tombstone"
+  },
+};
+
+export const sp94S3E10 = {
+  id: "sp94S3E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "Venom Returns",
+    es: "Venom regresa"
+  },
+};
+
+export const sp94S3E11 = {
+  id: "sp94S3E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "Carnage",
+    es: "Carnage"
+  },
+};
+
+export const sp94S3E12 = {
+  id: "sp94S3E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Spot",
+    es: "The Spot"
+  },
+};
+
+export const sp94S3E13 = {
+  id: "sp94S3E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "Goblin War!",
+    es: "¡Guerra de duendes!"
+  },
+};
+
+export const sp94S3E14 = {
+  id: "sp94S3E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "Turning Point",
+    es: "Punto de inflexión"
+  },
+};
