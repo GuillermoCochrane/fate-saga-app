75a521d8ef5018ab6f339f663eb6815349004e43
Guille <cochraneguillermo@gmail.com>
2026-04-07 15:57:52 -0300

    Fantastic Four (1994) (S1)auditada y corregida

--- assets/data/mcu/animacion90s/ff94S1.js
+++ assets/data/mcu/animacion90s/ff94S1.js
@@ -1,9 +1,23 @@
-import { ff94S1E1 } from "./ff94S1/episodes.js";
+import { ff94S1E1, ff94S1E2, ff94S1E3, ff94S1E4, ff94S1E5, ff94S1E6, ff94S1E7, ff94S1E8, ff94S1E9, ff94S1E10, ff94S1E11, ff94S1E12, ff94S1E13 } from "./ff94S1/episodes.js";
 
 export const ff94S1 = {
   id: "ff94S1",
   label: "Fantastic Four (1994) (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 1994,
+    month: 9,
+    day: 24
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    ff94S1E1
+    ff94S1E1, ff94S1E2, ff94S1E3, ff94S1E4, ff94S1E5, ff94S1E6, ff94S1E7,
+    ff94S1E8, ff94S1E9, ff94S1E10, ff94S1E11, ff94S1E12, ff94S1E13
   ],
 };
--- assets/data/mcu/animacion90s/ff94S1/episodes.js
+++ assets/data/mcu/animacion90s/ff94S1/episodes.js
@@ -1,7 +1,129 @@
 export const ff94S1E1 = {
   id: "ff94S1E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Origin of the Fantastic Four"},
+    en: "The Origin of the Fantastic Four",
+    es: "El origen de los 4 Fantásticos"
   },
 };
 
+export const ff94S1E2 = {
+  id: "ff94S1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Silver Surfer and the Coming of Galactus (Part 1)",
+    es: "Silver Surfer y la llegada de Galactus (Parte 1)"
+  },
+};
+
+export const ff94S1E3 = {
+  id: "ff94S1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Silver Surfer and the Coming of Galactus (Part 2)",
+    es: "Silver Surfer y la llegada de Galactus (Parte 2)"
+  },
+};
+
+export const ff94S1E4 = {
+  id: "ff94S1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Silver Surfer and the Coming of Galactus (Part 3)",
+    es: "Silver Surfer y la llegada de Galactus (Parte 3)"
+  },
+};
+
+export const ff94S1E5 = {
+  id: "ff94S1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Super Skrull",
+    es: "Super Skrull"
+  },
+};
+
+export const ff94S1E6 = {
+  id: "ff94S1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Mask of Doom (Part 1)",
+    es: "La máscara de Doom (Parte 1)"
+  },
+};
+
+export const ff94S1E7 = {
+  id: "ff94S1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The Mask of Doom (Part 2)",
+    es: "La máscara de Doom (Parte 2)"
+  },
+};
+
+export const ff94S1E8 = {
+  id: "ff94S1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Mask of Doom (Part 3)",
+    es: "La máscara de Doom (Parte 3)"
+  },
+};
+
+export const ff94S1E9 = {
+  id: "ff94S1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The Inhumans Saga (Part 1)",
+    es: "La saga de los Inhumanos (Parte 1)"
+  },
+};
+
+export const ff94S1E10 = {
+  id: "ff94S1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Inhumans Saga (Part 2)",
+    es: "La saga de los Inhumanos (Parte 2)"
+  },
+};
+
+export const ff94S1E11 = {
+  id: "ff94S1E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Inhumans Saga (Part 3)",
+    es: "La saga de los Inhumanos (Parte 3)"
+  },
+};
+
+export const ff94S1E12 = {
+  id: "ff94S1E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Inhumans Saga (Part 4)",
+    es: "La saga de los Inhumanos (Parte 4)"
+  },
+};
+
+export const ff94S1E13 = {
+  id: "ff94S1E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "The Inhumans Saga (Part 5)",
+    es: "La saga de los Inhumanos (Parte 5)"
+  },
+};
