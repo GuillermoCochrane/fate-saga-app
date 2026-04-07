bec592637400bfa507d4af7dbdf00e1983b15fe7
Guille <cochraneguillermo@gmail.com>
2026-04-07 16:12:09 -0300

    Fantastic Four (1994) (S2) auditada y corregida

--- assets/data/mcu/animacion90s/ff94S2.js
+++ assets/data/mcu/animacion90s/ff94S2.js
@@ -1,9 +1,23 @@
-import { ff94S2E1 } from "./ff94S2/episodes.js";
+import { ff94S2E1, ff94S2E2, ff94S2E3, ff94S2E4, ff94S2E5, ff94S2E6, ff94S2E7, ff94S2E8, ff94S2E9, ff94S2E10, ff94S2E11, ff94S2E12, ff94S2E13 } from "./ff94S2/episodes.js";
 
 export const ff94S2 = {
   id: "ff94S2",
   label: "Fantastic Four (1994) (S2)",
+  format: "Serie",
+  season: 2,
+  release: {
+    year: 1995,
+    month: 9,
+    day: 23
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    ff94S2E1
+    ff94S2E1, ff94S2E2, ff94S2E3, ff94S2E4, ff94S2E5, ff94S2E6, ff94S2E7,
+    ff94S2E8, ff94S2E9, ff94S2E10, ff94S2E11, ff94S2E12, ff94S2E13
   ],
 };
--- assets/data/mcu/animacion90s/ff94S2/episodes.js
+++ assets/data/mcu/animacion90s/ff94S2/episodes.js
@@ -1,7 +1,129 @@
 export const ff94S2E1 = {
   id: "ff94S2E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"To Battle the Living Planet"},
+    en: "To Battle the Living Planet",
+    es: "Enfrentar al planeta viviente"
   },
 };
 
+export const ff94S2E2 = {
+  id: "ff94S2E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Sentry Sinister",
+    es: "El centinela siniestro"
+  },
+};
+
+export const ff94S2E3 = {
+  id: "ff94S2E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Prisoner of Planet X",
+    es: "El prisionero del Planeta X"
+  },
+};
+
+export const ff94S2E4 = {
+  id: "ff94S2E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Terrible Tribunal",
+    es: "El terrible tribunal"
+  },
+};
+
+export const ff94S2E5 = {
+  id: "ff94S2E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "And the Wind Cries... Medusa",
+    es: "Y el viento llora... Medusa"
+  },
+};
+
+export const ff94S2E6 = {
+  id: "ff94S2E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Behold, a Distant Storm",
+    es: "He aquí, una tormenta lejana"
+  },
+};
+
+export const ff94S2E7 = {
+  id: "ff94S2E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The Dark Swan",
+    es: "El cisne oscuro"
+  },
+};
+
+export const ff94S2E8 = {
+  id: "ff94S2E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Altered World",
+    es: "El mundo alterado"
+  },
+};
+
+export const ff94S2E9 = {
+  id: "ff94S2E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The Power... to Stop Galactus!",
+    es: "El poder... ¡para detener a Galactus!"
+  },
+};
+
+export const ff94S2E10 = {
+  id: "ff94S2E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Battle of the Super-Heroes",
+    es: "La batalla de los superhéroes"
+  },
+};
+
+export const ff94S2E11 = {
+  id: "ff94S2E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Micro-World of Doctor Doom",
+    es: "El micro mundo de Doctor Doom"
+  },
+};
+
+export const ff94S2E12 = {
+  id: "ff94S2E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Phantom of the Fair",
+    es: "El fantasma de la feria"
+  },
+};
+
+export const ff94S2E13 = {
+  id: "ff94S2E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "The Good, the Bad and the Hulk",
+    es: "El bueno, el malo y Hulk"
+  },
+};
