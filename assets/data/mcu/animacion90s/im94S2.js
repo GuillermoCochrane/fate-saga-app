f526148d791ce3157c893fee5fed86d5ff5c0f43
Guille <cochraneguillermo@gmail.com>
2026-04-07 16:16:03 -0300

    Iron Man (1994) (S2) auditada y corregida

--- assets/data/mcu/animacion90s/im94S2.js
+++ assets/data/mcu/animacion90s/im94S2.js
@@ -1,9 +1,23 @@
-import { im94S2E1 } from "./im94S2/episodes.js";
+import { im94S2E1, im94S2E2, im94S2E3, im94S2E4, im94S2E5, im94S2E6, im94S2E7, im94S2E8, im94S2E9, im94S2E10, im94S2E11, im94S2E12, im94S2E13 } from "./im94S2/episodes.js";
 
 export const im94S2 = {
   id: "im94S2",
   label: "Iron Man (1994) (S2)",
+  format: "Serie",
+  season: 2,
+  release: {
+    year: 1995,
+    month: 9,
+    day: 23
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    im94S2E1
+    im94S2E1, im94S2E2, im94S2E3, im94S2E4, im94S2E5, im94S2E6, im94S2E7,
+    im94S2E8, im94S2E9, im94S2E10, im94S2E11, im94S2E12, im94S2E13
   ],
 };
--- assets/data/mcu/animacion90s/im94S2/episodes.js
+++ assets/data/mcu/animacion90s/im94S2/episodes.js
@@ -1,7 +1,129 @@
 export const im94S2E1 = {
   id: "im94S2E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Beast Within"},
+    en: "The Beast Within",
+    es: "La bestia interior"
   },
 };
 
+export const im94S2E2 = {
+  id: "im94S2E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Fire and Rain",
+    es: "Fuego y lluvia"
+  },
+};
+
+export const im94S2E3 = {
+  id: "im94S2E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Cell of Iron",
+    es: "Celda de hierro"
+  },
+};
+
+export const im94S2E4 = {
+  id: "im94S2E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Not Far from the Tree",
+    es: "No muy lejos del árbol"
+  },
+};
+
+export const im94S2E5 = {
+  id: "im94S2E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Beauty Knows No Pain",
+    es: "La belleza no conoce el dolor"
+  },
+};
+
+export const im94S2E6 = {
+  id: "im94S2E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Iron Man: On the Inside",
+    es: "Iron Man: En el interior"
+  },
+};
+
+export const im94S2E7 = {
+  id: "im94S2E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Distant Boundaries",
+    es: "Fronteras distantes"
+  },
+};
+
+export const im94S2E8 = {
+  id: "im94S2E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Armor Wars (Part 1)",
+    es: "Las guerras de armaduras (Parte 1)"
+  },
+};
+
+export const im94S2E9 = {
+  id: "im94S2E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The Armor Wars (Part 2)",
+    es: "Las guerras de armaduras (Parte 2)"
+  },
+};
+
+export const im94S2E10 = {
+  id: "im94S2E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Armor Wars (Part 3)",
+    es: "Las guerras de armaduras (Parte 3)"
+  },
+};
+
+export const im94S2E11 = {
+  id: "im94S2E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Armor Wars (Part 4)",
+    es: "Las guerras de armaduras (Parte 4)"
+  },
+};
+
+export const im94S2E12 = {
+  id: "im94S2E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "Empowered",
+    es: "Empoderado"
+  },
+};
+
+export const im94S2E13 = {
+  id: "im94S2E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "Hulk Buster",
+    es: "Hulk Buster"
+  },
+};
