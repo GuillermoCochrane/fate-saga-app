ae6affce47b983cdd736294dede8ccb252fd048a
Guille <cochraneguillermo@gmail.com>
2026-04-07 16:14:16 -0300

    Iron Man (1994) (S1) auditada y corregida

--- assets/data/mcu/animacion90s/im94S1.js
+++ assets/data/mcu/animacion90s/im94S1.js
@@ -1,9 +1,23 @@
-import { im94S1E1 } from "./im94S1/episodes.js";
+import { im94S1E1, im94S1E2, im94S1E3, im94S1E4, im94S1E5, im94S1E6, im94S1E7, im94S1E8, im94S1E9, im94S1E10, im94S1E11, im94S1E12, im94S1E13 } from "./im94S1/episodes.js";
 
 export const im94S1 = {
   id: "im94S1",
   label: "Iron Man (1994) (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 1994,
+    month: 9,
+    day: 24
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    im94S1E1
+    im94S1E1, im94S1E2, im94S1E3, im94S1E4, im94S1E5, im94S1E6, im94S1E7,
+    im94S1E8, im94S1E9, im94S1E10, im94S1E11, im94S1E12, im94S1E13
   ],
 };
--- assets/data/mcu/animacion90s/im94S1/episodes.js
+++ assets/data/mcu/animacion90s/im94S1/episodes.js
@@ -1,7 +1,129 @@
 export const im94S1E1 = {
   id: "im94S1E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Origin of Iron Man"},
+    en: "The Origin of Iron Man",
+    es: "El origen de Iron Man"
   },
 };
 
+export const im94S1E2 = {
+  id: "im94S1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Wedding of Iron Man",
+    es: "La boda de Iron Man"
+  },
+};
+
+export const im94S1E3 = {
+  id: "im94S1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Enemy Within",
+    es: "El enemigo interior"
+  },
+};
+
+export const im94S1E4 = {
+  id: "im94S1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Origin of the Mandarin",
+    es: "El origen del Mandarin"
+  },
+};
+
+export const im94S1E5 = {
+  id: "im94S1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Hands of the Mandarin (Part 1)",
+    es: "Las manos del Mandarin (Parte 1)"
+  },
+};
+
+export const im94S1E6 = {
+  id: "im94S1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Hands of the Mandarin (Part 2)",
+    es: "Las manos del Mandarin (Parte 2)"
+  },
+};
+
+export const im94S1E7 = {
+  id: "im94S1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The Hands of the Mandarin (Part 3)",
+    es: "Las manos del Mandarin (Parte 3)"
+  },
+};
+
+export const im94S1E8 = {
+  id: "im94S1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Hands of the Mandarin (Part 4)",
+    es: "Las manos del Mandarin (Parte 4)"
+  },
+};
+
+export const im94S1E9 = {
+  id: "im94S1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The Hands of the Mandarin (Part 5)",
+    es: "Las manos del Mandarin (Parte 5)"
+  },
+};
+
+export const im94S1E10 = {
+  id: "im94S1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Hands of the Mandarin (Part 6)",
+    es: "Las manos del Mandarin (Parte 6)"
+  },
+};
+
+export const im94S1E11 = {
+  id: "im94S1E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Origin of the Mandarin (Part 2)",
+    es: "El origen del Mandarin (Parte 2)"
+  },
+};
+
+export const im94S1E12 = {
+  id: "im94S1E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Origin of the Mandarin (Part 3)",
+    es: "El origen del Mandarin (Parte 3)"
+  },
+};
+
+export const im94S1E13 = {
+  id: "im94S1E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "The Origin of the Mandarin (Part 4)",
+    es: "El origen del Mandarin (Parte 4)"
+  },
+};
