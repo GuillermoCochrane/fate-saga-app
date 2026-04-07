faed8249f293b6e61675b49c435d0094beb49183
Guille <cochraneguillermo@gmail.com>
2026-04-07 15:55:17 -0300

    Spider-Man: The Animated Series (1994) (S4) auditada y corregida

--- assets/data/mcu/animacion90s/sp94S4.js
+++ assets/data/mcu/animacion90s/sp94S4.js
@@ -1,9 +1,23 @@
-import { sp94S4E1 } from "./sp94S4/episodes.js";
+import { sp94S4E1, sp94S4E2, sp94S4E3, sp94S4E4, sp94S4E5, sp94S4E6, sp94S4E7, sp94S4E8, sp94S4E9, sp94S4E10, sp94S4E11, sp94S4E12, sp94S4E13, sp94S4E14, sp94S4E15 } from "./sp94S4/episodes.js";
 
 export const sp94S4 = {
   id: "sp94S4",
   label: "Spider-Man: The Animated Series (1994) (S4)",
+  format: "Serie",
+  season: 4,
+  release: {
+    year: 1997,
+    month: 2,
+    day: 1
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    sp94S4E1
+    sp94S4E1, sp94S4E2, sp94S4E3, sp94S4E4, sp94S4E5, sp94S4E6, sp94S4E7, sp94S4E8,
+    sp94S4E9, sp94S4E10, sp94S4E11, sp94S4E12, sp94S4E13, sp94S4E14, sp94S4E15
   ],
 };
--- assets/data/mcu/animacion90s/sp94S4/episodes.js
+++ assets/data/mcu/animacion90s/sp94S4/episodes.js
@@ -1,7 +1,149 @@
 export const sp94S4E1 = {
   id: "sp94S4E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Venom Returns"},
+    en: "Guilty",
+    es: "Culpable"
   },
 };
 
+export const sp94S4E2 = {
+  id: "sp94S4E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Cat",
+    es: "La Gata"
+  },
+};
+
+export const sp94S4E3 = {
+  id: "sp94S4E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Black Cat",
+    es: "La Gata Negra"
+  },
+};
+
+export const sp94S4E4 = {
+  id: "sp94S4E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Return of Kraven",
+    es: "El regreso de Kraven"
+  },
+};
+
+export const sp94S4E5 = {
+  id: "sp94S4E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Partners",
+    es: "Socios"
+  },
+};
+
+export const sp94S4E6 = {
+  id: "sp94S4E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Awakening",
+    es: "El despertar"
+  },
+};
+
+export const sp94S4E7 = {
+  id: "sp94S4E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The Vampire Queen",
+    es: "La reina vampiro"
+  },
+};
+
+export const sp94S4E8 = {
+  id: "sp94S4E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Return of the Green Goblin",
+    es: "El regreso del Duende Verde"
+  },
+};
+
+export const sp94S4E9 = {
+  id: "sp94S4E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The Haunting of Mary Jane Watson",
+    es: "La posesión de Mary Jane Watson"
+  },
+};
+
+export const sp94S4E10 = {
+  id: "sp94S4E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Lizard King",
+    es: "El rey Lagarto"
+  },
+};
+
+export const sp94S4E11 = {
+  id: "sp94S4E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Prowler",
+    es: "The Prowler"
+  },
+};
+
+export const sp94S4E12 = {
+  id: "sp94S4E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Wedding",
+    es: "La boda"
+  },
+};
+
+export const sp94S4E13 = {
+  id: "sp94S4E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "Six Forgotten Warriors (Part 1)",
+    es: "Seis guerreros olvidados (Parte 1)"
+  },
+};
+
+export const sp94S4E14 = {
+  id: "sp94S4E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "Six Forgotten Warriors (Part 2)",
+    es: "Seis guerreros olvidados (Parte 2)"
+  },
+};
+
+export const sp94S4E15 = {
+  id: "sp94S4E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "Six Forgotten Warriors (Part 3)",
+    es: "Seis guerreros olvidados (Parte 3)"
+  },
+};
