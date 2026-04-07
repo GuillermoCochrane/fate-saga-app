68ede54837752bc05afd764bff1b28a9e3b8393d
Guille <cochraneguillermo@gmail.com>
2026-04-07 15:56:13 -0300

    Spider-Man: The Animated Series (1994) (S5) auditada y corregida

--- assets/data/mcu/animacion90s/sp94S5.js
+++ assets/data/mcu/animacion90s/sp94S5.js
@@ -1,9 +1,23 @@
-import { sp94S5E1 } from "./sp94S5/episodes.js";
+import { sp94S5E1, sp94S5E2, sp94S5E3, sp94S5E4, sp94S5E5, sp94S5E6, sp94S5E7, sp94S5E8, sp94S5E9, sp94S5E10, sp94S5E11, sp94S5E12, sp94S5E13 } from "./sp94S5/episodes.js";
 
 export const sp94S5 = {
   id: "sp94S5",
   label: "Spider-Man: The Animated Series (1994) (S5)",
+  format: "Serie",
+  season: 5,
+  release: {
+    year: 1997,
+    month: 9,
+    day: 12
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    sp94S5E1
+    sp94S5E1, sp94S5E2, sp94S5E3, sp94S5E4, sp94S5E5, sp94S5E6, sp94S5E7, sp94S5E8,
+    sp94S5E9, sp94S5E10, sp94S5E11, sp94S5E12, sp94S5E13
   ],
 };
--- assets/data/mcu/animacion90s/sp94S5/episodes.js
+++ assets/data/mcu/animacion90s/sp94S5/episodes.js
@@ -1,7 +1,129 @@
 export const sp94S5E1 = {
   id: "sp94S5E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Arrival"},
+    en: "Arrival",
+    es: "Llegada"
   },
 };
 
+export const sp94S5E2 = {
+  id: "sp94S5E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Secret Wars (Part 1)",
+    es: "Las guerras secretas (Parte 1)"
+  },
+};
+
+export const sp94S5E3 = {
+  id: "sp94S5E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Secret Wars (Part 2)",
+    es: "Las guerras secretas (Parte 2)"
+  },
+};
+
+export const sp94S5E4 = {
+  id: "sp94S5E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Secret Wars (Part 3)",
+    es: "Las guerras secretas (Parte 3)"
+  },
+};
+
+export const sp94S5E5 = {
+  id: "sp94S5E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Venom Returns",
+    es: "Venom regresa"
+  },
+};
+
+export const sp94S5E6 = {
+  id: "sp94S5E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Carnage",
+    es: "Carnage"
+  },
+};
+
+export const sp94S5E7 = {
+  id: "sp94S5E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The Spot",
+    es: "The Spot"
+  },
+};
+
+export const sp94S5E8 = {
+  id: "sp94S5E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Goblin War!",
+    es: "¡Guerra de duendes!"
+  },
+};
+
+export const sp94S5E9 = {
+  id: "sp94S5E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Turning Point",
+    es: "Punto de inflexión"
+  },
+};
+
+export const sp94S5E10 = {
+  id: "sp94S5E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Haunting of Mary Jane Watson",
+    es: "La posesión de Mary Jane Watson"
+  },
+};
+
+export const sp94S5E11 = {
+  id: "sp94S5E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Lizard King",
+    es: "El rey Lagarto"
+  },
+};
+
+export const sp94S5E12 = {
+  id: "sp94S5E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Prowler",
+    es: "The Prowler"
+  },
+};
+
+export const sp94S5E13 = {
+  id: "sp94S5E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "The Wedding",
+    es: "La boda"
+  },
+};
