b3d536cf42e3e8643a3acc066424982bc1d83f65
Guille <cochraneguillermo@gmail.com>
2026-04-08 06:16:07 -0300

    Avengers: United They Stand (1999) (S1) auditada y corregida

--- assets/data/mcu/animacion90s/av99S1.js
+++ assets/data/mcu/animacion90s/av99S1.js
@@ -1,9 +1,23 @@
-import { av99S1E1 } from "./av99S1/episodes.js";
+import { av99S1E1, av99S1E2, av99S1E3, av99S1E4, av99S1E5, av99S1E6, av99S1E7, av99S1E8, av99S1E9, av99S1E10, av99S1E11, av99S1E12, av99S1E13 } from "./av99S1/episodes.js";
 
 export const av99S1 = {
   id: "av99S1",
   label: "Avengers: United They Stand (1999) (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 1999,
+    month: 10,
+    day: 30
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    av99S1E1
+    av99S1E1, av99S1E2, av99S1E3, av99S1E4, av99S1E5, av99S1E6, av99S1E7,
+    av99S1E8, av99S1E9, av99S1E10, av99S1E11, av99S1E12, av99S1E13
   ],
 };
--- assets/data/mcu/animacion90s/av99S1/episodes.js
+++ assets/data/mcu/animacion90s/av99S1/episodes.js
@@ -1,7 +1,129 @@
 export const av99S1E1 = {
   id: "av99S1E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Origin of the Avengers"},
+    en: "The Origin of the Avengers",
+    es: "El origen de los Vengadores"
   },
 };
 
+export const av99S1E2 = {
+  id: "av99S1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Avengers Assemble",
+    es: "Los Vengadores se reúnen"
+  },
+};
+
+export const av99S1E3 = {
+  id: "av99S1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Kang",
+    es: "Kang"
+  },
+};
+
+export const av99S1E4 = {
+  id: "av99S1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Ultron Threat",
+    es: "La amenaza de Ultrón"
+  },
+};
+
+export const av99S1E5 = {
+  id: "av99S1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Masters of Evil",
+    es: "Los Maestros del Mal"
+  },
+};
+
+export const av99S1E6 = {
+  id: "av99S1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Return of the Sub-Mariner",
+    es: "El regreso del Sub-Mariner"
+  },
+};
+
+export const av99S1E7 = {
+  id: "av99S1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The Fantastic Four",
+    es: "Los 4 Fantásticos"
+  },
+};
+
+export const av99S1E8 = {
+  id: "av99S1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The X-Men",
+    es: "Los X-Men"
+  },
+};
+
+export const av99S1E9 = {
+  id: "av99S1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The Spider-Man",
+    es: "Spider-Man"
+  },
+};
+
+export const av99S1E10 = {
+  id: "av99S1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Hulk",
+    es: "Hulk"
+  },
+};
+
+export const av99S1E11 = {
+  id: "av99S1E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Silver Surfer",
+    es: "Silver Surfer"
+  },
+};
+
+export const av99S1E12 = {
+  id: "av99S1E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Doctor Strange",
+    es: "Doctor Strange"
+  },
+};
+
+export const av99S1E13 = {
+  id: "av99S1E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "The Black Panther",
+    es: "Black Panther"
+  },
+};
