8fb1cb930608195eb87dce6d7a5e1803e1bba440
Guille <cochraneguillermo@gmail.com>
2026-04-08 06:13:24 -0300

    Silver Surfer (1998) (S1) auditada y corregida

--- assets/data/mcu/animacion90s/ss98S1.js
+++ assets/data/mcu/animacion90s/ss98S1.js
@@ -1,9 +1,23 @@
-import { ss98S1E1 } from "./ss98S1/episodes.js";
+import { ss98S1E1, ss98S1E2, ss98S1E3, ss98S1E4, ss98S1E5, ss98S1E6, ss98S1E7, ss98S1E8, ss98S1E9, ss98S1E10, ss98S1E11, ss98S1E12, ss98S1E13 } from "./ss98S1/episodes.js";
 
 export const ss98S1 = {
   id: "ss98S1",
   label: "Silver Surfer (1998) (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 1998,
+    month: 11,
+    day: 7
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    ss98S1E1
+    ss98S1E1, ss98S1E2, ss98S1E3, ss98S1E4, ss98S1E5, ss98S1E6, ss98S1E7,
+    ss98S1E8, ss98S1E9, ss98S1E10, ss98S1E11, ss98S1E12, ss98S1E13
   ],
 };
--- assets/data/mcu/animacion90s/ss98S1/episodes.js
+++ assets/data/mcu/animacion90s/ss98S1/episodes.js
@@ -1,7 +1,129 @@
 export const ss98S1E1 = {
   id: "ss98S1E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Origin of the Silver Surfer"},
+    en: "The Origin of the Silver Surfer (Part 1)",
+    es: "El origen de Silver Surfer (Parte 1)"
   },
 };
 
+export const ss98S1E2 = {
+  id: "ss98S1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Origin of the Silver Surfer (Part 2)",
+    es: "El origen de Silver Surfer (Parte 2)"
+  },
+};
+
+export const ss98S1E3 = {
+  id: "ss98S1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Origin of the Silver Surfer (Part 3)",
+    es: "El origen de Silver Surfer (Parte 3)"
+  },
+};
+
+export const ss98S1E4 = {
+  id: "ss98S1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Second Coming",
+    es: "Segunda venida"
+  },
+};
+
+export const ss98S1E5 = {
+  id: "ss98S1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Planet of Dr. Moreau",
+    es: "El planeta del Dr. Moreau"
+  },
+};
+
+export const ss98S1E6 = {
+  id: "ss98S1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Learning Curve (Part 1)",
+    es: "Curva de aprendizaje (Parte 1)"
+  },
+};
+
+export const ss98S1E7 = {
+  id: "ss98S1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Learning Curve (Part 2)",
+    es: "Curva de aprendizaje (Parte 2)"
+  },
+};
+
+export const ss98S1E8 = {
+  id: "ss98S1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Innocent",
+    es: "Inocente"
+  },
+};
+
+export const ss98S1E9 = {
+  id: "ss98S1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Antibody",
+    es: "Anticuerpo"
+  },
+};
+
+export const ss98S1E10 = {
+  id: "ss98S1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The End of Eternity",
+    es: "El fin de la eternidad"
+  },
+};
+
+export const ss98S1E11 = {
+  id: "ss98S1E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Return",
+    es: "El regreso"
+  },
+};
+
+export const ss98S1E12 = {
+  id: "ss98S1E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Awakening",
+    es: "El despertar"
+  },
+};
+
+export const ss98S1E13 = {
+  id: "ss98S1E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "Rebirth",
+    es: "Renacimiento"
+  },
+};
