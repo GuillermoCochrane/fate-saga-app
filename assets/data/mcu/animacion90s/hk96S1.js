775dfe76265c374882bba59364954e85c5a35f71
Guille <cochraneguillermo@gmail.com>
2026-04-08 06:11:21 -0300

    The Incredible Hulk (1996) (S1) auditada y corregida

--- assets/data/mcu/animacion90s/hk96S1.js
+++ assets/data/mcu/animacion90s/hk96S1.js
@@ -1,9 +1,23 @@
-import { hk96S1E1 } from "./hk96S1/episodes.js";
+import { hk96S1E1, hk96S1E2, hk96S1E3, hk96S1E4, hk96S1E5, hk96S1E6, hk96S1E7, hk96S1E8, hk96S1E9, hk96S1E10, hk96S1E11, hk96S1E12, hk96S1E13 } from "./hk96S1/episodes.js";
 
 export const hk96S1 = {
   id: "hk96S1",
   label: "The Incredible Hulk (1996) (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 1996,
+    month: 9,
+    day: 8
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    hk96S1E1
+    hk96S1E1, hk96S1E2, hk96S1E3, hk96S1E4, hk96S1E5, hk96S1E6, hk96S1E7,
+    hk96S1E8, hk96S1E9, hk96S1E10, hk96S1E11, hk96S1E12, hk96S1E13
   ],
 };
--- assets/data/mcu/animacion90s/hk96S1/episodes.js
+++ assets/data/mcu/animacion90s/hk96S1/episodes.js
@@ -1,7 +1,129 @@
 export const hk96S1E1 = {
   id: "hk96S1E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Return of the Beast"},
+    en: "Return of the Beast (Part 1)",
+    es: "El regreso de la bestia (Parte 1)"
   },
 };
 
+export const hk96S1E2 = {
+  id: "hk96S1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Return of the Beast (Part 2)",
+    es: "El regreso de la bestia (Parte 2)"
+  },
+};
+
+export const hk96S1E3 = {
+  id: "hk96S1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Where Monsters Dwell",
+    es: "Donde habitan los monstruos"
+  },
+};
+
+export const hk96S1E4 = {
+  id: "hk96S1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Innocent Blood",
+    es: "Sangre inocente"
+  },
+};
+
+export const hk96S1E5 = {
+  id: "hk96S1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Mortal Bounds",
+    es: "Lazos mortales"
+  },
+};
+
+export const hk96S1E6 = {
+  id: "hk96S1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "And the Wind Cries... The Hulk",
+    es: "Y el viento llora... Hulk"
+  },
+};
+
+export const hk96S1E7 = {
+  id: "hk96S1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Darkness and Light (Part 1)",
+    es: "Oscuridad y luz (Parte 1)"
+  },
+};
+
+export const hk96S1E8 = {
+  id: "hk96S1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Darkness and Light (Part 2)",
+    es: "Oscuridad y luz (Parte 2)"
+  },
+};
+
+export const hk96S1E9 = {
+  id: "hk96S1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Darkness and Light (Part 3)",
+    es: "Oscuridad y luz (Parte 3)"
+  },
+};
+
+export const hk96S1E10 = {
+  id: "hk96S1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "Fantastic Fortitude",
+    es: "Fortaleza fantástica"
+  },
+};
+
+export const hk96S1E11 = {
+  id: "hk96S1E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "Man to Man, Beast to Beast",
+    es: "Hombre a hombre, bestia a bestia"
+  },
+};
+
+export const hk96S1E12 = {
+  id: "hk96S1E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "Doomed",
+    es: "Condenado"
+  },
+};
+
+export const hk96S1E13 = {
+  id: "hk96S1E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "Mind Over Anti-Matter",
+    es: "Mente sobre antimateria"
+  },
+};
