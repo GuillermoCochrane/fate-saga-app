4c92fae0cf3581bd704d1eab0cd455e522ed7a5a
Guille <cochraneguillermo@gmail.com>
2026-04-08 06:14:38 -0300

    Spider-Man Unlimited (1999) (S1) auditada y corregida

--- assets/data/mcu/animacion90s/sp99S1.js
+++ assets/data/mcu/animacion90s/sp99S1.js
@@ -1,9 +1,23 @@
-import { sp99S1E1 } from "./sp99S1/episodes.js";
+import { sp99S1E1, sp99S1E2, sp99S1E3, sp99S1E4, sp99S1E5, sp99S1E6, sp99S1E7, sp99S1E8, sp99S1E9, sp99S1E10, sp99S1E11, sp99S1E12, sp99S1E13 } from "./sp99S1/episodes.js";
 
 export const sp99S1 = {
   id: "sp99S1",
   label: "Spider-Man Unlimited (1999) (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 1999,
+    month: 10,
+    day: 2
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    sp99S1E1
+    sp99S1E1, sp99S1E2, sp99S1E3, sp99S1E4, sp99S1E5, sp99S1E6, sp99S1E7,
+    sp99S1E8, sp99S1E9, sp99S1E10, sp99S1E11, sp99S1E12, sp99S1E13
   ],
 };
--- assets/data/mcu/animacion90s/sp99S1/episodes.js
+++ assets/data/mcu/animacion90s/sp99S1/episodes.js
@@ -1,7 +1,129 @@
 export const sp99S1E1 = {
   id: "sp99S1E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Worlds Apart"},
+    en: "Worlds Apart (Part 1)",
+    es: "Mundos aparte (Parte 1)"
   },
 };
 
+export const sp99S1E2 = {
+  id: "sp99S1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Worlds Apart (Part 2)",
+    es: "Mundos aparte (Parte 2)"
+  },
+};
+
+export const sp99S1E3 = {
+  id: "sp99S1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Where Evil Nests",
+    es: "Donde anida el mal"
+  },
+};
+
+export const sp99S1E4 = {
+  id: "sp99S1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Deadly Choices",
+    es: "Decisiones mortales"
+  },
+};
+
+export const sp99S1E5 = {
+  id: "sp99S1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Lost Kingdom",
+    es: "El reino perdido"
+  },
+};
+
+export const sp99S1E6 = {
+  id: "sp99S1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Venom's Revenge",
+    es: "La venganza de Venom"
+  },
+};
+
+export const sp99S1E7 = {
+  id: "sp99S1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Destiny Unleashed",
+    es: "El destino desatado"
+  },
+};
+
+export const sp99S1E8 = {
+  id: "sp99S1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Sinister Six",
+    es: "Los Seis Siniestros"
+  },
+};
+
+export const sp99S1E9 = {
+  id: "sp99S1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Morbius",
+    es: "Morbius"
+  },
+};
+
+export const sp99S1E10 = {
+  id: "sp99S1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Return of the Green Goblin",
+    es: "El regreso del Duende Verde"
+  },
+};
+
+export const sp99S1E11 = {
+  id: "sp99S1E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The End of Spider-Man? (Part 1)",
+    es: "¿El fin de Spider-Man? (Parte 1)"
+  },
+};
+
+export const sp99S1E12 = {
+  id: "sp99S1E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The End of Spider-Man? (Part 2)",
+    es: "¿El fin de Spider-Man? (Parte 2)"
+  },
+};
+
+export const sp99S1E13 = {
+  id: "sp99S1E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "The End of Spider-Man? (Part 3)",
+    es: "¿El fin de Spider-Man? (Parte 3)"
+  },
+};
