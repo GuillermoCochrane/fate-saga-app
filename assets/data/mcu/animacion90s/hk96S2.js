0469837d5c7965cc9bf79464aae91fa2a7bbc3a6
Guille <cochraneguillermo@gmail.com>
2026-04-08 06:12:16 -0300

    The Incredible Hulk (1996) (S2) auditada y corregida

--- assets/data/mcu/animacion90s/hk96S2.js
+++ assets/data/mcu/animacion90s/hk96S2.js
@@ -1,9 +1,22 @@
-import { hk96S2E1 } from "./hk96S2/episodes.js";
+import { hk96S2E1, hk96S2E2, hk96S2E3, hk96S2E4, hk96S2E5, hk96S2E6, hk96S2E7, hk96S2E8 } from "./hk96S2/episodes.js";
 
 export const hk96S2 = {
   id: "hk96S2",
   label: "The Incredible Hulk (1996) (S2)",
+  format: "Serie",
+  season: 2,
+  release: {
+    year: 1997,
+    month: 9,
+    day: 7
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    hk96S2E1
+    hk96S2E1, hk96S2E2, hk96S2E3, hk96S2E4, hk96S2E5, hk96S2E6, hk96S2E7, hk96S2E8
   ],
 };
--- assets/data/mcu/animacion90s/hk96S2/episodes.js
+++ assets/data/mcu/animacion90s/hk96S2/episodes.js
@@ -1,7 +1,79 @@
 export const hk96S2E1 = {
   id: "hk96S2E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Darkness and Light"},
+    en: "Darkness and Light (Part 4)",
+    es: "Oscuridad y luz (Parte 4)"
   },
 };
 
+export const hk96S2E2 = {
+  id: "hk96S2E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Darkness and Light (Part 5)",
+    es: "Oscuridad y luz (Parte 5)"
+  },
+};
+
+export const hk96S2E3 = {
+  id: "hk96S2E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Hollywood Hulk",
+    es: "Hulk Hollywood"
+  },
+};
+
+export const hk96S2E4 = {
+  id: "hk96S2E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Lost Village",
+    es: "El pueblo perdido"
+  },
+};
+
+export const hk96S2E5 = {
+  id: "hk96S2E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Mortal Bounds (Part 2)",
+    es: "Lazos mortales (Parte 2)"
+  },
+};
+
+export const hk96S2E6 = {
+  id: "hk96S2E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Innocent Blood (Part 2)",
+    es: "Sangre inocente (Parte 2)"
+  },
+};
+
+export const hk96S2E7 = {
+  id: "hk96S2E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Masters of Mind",
+    es: "Maestros de la mente"
+  },
+};
+
+export const hk96S2E8 = {
+  id: "hk96S2E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Hulk vs. The Hulk",
+    es: "Hulk contra Hulk"
+  },
+};
