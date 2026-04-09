12033520a2f80bd695ac7b100869fa29ed09df82
Guille <cochraneguillermo@gmail.com>
2026-04-09 02:30:24 -0300

    X-Men: Evolution (2000) (S3) auditada y corregida

--- assets/data/mcu/animacion2000s/xme00S3.js
+++ assets/data/mcu/animacion2000s/xme00S3.js
@@ -1,9 +1,23 @@
-import { xme00S3E1 } from "./xme00S3/episodes.js";
+import { xme00S3E1, xme00S3E2, xme00S3E3, xme00S3E4, xme00S3E5, xme00S3E6, xme00S3E7, xme00S3E8, xme00S3E9, xme00S3E10, xme00S3E11, xme00S3E12, xme00S3E13 } from "./xme00S3/episodes.js";
 
 export const xme00S3 = {
   id: "xme00S3",
   label: "X-Men: Evolution (2000) (S3)",
+  format: "Serie",
+  season: 3,
+  release: {
+    year: 2002,
+    month: 9,
+    day: 28
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    xme00S3E1
+    xme00S3E1, xme00S3E2, xme00S3E3, xme00S3E4, xme00S3E5, xme00S3E6, xme00S3E7,
+    xme00S3E8, xme00S3E9, xme00S3E10, xme00S3E11, xme00S3E12, xme00S3E13
   ],
 };
--- assets/data/mcu/animacion2000s/xme00S3/episodes.js
+++ assets/data/mcu/animacion2000s/xme00S3/episodes.js
@@ -1,7 +1,129 @@
 export const xme00S3E1 = {
   id: "xme00S3E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Day of Reckoning"},
+    en: "Day of Reckoning (Part 1)",
+    es: "Día de ajuste de cuentas (Parte 1)"
   },
 };
 
+export const xme00S3E2 = {
+  id: "xme00S3E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Day of Reckoning (Part 2)",
+    es: "Día de ajuste de cuentas (Parte 2)"
+  },
+};
+
+export const xme00S3E3 = {
+  id: "xme00S3E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Day of Reckoning (Part 3)",
+    es: "Día de ajuste de cuentas (Parte 3)"
+  },
+};
+
+export const xme00S3E4 = {
+  id: "xme00S3E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Stuff of Heroes",
+    es: "La esencia de los héroes"
+  },
+};
+
+export const xme00S3E5 = {
+  id: "xme00S3E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Mainstream",
+    es: "Corriente principal"
+  },
+};
+
+export const xme00S3E6 = {
+  id: "xme00S3E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Cauldron (Part 1)",
+    es: "El caldero (Parte 1)"
+  },
+};
+
+export const xme00S3E7 = {
+  id: "xme00S3E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The Cauldron (Part 2)",
+    es: "El caldero (Parte 2)"
+  },
+};
+
+export const xme00S3E8 = {
+  id: "xme00S3E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Growing Pains (Part 2)",
+    es: "Problemas de crecimiento (Parte 2)"
+  },
+};
+
+export const xme00S3E9 = {
+  id: "xme00S3E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Power Surge (Part 2)",
+    es: "Sobrecarga de poder (Parte 2)"
+  },
+};
+
+export const xme00S3E10 = {
+  id: "xme00S3E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "Bada-Bing Bada-Boom (Part 2)",
+    es: "Bada-Bing Bada-Boom (Parte 2)"
+  },
+};
+
+export const xme00S3E11 = {
+  id: "xme00S3E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "African Storm (Part 2)",
+    es: "Tormenta africana (Parte 2)"
+  },
+};
+
+export const xme00S3E12 = {
+  id: "xme00S3E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "Joyride (Part 2)",
+    es: "Paseo alegre (Parte 2)"
+  },
+};
+
+export const xme00S3E13 = {
+  id: "xme00S3E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "Walk on the Wild Side (Part 2)",
+    es: "Un paseo por el lado salvaje (Parte 2)"
+  },
+};
