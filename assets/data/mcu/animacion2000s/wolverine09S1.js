1d2e1c72ea80c0d84bc3a1c0b215829f00c000ab
Guille <cochraneguillermo@gmail.com>
2026-04-09 02:37:07 -0300

    Wolverine and the X-Men (2009) (S1) auditada y corregida

--- assets/data/mcu/animacion2000s/wolverine09S1.js
+++ assets/data/mcu/animacion2000s/wolverine09S1.js
@@ -1,9 +1,25 @@
-import { wolverine09S1E1 } from "./wolverine09S1/episodes.js";
+import { wol09S1E1, wol09S1E2, wol09S1E3, wol09S1E4, wol09S1E5, wol09S1E6, wol09S1E7, wol09S1E8, wol09S1E9, wol09S1E10, wol09S1E11, wol09S1E12, wol09S1E13, wol09S1E14, wol09S1E15, wol09S1E16, wol09S1E17, wol09S1E18, wol09S1E19, wol09S1E20, wol09S1E21, wol09S1E22, wol09S1E23, wol09S1E24, wol09S1E25, wol09S1E26 } from "./wolverine09S1/episodes.js";
 
 export const wolverine09S1 = {
   id: "wolverine09S1",
   label: "Wolverine and the X-Men (2009) (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 2009,
+    month: 1,
+    day: 23
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    wolverine09S1E1
+    wol09S1E1, wol09S1E2, wol09S1E3, wol09S1E4, wol09S1E5, wol09S1E6, wol09S1E7, wol09S1E8,
+    wol09S1E9, wol09S1E10, wol09S1E11, wol09S1E12, wol09S1E13, wol09S1E14, wol09S1E15, wol09S1E16,
+    wol09S1E17, wol09S1E18, wol09S1E19, wol09S1E20, wol09S1E21, wol09S1E22, wol09S1E23, wol09S1E24,
+    wol09S1E25, wol09S1E26
   ],
 };
--- assets/data/mcu/animacion2000s/wolverine09S1/episodes.js
+++ assets/data/mcu/animacion2000s/wolverine09S1/episodes.js
@@ -1,7 +1,259 @@
-export const wolverine09S1E1 = {
-  id: "wolverine09S1E1",
+export const wol09S1E1 = {
+  id: "wol09S1E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Hindsight (Part 1)"},
+    en: "Hindsight (Part 1)",
+    es: "Retrospectiva (Parte 1)"
   },
 };
 
+export const wol09S1E2 = {
+  id: "wol09S1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Hindsight (Part 2)",
+    es: "Retrospectiva (Parte 2)"
+  },
+};
+
+export const wol09S1E3 = {
+  id: "wol09S1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Hindsight (Part 3)",
+    es: "Retrospectiva (Parte 3)"
+  },
+};
+
+export const wol09S1E4 = {
+  id: "wol09S1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Overflow",
+    es: "Desbordamiento"
+  },
+};
+
+export const wol09S1E5 = {
+  id: "wol09S1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Thieves' Gambit",
+    es: "Apostando por los ladrones"
+  },
+};
+
+export const wol09S1E6 = {
+  id: "wol09S1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "X-Calibre",
+    es: "X-Calibre"
+  },
+};
+
+export const wol09S1E7 = {
+  id: "wol09S1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Wolverine vs. the Hulk",
+    es: "Wolverine contra Hulk"
+  },
+};
+
+export const wol09S1E8 = {
+  id: "wol09S1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Time Bomb",
+    es: "Bomba de tiempo"
+  },
+};
+
+export const wol09S1E9 = {
+  id: "wol09S1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Future X",
+    es: "X del futuro"
+  },
+};
+
+export const wol09S1E10 = {
+  id: "wol09S1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "Greetings from Genosha",
+    es: "Saludos desde Genosha"
+  },
+};
+
+export const wol09S1E11 = {
+  id: "wol09S1E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "Past Discretions",
+    es: "Discreciones del pasado"
+  },
+};
+
+export const wol09S1E12 = {
+  id: "wol09S1E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "eXcessive Force",
+    es: "Fuerza eXcesiva"
+  },
+};
+
+export const wol09S1E13 = {
+  id: "wol09S1E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "Battle of the Atom",
+    es: "Batalla del átomo"
+  },
+};
+
+export const wol09S1E14 = {
+  id: "wol09S1E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "Stolen Lives",
+    es: "Vidas robadas"
+  },
+};
+
+export const wol09S1E15 = {
+  id: "wol09S1E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "Hunting Grounds",
+    es: "Terrenos de caza"
+  },
+};
+
+export const wol09S1E16 = {
+  id: "wol09S1E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "Bad Blood",
+    es: "Mala sangre"
+  },
+};
+
+export const wol09S1E17 = {
+  id: "wol09S1E17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "Code of Conduct",
+    es: "Código de conducta"
+  },
+};
+
+export const wol09S1E18 = {
+  id: "wol09S1E18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "Backlash",
+    es: "Reacción violenta"
+  },
+};
+
+export const wol09S1E19 = {
+  id: "wol09S1E19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "Guardian Angel",
+    es: "Ángel guardián"
+  },
+};
+
+export const wol09S1E20 = {
+  id: "wol09S1E20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "Breakdown",
+    es: "Avería"
+  },
+};
+
+export const wol09S1E21 = {
+  id: "wol09S1E21",
+  number: 21,
+  completed: false,
+  label: {
+    en: "Rover",
+    es: "Rover"
+  },
+};
+
+export const wol09S1E22 = {
+  id: "wol09S1E22",
+  number: 22,
+  completed: false,
+  label: {
+    en: "Aces and Eights",
+    es: "Ases y ochos"
+  },
+};
+
+export const wol09S1E23 = {
+  id: "wol09S1E23",
+  number: 23,
+  completed: false,
+  label: {
+    en: "Shadows of the Past",
+    es: "Sombras del pasado"
+  },
+};
+
+export const wol09S1E24 = {
+  id: "wol09S1E24",
+  number: 24,
+  completed: false,
+  label: {
+    en: "Foresight (Part 1)",
+    es: "Previsión (Parte 1)"
+  },
+};
+
+export const wol09S1E25 = {
+  id: "wol09S1E25",
+  number: 25,
+  completed: false,
+  label: {
+    en: "Foresight (Part 2)",
+    es: "Previsión (Parte 2)"
+  },
+};
+
+export const wol09S1E26 = {
+  id: "wol09S1E26",
+  number: 26,
+  completed: false,
+  label: {
+    en: "Foresight (Part 3)",
+    es: "Previsión (Parte 3)"
+  },
+};
