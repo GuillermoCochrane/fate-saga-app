cb734825b7800e0542073cd607a85abc1fd62b6c
Guille <cochraneguillermo@gmail.com>
2026-04-09 02:31:31 -0300

    X-Men: Evolution (2000) (S4) auditada y corregida

--- assets/data/mcu/animacion2000s/xme00S4.js
+++ assets/data/mcu/animacion2000s/xme00S4.js
@@ -1,9 +1,22 @@
-import { xme00S4E1 } from "./xme00S4/episodes.js";
+import { xme00S4E1, xme00S4E2, xme00S4E3, xme00S4E4, xme00S4E5, xme00S4E6, xme00S4E7, xme00S4E8, xme00S4E9 } from "./xme00S4/episodes.js";
 
 export const xme00S4 = {
   id: "xme00S4",
   label: "X-Men: Evolution (2000) (S4)",
+  format: "Serie",
+  season: 4,
+  release: {
+    year: 2003,
+    month: 8,
+    day: 30
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    xme00S4E1
+    xme00S4E1, xme00S4E2, xme00S4E3, xme00S4E4, xme00S4E5, xme00S4E6, xme00S4E7, xme00S4E8, xme00S4E9
   ],
 };
--- assets/data/mcu/animacion2000s/xme00S4/episodes.js
+++ assets/data/mcu/animacion2000s/xme00S4/episodes.js
@@ -1,7 +1,99 @@
 export const xme00S4E1 = {
   id: "xme00S4E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Impact"},
+    en: "Impact (Part 1)",
+    es: "Impacto (Parte 1)"
   },
 };
 
+export const xme00S4E2 = {
+  id: "xme00S4E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Impact (Part 2)",
+    es: "Impacto (Parte 2)"
+  },
+};
+
+export const xme00S4E3 = {
+  id: "xme00S4E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Impact (Part 3)",
+    es: "Impacto (Parte 3)"
+  },
+};
+
+export const xme00S4E4 = {
+  id: "xme00S4E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "No Good Deed",
+    es: "Ninguna buena acción"
+  },
+};
+
+export const xme00S4E5 = {
+  id: "xme00S4E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Target X",
+    es: "Objetivo X"
+  },
+};
+
+export const xme00S4E6 = {
+  id: "xme00S4E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Sins of the Son",
+    es: "Pecados del hijo"
+  },
+};
+
+export const xme00S4E7 = {
+  id: "xme00S4E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Uprising",
+    es: "Levantamiento"
+  },
+};
+
+export const xme00S4E8 = {
+  id: "xme00S4E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Crusade",
+    es: "Cruzada"
+  },
+};
+
+export const xme00S4E9 = {
+  id: "xme00S4E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Ascension (Part 1)",
+    es: "Ascensión (Parte 1)"
+  },
+};
+
+export const xme00S4E10 = {
+  id: "xme00S4E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "Ascension (Part 2)",
+    es: "Ascensión (Parte 2)"
+  },
+};
