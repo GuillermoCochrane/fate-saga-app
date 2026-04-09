ae1671365edaa58dca4322f3a42c9fb855365b7a
Guille <cochraneguillermo@gmail.com>
2026-04-09 02:28:46 -0300

    X-Men: Evolution (2000) (S2) auditada y corregida

--- assets/data/mcu/animacion2000s/xme00S2.js
+++ assets/data/mcu/animacion2000s/xme00S2.js
@@ -1,9 +1,23 @@
-import { xme00S2E1 } from "./xme00S2/episodes.js";
+import { xme00S2E1, xme00S2E2, xme00S2E3, xme00S2E4, xme00S2E5, xme00S2E6, xme00S2E7, xme00S2E8, xme00S2E9, xme00S2E10, xme00S2E11, xme00S2E12, xme00S2E13, xme00S2E14, xme00S2E15, xme00S2E16, xme00S2E17 } from "./xme00S2/episodes.js";
 
 export const xme00S2 = {
   id: "xme00S2",
   label: "X-Men: Evolution (2000) (S2)",
+  format: "Serie",
+  season: 2,
+  release: {
+    year: 2001,
+    month: 9,
+    day: 29
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    xme00S2E1
+    xme00S2E1, xme00S2E2, xme00S2E3, xme00S2E4, xme00S2E5, xme00S2E6, xme00S2E7, xme00S2E8,
+    xme00S2E9, xme00S2E10, xme00S2E11, xme00S2E12, xme00S2E13, xme00S2E14, xme00S2E15, xme00S2E16, xme00S2E17
   ],
 };
--- assets/data/mcu/animacion2000s/xme00S2/episodes.js
+++ assets/data/mcu/animacion2000s/xme00S2/episodes.js
@@ -1,7 +1,169 @@
 export const xme00S2E1 = {
   id: "xme00S2E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Growing Pains"},
+    en: "Growing Pains",
+    es: "Problemas de crecimiento"
   },
 };
 
+export const xme00S2E2 = {
+  id: "xme00S2E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Power Surge",
+    es: "Sobrecarga de poder"
+  },
+};
+
+export const xme00S2E3 = {
+  id: "xme00S2E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Bada-Bing Bada-Boom",
+    es: "Bada-Bing Bada-Boom"
+  },
+};
+
+export const xme00S2E4 = {
+  id: "xme00S2E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Fun and Games",
+    es: "Diversión y juegos"
+  },
+};
+
+export const xme00S2E5 = {
+  id: "xme00S2E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Beast of Bayville",
+    es: "La bestia de Bayville"
+  },
+};
+
+export const xme00S2E6 = {
+  id: "xme00S2E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Adrift",
+    es: "A la deriva"
+  },
+};
+
+export const xme00S2E7 = {
+  id: "xme00S2E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "On Angel's Wings",
+    es: "En las alas de Ángel"
+  },
+};
+
+export const xme00S2E8 = {
+  id: "xme00S2E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "African Storm",
+    es: "Tormenta africana"
+  },
+};
+
+export const xme00S2E9 = {
+  id: "xme00S2E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Joyride",
+    es: "Paseo alegre"
+  },
+};
+
+export const xme00S2E10 = {
+  id: "xme00S2E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "Walk on the Wild Side",
+    es: "Un paseo por el lado salvaje"
+  },
+};
+
+export const xme00S2E11 = {
+  id: "xme00S2E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Stuff of Heroes",
+    es: "La esencia de los héroes"
+  },
+};
+
+export const xme00S2E12 = {
+  id: "xme00S2E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "Mainstream",
+    es: "Corriente principal"
+  },
+};
+
+export const xme00S2E13 = {
+  id: "xme00S2E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "The Cauldron (Part 1)",
+    es: "El caldero (Parte 1)"
+  },
+};
+
+export const xme00S2E14 = {
+  id: "xme00S2E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "The Cauldron (Part 2)",
+    es: "El caldero (Parte 2)"
+  },
+};
+
+export const xme00S2E15 = {
+  id: "xme00S2E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "Growing Pains (Part 2)",
+    es: "Problemas de crecimiento (Parte 2)"
+  },
+};
+
+export const xme00S2E16 = {
+  id: "xme00S2E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "Power Surge (Part 2)",
+    es: "Sobrecarga de poder (Parte 2)"
+  },
+};
+
+export const xme00S2E17 = {
+  id: "xme00S2E17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "Bada-Bing Bada-Boom (Part 2)",
+    es: "Bada-Bing Bada-Boom (Parte 2)"
+  },
+};
