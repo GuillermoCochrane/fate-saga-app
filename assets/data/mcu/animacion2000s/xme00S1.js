b5771adb19883ed7ff3f6706237a6dbc1b8b6cb9
Guille <cochraneguillermo@gmail.com>
2026-04-09 02:27:19 -0300

    X-Men: Evolution (2000) (S1) auditada y corregida

--- assets/data/mcu/animacion2000s/xme00S1.js
+++ assets/data/mcu/animacion2000s/xme00S1.js
@@ -1,9 +1,23 @@
-import { xme00S1E1 } from "./xme00S1/episodes.js";
+import { xme00S1E1, xme00S1E2, xme00S1E3, xme00S1E4, xme00S1E5, xme00S1E6, xme00S1E7, xme00S1E8, xme00S1E9, xme00S1E10, xme00S1E11, xme00S1E12, xme00S1E13 } from "./xme00S1/episodes.js";
 
 export const xme00S1 = {
   id: "xme00S1",
   label: "X-Men: Evolution (2000) (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 2000,
+    month: 11,
+    day: 4
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    xme00S1E1
+    xme00S1E1, xme00S1E2, xme00S1E3, xme00S1E4, xme00S1E5, xme00S1E6, xme00S1E7,
+    xme00S1E8, xme00S1E9, xme00S1E10, xme00S1E11, xme00S1E12, xme00S1E13
   ],
 };
--- assets/data/mcu/animacion2000s/xme00S1/episodes.js
+++ assets/data/mcu/animacion2000s/xme00S1/episodes.js
@@ -1,7 +1,129 @@
 export const xme00S1E1 = {
   id: "xme00S1E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Tomorrow People"},
+    en: "The Tomorrow People",
+    es: "La gente del mañana"
   },
 };
 
+export const xme00S1E2 = {
+  id: "xme00S1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Mutant Crush",
+    es: "Aplastamiento mutante"
+  },
+};
+
+export const xme00S1E3 = {
+  id: "xme00S1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Rogue Recruit",
+    es: "Recluta Rogue"
+  },
+};
+
+export const xme00S1E4 = {
+  id: "xme00S1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Strategy X",
+    es: "Estrategia X"
+  },
+};
+
+export const xme00S1E5 = {
+  id: "xme00S1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The X-Impulse",
+    es: "El impulso X"
+  },
+};
+
+export const xme00S1E6 = {
+  id: "xme00S1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Survival of the Fittest",
+    es: "Supervivencia del más fuerte"
+  },
+};
+
+export const xme00S1E7 = {
+  id: "xme00S1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Shadowed Past",
+    es: "Pasado sombrío"
+  },
+};
+
+export const xme00S1E8 = {
+  id: "xme00S1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Operation Rebirth",
+    es: "Operación renacimiento"
+  },
+};
+
+export const xme00S1E9 = {
+  id: "xme00S1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Mindbender",
+    es: "Perturbador de mentes"
+  },
+};
+
+export const xme00S1E10 = {
+  id: "xme00S1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "Walk on the Wild Side",
+    es: "Un paseo por el lado salvaje"
+  },
+};
+
+export const xme00S1E11 = {
+  id: "xme00S1E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Beast of Bayville",
+    es: "La bestia de Bayville"
+  },
+};
+
+export const xme00S1E12 = {
+  id: "xme00S1E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "Adrift",
+    es: "A la deriva"
+  },
+};
+
+export const xme00S1E13 = {
+  id: "xme00S1E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "On Angel's Wings",
+    es: "En las alas de Ángel"
+  },
+};
