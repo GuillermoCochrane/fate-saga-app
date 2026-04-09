f47439d701057bbfe43230cb7e44b8d8e89e043d
Guille <cochraneguillermo@gmail.com>
2026-04-09 02:33:03 -0300

    Fantastic Four: World's Greatest Heroes (2006) (S1) auditada y corregida

--- assets/data/mcu/animacion2000s/fwwgh06S1.js
+++ assets/data/mcu/animacion2000s/fwwgh06S1.js
@@ -1,9 +1,25 @@
-import { fwwgh06S1E1 } from "./fwwgh06S1/episodes.js";
+import { fwwgh06S1E1, fwwgh06S1E2, fwwgh06S1E3, fwwgh06S1E4, fwwgh06S1E5, fwwgh06S1E6, fwwgh06S1E7, fwwgh06S1E8, fwwgh06S1E9, fwwgh06S1E10, fwwgh06S1E11, fwwgh06S1E12, fwwgh06S1E13, fwwgh06S1E14, fwwgh06S1E15, fwwgh06S1E16, fwwgh06S1E17, fwwgh06S1E18, fwwgh06S1E19, fwwgh06S1E20, fwwgh06S1E21, fwwgh06S1E22, fwwgh06S1E23, fwwgh06S1E24, fwwgh06S1E25, fwwgh06S1E26 } from "./fwwgh06S1/episodes.js";
 
 export const fwwgh06S1 = {
   id: "fwwgh06S1",
   label: "Fantastic Four: World's Greatest Heroes (2006) (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 2006,
+    month: 9,
+    day: 2
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    fwwgh06S1E1
+    fwwgh06S1E1, fwwgh06S1E2, fwwgh06S1E3, fwwgh06S1E4, fwwgh06S1E5, fwwgh06S1E6, fwwgh06S1E7, fwwgh06S1E8,
+    fwwgh06S1E9, fwwgh06S1E10, fwwgh06S1E11, fwwgh06S1E12, fwwgh06S1E13, fwwgh06S1E14, fwwgh06S1E15, fwwgh06S1E16,
+    fwwgh06S1E17, fwwgh06S1E18, fwwgh06S1E19, fwwgh06S1E20, fwwgh06S1E21, fwwgh06S1E22, fwwgh06S1E23, fwwgh06S1E24,
+    fwwgh06S1E25, fwwgh06S1E26
   ],
 };
--- assets/data/mcu/animacion2000s/fwwgh06S1/episodes.js
+++ assets/data/mcu/animacion2000s/fwwgh06S1/episodes.js
@@ -1,7 +1,259 @@
 export const fwwgh06S1E1 = {
   id: "fwwgh06S1E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Doomsday"},
+    en: "Doomsday",
+    es: "Día del juicio"
   },
 };
 
+export const fwwgh06S1E2 = {
+  id: "fwwgh06S1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Trial by Fire",
+    es: "Prueba de fuego"
+  },
+};
+
+export const fwwgh06S1E3 = {
+  id: "fwwgh06S1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Zoned Out",
+    es: "Fuera de zona"
+  },
+};
+
+export const fwwgh06S1E4 = {
+  id: "fwwgh06S1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Cure",
+    es: "La cura"
+  },
+};
+
+export const fwwgh06S1E5 = {
+  id: "fwwgh06S1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Impossible Man",
+    es: "El hombre imposible"
+  },
+};
+
+export const fwwgh06S1E6 = {
+  id: "fwwgh06S1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Hard Knocks",
+    es: "Duros golpes"
+  },
+};
+
+export const fwwgh06S1E7 = {
+  id: "fwwgh06S1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "My Neighbor, the Super-Skrull",
+    es: "Mi vecino, el Super-Skrull"
+  },
+};
+
+export const fwwgh06S1E8 = {
+  id: "fwwgh06S1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The World's Greatest Hero",
+    es: "El héroe más grande del mundo"
+  },
+};
+
+export const fwwgh06S1E9 = {
+  id: "fwwgh06S1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Molehattan",
+    es: "Molehattan"
+  },
+};
+
+export const fwwgh06S1E10 = {
+  id: "fwwgh06S1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Last Time",
+    es: "La última vez"
+  },
+};
+
+export const fwwgh06S1E11 = {
+  id: "fwwgh06S1E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Mask of Doom",
+    es: "La máscara de Doom"
+  },
+};
+
+export const fwwgh06S1E12 = {
+  id: "fwwgh06S1E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Fantastic Four and the Sub-Mariner",
+    es: "Los 4 Fantásticos y el Sub-Mariner"
+  },
+};
+
+export const fwwgh06S1E13 = {
+  id: "fwwgh06S1E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "Strings",
+    es: "Hilos"
+  },
+};
+
+export const fwwgh06S1E14 = {
+  id: "fwwgh06S1E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "The Ant-Man",
+    es: "Ant-Man"
+  },
+};
+
+export const fwwgh06S1E15 = {
+  id: "fwwgh06S1E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "Out of Time",
+    es: "Fuera de tiempo"
+  },
+};
+
+export const fwwgh06S1E16 = {
+  id: "fwwgh06S1E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "The Atlantis Attack",
+    es: "El ataque de Atlantis"
+  },
+};
+
+export const fwwgh06S1E17 = {
+  id: "fwwgh06S1E17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "The New Fantastic Four",
+    es: "Los nuevos 4 Fantásticos"
+  },
+};
+
+export const fwwgh06S1E18 = {
+  id: "fwwgh06S1E18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "The Power of the Super-Skrull",
+    es: "El poder del Super-Skrull"
+  },
+};
+
+export const fwwgh06S1E19 = {
+  id: "fwwgh06S1E19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "The Return of the Mole Man",
+    es: "El regreso del Hombre Topo"
+  },
+};
+
+export const fwwgh06S1E20 = {
+  id: "fwwgh06S1E20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "The Fantastic Four Meet the Hulk",
+    es: "Los 4 Fantásticos se encuentran con Hulk"
+  },
+};
+
+export const fwwgh06S1E21 = {
+  id: "fwwgh06S1E21",
+  number: 21,
+  completed: false,
+  label: {
+    en: "The Fantastic Four Meet Spider-Man",
+    es: "Los 4 Fantásticos se encuentran con Spider-Man"
+  },
+};
+
+export const fwwgh06S1E22 = {
+  id: "fwwgh06S1E22",
+  number: 22,
+  completed: false,
+  label: {
+    en: "The Fantastic Four Meet the X-Men",
+    es: "Los 4 Fantásticos se encuentran con los X-Men"
+  },
+};
+
+export const fwwgh06S1E23 = {
+  id: "fwwgh06S1E23",
+  number: 23,
+  completed: false,
+  label: {
+    en: "The Fantastic Four Meet Doctor Doom",
+    es: "Los 4 Fantásticos se encuentran con Doctor Doom"
+  },
+};
+
+export const fwwgh06S1E24 = {
+  id: "fwwgh06S1E24",
+  number: 24,
+  completed: false,
+  label: {
+    en: "The Fantastic Four Meet the Silver Surfer",
+    es: "Los 4 Fantásticos se encuentran con Silver Surfer"
+  },
+};
+
+export const fwwgh06S1E25 = {
+  id: "fwwgh06S1E25",
+  number: 25,
+  completed: false,
+  label: {
+    en: "The Fantastic Four Meet the Inhumans",
+    es: "Los 4 Fantásticos se encuentran con los Inhumanos"
+  },
+};
+
+export const fwwgh06S1E26 = {
+  id: "fwwgh06S1E26",
+  number: 26,
+  completed: false,
+  label: {
+    en: "The Fantastic Four Meet the Avengers",
+    es: "Los 4 Fantásticos se encuentran con los Vengadores"
+  },
+};
