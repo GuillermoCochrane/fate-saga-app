77175edb44728a99ca5f6b51d6bb88b9eed8f0cd
Guille <cochraneguillermo@gmail.com>
2026-04-10 19:44:33 -0300

    Ultimate Spider-Man (2012) (S1) auditada y corregida

--- assets/data/mcu/animacion2010s/usm12S1.js
+++ assets/data/mcu/animacion2010s/usm12S1.js
@@ -1,9 +1,25 @@
-import { usm12S1E1 } from "./usm12S1/episodes.js";
+import { usm12S1E1, usm12S1E2, usm12S1E3, usm12S1E4, usm12S1E5, usm12S1E6, usm12S1E7, usm12S1E8, usm12S1E9, usm12S1E10, usm12S1E11, usm12S1E12, usm12S1E13, usm12S1E14, usm12S1E15, usm12S1E16, usm12S1E17, usm12S1E18, usm12S1E19, usm12S1E20, usm12S1E21, usm12S1E22, usm12S1E23, usm12S1E24, usm12S1E25, usm12S1E26 } from "./usm12S1/episodes.js";
 
 export const usm12S1 = {
   id: "usm12S1",
   label: "Ultimate Spider-Man (2012) (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 2012,
+    month: 4,
+    day: 1
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    usm12S1E1
+    usm12S1E1, usm12S1E2, usm12S1E3, usm12S1E4, usm12S1E5, usm12S1E6, usm12S1E7, usm12S1E8,
+    usm12S1E9, usm12S1E10, usm12S1E11, usm12S1E12, usm12S1E13, usm12S1E14, usm12S1E15, usm12S1E16,
+    usm12S1E17, usm12S1E18, usm12S1E19, usm12S1E20, usm12S1E21, usm12S1E22, usm12S1E23, usm12S1E24,
+    usm12S1E25, usm12S1E26
   ],
 };
--- assets/data/mcu/animacion2010s/usm12S1/episodes.js
+++ assets/data/mcu/animacion2010s/usm12S1/episodes.js
@@ -1,7 +1,259 @@
 export const usm12S1E1 = {
   id: "usm12S1E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Great Power"},
+    en: "Great Power",
+    es: "Gran poder"
   },
 };
 
+export const usm12S1E2 = {
+  id: "usm12S1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Great Responsibility",
+    es: "Gran responsabilidad"
+  },
+};
+
+export const usm12S1E3 = {
+  id: "usm12S1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Doomed",
+    es: "Condenado"
+  },
+};
+
+export const usm12S1E4 = {
+  id: "usm12S1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Venom",
+    es: "Venom"
+  },
+};
+
+export const usm12S1E5 = {
+  id: "usm12S1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Flight of the Iron Spider",
+    es: "El vuelo de la Araña de Hierro"
+  },
+};
+
+export const usm12S1E6 = {
+  id: "usm12S1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Why I Hate Halloween",
+    es: "Por qué odio Halloween"
+  },
+};
+
+export const usm12S1E7 = {
+  id: "usm12S1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Parental Guidance",
+    es: "Guía parental"
+  },
+};
+
+export const usm12S1E8 = {
+  id: "usm12S1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "For Your Eye Only",
+    es: "Solo para tus ojos"
+  },
+};
+
+export const usm12S1E9 = {
+  id: "usm12S1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Freaky",
+    es: "Extraño"
+  },
+};
+
+export const usm12S1E10 = {
+  id: "usm12S1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Iron Octopus",
+    es: "El Pulpo de Hierro"
+  },
+};
+
+export const usm12S1E11 = {
+  id: "usm12S1E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "Not a Toy",
+    es: "No es un juguete"
+  },
+};
+
+export const usm12S1E12 = {
+  id: "usm12S1E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Attack of the Beetle",
+    es: "El ataque del Escarabajo"
+  },
+};
+
+export const usm12S1E13 = {
+  id: "usm12S1E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "Revealed",
+    es: "Revelado"
+  },
+};
+
+export const usm12S1E14 = {
+  id: "usm12S1E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "Rise of the Goblin",
+    es: "El ascenso del Duende"
+  },
+};
+
+export const usm12S1E15 = {
+  id: "usm12S1E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "The Hobgoblin",
+    es: "El Hobgoblin"
+  },
+};
+
+export const usm12S1E16 = {
+  id: "usm12S1E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "The Sinister Six",
+    es: "Los Seis Siniestros"
+  },
+};
+
+export const usm12S1E17 = {
+  id: "usm12S1E17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "The Incredible Spider-Hulk",
+    es: "El increíble Spider-Hulk"
+  },
+};
+
+export const usm12S1E18 = {
+  id: "usm12S1E18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "The Return of the Sinister Six",
+    es: "El regreso de los Seis Siniestros"
+  },
+};
+
+export const usm12S1E19 = {
+  id: "usm12S1E19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "The Amazing Spider-Man",
+    es: "El asombroso Spider-Man"
+  },
+};
+
+export const usm12S1E20 = {
+  id: "usm12S1E20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "The Savage Spider-Man",
+    es: "El Spider-Man salvaje"
+  },
+};
+
+export const usm12S1E21 = {
+  id: "usm12S1E21",
+  number: 21,
+  completed: false,
+  label: {
+    en: "The Spider-Verse (Part 1)",
+    es: "El Spider-Verso (Parte 1)"
+  },
+};
+
+export const usm12S1E22 = {
+  id: "usm12S1E22",
+  number: 22,
+  completed: false,
+  label: {
+    en: "The Spider-Verse (Part 2)",
+    es: "El Spider-Verso (Parte 2)"
+  },
+};
+
+export const usm12S1E23 = {
+  id: "usm12S1E23",
+  number: 23,
+  completed: false,
+  label: {
+    en: "The Spider-Verse (Part 3)",
+    es: "El Spider-Verso (Parte 3)"
+  },
+};
+
+export const usm12S1E24 = {
+  id: "usm12S1E24",
+  number: 24,
+  completed: false,
+  label: {
+    en: "The Spider-Verse (Part 4)",
+    es: "El Spider-Verso (Parte 4)"
+  },
+};
+
+export const usm12S1E25 = {
+  id: "usm12S1E25",
+  number: 25,
+  completed: false,
+  label: {
+    en: "The Return of the Spider-Verse (Part 1)",
+    es: "El regreso del Spider-Verso (Parte 1)"
+  },
+};
+
+export const usm12S1E26 = {
+  id: "usm12S1E26",
+  number: 26,
+  completed: false,
+  label: {
+    en: "The Return of the Spider-Verse (Part 2)",
+    es: "El regreso del Spider-Verso (Parte 2)"
+  },
+};
