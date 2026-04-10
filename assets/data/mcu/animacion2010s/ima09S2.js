be4faa9538b02e76f0ece6f1797dbad5597e23c1
Guille <cochraneguillermo@gmail.com>
2026-04-10 19:40:27 -0300

    Iron Man: Armored Adventures (2009) (S2) auditada y corregida

--- assets/data/mcu/animacion2010s/ima09S2.js
+++ assets/data/mcu/animacion2010s/ima09S2.js
@@ -1,9 +1,25 @@
-import { ima09S2E1 } from "./ima09S2/episodes.js";
+import { ima09S2E1, ima09S2E2, ima09S2E3, ima09S2E4, ima09S2E5, ima09S2E6, ima09S2E7, ima09S2E8, ima09S2E9, ima09S2E10, ima09S2E11, ima09S2E12, ima09S2E13, ima09S2E14, ima09S2E15, ima09S2E16, ima09S2E17, ima09S2E18, ima09S2E19, ima09S2E20, ima09S2E21, ima09S2E22, ima09S2E23, ima09S2E24, ima09S2E25, ima09S2E26 } from "./ima09S2/episodes.js";
 
 export const ima09S2 = {
   id: "ima09S2",
   label: "Iron Man: Armored Adventures (2009) (S2)",
+  format: "Serie",
+  season: 2,
+  release: {
+    year: 2011,
+    month: 7,
+    day: 4
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    ima09S2E1
+    ima09S2E1, ima09S2E2, ima09S2E3, ima09S2E4, ima09S2E5, ima09S2E6, ima09S2E7, ima09S2E8,
+    ima09S2E9, ima09S2E10, ima09S2E11, ima09S2E12, ima09S2E13, ima09S2E14, ima09S2E15, ima09S2E16,
+    ima09S2E17, ima09S2E18, ima09S2E19, ima09S2E20, ima09S2E21, ima09S2E22, ima09S2E23, ima09S2E24,
+    ima09S2E25, ima09S2E26
   ],
 };
--- assets/data/mcu/animacion2010s/ima09S2/episodes.js
+++ assets/data/mcu/animacion2010s/ima09S2/episodes.js
@@ -1,7 +1,259 @@
 export const ima09S2E1 = {
   id: "ima09S2E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Hammer of Thor"},
+    en: "The Hammer of Thor (Part 3)",
+    es: "El martillo de Thor (Parte 3)"
   },
 };
 
+export const ima09S2E2 = {
+  id: "ima09S2E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Fury of the Crimson Dynamo (Part 3)",
+    es: "La furia del Dynamo Carmesí (Parte 3)"
+  },
+};
+
+export const ima09S2E3 = {
+  id: "ima09S2E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Curse of the Iron Fist (Part 3)",
+    es: "La maldición del Puño de Hierro (Parte 3)"
+  },
+};
+
+export const ima09S2E4 = {
+  id: "ima09S2E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Designed Only for Chaos (Part 3)",
+    es: "Diseñado solo para el caos (Parte 3)"
+  },
+};
+
+export const ima09S2E5 = {
+  id: "ima09S2E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Man in the Iron Mask (Part 3)",
+    es: "El hombre de la máscara de hierro (Parte 3)"
+  },
+};
+
+export const ima09S2E6 = {
+  id: "ima09S2E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Need for Speed (Part 3)",
+    es: "Necesidad de velocidad (Parte 3)"
+  },
+};
+
+export const ima09S2E7 = {
+  id: "ima09S2E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Iron Man vs. the Crimson Dynamo (Part 3)",
+    es: "Iron Man contra el Dynamo Carmesí (Parte 3)"
+  },
+};
+
+export const ima09S2E8 = {
+  id: "ima09S2E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Legend of the Iron Fist (Part 3)",
+    es: "La leyenda del Puño de Hierro (Parte 3)"
+  },
+};
+
+export const ima09S2E9 = {
+  id: "ima09S2E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The Serpent's Egg (Part 3)",
+    es: "El huevo de la serpiente (Parte 3)"
+  },
+};
+
+export const ima09S2E10 = {
+  id: "ima09S2E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Sins of the Father (Part 3)",
+    es: "Los pecados del padre (Parte 3)"
+  },
+};
+
+export const ima09S2E11 = {
+  id: "ima09S2E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Invincible Iron Fist (Part 3)",
+    es: "El invencible Puño de Hierro (Parte 3)"
+  },
+};
+
+export const ima09S2E12 = {
+  id: "ima09S2E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Return of the Crimson Dynamo (Part 3)",
+    es: "El regreso del Dynamo Carmesí (Parte 3)"
+  },
+};
+
+export const ima09S2E13 = {
+  id: "ima09S2E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "The Hammer of Thor (Part 4)",
+    es: "El martillo de Thor (Parte 4)"
+  },
+};
+
+export const ima09S2E14 = {
+  id: "ima09S2E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "The Fury of the Crimson Dynamo (Part 4)",
+    es: "La furia del Dynamo Carmesí (Parte 4)"
+  },
+};
+
+export const ima09S2E15 = {
+  id: "ima09S2E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "The Curse of the Iron Fist (Part 4)",
+    es: "La maldición del Puño de Hierro (Parte 4)"
+  },
+};
+
+export const ima09S2E16 = {
+  id: "ima09S2E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "Designed Only for Chaos (Part 4)",
+    es: "Diseñado solo para el caos (Parte 4)"
+  },
+};
+
+export const ima09S2E17 = {
+  id: "ima09S2E17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "The Man in the Iron Mask (Part 4)",
+    es: "El hombre de la máscara de hierro (Parte 4)"
+  },
+};
+
+export const ima09S2E18 = {
+  id: "ima09S2E18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "The Need for Speed (Part 4)",
+    es: "Necesidad de velocidad (Parte 4)"
+  },
+};
+
+export const ima09S2E19 = {
+  id: "ima09S2E19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "Iron Man vs. the Crimson Dynamo (Part 4)",
+    es: "Iron Man contra el Dynamo Carmesí (Parte 4)"
+  },
+};
+
+export const ima09S2E20 = {
+  id: "ima09S2E20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "The Legend of the Iron Fist (Part 4)",
+    es: "La leyenda del Puño de Hierro (Parte 4)"
+  },
+};
+
+export const ima09S2E21 = {
+  id: "ima09S2E21",
+  number: 21,
+  completed: false,
+  label: {
+    en: "The Serpent's Egg (Part 4)",
+    es: "El huevo de la serpiente (Parte 4)"
+  },
+};
+
+export const ima09S2E22 = {
+  id: "ima09S2E22",
+  number: 22,
+  completed: false,
+  label: {
+    en: "The Sins of the Father (Part 4)",
+    es: "Los pecados del padre (Parte 4)"
+  },
+};
+
+export const ima09S2E23 = {
+  id: "ima09S2E23",
+  number: 23,
+  completed: false,
+  label: {
+    en: "The Invincible Iron Fist (Part 4)",
+    es: "El invencible Puño de Hierro (Parte 4)"
+  },
+};
+
+export const ima09S2E24 = {
+  id: "ima09S2E24",
+  number: 24,
+  completed: false,
+  label: {
+    en: "The Return of the Crimson Dynamo (Part 4)",
+    es: "El regreso del Dynamo Carmesí (Parte 4)"
+  },
+};
+
+export const ima09S2E25 = {
+  id: "ima09S2E25",
+  number: 25,
+  completed: false,
+  label: {
+    en: "The Rise of the Mandarin (Part 1)",
+    es: "El ascenso del Mandarin (Parte 1)"
+  },
+};
+
+export const ima09S2E26 = {
+  id: "ima09S2E26",
+  number: 26,
+  completed: false,
+  label: {
+    en: "The Rise of the Mandarin (Part 2)",
+    es: "El ascenso del Mandarin (Parte 2)"
+  },
+};
