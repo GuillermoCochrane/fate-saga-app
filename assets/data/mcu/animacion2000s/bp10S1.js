12059953f4e1cb282718f9953fe83a15286a0d21
Guille <cochraneguillermo@gmail.com>
2026-04-10 19:28:29 -0300

    Black Panther (2010) (S1) auditada y corregida

--- assets/data/mcu/animacion2000s/bp10S1.js
+++ assets/data/mcu/animacion2000s/bp10S1.js
@@ -1,9 +1,22 @@
-import { bp10S1E1 } from "./bp10S1/episodes.js";
+import { bp10S1E1, bp10S1E2, bp10S1E3, bp10S1E4, bp10S1E5, bp10S1E6 } from "./bp10S1/episodes.js";
 
 export const bp10S1 = {
   id: "bp10S1",
   label: "Black Panther (2010) (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 2010,
+    month: 1,
+    day: 16
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    bp10S1E1
+    bp10S1E1, bp10S1E2, bp10S1E3, bp10S1E4, bp10S1E5, bp10S1E6
   ],
 };
--- assets/data/mcu/animacion2000s/bp10S1/episodes.js
+++ assets/data/mcu/animacion2000s/bp10S1/episodes.js
@@ -1,7 +1,59 @@
 export const bp10S1E1 = {
   id: "bp10S1E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Panther's Rage"},
+    en: "The Panther's Rage (Part 1)",
+    es: "La ira de la pantera (Parte 1)"
   },
 };
 
+export const bp10S1E2 = {
+  id: "bp10S1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Panther's Rage (Part 2)",
+    es: "La ira de la pantera (Parte 2)"
+  },
+};
+
+export const bp10S1E3 = {
+  id: "bp10S1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Panther's Rage (Part 3)",
+    es: "La ira de la pantera (Parte 3)"
+  },
+};
+
+export const bp10S1E4 = {
+  id: "bp10S1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Panther's Rage (Part 4)",
+    es: "La ira de la pantera (Parte 4)"
+  },
+};
+
+export const bp10S1E5 = {
+  id: "bp10S1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Panther's Rage (Part 5)",
+    es: "La ira de la pantera (Parte 5)"
+  },
+};
+
+export const bp10S1E6 = {
+  id: "bp10S1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Panther's Rage (Part 6)",
+    es: "La ira de la pantera (Parte 6)"
+  },
+};
