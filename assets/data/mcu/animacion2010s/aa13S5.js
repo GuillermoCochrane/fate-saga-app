f1d4156d5f9bec2e007d02e9c9cf1ddcb25cd963
Guille <cochraneguillermo@gmail.com>
2026-04-12 20:27:58 -0300

    Avengers Assemble (2013) (S5) auditada y corregida

--- assets/data/mcu/animacion2010s/aa13S5.js
+++ assets/data/mcu/animacion2010s/aa13S5.js
@@ -1,9 +1,25 @@
-import { aa13S5E1 } from "./aa13S5/episodes.js";
+import { aa13S5E1, aa13S5E2, aa13S5E3, aa13S5E4, aa13S5E5, aa13S5E6, aa13S5E7, aa13S5E8, aa13S5E9, aa13S5E10, aa13S5E11, aa13S5E12, aa13S5E13, aa13S5E14, aa13S5E15, aa13S5E16, aa13S5E17, aa13S5E18, aa13S5E19, aa13S5E20, aa13S5E21, aa13S5E22, aa13S5E23, aa13S5E24, aa13S5E25, aa13S5E26 } from "./aa13S5/episodes.js";
 
 export const aa13S5 = {
   id: "aa13S5",
   label: "Avengers Assemble (2013) (S5)",
+  format: "Serie",
+  season: 5,
+  release: {
+    year: 2018,
+    month: 3,
+    day: 11
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    aa13S5E1
+    aa13S5E1, aa13S5E2, aa13S5E3, aa13S5E4, aa13S5E5, aa13S5E6, aa13S5E7, aa13S5E8,
+    aa13S5E9, aa13S5E10, aa13S5E11, aa13S5E12, aa13S5E13, aa13S5E14, aa13S5E15, aa13S5E16,
+    aa13S5E17, aa13S5E18, aa13S5E19, aa13S5E20, aa13S5E21, aa13S5E22, aa13S5E23, aa13S5E24,
+    aa13S5E25, aa13S5E26
   ],
 };
--- assets/data/mcu/animacion2010s/aa13S5/episodes.js
+++ assets/data/mcu/animacion2010s/aa13S5/episodes.js
@@ -1,7 +1,259 @@
 export const aa13S5E1 = {
   id: "aa13S5E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Return"},
+    en: "The Return",
+    es: "El regreso"
   },
 };
 
+export const aa13S5E2 = {
+  id: "aa13S5E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Return (Part 2)",
+    es: "El regreso (Parte 2)"
+  },
+};
+
+export const aa13S5E3 = {
+  id: "aa13S5E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Return (Part 3)",
+    es: "El regreso (Parte 3)"
+  },
+};
+
+export const aa13S5E4 = {
+  id: "aa13S5E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Return (Part 4)",
+    es: "El regreso (Parte 4)"
+  },
+};
+
+export const aa13S5E5 = {
+  id: "aa13S5E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Return (Part 5)",
+    es: "El regreso (Parte 5)"
+  },
+};
+
+export const aa13S5E6 = {
+  id: "aa13S5E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Return (Part 6)",
+    es: "El regreso (Parte 6)"
+  },
+};
+
+export const aa13S5E7 = {
+  id: "aa13S5E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The Return (Part 7)",
+    es: "El regreso (Parte 7)"
+  },
+};
+
+export const aa13S5E8 = {
+  id: "aa13S5E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Return (Part 8)",
+    es: "El regreso (Parte 8)"
+  },
+};
+
+export const aa13S5E9 = {
+  id: "aa13S5E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The Return (Part 9)",
+    es: "El regreso (Parte 9)"
+  },
+};
+
+export const aa13S5E10 = {
+  id: "aa13S5E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Return (Part 10)",
+    es: "El regreso (Parte 10)"
+  },
+};
+
+export const aa13S5E11 = {
+  id: "aa13S5E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Return (Part 11)",
+    es: "El regreso (Parte 11)"
+  },
+};
+
+export const aa13S5E12 = {
+  id: "aa13S5E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Return (Part 12)",
+    es: "El regreso (Parte 12)"
+  },
+};
+
+export const aa13S5E13 = {
+  id: "aa13S5E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "The Return (Part 13)",
+    es: "El regreso (Parte 13)"
+  },
+};
+
+export const aa13S5E14 = {
+  id: "aa13S5E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "The Return (Part 14)",
+    es: "El regreso (Parte 14)"
+  },
+};
+
+export const aa13S5E15 = {
+  id: "aa13S5E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "The Return (Part 15)",
+    es: "El regreso (Parte 15)"
+  },
+};
+
+export const aa13S5E16 = {
+  id: "aa13S5E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "The Return (Part 16)",
+    es: "El regreso (Parte 16)"
+  },
+};
+
+export const aa13S5E17 = {
+  id: "aa13S5E17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "The Return (Part 17)",
+    es: "El regreso (Parte 17)"
+  },
+};
+
+export const aa13S5E18 = {
+  id: "aa13S5E18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "The Return (Part 18)",
+    es: "El regreso (Parte 18)"
+  },
+};
+
+export const aa13S5E19 = {
+  id: "aa13S5E19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "The Return (Part 19)",
+    es: "El regreso (Parte 19)"
+  },
+};
+
+export const aa13S5E20 = {
+  id: "aa13S5E20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "The Return (Part 20)",
+    es: "El regreso (Parte 20)"
+  },
+};
+
+export const aa13S5E21 = {
+  id: "aa13S5E21",
+  number: 21,
+  completed: false,
+  label: {
+    en: "The Return (Part 21)",
+    es: "El regreso (Parte 21)"
+  },
+};
+
+export const aa13S5E22 = {
+  id: "aa13S5E22",
+  number: 22,
+  completed: false,
+  label: {
+    en: "The Return (Part 22)",
+    es: "El regreso (Parte 22)"
+  },
+};
+
+export const aa13S5E23 = {
+  id: "aa13S5E23",
+  number: 23,
+  completed: false,
+  label: {
+    en: "The Return (Part 23)",
+    es: "El regreso (Parte 23)"
+  },
+};
+
+export const aa13S5E24 = {
+  id: "aa13S5E24",
+  number: 24,
+  completed: false,
+  label: {
+    en: "The Return (Part 24)",
+    es: "El regreso (Parte 24)"
+  },
+};
+
+export const aa13S5E25 = {
+  id: "aa13S5E25",
+  number: 25,
+  completed: false,
+  label: {
+    en: "The Return (Part 25)",
+    es: "El regreso (Parte 25)"
+  },
+};
+
+export const aa13S5E26 = {
+  id: "aa13S5E26",
+  number: 26,
+  completed: false,
+  label: {
+    en: "The Return (Part 26)",
+    es: "El regreso (Parte 26)"
+  },
+};
