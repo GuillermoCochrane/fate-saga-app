5e5e97cb8157ba0ba6a533187e18dc500e8a95c7
Guille <cochraneguillermo@gmail.com>
2026-04-11 22:24:04 -0300

    Avengers Assemble (2013) (S4) auditada y corregida

--- assets/data/mcu/animacion2010s/aa13S4.js
+++ assets/data/mcu/animacion2010s/aa13S4.js
@@ -1,9 +1,25 @@
-import { aa13S4E1 } from "./aa13S4/episodes.js";
+import { aa13S4E1, aa13S4E2, aa13S4E3, aa13S4E4, aa13S4E5, aa13S4E6, aa13S4E7, aa13S4E8, aa13S4E9, aa13S4E10, aa13S4E11, aa13S4E12, aa13S4E13, aa13S4E14, aa13S4E15, aa13S4E16, aa13S4E17, aa13S4E18, aa13S4E19, aa13S4E20, aa13S4E21, aa13S4E22, aa13S4E23, aa13S4E24, aa13S4E25, aa13S4E26 } from "./aa13S4/episodes.js";
 
 export const aa13S4 = {
   id: "aa13S4",
   label: "Avengers Assemble (2013) (S4)",
+  format: "Serie",
+  season: 4,
+  release: {
+    year: 2017,
+    month: 6,
+    day: 17
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    aa13S4E1
+    aa13S4E1, aa13S4E2, aa13S4E3, aa13S4E4, aa13S4E5, aa13S4E6, aa13S4E7, aa13S4E8,
+    aa13S4E9, aa13S4E10, aa13S4E11, aa13S4E12, aa13S4E13, aa13S4E14, aa13S4E15, aa13S4E16,
+    aa13S4E17, aa13S4E18, aa13S4E19, aa13S4E20, aa13S4E21, aa13S4E22, aa13S4E23, aa13S4E24,
+    aa13S4E25, aa13S4E26
   ],
 };
--- assets/data/mcu/animacion2010s/aa13S4/episodes.js
+++ assets/data/mcu/animacion2010s/aa13S4/episodes.js
@@ -1,7 +1,259 @@
 export const aa13S4E1 = {
   id: "aa13S4E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The New Avengers"},
+    en: "The New Avengers",
+    es: "Los nuevos Vengadores"
   },
 };
 
+export const aa13S4E2 = {
+  id: "aa13S4E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The New Avengers (Part 2)",
+    es: "Los nuevos Vengadores (Parte 2)"
+  },
+};
+
+export const aa13S4E3 = {
+  id: "aa13S4E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The New Avengers (Part 3)",
+    es: "Los nuevos Vengadores (Parte 3)"
+  },
+};
+
+export const aa13S4E4 = {
+  id: "aa13S4E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The New Avengers (Part 4)",
+    es: "Los nuevos Vengadores (Parte 4)"
+  },
+};
+
+export const aa13S4E5 = {
+  id: "aa13S4E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The New Avengers (Part 5)",
+    es: "Los nuevos Vengadores (Parte 5)"
+  },
+};
+
+export const aa13S4E6 = {
+  id: "aa13S4E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The New Avengers (Part 6)",
+    es: "Los nuevos Vengadores (Parte 6)"
+  },
+};
+
+export const aa13S4E7 = {
+  id: "aa13S4E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The New Avengers (Part 7)",
+    es: "Los nuevos Vengadores (Parte 7)"
+  },
+};
+
+export const aa13S4E8 = {
+  id: "aa13S4E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The New Avengers (Part 8)",
+    es: "Los nuevos Vengadores (Parte 8)"
+  },
+};
+
+export const aa13S4E9 = {
+  id: "aa13S4E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The New Avengers (Part 9)",
+    es: "Los nuevos Vengadores (Parte 9)"
+  },
+};
+
+export const aa13S4E10 = {
+  id: "aa13S4E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The New Avengers (Part 10)",
+    es: "Los nuevos Vengadores (Parte 10)"
+  },
+};
+
+export const aa13S4E11 = {
+  id: "aa13S4E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The New Avengers (Part 11)",
+    es: "Los nuevos Vengadores (Parte 11)"
+  },
+};
+
+export const aa13S4E12 = {
+  id: "aa13S4E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The New Avengers (Part 12)",
+    es: "Los nuevos Vengadores (Parte 12)"
+  },
+};
+
+export const aa13S4E13 = {
+  id: "aa13S4E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "The New Avengers (Part 13)",
+    es: "Los nuevos Vengadores (Parte 13)"
+  },
+};
+
+export const aa13S4E14 = {
+  id: "aa13S4E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "The New Avengers (Part 14)",
+    es: "Los nuevos Vengadores (Parte 14)"
+  },
+};
+
+export const aa13S4E15 = {
+  id: "aa13S4E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "The New Avengers (Part 15)",
+    es: "Los nuevos Vengadores (Parte 15)"
+  },
+};
+
+export const aa13S4E16 = {
+  id: "aa13S4E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "The New Avengers (Part 16)",
+    es: "Los nuevos Vengadores (Parte 16)"
+  },
+};
+
+export const aa13S4E17 = {
+  id: "aa13S4E17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "The New Avengers (Part 17)",
+    es: "Los nuevos Vengadores (Parte 17)"
+  },
+};
+
+export const aa13S4E18 = {
+  id: "aa13S4E18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "The New Avengers (Part 18)",
+    es: "Los nuevos Vengadores (Parte 18)"
+  },
+};
+
+export const aa13S4E19 = {
+  id: "aa13S4E19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "The New Avengers (Part 19)",
+    es: "Los nuevos Vengadores (Parte 19)"
+  },
+};
+
+export const aa13S4E20 = {
+  id: "aa13S4E20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "The New Avengers (Part 20)",
+    es: "Los nuevos Vengadores (Parte 20)"
+  },
+};
+
+export const aa13S4E21 = {
+  id: "aa13S4E21",
+  number: 21,
+  completed: false,
+  label: {
+    en: "The New Avengers (Part 21)",
+    es: "Los nuevos Vengadores (Parte 21)"
+  },
+};
+
+export const aa13S4E22 = {
+  id: "aa13S4E22",
+  number: 22,
+  completed: false,
+  label: {
+    en: "The New Avengers (Part 22)",
+    es: "Los nuevos Vengadores (Parte 22)"
+  },
+};
+
+export const aa13S4E23 = {
+  id: "aa13S4E23",
+  number: 23,
+  completed: false,
+  label: {
+    en: "The New Avengers (Part 23)",
+    es: "Los nuevos Vengadores (Parte 23)"
+  },
+};
+
+export const aa13S4E24 = {
+  id: "aa13S4E24",
+  number: 24,
+  completed: false,
+  label: {
+    en: "The New Avengers (Part 24)",
+    es: "Los nuevos Vengadores (Parte 24)"
+  },
+};
+
+export const aa13S4E25 = {
+  id: "aa13S4E25",
+  number: 25,
+  completed: false,
+  label: {
+    en: "The New Avengers (Part 25)",
+    es: "Los nuevos Vengadores (Parte 25)"
+  },
+};
+
+export const aa13S4E26 = {
+  id: "aa13S4E26",
+  number: 26,
+  completed: false,
+  label: {
+    en: "The New Avengers (Part 26)",
+    es: "Los nuevos Vengadores (Parte 26)"
+  },
+};
