cca8c40125465b14a8dae3485b7fe84c91e5f224
Guille <cochraneguillermo@gmail.com>
2026-04-12 20:34:27 -0300

    Guardians of the Galaxy (2015) (S2) auditada y corregida

--- assets/data/mcu/animacion2010s/gotg15S2.js
+++ assets/data/mcu/animacion2010s/gotg15S2.js
@@ -1,9 +1,25 @@
-import { gotg15S2E1 } from "./gotg15S2/episodes.js";
+import { gotg15S2E1, gotg15S2E2, gotg15S2E3, gotg15S2E4, gotg15S2E5, gotg15S2E6, gotg15S2E7, gotg15S2E8, gotg15S2E9, gotg15S2E10, gotg15S2E11, gotg15S2E12, gotg15S2E13, gotg15S2E14, gotg15S2E15, gotg15S2E16, gotg15S2E17, gotg15S2E18, gotg15S2E19, gotg15S2E20, gotg15S2E21, gotg15S2E22, gotg15S2E23, gotg15S2E24, gotg15S2E25, gotg15S2E26 } from "./gotg15S2/episodes.js";
 
 export const gotg15S2 = {
   id: "gotg15S2",
   label: "Guardians of the Galaxy (2015) (S2)",
+  format: "Serie",
+  season: 2,
+  release: {
+    year: 2016,
+    month: 3,
+    day: 18
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    gotg15S2E1
+    gotg15S2E1, gotg15S2E2, gotg15S2E3, gotg15S2E4, gotg15S2E5, gotg15S2E6, gotg15S2E7, gotg15S2E8,
+    gotg15S2E9, gotg15S2E10, gotg15S2E11, gotg15S2E12, gotg15S2E13, gotg15S2E14, gotg15S2E15, gotg15S2E16,
+    gotg15S2E17, gotg15S2E18, gotg15S2E19, gotg15S2E20, gotg15S2E21, gotg15S2E22, gotg15S2E23, gotg15S2E24,
+    gotg15S2E25, gotg15S2E26
   ],
 };
--- assets/data/mcu/animacion2010s/gotg15S2/episodes.js
+++ assets/data/mcu/animacion2010s/gotg15S2/episodes.js
@@ -1,7 +1,259 @@
 export const gotg15S2E1 = {
   id: "gotg15S2E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Stuck in the Metal with You"},
+    en: "Stuck in the Metal with You (Part 1)",
+    es: "Atrapado en el metal contigo (Parte 1)"
   },
 };
 
+export const gotg15S2E2 = {
+  id: "gotg15S2E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Stuck in the Metal with You (Part 2)",
+    es: "Atrapado en el metal contigo (Parte 2)"
+  },
+};
+
+export const gotg15S2E3 = {
+  id: "gotg15S2E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Stuck in the Metal with You (Part 3)",
+    es: "Atrapado en el metal contigo (Parte 3)"
+  },
+};
+
+export const gotg15S2E4 = {
+  id: "gotg15S2E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Stuck in the Metal with You (Part 4)",
+    es: "Atrapado en el metal contigo (Parte 4)"
+  },
+};
+
+export const gotg15S2E5 = {
+  id: "gotg15S2E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Stuck in the Metal with You (Part 5)",
+    es: "Atrapado en el metal contigo (Parte 5)"
+  },
+};
+
+export const gotg15S2E6 = {
+  id: "gotg15S2E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Stuck in the Metal with You (Part 6)",
+    es: "Atrapado en el metal contigo (Parte 6)"
+  },
+};
+
+export const gotg15S2E7 = {
+  id: "gotg15S2E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Stuck in the Metal with You (Part 7)",
+    es: "Atrapado en el metal contigo (Parte 7)"
+  },
+};
+
+export const gotg15S2E8 = {
+  id: "gotg15S2E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Stuck in the Metal with You (Part 8)",
+    es: "Atrapado en el metal contigo (Parte 8)"
+  },
+};
+
+export const gotg15S2E9 = {
+  id: "gotg15S2E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Stuck in the Metal with You (Part 9)",
+    es: "Atrapado en el metal contigo (Parte 9)"
+  },
+};
+
+export const gotg15S2E10 = {
+  id: "gotg15S2E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "Stuck in the Metal with You (Part 10)",
+    es: "Atrapado en el metal contigo (Parte 10)"
+  },
+};
+
+export const gotg15S2E11 = {
+  id: "gotg15S2E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "Stuck in the Metal with You (Part 11)",
+    es: "Atrapado en el metal contigo (Parte 11)"
+  },
+};
+
+export const gotg15S2E12 = {
+  id: "gotg15S2E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "Stuck in the Metal with You (Part 12)",
+    es: "Atrapado en el metal contigo (Parte 12)"
+  },
+};
+
+export const gotg15S2E13 = {
+  id: "gotg15S2E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "Stuck in the Metal with You (Part 13)",
+    es: "Atrapado en el metal contigo (Parte 13)"
+  },
+};
+
+export const gotg15S2E14 = {
+  id: "gotg15S2E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "Stuck in the Metal with You (Part 14)",
+    es: "Atrapado en el metal contigo (Parte 14)"
+  },
+};
+
+export const gotg15S2E15 = {
+  id: "gotg15S2E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "Stuck in the Metal with You (Part 15)",
+    es: "Atrapado en el metal contigo (Parte 15)"
+  },
+};
+
+export const gotg15S2E16 = {
+  id: "gotg15S2E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "Stuck in the Metal with You (Part 16)",
+    es: "Atrapado en el metal contigo (Parte 16)"
+  },
+};
+
+export const gotg15S2E17 = {
+  id: "gotg15S2E17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "Stuck in the Metal with You (Part 17)",
+    es: "Atrapado en el metal contigo (Parte 17)"
+  },
+};
+
+export const gotg15S2E18 = {
+  id: "gotg15S2E18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "Stuck in the Metal with You (Part 18)",
+    es: "Atrapado en el metal contigo (Parte 18)"
+  },
+};
+
+export const gotg15S2E19 = {
+  id: "gotg15S2E19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "Stuck in the Metal with You (Part 19)",
+    es: "Atrapado en el metal contigo (Parte 19)"
+  },
+};
+
+export const gotg15S2E20 = {
+  id: "gotg15S2E20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "Stuck in the Metal with You (Part 20)",
+    es: "Atrapado en el metal contigo (Parte 20)"
+  },
+};
+
+export const gotg15S2E21 = {
+  id: "gotg15S2E21",
+  number: 21,
+  completed: false,
+  label: {
+    en: "Stuck in the Metal with You (Part 21)",
+    es: "Atrapado en el metal contigo (Parte 21)"
+  },
+};
+
+export const gotg15S2E22 = {
+  id: "gotg15S2E22",
+  number: 22,
+  completed: false,
+  label: {
+    en: "Stuck in the Metal with You (Part 22)",
+    es: "Atrapado en el metal contigo (Parte 22)"
+  },
+};
+
+export const gotg15S2E23 = {
+  id: "gotg15S2E23",
+  number: 23,
+  completed: false,
+  label: {
+    en: "Stuck in the Metal with You (Part 23)",
+    es: "Atrapado en el metal contigo (Parte 23)"
+  },
+};
+
+export const gotg15S2E24 = {
+  id: "gotg15S2E24",
+  number: 24,
+  completed: false,
+  label: {
+    en: "Stuck in the Metal with You (Part 24)",
+    es: "Atrapado en el metal contigo (Parte 24)"
+  },
+};
+
+export const gotg15S2E25 = {
+  id: "gotg15S2E25",
+  number: 25,
+  completed: false,
+  label: {
+    en: "Stuck in the Metal with You (Part 25)",
+    es: "Atrapado en el metal contigo (Parte 25)"
+  },
+};
+
+export const gotg15S2E26 = {
+  id: "gotg15S2E26",
+  number: 26,
+  completed: false,
+  label: {
+    en: "Stuck in the Metal with You (Part 26)",
+    es: "Atrapado en el metal contigo (Parte 26)"
+  },
+};
