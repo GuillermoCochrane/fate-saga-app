1244014389a8fc1dd7e370116f377c4369b0eddc
Guille <cochraneguillermo@gmail.com>
2026-04-12 20:35:35 -0300

    Guardians of the Galaxy (2015) (S3) auditada y corregida

--- assets/data/mcu/animacion2010s/gotg15S3.js
+++ assets/data/mcu/animacion2010s/gotg15S3.js
@@ -1,9 +1,25 @@
-import { gotg15S3E1 } from "./gotg15S3/episodes.js";
+import { gotg15S3E1, gotg15S3E2, gotg15S3E3, gotg15S3E4, gotg15S3E5, gotg15S3E6, gotg15S3E7, gotg15S3E8, gotg15S3E9, gotg15S3E10, gotg15S3E11, gotg15S3E12, gotg15S3E13, gotg15S3E14, gotg15S3E15, gotg15S3E16, gotg15S3E17, gotg15S3E18, gotg15S3E19, gotg15S3E20, gotg15S3E21, gotg15S3E22, gotg15S3E23, gotg15S3E24, gotg15S3E25, gotg15S3E26 } from "./gotg15S3/episodes.js";
 
 export const gotg15S3 = {
   id: "gotg15S3",
   label: "Guardians of the Galaxy (2015) (S3)",
+  format: "Serie",
+  season: 3,
+  release: {
+    year: 2017,
+    month: 8,
+    day: 5
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    gotg15S3E1
+    gotg15S3E1, gotg15S3E2, gotg15S3E3, gotg15S3E4, gotg15S3E5, gotg15S3E6, gotg15S3E7, gotg15S3E8,
+    gotg15S3E9, gotg15S3E10, gotg15S3E11, gotg15S3E12, gotg15S3E13, gotg15S3E14, gotg15S3E15, gotg15S3E16,
+    gotg15S3E17, gotg15S3E18, gotg15S3E19, gotg15S3E20, gotg15S3E21, gotg15S3E22, gotg15S3E23, gotg15S3E24,
+    gotg15S3E25, gotg15S3E26
   ],
 };
--- assets/data/mcu/animacion2010s/gotg15S3/episodes.js
+++ assets/data/mcu/animacion2010s/gotg15S3/episodes.js
@@ -1,7 +1,259 @@
 export const gotg15S3E1 = {
   id: "gotg15S3E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Mission: Breakout"},
+    en: "Mission: Breakout (Part 1)",
+    es: "Misión: Escape (Parte 1)"
   },
 };
 
+export const gotg15S3E2 = {
+  id: "gotg15S3E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Mission: Breakout (Part 2)",
+    es: "Misión: Escape (Parte 2)"
+  },
+};
+
+export const gotg15S3E3 = {
+  id: "gotg15S3E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Mission: Breakout (Part 3)",
+    es: "Misión: Escape (Parte 3)"
+  },
+};
+
+export const gotg15S3E4 = {
+  id: "gotg15S3E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Mission: Breakout (Part 4)",
+    es: "Misión: Escape (Parte 4)"
+  },
+};
+
+export const gotg15S3E5 = {
+  id: "gotg15S3E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Mission: Breakout (Part 5)",
+    es: "Misión: Escape (Parte 5)"
+  },
+};
+
+export const gotg15S3E6 = {
+  id: "gotg15S3E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Mission: Breakout (Part 6)",
+    es: "Misión: Escape (Parte 6)"
+  },
+};
+
+export const gotg15S3E7 = {
+  id: "gotg15S3E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Mission: Breakout (Part 7)",
+    es: "Misión: Escape (Parte 7)"
+  },
+};
+
+export const gotg15S3E8 = {
+  id: "gotg15S3E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Mission: Breakout (Part 8)",
+    es: "Misión: Escape (Parte 8)"
+  },
+};
+
+export const gotg15S3E9 = {
+  id: "gotg15S3E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Mission: Breakout (Part 9)",
+    es: "Misión: Escape (Parte 9)"
+  },
+};
+
+export const gotg15S3E10 = {
+  id: "gotg15S3E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "Mission: Breakout (Part 10)",
+    es: "Misión: Escape (Parte 10)"
+  },
+};
+
+export const gotg15S3E11 = {
+  id: "gotg15S3E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "Mission: Breakout (Part 11)",
+    es: "Misión: Escape (Parte 11)"
+  },
+};
+
+export const gotg15S3E12 = {
+  id: "gotg15S3E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "Mission: Breakout (Part 12)",
+    es: "Misión: Escape (Parte 12)"
+  },
+};
+
+export const gotg15S3E13 = {
+  id: "gotg15S3E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "Mission: Breakout (Part 13)",
+    es: "Misión: Escape (Parte 13)"
+  },
+};
+
+export const gotg15S3E14 = {
+  id: "gotg15S3E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "Mission: Breakout (Part 14)",
+    es: "Misión: Escape (Parte 14)"
+  },
+};
+
+export const gotg15S3E15 = {
+  id: "gotg15S3E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "Mission: Breakout (Part 15)",
+    es: "Misión: Escape (Parte 15)"
+  },
+};
+
+export const gotg15S3E16 = {
+  id: "gotg15S3E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "Mission: Breakout (Part 16)",
+    es: "Misión: Escape (Parte 16)"
+  },
+};
+
+export const gotg15S3E17 = {
+  id: "gotg15S3E17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "Mission: Breakout (Part 17)",
+    es: "Misión: Escape (Parte 17)"
+  },
+};
+
+export const gotg15S3E18 = {
+  id: "gotg15S3E18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "Mission: Breakout (Part 18)",
+    es: "Misión: Escape (Parte 18)"
+  },
+};
+
+export const gotg15S3E19 = {
+  id: "gotg15S3E19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "Mission: Breakout (Part 19)",
+    es: "Misión: Escape (Parte 19)"
+  },
+};
+
+export const gotg15S3E20 = {
+  id: "gotg15S3E20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "Mission: Breakout (Part 20)",
+    es: "Misión: Escape (Parte 20)"
+  },
+};
+
+export const gotg15S3E21 = {
+  id: "gotg15S3E21",
+  number: 21,
+  completed: false,
+  label: {
+    en: "Mission: Breakout (Part 21)",
+    es: "Misión: Escape (Parte 21)"
+  },
+};
+
+export const gotg15S3E22 = {
+  id: "gotg15S3E22",
+  number: 22,
+  completed: false,
+  label: {
+    en: "Mission: Breakout (Part 22)",
+    es: "Misión: Escape (Parte 22)"
+  },
+};
+
+export const gotg15S3E23 = {
+  id: "gotg15S3E23",
+  number: 23,
+  completed: false,
+  label: {
+    en: "Mission: Breakout (Part 23)",
+    es: "Misión: Escape (Parte 23)"
+  },
+};
+
+export const gotg15S3E24 = {
+  id: "gotg15S3E24",
+  number: 24,
+  completed: false,
+  label: {
+    en: "Mission: Breakout (Part 24)",
+    es: "Misión: Escape (Parte 24)"
+  },
+};
+
+export const gotg15S3E25 = {
+  id: "gotg15S3E25",
+  number: 25,
+  completed: false,
+  label: {
+    en: "Mission: Breakout (Part 25)",
+    es: "Misión: Escape (Parte 25)"
+  },
+};
+
+export const gotg15S3E26 = {
+  id: "gotg15S3E26",
+  number: 26,
+  completed: false,
+  label: {
+    en: "Mission: Breakout (Part 26)",
+    es: "Misión: Escape (Parte 26)"
+  },
+};
