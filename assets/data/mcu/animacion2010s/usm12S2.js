6ff69b82997a0ea6a73762ce9e9feeccbca42e0b
Guille <cochraneguillermo@gmail.com>
2026-04-11 22:17:10 -0300

    Ultimate Spider-Man (2012) (S2) auditada y corregida

--- assets/data/mcu/animacion2010s/usm12S2.js
+++ assets/data/mcu/animacion2010s/usm12S2.js
@@ -1,9 +1,25 @@
-import { usm12S2E1 } from "./usm12S2/episodes.js";
+import { usm12S2E1, usm12S2E2, usm12S2E3, usm12S2E4, usm12S2E5, usm12S2E6, usm12S2E7, usm12S2E8, usm12S2E9, usm12S2E10, usm12S2E11, usm12S2E12, usm12S2E13, usm12S2E14, usm12S2E15, usm12S2E16, usm12S2E17, usm12S2E18, usm12S2E19, usm12S2E20, usm12S2E21, usm12S2E22, usm12S2E23, usm12S2E24, usm12S2E25, usm12S2E26 } from "./usm12S2/episodes.js";
 
 export const usm12S2 = {
   id: "usm12S2",
   label: "Ultimate Spider-Man (2012) (S2)",
+  format: "Serie",
+  season: 2,
+  release: {
+    year: 2013,
+    month: 1,
+    day: 21
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    usm12S2E1
+    usm12S2E1, usm12S2E2, usm12S2E3, usm12S2E4, usm12S2E5, usm12S2E6, usm12S2E7, usm12S2E8,
+    usm12S2E9, usm12S2E10, usm12S2E11, usm12S2E12, usm12S2E13, usm12S2E14, usm12S2E15, usm12S2E16,
+    usm12S2E17, usm12S2E18, usm12S2E19, usm12S2E20, usm12S2E21, usm12S2E22, usm12S2E23, usm12S2E24,
+    usm12S2E25, usm12S2E26
   ],
 };
--- assets/data/mcu/animacion2010s/usm12S2/episodes.js
+++ assets/data/mcu/animacion2010s/usm12S2/episodes.js
@@ -1,7 +1,259 @@
 export const usm12S2E1 = {
   id: "usm12S2E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Lizard"},
+    en: "The Lizard",
+    es: "El Lagarto"
   },
 };
 
+export const usm12S2E2 = {
+  id: "usm12S2E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Electro",
+    es: "Electro"
+  },
+};
+
+export const usm12S2E3 = {
+  id: "usm12S2E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Rhino",
+    es: "Rhino"
+  },
+};
+
+export const usm12S2E4 = {
+  id: "usm12S2E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Kraven",
+    es: "Kraven"
+  },
+};
+
+export const usm12S2E5 = {
+  id: "usm12S2E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Vulture",
+    es: "El Buitre"
+  },
+};
+
+export const usm12S2E6 = {
+  id: "usm12S2E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Mysterio",
+    es: "Mysterio"
+  },
+};
+
+export const usm12S2E7 = {
+  id: "usm12S2E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The Sandman",
+    es: "Sandman"
+  },
+};
+
+export const usm12S2E8 = {
+  id: "usm12S2E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Goblin",
+    es: "El Duende"
+  },
+};
+
+export const usm12S2E9 = {
+  id: "usm12S2E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The Venom",
+    es: "Venom"
+  },
+};
+
+export const usm12S2E10 = {
+  id: "usm12S2E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Carnage",
+    es: "Carnage"
+  },
+};
+
+export const usm12S2E11 = {
+  id: "usm12S2E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Spider-Verse (Part 1)",
+    es: "El Spider-Verso (Parte 1)"
+  },
+};
+
+export const usm12S2E12 = {
+  id: "usm12S2E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Spider-Verse (Part 2)",
+    es: "El Spider-Verso (Parte 2)"
+  },
+};
+
+export const usm12S2E13 = {
+  id: "usm12S2E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "The Spider-Verse (Part 3)",
+    es: "El Spider-Verso (Parte 3)"
+  },
+};
+
+export const usm12S2E14 = {
+  id: "usm12S2E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "The Spider-Verse (Part 4)",
+    es: "El Spider-Verso (Parte 4)"
+  },
+};
+
+export const usm12S2E15 = {
+  id: "usm12S2E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "The Return of the Sinister Six",
+    es: "El regreso de los Seis Siniestros"
+  },
+};
+
+export const usm12S2E16 = {
+  id: "usm12S2E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "The Amazing Spider-Man (Part 2)",
+    es: "El asombroso Spider-Man (Parte 2)"
+  },
+};
+
+export const usm12S2E17 = {
+  id: "usm12S2E17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "The Savage Spider-Man (Part 2)",
+    es: "El Spider-Man salvaje (Parte 2)"
+  },
+};
+
+export const usm12S2E18 = {
+  id: "usm12S2E18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "The Ultimate Spider-Man",
+    es: "El Ultimate Spider-Man"
+  },
+};
+
+export const usm12S2E19 = {
+  id: "usm12S2E19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "The Sinister Six (Part 2)",
+    es: "Los Seis Siniestros (Parte 2)"
+  },
+};
+
+export const usm12S2E20 = {
+  id: "usm12S2E20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "The Return of the Sinister Six (Part 2)",
+    es: "El regreso de los Seis Siniestros (Parte 2)"
+  },
+};
+
+export const usm12S2E21 = {
+  id: "usm12S2E21",
+  number: 21,
+  completed: false,
+  label: {
+    en: "The Amazing Spider-Man (Part 3)",
+    es: "El asombroso Spider-Man (Parte 3)"
+  },
+};
+
+export const usm12S2E22 = {
+  id: "usm12S2E22",
+  number: 22,
+  completed: false,
+  label: {
+    en: "The Savage Spider-Man (Part 3)",
+    es: "El Spider-Man salvaje (Parte 3)"
+  },
+};
+
+export const usm12S2E23 = {
+  id: "usm12S2E23",
+  number: 23,
+  completed: false,
+  label: {
+    en: "The Ultimate Spider-Man (Part 2)",
+    es: "El Ultimate Spider-Man (Parte 2)"
+  },
+};
+
+export const usm12S2E24 = {
+  id: "usm12S2E24",
+  number: 24,
+  completed: false,
+  label: {
+    en: "The Sinister Six (Part 3)",
+    es: "Los Seis Siniestros (Parte 3)"
+  },
+};
+
+export const usm12S2E25 = {
+  id: "usm12S2E25",
+  number: 25,
+  completed: false,
+  label: {
+    en: "The Return of the Sinister Six (Part 3)",
+    es: "El regreso de los Seis Siniestros (Parte 3)"
+  },
+};
+
+export const usm12S2E26 = {
+  id: "usm12S2E26",
+  number: 26,
+  completed: false,
+  label: {
+    en: "The Ultimate Spider-Man (Part 3)",
+    es: "El Ultimate Spider-Man (Parte 3)"
+  },
+};
