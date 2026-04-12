f575e1fcd15a3c3a92f4d233cf64219cdc45b7eb
Guille <cochraneguillermo@gmail.com>
2026-04-11 22:19:47 -0300

    Ultimate Spider-Man (2012) (S4) auditada y corregida

--- assets/data/mcu/animacion2010s/usm12S4.js
+++ assets/data/mcu/animacion2010s/usm12S4.js
@@ -1,9 +1,25 @@
-import { usm12S4E1 } from "./usm12S4/episodes.js";
+import { usm12S4E1, usm12S4E2, usm12S4E3, usm12S4E4, usm12S4E5, usm12S4E6, usm12S4E7, usm12S4E8, usm12S4E9, usm12S4E10, usm12S4E11, usm12S4E12, usm12S4E13, usm12S4E14, usm12S4E15, usm12S4E16, usm12S4E17, usm12S4E18, usm12S4E19, usm12S4E20, usm12S4E21, usm12S4E22, usm12S4E23, usm12S4E24, usm12S4E25, usm12S4E26 } from "./usm12S4/episodes.js";
 
 export const usm12S4 = {
   id: "usm12S4",
   label: "Ultimate Spider-Man (2012) (S4)",
+  format: "Serie",
+  season: 4,
+  release: {
+    year: 2016,
+    month: 2,
+    day: 21
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    usm12S4E1
+    usm12S4E1, usm12S4E2, usm12S4E3, usm12S4E4, usm12S4E5, usm12S4E6, usm12S4E7, usm12S4E8,
+    usm12S4E9, usm12S4E10, usm12S4E11, usm12S4E12, usm12S4E13, usm12S4E14, usm12S4E15, usm12S4E16,
+    usm12S4E17, usm12S4E18, usm12S4E19, usm12S4E20, usm12S4E21, usm12S4E22, usm12S4E23, usm12S4E24,
+    usm12S4E25, usm12S4E26
   ],
 };
--- assets/data/mcu/animacion2010s/usm12S4/episodes.js
+++ assets/data/mcu/animacion2010s/usm12S4/episodes.js
@@ -1,7 +1,259 @@
 export const usm12S4E1 = {
   id: "usm12S4E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Return of the Goblin"},
+    en: "The Return of the Goblin (Part 1)",
+    es: "El regreso del Duende (Parte 1)"
   },
 };
 
+export const usm12S4E2 = {
+  id: "usm12S4E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Return of the Goblin (Part 2)",
+    es: "El regreso del Duende (Parte 2)"
+  },
+};
+
+export const usm12S4E3 = {
+  id: "usm12S4E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Return of the Goblin (Part 3)",
+    es: "El regreso del Duende (Parte 3)"
+  },
+};
+
+export const usm12S4E4 = {
+  id: "usm12S4E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Return of the Goblin (Part 4)",
+    es: "El regreso del Duende (Parte 4)"
+  },
+};
+
+export const usm12S4E5 = {
+  id: "usm12S4E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Sinister Six (Part 1)",
+    es: "Los Seis Siniestros (Parte 1)"
+  },
+};
+
+export const usm12S4E6 = {
+  id: "usm12S4E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Sinister Six (Part 2)",
+    es: "Los Seis Siniestros (Parte 2)"
+  },
+};
+
+export const usm12S4E7 = {
+  id: "usm12S4E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The Sinister Six (Part 3)",
+    es: "Los Seis Siniestros (Parte 3)"
+  },
+};
+
+export const usm12S4E8 = {
+  id: "usm12S4E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Sinister Six (Part 4)",
+    es: "Los Seis Siniestros (Parte 4)"
+  },
+};
+
+export const usm12S4E9 = {
+  id: "usm12S4E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The Return of the Sinister Six (Part 1)",
+    es: "El regreso de los Seis Siniestros (Parte 1)"
+  },
+};
+
+export const usm12S4E10 = {
+  id: "usm12S4E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Return of the Sinister Six (Part 2)",
+    es: "El regreso de los Seis Siniestros (Parte 2)"
+  },
+};
+
+export const usm12S4E11 = {
+  id: "usm12S4E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Return of the Sinister Six (Part 3)",
+    es: "El regreso de los Seis Siniestros (Parte 3)"
+  },
+};
+
+export const usm12S4E12 = {
+  id: "usm12S4E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Return of the Sinister Six (Part 4)",
+    es: "El regreso de los Seis Siniestros (Parte 4)"
+  },
+};
+
+export const usm12S4E13 = {
+  id: "usm12S4E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "The Ultimate Spider-Man (Part 1)",
+    es: "El Ultimate Spider-Man (Parte 1)"
+  },
+};
+
+export const usm12S4E14 = {
+  id: "usm12S4E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "The Ultimate Spider-Man (Part 2)",
+    es: "El Ultimate Spider-Man (Parte 2)"
+  },
+};
+
+export const usm12S4E15 = {
+  id: "usm12S4E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "The Ultimate Spider-Man (Part 3)",
+    es: "El Ultimate Spider-Man (Parte 3)"
+  },
+};
+
+export const usm12S4E16 = {
+  id: "usm12S4E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "The Ultimate Spider-Man (Part 4)",
+    es: "El Ultimate Spider-Man (Parte 4)"
+  },
+};
+
+export const usm12S4E17 = {
+  id: "usm12S4E17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "The Spider-Verse (Part 1)",
+    es: "El Spider-Verso (Parte 1)"
+  },
+};
+
+export const usm12S4E18 = {
+  id: "usm12S4E18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "The Spider-Verse (Part 2)",
+    es: "El Spider-Verso (Parte 2)"
+  },
+};
+
+export const usm12S4E19 = {
+  id: "usm12S4E19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "The Spider-Verse (Part 3)",
+    es: "El Spider-Verso (Parte 3)"
+  },
+};
+
+export const usm12S4E20 = {
+  id: "usm12S4E20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "The Spider-Verse (Part 4)",
+    es: "El Spider-Verso (Parte 4)"
+  },
+};
+
+export const usm12S4E21 = {
+  id: "usm12S4E21",
+  number: 21,
+  completed: false,
+  label: {
+    en: "The Return of the Spider-Verse (Part 1)",
+    es: "El regreso del Spider-Verso (Parte 1)"
+  },
+};
+
+export const usm12S4E22 = {
+  id: "usm12S4E22",
+  number: 22,
+  completed: false,
+  label: {
+    en: "The Return of the Spider-Verse (Part 2)",
+    es: "El regreso del Spider-Verso (Parte 2)"
+  },
+};
+
+export const usm12S4E23 = {
+  id: "usm12S4E23",
+  number: 23,
+  completed: false,
+  label: {
+    en: "The Return of the Spider-Verse (Part 3)",
+    es: "El regreso del Spider-Verso (Parte 3)"
+  },
+};
+
+export const usm12S4E24 = {
+  id: "usm12S4E24",
+  number: 24,
+  completed: false,
+  label: {
+    en: "The Return of the Spider-Verse (Part 4)",
+    es: "El regreso del Spider-Verso (Parte 4)"
+  },
+};
+
+export const usm12S4E25 = {
+  id: "usm12S4E25",
+  number: 25,
+  completed: false,
+  label: {
+    en: "The Amazing Spider-Man (Part 5)",
+    es: "El asombroso Spider-Man (Parte 5)"
+  },
+};
+
+export const usm12S4E26 = {
+  id: "usm12S4E26",
+  number: 26,
+  completed: false,
+  label: {
+    en: "The Savage Spider-Man (Part 5)",
+    es: "El Spider-Man salvaje (Parte 5)"
+  },
+};
