f3257ef4b2dd8a2370ef1859bfdab7a0f7dbbeca
Guille <cochraneguillermo@gmail.com>
2026-04-11 22:21:53 -0300

    Avengers Assemble (2013) (S2) auditada y corregida

--- assets/data/mcu/animacion2010s/aa13S2.js
+++ assets/data/mcu/animacion2010s/aa13S2.js
@@ -1,9 +1,25 @@
-import { aa13S2E1 } from "./aa13S2/episodes.js";
+import { aa13S2E1, aa13S2E2, aa13S2E3, aa13S2E4, aa13S2E5, aa13S2E6, aa13S2E7, aa13S2E8, aa13S2E9, aa13S2E10, aa13S2E11, aa13S2E12, aa13S2E13, aa13S2E14, aa13S2E15, aa13S2E16, aa13S2E17, aa13S2E18, aa13S2E19, aa13S2E20, aa13S2E21, aa13S2E22, aa13S2E23, aa13S2E24, aa13S2E25, aa13S2E26 } from "./aa13S2/episodes.js";
 
 export const aa13S2 = {
   id: "aa13S2",
   label: "Avengers Assemble (2013) (S2)",
+  format: "Serie",
+  season: 2,
+  release: {
+    year: 2014,
+    month: 9,
+    day: 28
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    aa13S2E1
+    aa13S2E1, aa13S2E2, aa13S2E3, aa13S2E4, aa13S2E5, aa13S2E6, aa13S2E7, aa13S2E8,
+    aa13S2E9, aa13S2E10, aa13S2E11, aa13S2E12, aa13S2E13, aa13S2E14, aa13S2E15, aa13S2E16,
+    aa13S2E17, aa13S2E18, aa13S2E19, aa13S2E20, aa13S2E21, aa13S2E22, aa13S2E23, aa13S2E24,
+    aa13S2E25, aa13S2E26
   ],
 };
--- assets/data/mcu/animacion2010s/aa13S2/episodes.js
+++ assets/data/mcu/animacion2010s/aa13S2/episodes.js
@@ -1,7 +1,259 @@
 export const aa13S2E1 = {
   id: "aa13S2E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Arsenal"},
+    en: "The Arsenal",
+    es: "El arsenal"
   },
 };
 
+export const aa13S2E2 = {
+  id: "aa13S2E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Arsenal (Part 2)",
+    es: "El arsenal (Parte 2)"
+  },
+};
+
+export const aa13S2E3 = {
+  id: "aa13S2E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Arsenal (Part 3)",
+    es: "El arsenal (Parte 3)"
+  },
+};
+
+export const aa13S2E4 = {
+  id: "aa13S2E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Arsenal (Part 4)",
+    es: "El arsenal (Parte 4)"
+  },
+};
+
+export const aa13S2E5 = {
+  id: "aa13S2E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Arsenal (Part 5)",
+    es: "El arsenal (Parte 5)"
+  },
+};
+
+export const aa13S2E6 = {
+  id: "aa13S2E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Arsenal (Part 6)",
+    es: "El arsenal (Parte 6)"
+  },
+};
+
+export const aa13S2E7 = {
+  id: "aa13S2E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The Arsenal (Part 7)",
+    es: "El arsenal (Parte 7)"
+  },
+};
+
+export const aa13S2E8 = {
+  id: "aa13S2E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Arsenal (Part 8)",
+    es: "El arsenal (Parte 8)"
+  },
+};
+
+export const aa13S2E9 = {
+  id: "aa13S2E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The Arsenal (Part 9)",
+    es: "El arsenal (Parte 9)"
+  },
+};
+
+export const aa13S2E10 = {
+  id: "aa13S2E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Arsenal (Part 10)",
+    es: "El arsenal (Parte 10)"
+  },
+};
+
+export const aa13S2E11 = {
+  id: "aa13S2E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Arsenal (Part 11)",
+    es: "El arsenal (Parte 11)"
+  },
+};
+
+export const aa13S2E12 = {
+  id: "aa13S2E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Arsenal (Part 12)",
+    es: "El arsenal (Parte 12)"
+  },
+};
+
+export const aa13S2E13 = {
+  id: "aa13S2E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "The Arsenal (Part 13)",
+    es: "El arsenal (Parte 13)"
+  },
+};
+
+export const aa13S2E14 = {
+  id: "aa13S2E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "The Arsenal (Part 14)",
+    es: "El arsenal (Parte 14)"
+  },
+};
+
+export const aa13S2E15 = {
+  id: "aa13S2E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "The Arsenal (Part 15)",
+    es: "El arsenal (Parte 15)"
+  },
+};
+
+export const aa13S2E16 = {
+  id: "aa13S2E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "The Arsenal (Part 16)",
+    es: "El arsenal (Parte 16)"
+  },
+};
+
+export const aa13S2E17 = {
+  id: "aa13S2E17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "The Arsenal (Part 17)",
+    es: "El arsenal (Parte 17)"
+  },
+};
+
+export const aa13S2E18 = {
+  id: "aa13S2E18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "The Arsenal (Part 18)",
+    es: "El arsenal (Parte 18)"
+  },
+};
+
+export const aa13S2E19 = {
+  id: "aa13S2E19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "The Arsenal (Part 19)",
+    es: "El arsenal (Parte 19)"
+  },
+};
+
+export const aa13S2E20 = {
+  id: "aa13S2E20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "The Arsenal (Part 20)",
+    es: "El arsenal (Parte 20)"
+  },
+};
+
+export const aa13S2E21 = {
+  id: "aa13S2E21",
+  number: 21,
+  completed: false,
+  label: {
+    en: "The Arsenal (Part 21)",
+    es: "El arsenal (Parte 21)"
+  },
+};
+
+export const aa13S2E22 = {
+  id: "aa13S2E22",
+  number: 22,
+  completed: false,
+  label: {
+    en: "The Arsenal (Part 22)",
+    es: "El arsenal (Parte 22)"
+  },
+};
+
+export const aa13S2E23 = {
+  id: "aa13S2E23",
+  number: 23,
+  completed: false,
+  label: {
+    en: "The Arsenal (Part 23)",
+    es: "El arsenal (Parte 23)"
+  },
+};
+
+export const aa13S2E24 = {
+  id: "aa13S2E24",
+  number: 24,
+  completed: false,
+  label: {
+    en: "The Arsenal (Part 24)",
+    es: "El arsenal (Parte 24)"
+  },
+};
+
+export const aa13S2E25 = {
+  id: "aa13S2E25",
+  number: 25,
+  completed: false,
+  label: {
+    en: "The Arsenal (Part 25)",
+    es: "El arsenal (Parte 25)"
+  },
+};
+
+export const aa13S2E26 = {
+  id: "aa13S2E26",
+  number: 26,
+  completed: false,
+  label: {
+    en: "The Arsenal (Part 26)",
+    es: "El arsenal (Parte 26)"
+  },
+};
