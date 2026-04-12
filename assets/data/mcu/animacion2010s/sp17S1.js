beab932e4aad4be84f3e8fd8991aa8e9ac05190e
Guille <cochraneguillermo@gmail.com>
2026-04-12 20:36:50 -0300

    Marvel's Spider-Man (2017) (S1) auditada y corregida

--- assets/data/mcu/animacion2010s/sp17S1.js
+++ assets/data/mcu/animacion2010s/sp17S1.js
@@ -1,9 +1,25 @@
-import { sp17S1E1 } from "./sp17S1/episodes.js";
+import { sp17S1E1, sp17S1E2, sp17S1E3, sp17S1E4, sp17S1E5, sp17S1E6, sp17S1E7, sp17S1E8, sp17S1E9, sp17S1E10, sp17S1E11, sp17S1E12, sp17S1E13, sp17S1E14, sp17S1E15, sp17S1E16, sp17S1E17, sp17S1E18, sp17S1E19, sp17S1E20, sp17S1E21, sp17S1E22, sp17S1E23, sp17S1E24, sp17S1E25, sp17S1E26 } from "./sp17S1/episodes.js";
 
 export const sp17S1 = {
   id: "sp17S1",
   label: "Marvel's Spider-Man (2017) (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 2017,
+    month: 8,
+    day: 19
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    sp17S1E1
+    sp17S1E1, sp17S1E2, sp17S1E3, sp17S1E4, sp17S1E5, sp17S1E6, sp17S1E7, sp17S1E8,
+    sp17S1E9, sp17S1E10, sp17S1E11, sp17S1E12, sp17S1E13, sp17S1E14, sp17S1E15, sp17S1E16,
+    sp17S1E17, sp17S1E18, sp17S1E19, sp17S1E20, sp17S1E21, sp17S1E22, sp17S1E23, sp17S1E24,
+    sp17S1E25, sp17S1E26
   ],
 };
--- assets/data/mcu/animacion2010s/sp17S1/episodes.js
+++ assets/data/mcu/animacion2010s/sp17S1/episodes.js
@@ -1,7 +1,259 @@
 export const sp17S1E1 = {
   id: "sp17S1E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Horizon High (Part 1)"},
+    en: "Horizon High (Part 1)",
+    es: "Horizon High (Parte 1)"
   },
 };
 
+export const sp17S1E2 = {
+  id: "sp17S1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Horizon High (Part 2)",
+    es: "Horizon High (Parte 2)"
+  },
+};
+
+export const sp17S1E3 = {
+  id: "sp17S1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Horizon High (Part 3)",
+    es: "Horizon High (Parte 3)"
+  },
+};
+
+export const sp17S1E4 = {
+  id: "sp17S1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Horizon High (Part 4)",
+    es: "Horizon High (Parte 4)"
+  },
+};
+
+export const sp17S1E5 = {
+  id: "sp17S1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Horizon High (Part 5)",
+    es: "Horizon High (Parte 5)"
+  },
+};
+
+export const sp17S1E6 = {
+  id: "sp17S1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Horizon High (Part 6)",
+    es: "Horizon High (Parte 6)"
+  },
+};
+
+export const sp17S1E7 = {
+  id: "sp17S1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Horizon High (Part 7)",
+    es: "Horizon High (Parte 7)"
+  },
+};
+
+export const sp17S1E8 = {
+  id: "sp17S1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Horizon High (Part 8)",
+    es: "Horizon High (Parte 8)"
+  },
+};
+
+export const sp17S1E9 = {
+  id: "sp17S1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Horizon High (Part 9)",
+    es: "Horizon High (Parte 9)"
+  },
+};
+
+export const sp17S1E10 = {
+  id: "sp17S1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "Horizon High (Part 10)",
+    es: "Horizon High (Parte 10)"
+  },
+};
+
+export const sp17S1E11 = {
+  id: "sp17S1E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "Horizon High (Part 11)",
+    es: "Horizon High (Parte 11)"
+  },
+};
+
+export const sp17S1E12 = {
+  id: "sp17S1E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "Horizon High (Part 12)",
+    es: "Horizon High (Parte 12)"
+  },
+};
+
+export const sp17S1E13 = {
+  id: "sp17S1E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "Horizon High (Part 13)",
+    es: "Horizon High (Parte 13)"
+  },
+};
+
+export const sp17S1E14 = {
+  id: "sp17S1E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "Horizon High (Part 14)",
+    es: "Horizon High (Parte 14)"
+  },
+};
+
+export const sp17S1E15 = {
+  id: "sp17S1E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "Horizon High (Part 15)",
+    es: "Horizon High (Parte 15)"
+  },
+};
+
+export const sp17S1E16 = {
+  id: "sp17S1E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "Horizon High (Part 16)",
+    es: "Horizon High (Parte 16)"
+  },
+};
+
+export const sp17S1E17 = {
+  id: "sp17S1E17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "Horizon High (Part 17)",
+    es: "Horizon High (Parte 17)"
+  },
+};
+
+export const sp17S1E18 = {
+  id: "sp17S1E18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "Horizon High (Part 18)",
+    es: "Horizon High (Parte 18)"
+  },
+};
+
+export const sp17S1E19 = {
+  id: "sp17S1E19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "Horizon High (Part 19)",
+    es: "Horizon High (Parte 19)"
+  },
+};
+
+export const sp17S1E20 = {
+  id: "sp17S1E20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "Horizon High (Part 20)",
+    es: "Horizon High (Parte 20)"
+  },
+};
+
+export const sp17S1E21 = {
+  id: "sp17S1E21",
+  number: 21,
+  completed: false,
+  label: {
+    en: "Horizon High (Part 21)",
+    es: "Horizon High (Parte 21)"
+  },
+};
+
+export const sp17S1E22 = {
+  id: "sp17S1E22",
+  number: 22,
+  completed: false,
+  label: {
+    en: "Horizon High (Part 22)",
+    es: "Horizon High (Parte 22)"
+  },
+};
+
+export const sp17S1E23 = {
+  id: "sp17S1E23",
+  number: 23,
+  completed: false,
+  label: {
+    en: "Horizon High (Part 23)",
+    es: "Horizon High (Parte 23)"
+  },
+};
+
+export const sp17S1E24 = {
+  id: "sp17S1E24",
+  number: 24,
+  completed: false,
+  label: {
+    en: "Horizon High (Part 24)",
+    es: "Horizon High (Parte 24)"
+  },
+};
+
+export const sp17S1E25 = {
+  id: "sp17S1E25",
+  number: 25,
+  completed: false,
+  label: {
+    en: "Horizon High (Part 25)",
+    es: "Horizon High (Parte 25)"
+  },
+};
+
+export const sp17S1E26 = {
+  id: "sp17S1E26",
+  number: 26,
+  completed: false,
+  label: {
+    en: "Horizon High (Part 26)",
+    es: "Horizon High (Parte 26)"
+  },
+};
