b2c5bbd532a585fe6113855d34dc741f14066c97
Guille <cochraneguillermo@gmail.com>
2026-04-11 22:20:51 -0300

    Avengers Assemble (2013) (S1)auditada y corregida

--- assets/data/mcu/animacion2010s/aa13S1.js
+++ assets/data/mcu/animacion2010s/aa13S1.js
@@ -1,9 +1,25 @@
-import { aa13S1E1 } from "./aa13S1/episodes.js";
+import { aa13S1E1, aa13S1E2, aa13S1E3, aa13S1E4, aa13S1E5, aa13S1E6, aa13S1E7, aa13S1E8, aa13S1E9, aa13S1E10, aa13S1E11, aa13S1E12, aa13S1E13, aa13S1E14, aa13S1E15, aa13S1E16, aa13S1E17, aa13S1E18, aa13S1E19, aa13S1E20, aa13S1E21, aa13S1E22, aa13S1E23, aa13S1E24, aa13S1E25, aa13S1E26 } from "./aa13S1/episodes.js";
 
 export const aa13S1 = {
   id: "aa13S1",
   label: "Avengers Assemble (2013) (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 2013,
+    month: 5,
+    day: 26
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    aa13S1E1
+    aa13S1E1, aa13S1E2, aa13S1E3, aa13S1E4, aa13S1E5, aa13S1E6, aa13S1E7, aa13S1E8,
+    aa13S1E9, aa13S1E10, aa13S1E11, aa13S1E12, aa13S1E13, aa13S1E14, aa13S1E15, aa13S1E16,
+    aa13S1E17, aa13S1E18, aa13S1E19, aa13S1E20, aa13S1E21, aa13S1E22, aa13S1E23, aa13S1E24,
+    aa13S1E25, aa13S1E26
   ],
 };
--- assets/data/mcu/animacion2010s/aa13S1/episodes.js
+++ assets/data/mcu/animacion2010s/aa13S1/episodes.js
@@ -1,7 +1,259 @@
 export const aa13S1E1 = {
   id: "aa13S1E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Avengers Protocol"},
+    en: "The Avengers Protocol (Part 1)",
+    es: "El protocolo de los Vengadores (Parte 1)"
   },
 };
 
+export const aa13S1E2 = {
+  id: "aa13S1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Avengers Protocol (Part 2)",
+    es: "El protocolo de los Vengadores (Parte 2)"
+  },
+};
+
+export const aa13S1E3 = {
+  id: "aa13S1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Avengers Protocol (Part 3)",
+    es: "El protocolo de los Vengadores (Parte 3)"
+  },
+};
+
+export const aa13S1E4 = {
+  id: "aa13S1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Avengers Protocol (Part 4)",
+    es: "El protocolo de los Vengadores (Parte 4)"
+  },
+};
+
+export const aa13S1E5 = {
+  id: "aa13S1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Avengers Protocol (Part 5)",
+    es: "El protocolo de los Vengadores (Parte 5)"
+  },
+};
+
+export const aa13S1E6 = {
+  id: "aa13S1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Avengers Protocol (Part 6)",
+    es: "El protocolo de los Vengadores (Parte 6)"
+  },
+};
+
+export const aa13S1E7 = {
+  id: "aa13S1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The Avengers Protocol (Part 7)",
+    es: "El protocolo de los Vengadores (Parte 7)"
+  },
+};
+
+export const aa13S1E8 = {
+  id: "aa13S1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Avengers Protocol (Part 8)",
+    es: "El protocolo de los Vengadores (Parte 8)"
+  },
+};
+
+export const aa13S1E9 = {
+  id: "aa13S1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The Avengers Protocol (Part 9)",
+    es: "El protocolo de los Vengadores (Parte 9)"
+  },
+};
+
+export const aa13S1E10 = {
+  id: "aa13S1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Avengers Protocol (Part 10)",
+    es: "El protocolo de los Vengadores (Parte 10)"
+  },
+};
+
+export const aa13S1E11 = {
+  id: "aa13S1E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Avengers Protocol (Part 11)",
+    es: "El protocolo de los Vengadores (Parte 11)"
+  },
+};
+
+export const aa13S1E12 = {
+  id: "aa13S1E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Avengers Protocol (Part 12)",
+    es: "El protocolo de los Vengadores (Parte 12)"
+  },
+};
+
+export const aa13S1E13 = {
+  id: "aa13S1E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "The Avengers Protocol (Part 13)",
+    es: "El protocolo de los Vengadores (Parte 13)"
+  },
+};
+
+export const aa13S1E14 = {
+  id: "aa13S1E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "The Avengers Protocol (Part 14)",
+    es: "El protocolo de los Vengadores (Parte 14)"
+  },
+};
+
+export const aa13S1E15 = {
+  id: "aa13S1E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "The Avengers Protocol (Part 15)",
+    es: "El protocolo de los Vengadores (Parte 15)"
+  },
+};
+
+export const aa13S1E16 = {
+  id: "aa13S1E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "The Avengers Protocol (Part 16)",
+    es: "El protocolo de los Vengadores (Parte 16)"
+  },
+};
+
+export const aa13S1E17 = {
+  id: "aa13S1E17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "The Avengers Protocol (Part 17)",
+    es: "El protocolo de los Vengadores (Parte 17)"
+  },
+};
+
+export const aa13S1E18 = {
+  id: "aa13S1E18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "The Avengers Protocol (Part 18)",
+    es: "El protocolo de los Vengadores (Parte 18)"
+  },
+};
+
+export const aa13S1E19 = {
+  id: "aa13S1E19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "The Avengers Protocol (Part 19)",
+    es: "El protocolo de los Vengadores (Parte 19)"
+  },
+};
+
+export const aa13S1E20 = {
+  id: "aa13S1E20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "The Avengers Protocol (Part 20)",
+    es: "El protocolo de los Vengadores (Parte 20)"
+  },
+};
+
+export const aa13S1E21 = {
+  id: "aa13S1E21",
+  number: 21,
+  completed: false,
+  label: {
+    en: "The Avengers Protocol (Part 21)",
+    es: "El protocolo de los Vengadores (Parte 21)"
+  },
+};
+
+export const aa13S1E22 = {
+  id: "aa13S1E22",
+  number: 22,
+  completed: false,
+  label: {
+    en: "The Avengers Protocol (Part 22)",
+    es: "El protocolo de los Vengadores (Parte 22)"
+  },
+};
+
+export const aa13S1E23 = {
+  id: "aa13S1E23",
+  number: 23,
+  completed: false,
+  label: {
+    en: "The Avengers Protocol (Part 23)",
+    es: "El protocolo de los Vengadores (Parte 23)"
+  },
+};
+
+export const aa13S1E24 = {
+  id: "aa13S1E24",
+  number: 24,
+  completed: false,
+  label: {
+    en: "The Avengers Protocol (Part 24)",
+    es: "El protocolo de los Vengadores (Parte 24)"
+  },
+};
+
+export const aa13S1E25 = {
+  id: "aa13S1E25",
+  number: 25,
+  completed: false,
+  label: {
+    en: "The Avengers Protocol (Part 25)",
+    es: "El protocolo de los Vengadores (Parte 25)"
+  },
+};
+
+export const aa13S1E26 = {
+  id: "aa13S1E26",
+  number: 26,
+  completed: false,
+  label: {
+    en: "The Avengers Protocol (Part 26)",
+    es: "El protocolo de los Vengadores (Parte 26)"
+  },
+};
