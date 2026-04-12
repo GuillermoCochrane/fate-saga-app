f00c0ad314547a2b02967f66fcca641060e91292
Guille <cochraneguillermo@gmail.com>
2026-04-12 20:29:52 -0300

    Hulk and the Agents of S.M.A.S.H. (2013) (S1) auditada y corregida

--- assets/data/mcu/animacion2010s/hulk13S1.js
+++ assets/data/mcu/animacion2010s/hulk13S1.js
@@ -1,9 +1,25 @@
-import { hulk13S1E1 } from "./hulk13S1/episodes.js";
+import { hulk13S1E1, hulk13S1E2, hulk13S1E3, hulk13S1E4, hulk13S1E5, hulk13S1E6, hulk13S1E7, hulk13S1E8, hulk13S1E9, hulk13S1E10, hulk13S1E11, hulk13S1E12, hulk13S1E13, hulk13S1E14, hulk13S1E15, hulk13S1E16, hulk13S1E17, hulk13S1E18, hulk13S1E19, hulk13S1E20, hulk13S1E21, hulk13S1E22, hulk13S1E23, hulk13S1E24, hulk13S1E25, hulk13S1E26 } from "./hulk13S1/episodes.js";
 
 export const hulk13S1 = {
   id: "hulk13S1",
   label: "Hulk and the Agents of S.M.A.S.H. (2013) (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 2013,
+    month: 8,
+    day: 11
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    hulk13S1E1
+    hulk13S1E1, hulk13S1E2, hulk13S1E3, hulk13S1E4, hulk13S1E5, hulk13S1E6, hulk13S1E7, hulk13S1E8,
+    hulk13S1E9, hulk13S1E10, hulk13S1E11, hulk13S1E12, hulk13S1E13, hulk13S1E14, hulk13S1E15, hulk13S1E16,
+    hulk13S1E17, hulk13S1E18, hulk13S1E19, hulk13S1E20, hulk13S1E21, hulk13S1E22, hulk13S1E23, hulk13S1E24,
+    hulk13S1E25, hulk13S1E26
   ],
 };
--- assets/data/mcu/animacion2010s/hulk13S1/episodes.js
+++ assets/data/mcu/animacion2010s/hulk13S1/episodes.js
@@ -1,7 +1,259 @@
 export const hulk13S1E1 = {
   id: "hulk13S1E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Doorway to Doom"},
+    en: "Doorway to Doom (Part 1)",
+    es: "Portal hacia la perdición (Parte 1)"
   },
 };
 
+export const hulk13S1E2 = {
+  id: "hulk13S1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Doorway to Doom (Part 2)",
+    es: "Portal hacia la perdición (Parte 2)"
+  },
+};
+
+export const hulk13S1E3 = {
+  id: "hulk13S1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Doorway to Doom (Part 3)",
+    es: "Portal hacia la perdición (Parte 3)"
+  },
+};
+
+export const hulk13S1E4 = {
+  id: "hulk13S1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Doorway to Doom (Part 4)",
+    es: "Portal hacia la perdición (Parte 4)"
+  },
+};
+
+export const hulk13S1E5 = {
+  id: "hulk13S1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Doorway to Doom (Part 5)",
+    es: "Portal hacia la perdición (Parte 5)"
+  },
+};
+
+export const hulk13S1E6 = {
+  id: "hulk13S1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Doorway to Doom (Part 6)",
+    es: "Portal hacia la perdición (Parte 6)"
+  },
+};
+
+export const hulk13S1E7 = {
+  id: "hulk13S1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Doorway to Doom (Part 7)",
+    es: "Portal hacia la perdición (Parte 7)"
+  },
+};
+
+export const hulk13S1E8 = {
+  id: "hulk13S1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Doorway to Doom (Part 8)",
+    es: "Portal hacia la perdición (Parte 8)"
+  },
+};
+
+export const hulk13S1E9 = {
+  id: "hulk13S1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Doorway to Doom (Part 9)",
+    es: "Portal hacia la perdición (Parte 9)"
+  },
+};
+
+export const hulk13S1E10 = {
+  id: "hulk13S1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "Doorway to Doom (Part 10)",
+    es: "Portal hacia la perdición (Parte 10)"
+  },
+};
+
+export const hulk13S1E11 = {
+  id: "hulk13S1E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "Doorway to Doom (Part 11)",
+    es: "Portal hacia la perdición (Parte 11)"
+  },
+};
+
+export const hulk13S1E12 = {
+  id: "hulk13S1E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "Doorway to Doom (Part 12)",
+    es: "Portal hacia la perdición (Parte 12)"
+  },
+};
+
+export const hulk13S1E13 = {
+  id: "hulk13S1E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "Doorway to Doom (Part 13)",
+    es: "Portal hacia la perdición (Parte 13)"
+  },
+};
+
+export const hulk13S1E14 = {
+  id: "hulk13S1E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "Doorway to Doom (Part 14)",
+    es: "Portal hacia la perdición (Parte 14)"
+  },
+};
+
+export const hulk13S1E15 = {
+  id: "hulk13S1E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "Doorway to Doom (Part 15)",
+    es: "Portal hacia la perdición (Parte 15)"
+  },
+};
+
+export const hulk13S1E16 = {
+  id: "hulk13S1E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "Doorway to Doom (Part 16)",
+    es: "Portal hacia la perdición (Parte 16)"
+  },
+};
+
+export const hulk13S1E17 = {
+  id: "hulk13S1E17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "Doorway to Doom (Part 17)",
+    es: "Portal hacia la perdición (Parte 17)"
+  },
+};
+
+export const hulk13S1E18 = {
+  id: "hulk13S1E18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "Doorway to Doom (Part 18)",
+    es: "Portal hacia la perdición (Parte 18)"
+  },
+};
+
+export const hulk13S1E19 = {
+  id: "hulk13S1E19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "Doorway to Doom (Part 19)",
+    es: "Portal hacia la perdición (Parte 19)"
+  },
+};
+
+export const hulk13S1E20 = {
+  id: "hulk13S1E20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "Doorway to Doom (Part 20)",
+    es: "Portal hacia la perdición (Parte 20)"
+  },
+};
+
+export const hulk13S1E21 = {
+  id: "hulk13S1E21",
+  number: 21,
+  completed: false,
+  label: {
+    en: "Doorway to Doom (Part 21)",
+    es: "Portal hacia la perdición (Parte 21)"
+  },
+};
+
+export const hulk13S1E22 = {
+  id: "hulk13S1E22",
+  number: 22,
+  completed: false,
+  label: {
+    en: "Doorway to Doom (Part 22)",
+    es: "Portal hacia la perdición (Parte 22)"
+  },
+};
+
+export const hulk13S1E23 = {
+  id: "hulk13S1E23",
+  number: 23,
+  completed: false,
+  label: {
+    en: "Doorway to Doom (Part 23)",
+    es: "Portal hacia la perdición (Parte 23)"
+  },
+};
+
+export const hulk13S1E24 = {
+  id: "hulk13S1E24",
+  number: 24,
+  completed: false,
+  label: {
+    en: "Doorway to Doom (Part 24)",
+    es: "Portal hacia la perdición (Parte 24)"
+  },
+};
+
+export const hulk13S1E25 = {
+  id: "hulk13S1E25",
+  number: 25,
+  completed: false,
+  label: {
+    en: "Doorway to Doom (Part 25)",
+    es: "Portal hacia la perdición (Parte 25)"
+  },
+};
+
+export const hulk13S1E26 = {
+  id: "hulk13S1E26",
+  number: 26,
+  completed: false,
+  label: {
+    en: "Doorway to Doom (Part 26)",
+    es: "Portal hacia la perdición (Parte 26)"
+  },
+};
