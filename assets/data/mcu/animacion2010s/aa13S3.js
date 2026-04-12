e08c16fa53545d2c6cffc17602f07374300d9547
Guille <cochraneguillermo@gmail.com>
2026-04-11 22:22:56 -0300

    Avengers Assemble (2013) (S3) auditada y corregida

--- assets/data/mcu/animacion2010s/aa13S3.js
+++ assets/data/mcu/animacion2010s/aa13S3.js
@@ -1,9 +1,25 @@
-import { aa13S3E1 } from "./aa13S3/episodes.js";
+import { aa13S3E1, aa13S3E2, aa13S3E3, aa13S3E4, aa13S3E5, aa13S3E6, aa13S3E7, aa13S3E8, aa13S3E9, aa13S3E10, aa13S3E11, aa13S3E12, aa13S3E13, aa13S3E14, aa13S3E15, aa13S3E16, aa13S3E17, aa13S3E18, aa13S3E19, aa13S3E20, aa13S3E21, aa13S3E22, aa13S3E23, aa13S3E24, aa13S3E25, aa13S3E26 } from "./aa13S3/episodes.js";
 
 export const aa13S3 = {
   id: "aa13S3",
   label: "Avengers Assemble (2013) (S3)",
+  format: "Serie",
+  season: 3,
+  release: {
+    year: 2016,
+    month: 3,
+    day: 13
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    aa13S3E1
+    aa13S3E1, aa13S3E2, aa13S3E3, aa13S3E4, aa13S3E5, aa13S3E6, aa13S3E7, aa13S3E8,
+    aa13S3E9, aa13S3E10, aa13S3E11, aa13S3E12, aa13S3E13, aa13S3E14, aa13S3E15, aa13S3E16,
+    aa13S3E17, aa13S3E18, aa13S3E19, aa13S3E20, aa13S3E21, aa13S3E22, aa13S3E23, aa13S3E24,
+    aa13S3E25, aa13S3E26
   ],
 };
--- assets/data/mcu/animacion2010s/aa13S3/episodes.js
+++ assets/data/mcu/animacion2010s/aa13S3/episodes.js
@@ -1,7 +1,259 @@
 export const aa13S3E1 = {
   id: "aa13S3E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Adapting to Change"},
+    en: "Adapting to Change",
+    es: "Adaptándose al cambio"
   },
 };
 
+export const aa13S3E2 = {
+  id: "aa13S3E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Adapting to Change (Part 2)",
+    es: "Adaptándose al cambio (Parte 2)"
+  },
+};
+
+export const aa13S3E3 = {
+  id: "aa13S3E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Adapting to Change (Part 3)",
+    es: "Adaptándose al cambio (Parte 3)"
+  },
+};
+
+export const aa13S3E4 = {
+  id: "aa13S3E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Adapting to Change (Part 4)",
+    es: "Adaptándose al cambio (Parte 4)"
+  },
+};
+
+export const aa13S3E5 = {
+  id: "aa13S3E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Adapting to Change (Part 5)",
+    es: "Adaptándose al cambio (Parte 5)"
+  },
+};
+
+export const aa13S3E6 = {
+  id: "aa13S3E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Adapting to Change (Part 6)",
+    es: "Adaptándose al cambio (Parte 6)"
+  },
+};
+
+export const aa13S3E7 = {
+  id: "aa13S3E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Adapting to Change (Part 7)",
+    es: "Adaptándose al cambio (Parte 7)"
+  },
+};
+
+export const aa13S3E8 = {
+  id: "aa13S3E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Adapting to Change (Part 8)",
+    es: "Adaptándose al cambio (Parte 8)"
+  },
+};
+
+export const aa13S3E9 = {
+  id: "aa13S3E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Adapting to Change (Part 9)",
+    es: "Adaptándose al cambio (Parte 9)"
+  },
+};
+
+export const aa13S3E10 = {
+  id: "aa13S3E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "Adapting to Change (Part 10)",
+    es: "Adaptándose al cambio (Parte 10)"
+  },
+};
+
+export const aa13S3E11 = {
+  id: "aa13S3E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "Adapting to Change (Part 11)",
+    es: "Adaptándose al cambio (Parte 11)"
+  },
+};
+
+export const aa13S3E12 = {
+  id: "aa13S3E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "Adapting to Change (Part 12)",
+    es: "Adaptándose al cambio (Parte 12)"
+  },
+};
+
+export const aa13S3E13 = {
+  id: "aa13S3E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "Adapting to Change (Part 13)",
+    es: "Adaptándose al cambio (Parte 13)"
+  },
+};
+
+export const aa13S3E14 = {
+  id: "aa13S3E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "Adapting to Change (Part 14)",
+    es: "Adaptándose al cambio (Parte 14)"
+  },
+};
+
+export const aa13S3E15 = {
+  id: "aa13S3E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "Adapting to Change (Part 15)",
+    es: "Adaptándose al cambio (Parte 15)"
+  },
+};
+
+export const aa13S3E16 = {
+  id: "aa13S3E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "Adapting to Change (Part 16)",
+    es: "Adaptándose al cambio (Parte 16)"
+  },
+};
+
+export const aa13S3E17 = {
+  id: "aa13S3E17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "Adapting to Change (Part 17)",
+    es: "Adaptándose al cambio (Parte 17)"
+  },
+};
+
+export const aa13S3E18 = {
+  id: "aa13S3E18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "Adapting to Change (Part 18)",
+    es: "Adaptándose al cambio (Parte 18)"
+  },
+};
+
+export const aa13S3E19 = {
+  id: "aa13S3E19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "Adapting to Change (Part 19)",
+    es: "Adaptándose al cambio (Parte 19)"
+  },
+};
+
+export const aa13S3E20 = {
+  id: "aa13S3E20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "Adapting to Change (Part 20)",
+    es: "Adaptándose al cambio (Parte 20)"
+  },
+};
+
+export const aa13S3E21 = {
+  id: "aa13S3E21",
+  number: 21,
+  completed: false,
+  label: {
+    en: "Adapting to Change (Part 21)",
+    es: "Adaptándose al cambio (Parte 21)"
+  },
+};
+
+export const aa13S3E22 = {
+  id: "aa13S3E22",
+  number: 22,
+  completed: false,
+  label: {
+    en: "Adapting to Change (Part 22)",
+    es: "Adaptándose al cambio (Parte 22)"
+  },
+};
+
+export const aa13S3E23 = {
+  id: "aa13S3E23",
+  number: 23,
+  completed: false,
+  label: {
+    en: "Adapting to Change (Part 23)",
+    es: "Adaptándose al cambio (Parte 23)"
+  },
+};
+
+export const aa13S3E24 = {
+  id: "aa13S3E24",
+  number: 24,
+  completed: false,
+  label: {
+    en: "Adapting to Change (Part 24)",
+    es: "Adaptándose al cambio (Parte 24)"
+  },
+};
+
+export const aa13S3E25 = {
+  id: "aa13S3E25",
+  number: 25,
+  completed: false,
+  label: {
+    en: "Adapting to Change (Part 25)",
+    es: "Adaptándose al cambio (Parte 25)"
+  },
+};
+
+export const aa13S3E26 = {
+  id: "aa13S3E26",
+  number: 26,
+  completed: false,
+  label: {
+    en: "Adapting to Change (Part 26)",
+    es: "Adaptándose al cambio (Parte 26)"
+  },
+};
