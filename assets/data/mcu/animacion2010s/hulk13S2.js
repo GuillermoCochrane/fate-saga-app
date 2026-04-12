813052d9026140277f42432f001d13d30a57ba6e
Guille <cochraneguillermo@gmail.com>
2026-04-12 20:31:27 -0300

    Hulk and the Agents of S.M.A.S.H. (2013) (S2) auditada y corregida

--- assets/data/mcu/animacion2010s/hulk13S2.js
+++ assets/data/mcu/animacion2010s/hulk13S2.js
@@ -1,9 +1,25 @@
-import { hulk13S2E1 } from "./hulk13S2/episodes.js";
+import { hulk13S2E1, hulk13S2E2, hulk13S2E3, hulk13S2E4, hulk13S2E5, hulk13S2E6, hulk13S2E7, hulk13S2E8, hulk13S2E9, hulk13S2E10, hulk13S2E11, hulk13S2E12, hulk13S2E13, hulk13S2E14, hulk13S2E15, hulk13S2E16, hulk13S2E17, hulk13S2E18, hulk13S2E19, hulk13S2E20, hulk13S2E21, hulk13S2E22, hulk13S2E23, hulk13S2E24, hulk13S2E25, hulk13S2E26 } from "./hulk13S2/episodes.js";
 
 export const hulk13S2 = {
   id: "hulk13S2",
   label: "Hulk and the Agents of S.M.A.S.H. (2013) (S2)",
+  format: "Serie",
+  season: 2,
+  release: {
+    year: 2014,
+    month: 10,
+    day: 12
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    hulk13S2E1
+    hulk13S2E1, hulk13S2E2, hulk13S2E3, hulk13S2E4, hulk13S2E5, hulk13S2E6, hulk13S2E7, hulk13S2E8,
+    hulk13S2E9, hulk13S2E10, hulk13S2E11, hulk13S2E12, hulk13S2E13, hulk13S2E14, hulk13S2E15, hulk13S2E16,
+    hulk13S2E17, hulk13S2E18, hulk13S2E19, hulk13S2E20, hulk13S2E21, hulk13S2E22, hulk13S2E23, hulk13S2E24,
+    hulk13S2E25, hulk13S2E26
   ],
 };
--- assets/data/mcu/animacion2010s/hulk13S2/episodes.js
+++ assets/data/mcu/animacion2010s/hulk13S2/episodes.js
@@ -1,7 +1,259 @@
 export const hulk13S2E1 = {
   id: "hulk13S2E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Planet Hulk"},
+    en: "Planet Hulk (Part 1)",
+    es: "Planeta Hulk (Parte 1)"
   },
 };
 
+export const hulk13S2E2 = {
+  id: "hulk13S2E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Planet Hulk (Part 2)",
+    es: "Planeta Hulk (Parte 2)"
+  },
+};
+
+export const hulk13S2E3 = {
+  id: "hulk13S2E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Planet Hulk (Part 3)",
+    es: "Planeta Hulk (Parte 3)"
+  },
+};
+
+export const hulk13S2E4 = {
+  id: "hulk13S2E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Planet Hulk (Part 4)",
+    es: "Planeta Hulk (Parte 4)"
+  },
+};
+
+export const hulk13S2E5 = {
+  id: "hulk13S2E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Planet Hulk (Part 5)",
+    es: "Planeta Hulk (Parte 5)"
+  },
+};
+
+export const hulk13S2E6 = {
+  id: "hulk13S2E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Planet Hulk (Part 6)",
+    es: "Planeta Hulk (Parte 6)"
+  },
+};
+
+export const hulk13S2E7 = {
+  id: "hulk13S2E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Planet Hulk (Part 7)",
+    es: "Planeta Hulk (Parte 7)"
+  },
+};
+
+export const hulk13S2E8 = {
+  id: "hulk13S2E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Planet Hulk (Part 8)",
+    es: "Planeta Hulk (Parte 8)"
+  },
+};
+
+export const hulk13S2E9 = {
+  id: "hulk13S2E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Planet Hulk (Part 9)",
+    es: "Planeta Hulk (Parte 9)"
+  },
+};
+
+export const hulk13S2E10 = {
+  id: "hulk13S2E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "Planet Hulk (Part 10)",
+    es: "Planeta Hulk (Parte 10)"
+  },
+};
+
+export const hulk13S2E11 = {
+  id: "hulk13S2E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "Planet Hulk (Part 11)",
+    es: "Planeta Hulk (Parte 11)"
+  },
+};
+
+export const hulk13S2E12 = {
+  id: "hulk13S2E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "Planet Hulk (Part 12)",
+    es: "Planeta Hulk (Parte 12)"
+  },
+};
+
+export const hulk13S2E13 = {
+  id: "hulk13S2E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "Planet Hulk (Part 13)",
+    es: "Planeta Hulk (Parte 13)"
+  },
+};
+
+export const hulk13S2E14 = {
+  id: "hulk13S2E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "Planet Hulk (Part 14)",
+    es: "Planeta Hulk (Parte 14)"
+  },
+};
+
+export const hulk13S2E15 = {
+  id: "hulk13S2E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "Planet Hulk (Part 15)",
+    es: "Planeta Hulk (Parte 15)"
+  },
+};
+
+export const hulk13S2E16 = {
+  id: "hulk13S2E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "Planet Hulk (Part 16)",
+    es: "Planeta Hulk (Parte 16)"
+  },
+};
+
+export const hulk13S2E17 = {
+  id: "hulk13S2E17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "Planet Hulk (Part 17)",
+    es: "Planeta Hulk (Parte 17)"
+  },
+};
+
+export const hulk13S2E18 = {
+  id: "hulk13S2E18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "Planet Hulk (Part 18)",
+    es: "Planeta Hulk (Parte 18)"
+  },
+};
+
+export const hulk13S2E19 = {
+  id: "hulk13S2E19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "Planet Hulk (Part 19)",
+    es: "Planeta Hulk (Parte 19)"
+  },
+};
+
+export const hulk13S2E20 = {
+  id: "hulk13S2E20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "Planet Hulk (Part 20)",
+    es: "Planeta Hulk (Parte 20)"
+  },
+};
+
+export const hulk13S2E21 = {
+  id: "hulk13S2E21",
+  number: 21,
+  completed: false,
+  label: {
+    en: "Planet Hulk (Part 21)",
+    es: "Planeta Hulk (Parte 21)"
+  },
+};
+
+export const hulk13S2E22 = {
+  id: "hulk13S2E22",
+  number: 22,
+  completed: false,
+  label: {
+    en: "Planet Hulk (Part 22)",
+    es: "Planeta Hulk (Parte 22)"
+  },
+};
+
+export const hulk13S2E23 = {
+  id: "hulk13S2E23",
+  number: 23,
+  completed: false,
+  label: {
+    en: "Planet Hulk (Part 23)",
+    es: "Planeta Hulk (Parte 23)"
+  },
+};
+
+export const hulk13S2E24 = {
+  id: "hulk13S2E24",
+  number: 24,
+  completed: false,
+  label: {
+    en: "Planet Hulk (Part 24)",
+    es: "Planeta Hulk (Parte 24)"
+  },
+};
+
+export const hulk13S2E25 = {
+  id: "hulk13S2E25",
+  number: 25,
+  completed: false,
+  label: {
+    en: "Planet Hulk (Part 25)",
+    es: "Planeta Hulk (Parte 25)"
+  },
+};
+
+export const hulk13S2E26 = {
+  id: "hulk13S2E26",
+  number: 26,
+  completed: false,
+  label: {
+    en: "Planet Hulk (Part 26)",
+    es: "Planeta Hulk (Parte 26)"
+  },
+};
