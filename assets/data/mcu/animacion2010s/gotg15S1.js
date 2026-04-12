156d0e8f8dd21fe0679988ad4ab7982467f5ba83
Guille <cochraneguillermo@gmail.com>
2026-04-12 20:33:09 -0300

    Guardians of the Galaxy (2015) (S1) auditada y corregida

--- assets/data/mcu/animacion2010s/gotg15S1.js
+++ assets/data/mcu/animacion2010s/gotg15S1.js
@@ -1,9 +1,25 @@
-import { gotg15S1E1 } from "./gotg15S1/episodes.js";
+import { gotg15S1E1, gotg15S1E2, gotg15S1E3, gotg15S1E4, gotg15S1E5, gotg15S1E6, gotg15S1E7, gotg15S1E8, gotg15S1E9, gotg15S1E10, gotg15S1E11, gotg15S1E12, gotg15S1E13, gotg15S1E14, gotg15S1E15, gotg15S1E16, gotg15S1E17, gotg15S1E18, gotg15S1E19, gotg15S1E20, gotg15S1E21, gotg15S1E22, gotg15S1E23, gotg15S1E24, gotg15S1E25, gotg15S1E26 } from "./gotg15S1/episodes.js";
 
 export const gotg15S1 = {
   id: "gotg15S1",
   label: "Guardians of the Galaxy (2015) (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 2015,
+    month: 9,
+    day: 26
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    gotg15S1E1
+    gotg15S1E1, gotg15S1E2, gotg15S1E3, gotg15S1E4, gotg15S1E5, gotg15S1E6, gotg15S1E7, gotg15S1E8,
+    gotg15S1E9, gotg15S1E10, gotg15S1E11, gotg15S1E12, gotg15S1E13, gotg15S1E14, gotg15S1E15, gotg15S1E16,
+    gotg15S1E17, gotg15S1E18, gotg15S1E19, gotg15S1E20, gotg15S1E21, gotg15S1E22, gotg15S1E23, gotg15S1E24,
+    gotg15S1E25, gotg15S1E26
   ],
 };
--- assets/data/mcu/animacion2010s/gotg15S1/episodes.js
+++ assets/data/mcu/animacion2010s/gotg15S1/episodes.js
@@ -1,7 +1,259 @@
 export const gotg15S1E1 = {
   id: "gotg15S1E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Road to Knowhere"},
+    en: "Road to Knowhere (Part 1)",
+    es: "Camino a Knowhere (Parte 1)"
   },
 };
 
+export const gotg15S1E2 = {
+  id: "gotg15S1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Road to Knowhere (Part 2)",
+    es: "Camino a Knowhere (Parte 2)"
+  },
+};
+
+export const gotg15S1E3 = {
+  id: "gotg15S1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Road to Knowhere (Part 3)",
+    es: "Camino a Knowhere (Parte 3)"
+  },
+};
+
+export const gotg15S1E4 = {
+  id: "gotg15S1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Road to Knowhere (Part 4)",
+    es: "Camino a Knowhere (Parte 4)"
+  },
+};
+
+export const gotg15S1E5 = {
+  id: "gotg15S1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Road to Knowhere (Part 5)",
+    es: "Camino a Knowhere (Parte 5)"
+  },
+};
+
+export const gotg15S1E6 = {
+  id: "gotg15S1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Road to Knowhere (Part 6)",
+    es: "Camino a Knowhere (Parte 6)"
+  },
+};
+
+export const gotg15S1E7 = {
+  id: "gotg15S1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Road to Knowhere (Part 7)",
+    es: "Camino a Knowhere (Parte 7)"
+  },
+};
+
+export const gotg15S1E8 = {
+  id: "gotg15S1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Road to Knowhere (Part 8)",
+    es: "Camino a Knowhere (Parte 8)"
+  },
+};
+
+export const gotg15S1E9 = {
+  id: "gotg15S1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Road to Knowhere (Part 9)",
+    es: "Camino a Knowhere (Parte 9)"
+  },
+};
+
+export const gotg15S1E10 = {
+  id: "gotg15S1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "Road to Knowhere (Part 10)",
+    es: "Camino a Knowhere (Parte 10)"
+  },
+};
+
+export const gotg15S1E11 = {
+  id: "gotg15S1E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "Road to Knowhere (Part 11)",
+    es: "Camino a Knowhere (Parte 11)"
+  },
+};
+
+export const gotg15S1E12 = {
+  id: "gotg15S1E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "Road to Knowhere (Part 12)",
+    es: "Camino a Knowhere (Parte 12)"
+  },
+};
+
+export const gotg15S1E13 = {
+  id: "gotg15S1E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "Road to Knowhere (Part 13)",
+    es: "Camino a Knowhere (Parte 13)"
+  },
+};
+
+export const gotg15S1E14 = {
+  id: "gotg15S1E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "Road to Knowhere (Part 14)",
+    es: "Camino a Knowhere (Parte 14)"
+  },
+};
+
+export const gotg15S1E15 = {
+  id: "gotg15S1E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "Road to Knowhere (Part 15)",
+    es: "Camino a Knowhere (Parte 15)"
+  },
+};
+
+export const gotg15S1E16 = {
+  id: "gotg15S1E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "Road to Knowhere (Part 16)",
+    es: "Camino a Knowhere (Parte 16)"
+  },
+};
+
+export const gotg15S1E17 = {
+  id: "gotg15S1E17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "Road to Knowhere (Part 17)",
+    es: "Camino a Knowhere (Parte 17)"
+  },
+};
+
+export const gotg15S1E18 = {
+  id: "gotg15S1E18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "Road to Knowhere (Part 18)",
+    es: "Camino a Knowhere (Parte 18)"
+  },
+};
+
+export const gotg15S1E19 = {
+  id: "gotg15S1E19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "Road to Knowhere (Part 19)",
+    es: "Camino a Knowhere (Parte 19)"
+  },
+};
+
+export const gotg15S1E20 = {
+  id: "gotg15S1E20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "Road to Knowhere (Part 20)",
+    es: "Camino a Knowhere (Parte 20)"
+  },
+};
+
+export const gotg15S1E21 = {
+  id: "gotg15S1E21",
+  number: 21,
+  completed: false,
+  label: {
+    en: "Road to Knowhere (Part 21)",
+    es: "Camino a Knowhere (Parte 21)"
+  },
+};
+
+export const gotg15S1E22 = {
+  id: "gotg15S1E22",
+  number: 22,
+  completed: false,
+  label: {
+    en: "Road to Knowhere (Part 22)",
+    es: "Camino a Knowhere (Parte 22)"
+  },
+};
+
+export const gotg15S1E23 = {
+  id: "gotg15S1E23",
+  number: 23,
+  completed: false,
+  label: {
+    en: "Road to Knowhere (Part 23)",
+    es: "Camino a Knowhere (Parte 23)"
+  },
+};
+
+export const gotg15S1E24 = {
+  id: "gotg15S1E24",
+  number: 24,
+  completed: false,
+  label: {
+    en: "Road to Knowhere (Part 24)",
+    es: "Camino a Knowhere (Parte 24)"
+  },
+};
+
+export const gotg15S1E25 = {
+  id: "gotg15S1E25",
+  number: 25,
+  completed: false,
+  label: {
+    en: "Road to Knowhere (Part 25)",
+    es: "Camino a Knowhere (Parte 25)"
+  },
+};
+
+export const gotg15S1E26 = {
+  id: "gotg15S1E26",
+  number: 26,
+  completed: false,
+  label: {
+    en: "Road to Knowhere (Part 26)",
+    es: "Camino a Knowhere (Parte 26)"
+  },
+};
