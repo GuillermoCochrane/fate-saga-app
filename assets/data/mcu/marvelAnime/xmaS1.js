b4377d3678d69c711bf5d7d7d8cc51f6f4a921b7
Guille <cochraneguillermo@gmail.com>
2026-04-14 19:36:22 -0300

    X-Men (2011) (S1) auditada y corregida

--- assets/data/mcu/marvelAnime/xmaS1.js
+++ assets/data/mcu/marvelAnime/xmaS1.js
@@ -1,9 +1,23 @@
-import { xmaS1E1 } from "./xmaS1/episodes.js";
+import { xmaS1E1, xmaS1E2, xmaS1E3, xmaS1E4, xmaS1E5, xmaS1E6, xmaS1E7, xmaS1E8, xmaS1E9, xmaS1E10, xmaS1E11, xmaS1E12 } from "./xmaS1/episodes.js";
 
 export const xmaS1 = {
   id: "xmaS1",
   label: "X-Men (2011) (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 2011,
+    month: 4,
+    day: 1
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    xmaS1E1
+    xmaS1E1, xmaS1E2, xmaS1E3, xmaS1E4, xmaS1E5, xmaS1E6,
+    xmaS1E7, xmaS1E8, xmaS1E9, xmaS1E10, xmaS1E11, xmaS1E12
   ],
 };
--- assets/data/mcu/marvelAnime/xmaS1/episodes.js
+++ assets/data/mcu/marvelAnime/xmaS1/episodes.js
@@ -1,7 +1,119 @@
 export const xmaS1E1 = {
   id: "xmaS1E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Birth of the X-Men"},
+    en: "The Birth of the X-Men",
+    es: "El nacimiento de los X-Men"
   },
 };
 
+export const xmaS1E2 = {
+  id: "xmaS1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Birth of the X-Men (Part 2)",
+    es: "El nacimiento de los X-Men (Parte 2)"
+  },
+};
+
+export const xmaS1E3 = {
+  id: "xmaS1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Birth of the X-Men (Part 3)",
+    es: "El nacimiento de los X-Men (Parte 3)"
+  },
+};
+
+export const xmaS1E4 = {
+  id: "xmaS1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Birth of the X-Men (Part 4)",
+    es: "El nacimiento de los X-Men (Parte 4)"
+  },
+};
+
+export const xmaS1E5 = {
+  id: "xmaS1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Birth of the X-Men (Part 5)",
+    es: "El nacimiento de los X-Men (Parte 5)"
+  },
+};
+
+export const xmaS1E6 = {
+  id: "xmaS1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Birth of the X-Men (Part 6)",
+    es: "El nacimiento de los X-Men (Parte 6)"
+  },
+};
+
+export const xmaS1E7 = {
+  id: "xmaS1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The Birth of the X-Men (Part 7)",
+    es: "El nacimiento de los X-Men (Parte 7)"
+  },
+};
+
+export const xmaS1E8 = {
+  id: "xmaS1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Birth of the X-Men (Part 8)",
+    es: "El nacimiento de los X-Men (Parte 8)"
+  },
+};
+
+export const xmaS1E9 = {
+  id: "xmaS1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The Birth of the X-Men (Part 9)",
+    es: "El nacimiento de los X-Men (Parte 9)"
+  },
+};
+
+export const xmaS1E10 = {
+  id: "xmaS1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Birth of the X-Men (Part 10)",
+    es: "El nacimiento de los X-Men (Parte 10)"
+  },
+};
+
+export const xmaS1E11 = {
+  id: "xmaS1E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Birth of the X-Men (Part 11)",
+    es: "El nacimiento de los X-Men (Parte 11)"
+  },
+};
+
+export const xmaS1E12 = {
+  id: "xmaS1E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Birth of the X-Men (Part 12)",
+    es: "El nacimiento de los X-Men (Parte 12)"
+  },
+};
