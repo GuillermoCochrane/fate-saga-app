df22014cc92fd6e59707e76ad22e44634865d126
Guille <cochraneguillermo@gmail.com>
2026-04-14 19:37:17 -0300

    Blade (2011) (S1) auditada y corregida

--- assets/data/mcu/marvelAnime/bldaS1.js
+++ assets/data/mcu/marvelAnime/bldaS1.js
@@ -1,9 +1,23 @@
-import { bldaS1E1 } from "./bldaS1/episodes.js";
+import { bldaS1E1, bldaS1E2, bldaS1E3, bldaS1E4, bldaS1E5, bldaS1E6, bldaS1E7, bldaS1E8, bldaS1E9, bldaS1E10, bldaS1E11, bldaS1E12 } from "./bldaS1/episodes.js";
 
 export const bldaS1 = {
   id: "bldaS1",
   label: "Blade (2011) (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 2011,
+    month: 7,
+    day: 1
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    bldaS1E1
+    bldaS1E1, bldaS1E2, bldaS1E3, bldaS1E4, bldaS1E5, bldaS1E6,
+    bldaS1E7, bldaS1E8, bldaS1E9, bldaS1E10, bldaS1E11, bldaS1E12
   ],
 };
--- assets/data/mcu/marvelAnime/bldaS1/episodes.js
+++ assets/data/mcu/marvelAnime/bldaS1/episodes.js
@@ -1,7 +1,119 @@
 export const bldaS1E1 = {
   id: "bldaS1E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Vampire Hunter"},
+    en: "The Vampire Hunter",
+    es: "El cazador de vampiros"
   },
 };
 
+export const bldaS1E2 = {
+  id: "bldaS1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Vampire Hunter (Part 2)",
+    es: "El cazador de vampiros (Parte 2)"
+  },
+};
+
+export const bldaS1E3 = {
+  id: "bldaS1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Vampire Hunter (Part 3)",
+    es: "El cazador de vampiros (Parte 3)"
+  },
+};
+
+export const bldaS1E4 = {
+  id: "bldaS1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Vampire Hunter (Part 4)",
+    es: "El cazador de vampiros (Parte 4)"
+  },
+};
+
+export const bldaS1E5 = {
+  id: "bldaS1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Vampire Hunter (Part 5)",
+    es: "El cazador de vampiros (Parte 5)"
+  },
+};
+
+export const bldaS1E6 = {
+  id: "bldaS1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Vampire Hunter (Part 6)",
+    es: "El cazador de vampiros (Parte 6)"
+  },
+};
+
+export const bldaS1E7 = {
+  id: "bldaS1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The Vampire Hunter (Part 7)",
+    es: "El cazador de vampiros (Parte 7)"
+  },
+};
+
+export const bldaS1E8 = {
+  id: "bldaS1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Vampire Hunter (Part 8)",
+    es: "El cazador de vampiros (Parte 8)"
+  },
+};
+
+export const bldaS1E9 = {
+  id: "bldaS1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The Vampire Hunter (Part 9)",
+    es: "El cazador de vampiros (Parte 9)"
+  },
+};
+
+export const bldaS1E10 = {
+  id: "bldaS1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Vampire Hunter (Part 10)",
+    es: "El cazador de vampiros (Parte 10)"
+  },
+};
+
+export const bldaS1E11 = {
+  id: "bldaS1E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Vampire Hunter (Part 11)",
+    es: "El cazador de vampiros (Parte 11)"
+  },
+};
+
+export const bldaS1E12 = {
+  id: "bldaS1E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Vampire Hunter (Part 12)",
+    es: "El cazador de vampiros (Parte 12)"
+  },
+};
