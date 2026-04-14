5fa2ab83d97a6fbca20b913b827002f284313df4
Guille <cochraneguillermo@gmail.com>
2026-04-13 22:12:37 -0300

    Marvel Future Avengers (2017) (S1) auditada y corregida

--- assets/data/mcu/animacion2010s/fa17S1.js
+++ assets/data/mcu/animacion2010s/fa17S1.js
@@ -1,9 +1,25 @@
-import { fa17S1E1 } from "./fa17S1/episodes.js";
+import { fa17S1E1, fa17S1E2, fa17S1E3, fa17S1E4, fa17S1E5, fa17S1E6, fa17S1E7, fa17S1E8, fa17S1E9, fa17S1E10, fa17S1E11, fa17S1E12, fa17S1E13, fa17S1E14, fa17S1E15, fa17S1E16, fa17S1E17, fa17S1E18, fa17S1E19, fa17S1E20, fa17S1E21, fa17S1E22, fa17S1E23, fa17S1E24, fa17S1E25, fa17S1E26 } from "./fa17S1/episodes.js";
 
 export const fa17S1 = {
   id: "fa17S1",
   label: "Marvel Future Avengers (2017) (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 2017,
+    month: 7,
+    day: 22
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    fa17S1E1
+    fa17S1E1, fa17S1E2, fa17S1E3, fa17S1E4, fa17S1E5, fa17S1E6, fa17S1E7, fa17S1E8,
+    fa17S1E9, fa17S1E10, fa17S1E11, fa17S1E12, fa17S1E13, fa17S1E14, fa17S1E15, fa17S1E16,
+    fa17S1E17, fa17S1E18, fa17S1E19, fa17S1E20, fa17S1E21, fa17S1E22, fa17S1E23, fa17S1E24,
+    fa17S1E25, fa17S1E26
   ],
 };
--- assets/data/mcu/animacion2010s/fa17S1/episodes.js
+++ assets/data/mcu/animacion2010s/fa17S1/episodes.js
@@ -1,7 +1,259 @@
 export const fa17S1E1 = {
   id: "fa17S1E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Assemble! Future Avengers"},
+    en: "Assemble! Future Avengers (Part 1)",
+    es: "¡Reúnanse! Future Avengers (Parte 1)"
   },
 };
 
+export const fa17S1E2 = {
+  id: "fa17S1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Assemble! Future Avengers (Part 2)",
+    es: "¡Reúnanse! Future Avengers (Parte 2)"
+  },
+};
+
+export const fa17S1E3 = {
+  id: "fa17S1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Assemble! Future Avengers (Part 3)",
+    es: "¡Reúnanse! Future Avengers (Parte 3)"
+  },
+};
+
+export const fa17S1E4 = {
+  id: "fa17S1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Assemble! Future Avengers (Part 4)",
+    es: "¡Reúnanse! Future Avengers (Parte 4)"
+  },
+};
+
+export const fa17S1E5 = {
+  id: "fa17S1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Assemble! Future Avengers (Part 5)",
+    es: "¡Reúnanse! Future Avengers (Parte 5)"
+  },
+};
+
+export const fa17S1E6 = {
+  id: "fa17S1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Assemble! Future Avengers (Part 6)",
+    es: "¡Reúnanse! Future Avengers (Parte 6)"
+  },
+};
+
+export const fa17S1E7 = {
+  id: "fa17S1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Assemble! Future Avengers (Part 7)",
+    es: "¡Reúnanse! Future Avengers (Parte 7)"
+  },
+};
+
+export const fa17S1E8 = {
+  id: "fa17S1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Assemble! Future Avengers (Part 8)",
+    es: "¡Reúnanse! Future Avengers (Parte 8)"
+  },
+};
+
+export const fa17S1E9 = {
+  id: "fa17S1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Assemble! Future Avengers (Part 9)",
+    es: "¡Reúnanse! Future Avengers (Parte 9)"
+  },
+};
+
+export const fa17S1E10 = {
+  id: "fa17S1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "Assemble! Future Avengers (Part 10)",
+    es: "¡Reúnanse! Future Avengers (Parte 10)"
+  },
+};
+
+export const fa17S1E11 = {
+  id: "fa17S1E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "Assemble! Future Avengers (Part 11)",
+    es: "¡Reúnanse! Future Avengers (Parte 11)"
+  },
+};
+
+export const fa17S1E12 = {
+  id: "fa17S1E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "Assemble! Future Avengers (Part 12)",
+    es: "¡Reúnanse! Future Avengers (Parte 12)"
+  },
+};
+
+export const fa17S1E13 = {
+  id: "fa17S1E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "Assemble! Future Avengers (Part 13)",
+    es: "¡Reúnanse! Future Avengers (Parte 13)"
+  },
+};
+
+export const fa17S1E14 = {
+  id: "fa17S1E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "Assemble! Future Avengers (Part 14)",
+    es: "¡Reúnanse! Future Avengers (Parte 14)"
+  },
+};
+
+export const fa17S1E15 = {
+  id: "fa17S1E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "Assemble! Future Avengers (Part 15)",
+    es: "¡Reúnanse! Future Avengers (Parte 15)"
+  },
+};
+
+export const fa17S1E16 = {
+  id: "fa17S1E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "Assemble! Future Avengers (Part 16)",
+    es: "¡Reúnanse! Future Avengers (Parte 16)"
+  },
+};
+
+export const fa17S1E17 = {
+  id: "fa17S1E17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "Assemble! Future Avengers (Part 17)",
+    es: "¡Reúnanse! Future Avengers (Parte 17)"
+  },
+};
+
+export const fa17S1E18 = {
+  id: "fa17S1E18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "Assemble! Future Avengers (Part 18)",
+    es: "¡Reúnanse! Future Avengers (Parte 18)"
+  },
+};
+
+export const fa17S1E19 = {
+  id: "fa17S1E19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "Assemble! Future Avengers (Part 19)",
+    es: "¡Reúnanse! Future Avengers (Parte 19)"
+  },
+};
+
+export const fa17S1E20 = {
+  id: "fa17S1E20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "Assemble! Future Avengers (Part 20)",
+    es: "¡Reúnanse! Future Avengers (Parte 20)"
+  },
+};
+
+export const fa17S1E21 = {
+  id: "fa17S1E21",
+  number: 21,
+  completed: false,
+  label: {
+    en: "Assemble! Future Avengers (Part 21)",
+    es: "¡Reúnanse! Future Avengers (Parte 21)"
+  },
+};
+
+export const fa17S1E22 = {
+  id: "fa17S1E22",
+  number: 22,
+  completed: false,
+  label: {
+    en: "Assemble! Future Avengers (Part 22)",
+    es: "¡Reúnanse! Future Avengers (Parte 22)"
+  },
+};
+
+export const fa17S1E23 = {
+  id: "fa17S1E23",
+  number: 23,
+  completed: false,
+  label: {
+    en: "Assemble! Future Avengers (Part 23)",
+    es: "¡Reúnanse! Future Avengers (Parte 23)"
+  },
+};
+
+export const fa17S1E24 = {
+  id: "fa17S1E24",
+  number: 24,
+  completed: false,
+  label: {
+    en: "Assemble! Future Avengers (Part 24)",
+    es: "¡Reúnanse! Future Avengers (Parte 24)"
+  },
+};
+
+export const fa17S1E25 = {
+  id: "fa17S1E25",
+  number: 25,
+  completed: false,
+  label: {
+    en: "Assemble! Future Avengers (Part 25)",
+    es: "¡Reúnanse! Future Avengers (Parte 25)"
+  },
+};
+
+export const fa17S1E26 = {
+  id: "fa17S1E26",
+  number: 26,
+  completed: false,
+  label: {
+    en: "Assemble! Future Avengers (Part 26)",
+    es: "¡Reúnanse! Future Avengers (Parte 26)"
+  },
+};
