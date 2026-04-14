02af8e3af6e5b72a1770643b7d805d4133ca97e5
Guille <cochraneguillermo@gmail.com>
2026-04-13 22:55:32 -0300

    Iron Man (2010) (S1) auditada y corregida

--- assets/data/mcu/marvelAnime/imaS1.js
+++ assets/data/mcu/marvelAnime/imaS1.js
@@ -1,9 +1,23 @@
-import { imaS1E1 } from "./imaS1/episodes.js";
+import { imaS1E1, imaS1E2, imaS1E3, imaS1E4, imaS1E5, imaS1E6, imaS1E7, imaS1E8, imaS1E9, imaS1E10, imaS1E11, imaS1E12 } from "./imaS1/episodes.js";
 
 export const imaS1 = {
   id: "imaS1",
   label: "Iron Man (2010) (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 2010,
+    month: 10,
+    day: 1
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    imaS1E1
+    imaS1E1, imaS1E2, imaS1E3, imaS1E4, imaS1E5, imaS1E6,
+    imaS1E7, imaS1E8, imaS1E9, imaS1E10, imaS1E11, imaS1E12
   ],
 };
--- assets/data/mcu/marvelAnime/imaS1/episodes.js
+++ assets/data/mcu/marvelAnime/imaS1/episodes.js
@@ -1,7 +1,119 @@
 export const imaS1E1 = {
   id: "imaS1E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Iron Man"},
+    en: "The Iron Man",
+    es: "El Iron Man"
   },
 };
 
+export const imaS1E2 = {
+  id: "imaS1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Iron Man (Part 2)",
+    es: "El Iron Man (Parte 2)"
+  },
+};
+
+export const imaS1E3 = {
+  id: "imaS1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Iron Man (Part 3)",
+    es: "El Iron Man (Parte 3)"
+  },
+};
+
+export const imaS1E4 = {
+  id: "imaS1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Iron Man (Part 4)",
+    es: "El Iron Man (Parte 4)"
+  },
+};
+
+export const imaS1E5 = {
+  id: "imaS1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Iron Man (Part 5)",
+    es: "El Iron Man (Parte 5)"
+  },
+};
+
+export const imaS1E6 = {
+  id: "imaS1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Iron Man (Part 6)",
+    es: "El Iron Man (Parte 6)"
+  },
+};
+
+export const imaS1E7 = {
+  id: "imaS1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The Iron Man (Part 7)",
+    es: "El Iron Man (Parte 7)"
+  },
+};
+
+export const imaS1E8 = {
+  id: "imaS1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Iron Man (Part 8)",
+    es: "El Iron Man (Parte 8)"
+  },
+};
+
+export const imaS1E9 = {
+  id: "imaS1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The Iron Man (Part 9)",
+    es: "El Iron Man (Parte 9)"
+  },
+};
+
+export const imaS1E10 = {
+  id: "imaS1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Iron Man (Part 10)",
+    es: "El Iron Man (Parte 10)"
+  },
+};
+
+export const imaS1E11 = {
+  id: "imaS1E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Iron Man (Part 11)",
+    es: "El Iron Man (Parte 11)"
+  },
+};
+
+export const imaS1E12 = {
+  id: "imaS1E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Iron Man (Part 12)",
+    es: "El Iron Man (Parte 12)"
+  },
+};
