0bb1d09bea43e0d1a1ed114aa0079827efcdf01c
Guille <cochraneguillermo@gmail.com>
2026-04-13 22:13:57 -0300

    Marvel Future Avengers (2017) (S2) auditada y corregida

--- assets/data/mcu/animacion2010s/fa17S2.js
+++ assets/data/mcu/animacion2010s/fa17S2.js
@@ -1,9 +1,25 @@
-import { fa17S2E1 } from "./fa17S2/episodes.js";
+import { fa17S2E1, fa17S2E2, fa17S2E3, fa17S2E4, fa17S2E5, fa17S2E6, fa17S2E7, fa17S2E8, fa17S2E9, fa17S2E10, fa17S2E11, fa17S2E12, fa17S2E13, fa17S2E14, fa17S2E15, fa17S2E16, fa17S2E17, fa17S2E18, fa17S2E19, fa17S2E20, fa17S2E21, fa17S2E22, fa17S2E23, fa17S2E24, fa17S2E25, fa17S2E26 } from "./fa17S2/episodes.js";
 
 export const fa17S2 = {
   id: "fa17S2",
   label: "Marvel Future Avengers (2017) (S2)",
+  format: "Serie",
+  season: 2,
+  release: {
+    year: 2018,
+    month: 6,
+    day: 30
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    fa17S2E1
+    fa17S2E1, fa17S2E2, fa17S2E3, fa17S2E4, fa17S2E5, fa17S2E6, fa17S2E7, fa17S2E8,
+    fa17S2E9, fa17S2E10, fa17S2E11, fa17S2E12, fa17S2E13, fa17S2E14, fa17S2E15, fa17S2E16,
+    fa17S2E17, fa17S2E18, fa17S2E19, fa17S2E20, fa17S2E21, fa17S2E22, fa17S2E23, fa17S2E24,
+    fa17S2E25, fa17S2E26
   ],
 };
--- assets/data/mcu/animacion2010s/fa17S2/episodes.js
+++ assets/data/mcu/animacion2010s/fa17S2/episodes.js
@@ -1,7 +1,259 @@
 export const fa17S2E1 = {
   id: "fa17S2E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Tomorrow War"},
+    en: "The Tomorrow War (Part 1)",
+    es: "La guerra del mañana (Parte 1)"
   },
 };
 
+export const fa17S2E2 = {
+  id: "fa17S2E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Tomorrow War (Part 2)",
+    es: "La guerra del mañana (Parte 2)"
+  },
+};
+
+export const fa17S2E3 = {
+  id: "fa17S2E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Tomorrow War (Part 3)",
+    es: "La guerra del mañana (Parte 3)"
+  },
+};
+
+export const fa17S2E4 = {
+  id: "fa17S2E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Tomorrow War (Part 4)",
+    es: "La guerra del mañana (Parte 4)"
+  },
+};
+
+export const fa17S2E5 = {
+  id: "fa17S2E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Tomorrow War (Part 5)",
+    es: "La guerra del mañana (Parte 5)"
+  },
+};
+
+export const fa17S2E6 = {
+  id: "fa17S2E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Tomorrow War (Part 6)",
+    es: "La guerra del mañana (Parte 6)"
+  },
+};
+
+export const fa17S2E7 = {
+  id: "fa17S2E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The Tomorrow War (Part 7)",
+    es: "La guerra del mañana (Parte 7)"
+  },
+};
+
+export const fa17S2E8 = {
+  id: "fa17S2E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Tomorrow War (Part 8)",
+    es: "La guerra del mañana (Parte 8)"
+  },
+};
+
+export const fa17S2E9 = {
+  id: "fa17S2E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The Tomorrow War (Part 9)",
+    es: "La guerra del mañana (Parte 9)"
+  },
+};
+
+export const fa17S2E10 = {
+  id: "fa17S2E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Tomorrow War (Part 10)",
+    es: "La guerra del mañana (Parte 10)"
+  },
+};
+
+export const fa17S2E11 = {
+  id: "fa17S2E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Tomorrow War (Part 11)",
+    es: "La guerra del mañana (Parte 11)"
+  },
+};
+
+export const fa17S2E12 = {
+  id: "fa17S2E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Tomorrow War (Part 12)",
+    es: "La guerra del mañana (Parte 12)"
+  },
+};
+
+export const fa17S2E13 = {
+  id: "fa17S2E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "The Tomorrow War (Part 13)",
+    es: "La guerra del mañana (Parte 13)"
+  },
+};
+
+export const fa17S2E14 = {
+  id: "fa17S2E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "The Tomorrow War (Part 14)",
+    es: "La guerra del mañana (Parte 14)"
+  },
+};
+
+export const fa17S2E15 = {
+  id: "fa17S2E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "The Tomorrow War (Part 15)",
+    es: "La guerra del mañana (Parte 15)"
+  },
+};
+
+export const fa17S2E16 = {
+  id: "fa17S2E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "The Tomorrow War (Part 16)",
+    es: "La guerra del mañana (Parte 16)"
+  },
+};
+
+export const fa17S2E17 = {
+  id: "fa17S2E17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "The Tomorrow War (Part 17)",
+    es: "La guerra del mañana (Parte 17)"
+  },
+};
+
+export const fa17S2E18 = {
+  id: "fa17S2E18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "The Tomorrow War (Part 18)",
+    es: "La guerra del mañana (Parte 18)"
+  },
+};
+
+export const fa17S2E19 = {
+  id: "fa17S2E19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "The Tomorrow War (Part 19)",
+    es: "La guerra del mañana (Parte 19)"
+  },
+};
+
+export const fa17S2E20 = {
+  id: "fa17S2E20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "The Tomorrow War (Part 20)",
+    es: "La guerra del mañana (Parte 20)"
+  },
+};
+
+export const fa17S2E21 = {
+  id: "fa17S2E21",
+  number: 21,
+  completed: false,
+  label: {
+    en: "The Tomorrow War (Part 21)",
+    es: "La guerra del mañana (Parte 21)"
+  },
+};
+
+export const fa17S2E22 = {
+  id: "fa17S2E22",
+  number: 22,
+  completed: false,
+  label: {
+    en: "The Tomorrow War (Part 22)",
+    es: "La guerra del mañana (Parte 22)"
+  },
+};
+
+export const fa17S2E23 = {
+  id: "fa17S2E23",
+  number: 23,
+  completed: false,
+  label: {
+    en: "The Tomorrow War (Part 23)",
+    es: "La guerra del mañana (Parte 23)"
+  },
+};
+
+export const fa17S2E24 = {
+  id: "fa17S2E24",
+  number: 24,
+  completed: false,
+  label: {
+    en: "The Tomorrow War (Part 24)",
+    es: "La guerra del mañana (Parte 24)"
+  },
+};
+
+export const fa17S2E25 = {
+  id: "fa17S2E25",
+  number: 25,
+  completed: false,
+  label: {
+    en: "The Tomorrow War (Part 25)",
+    es: "La guerra del mañana (Parte 25)"
+  },
+};
+
+export const fa17S2E26 = {
+  id: "fa17S2E26",
+  number: 26,
+  completed: false,
+  label: {
+    en: "The Tomorrow War (Part 26)",
+    es: "La guerra del mañana (Parte 26)"
+  },
+};
