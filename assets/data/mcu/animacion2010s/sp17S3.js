ee390351c96c481afc877aa87234d53650d7720c
Guille <cochraneguillermo@gmail.com>
2026-04-13 22:11:06 -0300

    Marvel's Spider-Man (2017) (S3) auditada y corregida

--- assets/data/mcu/animacion2010s/sp17S3.js
+++ assets/data/mcu/animacion2010s/sp17S3.js
@@ -1,9 +1,25 @@
-import { sp17S3E1 } from "./sp17S3/episodes.js";
+import { sp17S3E1, sp17S3E2, sp17S3E3, sp17S3E4, sp17S3E5, sp17S3E6, sp17S3E7, sp17S3E8, sp17S3E9, sp17S3E10, sp17S3E11, sp17S3E12, sp17S3E13, sp17S3E14, sp17S3E15, sp17S3E16, sp17S3E17, sp17S3E18, sp17S3E19, sp17S3E20, sp17S3E21, sp17S3E22, sp17S3E23, sp17S3E24, sp17S3E25, sp17S3E26 } from "./sp17S3/episodes.js";
 
 export const sp17S3 = {
   id: "sp17S3",
   label: "Marvel's Spider-Man (2017) (S3)",
+  format: "Serie",
+  season: 3,
+  release: {
+    year: 2019,
+    month: 2,
+    day: 10
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    sp17S3E1
+    sp17S3E1, sp17S3E2, sp17S3E3, sp17S3E4, sp17S3E5, sp17S3E6, sp17S3E7, sp17S3E8,
+    sp17S3E9, sp17S3E10, sp17S3E11, sp17S3E12, sp17S3E13, sp17S3E14, sp17S3E15, sp17S3E16,
+    sp17S3E17, sp17S3E18, sp17S3E19, sp17S3E20, sp17S3E21, sp17S3E22, sp17S3E23, sp17S3E24,
+    sp17S3E25, sp17S3E26
   ],
 };
--- assets/data/mcu/animacion2010s/sp17S3/episodes.js
+++ assets/data/mcu/animacion2010s/sp17S3/episodes.js
@@ -1,7 +1,259 @@
 export const sp17S3E1 = {
   id: "sp17S3E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Web of Venom"},
+    en: "Web of Venom (Part 1)",
+    es: "La telaraña de Venom (Parte 1)"
   },
 };
 
+export const sp17S3E2 = {
+  id: "sp17S3E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Web of Venom (Part 2)",
+    es: "La telaraña de Venom (Parte 2)"
+  },
+};
+
+export const sp17S3E3 = {
+  id: "sp17S3E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Web of Venom (Part 3)",
+    es: "La telaraña de Venom (Parte 3)"
+  },
+};
+
+export const sp17S3E4 = {
+  id: "sp17S3E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Web of Venom (Part 4)",
+    es: "La telaraña de Venom (Parte 4)"
+  },
+};
+
+export const sp17S3E5 = {
+  id: "sp17S3E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Web of Venom (Part 5)",
+    es: "La telaraña de Venom (Parte 5)"
+  },
+};
+
+export const sp17S3E6 = {
+  id: "sp17S3E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Web of Venom (Part 6)",
+    es: "La telaraña de Venom (Parte 6)"
+  },
+};
+
+export const sp17S3E7 = {
+  id: "sp17S3E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Web of Venom (Part 7)",
+    es: "La telaraña de Venom (Parte 7)"
+  },
+};
+
+export const sp17S3E8 = {
+  id: "sp17S3E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Web of Venom (Part 8)",
+    es: "La telaraña de Venom (Parte 8)"
+  },
+};
+
+export const sp17S3E9 = {
+  id: "sp17S3E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Web of Venom (Part 9)",
+    es: "La telaraña de Venom (Parte 9)"
+  },
+};
+
+export const sp17S3E10 = {
+  id: "sp17S3E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "Web of Venom (Part 10)",
+    es: "La telaraña de Venom (Parte 10)"
+  },
+};
+
+export const sp17S3E11 = {
+  id: "sp17S3E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "Web of Venom (Part 11)",
+    es: "La telaraña de Venom (Parte 11)"
+  },
+};
+
+export const sp17S3E12 = {
+  id: "sp17S3E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "Web of Venom (Part 12)",
+    es: "La telaraña de Venom (Parte 12)"
+  },
+};
+
+export const sp17S3E13 = {
+  id: "sp17S3E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "Web of Venom (Part 13)",
+    es: "La telaraña de Venom (Parte 13)"
+  },
+};
+
+export const sp17S3E14 = {
+  id: "sp17S3E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "Web of Venom (Part 14)",
+    es: "La telaraña de Venom (Parte 14)"
+  },
+};
+
+export const sp17S3E15 = {
+  id: "sp17S3E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "Web of Venom (Part 15)",
+    es: "La telaraña de Venom (Parte 15)"
+  },
+};
+
+export const sp17S3E16 = {
+  id: "sp17S3E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "Web of Venom (Part 16)",
+    es: "La telaraña de Venom (Parte 16)"
+  },
+};
+
+export const sp17S3E17 = {
+  id: "sp17S3E17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "Web of Venom (Part 17)",
+    es: "La telaraña de Venom (Parte 17)"
+  },
+};
+
+export const sp17S3E18 = {
+  id: "sp17S3E18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "Web of Venom (Part 18)",
+    es: "La telaraña de Venom (Parte 18)"
+  },
+};
+
+export const sp17S3E19 = {
+  id: "sp17S3E19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "Web of Venom (Part 19)",
+    es: "La telaraña de Venom (Parte 19)"
+  },
+};
+
+export const sp17S3E20 = {
+  id: "sp17S3E20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "Web of Venom (Part 20)",
+    es: "La telaraña de Venom (Parte 20)"
+  },
+};
+
+export const sp17S3E21 = {
+  id: "sp17S3E21",
+  number: 21,
+  completed: false,
+  label: {
+    en: "Web of Venom (Part 21)",
+    es: "La telaraña de Venom (Parte 21)"
+  },
+};
+
+export const sp17S3E22 = {
+  id: "sp17S3E22",
+  number: 22,
+  completed: false,
+  label: {
+    en: "Web of Venom (Part 22)",
+    es: "La telaraña de Venom (Parte 22)"
+  },
+};
+
+export const sp17S3E23 = {
+  id: "sp17S3E23",
+  number: 23,
+  completed: false,
+  label: {
+    en: "Web of Venom (Part 23)",
+    es: "La telaraña de Venom (Parte 23)"
+  },
+};
+
+export const sp17S3E24 = {
+  id: "sp17S3E24",
+  number: 24,
+  completed: false,
+  label: {
+    en: "Web of Venom (Part 24)",
+    es: "La telaraña de Venom (Parte 24)"
+  },
+};
+
+export const sp17S3E25 = {
+  id: "sp17S3E25",
+  number: 25,
+  completed: false,
+  label: {
+    en: "Web of Venom (Part 25)",
+    es: "La telaraña de Venom (Parte 25)"
+  },
+};
+
+export const sp17S3E26 = {
+  id: "sp17S3E26",
+  number: 26,
+  completed: false,
+  label: {
+    en: "Web of Venom (Part 26)",
+    es: "La telaraña de Venom (Parte 26)"
+  },
+};
