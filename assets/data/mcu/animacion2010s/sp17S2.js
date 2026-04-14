436ad11b3d93a836090e7ee6f2d7f9b608fcfe5b
Guille <cochraneguillermo@gmail.com>
2026-04-13 22:09:43 -0300

    Marvel's Spider-Man (2017) (S2) auditada y corregida

--- assets/data/mcu/animacion2010s/sp17S2.js
+++ assets/data/mcu/animacion2010s/sp17S2.js
@@ -1,9 +1,25 @@
-import { sp17S2E1 } from "./sp17S2/episodes.js";
+import { sp17S2E1, sp17S2E2, sp17S2E3, sp17S2E4, sp17S2E5, sp17S2E6, sp17S2E7, sp17S2E8, sp17S2E9, sp17S2E10, sp17S2E11, sp17S2E12, sp17S2E13, sp17S2E14, sp17S2E15, sp17S2E16, sp17S2E17, sp17S2E18, sp17S2E19, sp17S2E20, sp17S2E21, sp17S2E22, sp17S2E23, sp17S2E24, sp17S2E25, sp17S2E26 } from "./sp17S2/episodes.js";
 
 export const sp17S2 = {
   id: "sp17S2",
   label: "Marvel's Spider-Man (2017) (S2)",
+  format: "Serie",
+  season: 2,
+  release: {
+    year: 2018,
+    month: 6,
+    day: 18
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    sp17S2E1
+    sp17S2E1, sp17S2E2, sp17S2E3, sp17S2E4, sp17S2E5, sp17S2E6, sp17S2E7, sp17S2E8,
+    sp17S2E9, sp17S2E10, sp17S2E11, sp17S2E12, sp17S2E13, sp17S2E14, sp17S2E15, sp17S2E16,
+    sp17S2E17, sp17S2E18, sp17S2E19, sp17S2E20, sp17S2E21, sp17S2E22, sp17S2E23, sp17S2E24,
+    sp17S2E25, sp17S2E26
   ],
 };
--- assets/data/mcu/animacion2010s/sp17S2/episodes.js
+++ assets/data/mcu/animacion2010s/sp17S2/episodes.js
@@ -1,7 +1,259 @@
 export const sp17S2E1 = {
   id: "sp17S2E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Rise of Doc Ock (Part 1)"},
+    en: "The Rise of Doc Ock (Part 1)",
+    es: "El ascenso de Doc Ock (Parte 1)"
   },
 };
 
+export const sp17S2E2 = {
+  id: "sp17S2E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Rise of Doc Ock (Part 2)",
+    es: "El ascenso de Doc Ock (Parte 2)"
+  },
+};
+
+export const sp17S2E3 = {
+  id: "sp17S2E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Rise of Doc Ock (Part 3)",
+    es: "El ascenso de Doc Ock (Parte 3)"
+  },
+};
+
+export const sp17S2E4 = {
+  id: "sp17S2E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Rise of Doc Ock (Part 4)",
+    es: "El ascenso de Doc Ock (Parte 4)"
+  },
+};
+
+export const sp17S2E5 = {
+  id: "sp17S2E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Rise of Doc Ock (Part 5)",
+    es: "El ascenso de Doc Ock (Parte 5)"
+  },
+};
+
+export const sp17S2E6 = {
+  id: "sp17S2E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Rise of Doc Ock (Part 6)",
+    es: "El ascenso de Doc Ock (Parte 6)"
+  },
+};
+
+export const sp17S2E7 = {
+  id: "sp17S2E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The Rise of Doc Ock (Part 7)",
+    es: "El ascenso de Doc Ock (Parte 7)"
+  },
+};
+
+export const sp17S2E8 = {
+  id: "sp17S2E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Rise of Doc Ock (Part 8)",
+    es: "El ascenso de Doc Ock (Parte 8)"
+  },
+};
+
+export const sp17S2E9 = {
+  id: "sp17S2E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The Rise of Doc Ock (Part 9)",
+    es: "El ascenso de Doc Ock (Parte 9)"
+  },
+};
+
+export const sp17S2E10 = {
+  id: "sp17S2E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Rise of Doc Ock (Part 10)",
+    es: "El ascenso de Doc Ock (Parte 10)"
+  },
+};
+
+export const sp17S2E11 = {
+  id: "sp17S2E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Rise of Doc Ock (Part 11)",
+    es: "El ascenso de Doc Ock (Parte 11)"
+  },
+};
+
+export const sp17S2E12 = {
+  id: "sp17S2E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Rise of Doc Ock (Part 12)",
+    es: "El ascenso de Doc Ock (Parte 12)"
+  },
+};
+
+export const sp17S2E13 = {
+  id: "sp17S2E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "The Rise of Doc Ock (Part 13)",
+    es: "El ascenso de Doc Ock (Parte 13)"
+  },
+};
+
+export const sp17S2E14 = {
+  id: "sp17S2E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "The Rise of Doc Ock (Part 14)",
+    es: "El ascenso de Doc Ock (Parte 14)"
+  },
+};
+
+export const sp17S2E15 = {
+  id: "sp17S2E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "The Rise of Doc Ock (Part 15)",
+    es: "El ascenso de Doc Ock (Parte 15)"
+  },
+};
+
+export const sp17S2E16 = {
+  id: "sp17S2E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "The Rise of Doc Ock (Part 16)",
+    es: "El ascenso de Doc Ock (Parte 16)"
+  },
+};
+
+export const sp17S2E17 = {
+  id: "sp17S2E17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "The Rise of Doc Ock (Part 17)",
+    es: "El ascenso de Doc Ock (Parte 17)"
+  },
+};
+
+export const sp17S2E18 = {
+  id: "sp17S2E18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "The Rise of Doc Ock (Part 18)",
+    es: "El ascenso de Doc Ock (Parte 18)"
+  },
+};
+
+export const sp17S2E19 = {
+  id: "sp17S2E19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "The Rise of Doc Ock (Part 19)",
+    es: "El ascenso de Doc Ock (Parte 19)"
+  },
+};
+
+export const sp17S2E20 = {
+  id: "sp17S2E20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "The Rise of Doc Ock (Part 20)",
+    es: "El ascenso de Doc Ock (Parte 20)"
+  },
+};
+
+export const sp17S2E21 = {
+  id: "sp17S2E21",
+  number: 21,
+  completed: false,
+  label: {
+    en: "The Rise of Doc Ock (Part 21)",
+    es: "El ascenso de Doc Ock (Parte 21)"
+  },
+};
+
+export const sp17S2E22 = {
+  id: "sp17S2E22",
+  number: 22,
+  completed: false,
+  label: {
+    en: "The Rise of Doc Ock (Part 22)",
+    es: "El ascenso de Doc Ock (Parte 22)"
+  },
+};
+
+export const sp17S2E23 = {
+  id: "sp17S2E23",
+  number: 23,
+  completed: false,
+  label: {
+    en: "The Rise of Doc Ock (Part 23)",
+    es: "El ascenso de Doc Ock (Parte 23)"
+  },
+};
+
+export const sp17S2E24 = {
+  id: "sp17S2E24",
+  number: 24,
+  completed: false,
+  label: {
+    en: "The Rise of Doc Ock (Part 24)",
+    es: "El ascenso de Doc Ock (Parte 24)"
+  },
+};
+
+export const sp17S2E25 = {
+  id: "sp17S2E25",
+  number: 25,
+  completed: false,
+  label: {
+    en: "The Rise of Doc Ock (Part 25)",
+    es: "El ascenso de Doc Ock (Parte 25)"
+  },
+};
+
+export const sp17S2E26 = {
+  id: "sp17S2E26",
+  number: 26,
+  completed: false,
+  label: {
+    en: "The Rise of Doc Ock (Part 26)",
+    es: "El ascenso de Doc Ock (Parte 26)"
+  },
+};
