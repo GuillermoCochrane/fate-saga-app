cc297433e12a2a3e118eb8fc7b852e5471e7bacf
Guille <cochraneguillermo@gmail.com>
2026-04-14 19:34:53 -0300

    Wolverine (2011) (S1) auditada y corregida

--- assets/data/mcu/marvelAnime/wolaS1.js
+++ assets/data/mcu/marvelAnime/wolaS1.js
@@ -1,9 +1,23 @@
-import { wolaS1E1 } from "./wolaS1/episodes.js";
+import { wolaS1E1, wolaS1E2, wolaS1E3, wolaS1E4, wolaS1E5, wolaS1E6, wolaS1E7, wolaS1E8, wolaS1E9, wolaS1E10, wolaS1E11, wolaS1E12 } from "./wolaS1/episodes.js";
 
 export const wolaS1 = {
   id: "wolaS1",
   label: "Wolverine (2011) (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 2011,
+    month: 1,
+    day: 7
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    wolaS1E1
+    wolaS1E1, wolaS1E2, wolaS1E3, wolaS1E4, wolaS1E5, wolaS1E6,
+    wolaS1E7, wolaS1E8, wolaS1E9, wolaS1E10, wolaS1E11, wolaS1E12
   ],
 };
--- assets/data/mcu/marvelAnime/wolaS1/episodes.js
+++ assets/data/mcu/marvelAnime/wolaS1/episodes.js
@@ -1,7 +1,119 @@
 export const wolaS1E1 = {
   id: "wolaS1E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Wolverine"},
+    en: "The Wolverine",
+    es: "El Wolverine"
   },
 };
 
+export const wolaS1E2 = {
+  id: "wolaS1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Wolverine (Part 2)",
+    es: "El Wolverine (Parte 2)"
+  },
+};
+
+export const wolaS1E3 = {
+  id: "wolaS1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Wolverine (Part 3)",
+    es: "El Wolverine (Parte 3)"
+  },
+};
+
+export const wolaS1E4 = {
+  id: "wolaS1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Wolverine (Part 4)",
+    es: "El Wolverine (Parte 4)"
+  },
+};
+
+export const wolaS1E5 = {
+  id: "wolaS1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Wolverine (Part 5)",
+    es: "El Wolverine (Parte 5)"
+  },
+};
+
+export const wolaS1E6 = {
+  id: "wolaS1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Wolverine (Part 6)",
+    es: "El Wolverine (Parte 6)"
+  },
+};
+
+export const wolaS1E7 = {
+  id: "wolaS1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The Wolverine (Part 7)",
+    es: "El Wolverine (Parte 7)"
+  },
+};
+
+export const wolaS1E8 = {
+  id: "wolaS1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Wolverine (Part 8)",
+    es: "El Wolverine (Parte 8)"
+  },
+};
+
+export const wolaS1E9 = {
+  id: "wolaS1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The Wolverine (Part 9)",
+    es: "El Wolverine (Parte 9)"
+  },
+};
+
+export const wolaS1E10 = {
+  id: "wolaS1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Wolverine (Part 10)",
+    es: "El Wolverine (Parte 10)"
+  },
+};
+
+export const wolaS1E11 = {
+  id: "wolaS1E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Wolverine (Part 11)",
+    es: "El Wolverine (Parte 11)"
+  },
+};
+
+export const wolaS1E12 = {
+  id: "wolaS1E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Wolverine (Part 12)",
+    es: "El Wolverine (Parte 12)"
+  },
+};
