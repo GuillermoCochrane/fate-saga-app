7d4c0a82374e87b8eebea3b4298e37023a897095
Guille <cochraneguillermo@gmail.com>
2026-04-16 06:13:48 -0300

    Mutant X (2001) (S1) auditada y corregida

--- assets/data/mcu/rarezasLiveaction/mxS1.js
+++ assets/data/mcu/rarezasLiveaction/mxS1.js
@@ -1,9 +1,24 @@
-import { mxS1E1 } from "./mxS1/episodes.js";
+import { mxS1E1, mxS1E2, mxS1E3, mxS1E4, mxS1E5, mxS1E6, mxS1E7, mxS1E8, mxS1E9, mxS1E10, mxS1E11, mxS1E12, mxS1E13, mxS1E14, mxS1E15, mxS1E16, mxS1E17, mxS1E18, mxS1E19, mxS1E20, mxS1E21, mxS1E22 } from "./mxS1/episodes.js";
 
 export const mxS1 = {
   id: "mxS1",
   label: "Mutant X (2001) (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 2001,
+    month: 10,
+    day: 6
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    mxS1E1
+    mxS1E1, mxS1E2, mxS1E3, mxS1E4, mxS1E5, mxS1E6, mxS1E7, mxS1E8,
+    mxS1E9, mxS1E10, mxS1E11, mxS1E12, mxS1E13, mxS1E14, mxS1E15, mxS1E16,
+    mxS1E17, mxS1E18, mxS1E19, mxS1E20, mxS1E21, mxS1E22
   ],
 };
--- assets/data/mcu/rarezasLiveaction/mxS1/episodes.js
+++ assets/data/mcu/rarezasLiveaction/mxS1/episodes.js
@@ -1,7 +1,219 @@
 export const mxS1E1 = {
   id: "mxS1E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Shock of the New"},
+    en: "The Shock of the New",
+    es: "El impacto de lo nuevo"
   },
 };
 
+export const mxS1E2 = {
+  id: "mxS1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Shock of the New (Part 2)",
+    es: "El impacto de lo nuevo (Parte 2)"
+  },
+};
+
+export const mxS1E3 = {
+  id: "mxS1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Shock of the New (Part 3)",
+    es: "El impacto de lo nuevo (Parte 3)"
+  },
+};
+
+export const mxS1E4 = {
+  id: "mxS1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Shock of the New (Part 4)",
+    es: "El impacto de lo nuevo (Parte 4)"
+  },
+};
+
+export const mxS1E5 = {
+  id: "mxS1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Shock of the New (Part 5)",
+    es: "El impacto de lo nuevo (Parte 5)"
+  },
+};
+
+export const mxS1E6 = {
+  id: "mxS1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Shock of the New (Part 6)",
+    es: "El impacto de lo nuevo (Parte 6)"
+  },
+};
+
+export const mxS1E7 = {
+  id: "mxS1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The Shock of the New (Part 7)",
+    es: "El impacto de lo nuevo (Parte 7)"
+  },
+};
+
+export const mxS1E8 = {
+  id: "mxS1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Shock of the New (Part 8)",
+    es: "El impacto de lo nuevo (Parte 8)"
+  },
+};
+
+export const mxS1E9 = {
+  id: "mxS1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The Shock of the New (Part 9)",
+    es: "El impacto de lo nuevo (Parte 9)"
+  },
+};
+
+export const mxS1E10 = {
+  id: "mxS1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Shock of the New (Part 10)",
+    es: "El impacto de lo nuevo (Parte 10)"
+  },
+};
+
+export const mxS1E11 = {
+  id: "mxS1E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Shock of the New (Part 11)",
+    es: "El impacto de lo nuevo (Parte 11)"
+  },
+};
+
+export const mxS1E12 = {
+  id: "mxS1E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Shock of the New (Part 12)",
+    es: "El impacto de lo nuevo (Parte 12)"
+  },
+};
+
+export const mxS1E13 = {
+  id: "mxS1E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "The Shock of the New (Part 13)",
+    es: "El impacto de lo nuevo (Parte 13)"
+  },
+};
+
+export const mxS1E14 = {
+  id: "mxS1E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "The Shock of the New (Part 14)",
+    es: "El impacto de lo nuevo (Parte 14)"
+  },
+};
+
+export const mxS1E15 = {
+  id: "mxS1E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "The Shock of the New (Part 15)",
+    es: "El impacto de lo nuevo (Parte 15)"
+  },
+};
+
+export const mxS1E16 = {
+  id: "mxS1E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "The Shock of the New (Part 16)",
+    es: "El impacto de lo nuevo (Parte 16)"
+  },
+};
+
+export const mxS1E17 = {
+  id: "mxS1E17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "The Shock of the New (Part 17)",
+    es: "El impacto de lo nuevo (Parte 17)"
+  },
+};
+
+export const mxS1E18 = {
+  id: "mxS1E18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "The Shock of the New (Part 18)",
+    es: "El impacto de lo nuevo (Parte 18)"
+  },
+};
+
+export const mxS1E19 = {
+  id: "mxS1E19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "The Shock of the New (Part 19)",
+    es: "El impacto de lo nuevo (Parte 19)"
+  },
+};
+
+export const mxS1E20 = {
+  id: "mxS1E20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "The Shock of the New (Part 20)",
+    es: "El impacto de lo nuevo (Parte 20)"
+  },
+};
+
+export const mxS1E21 = {
+  id: "mxS1E21",
+  number: 21,
+  completed: false,
+  label: {
+    en: "The Shock of the New (Part 21)",
+    es: "El impacto de lo nuevo (Parte 21)"
+  },
+};
+
+export const mxS1E22 = {
+  id: "mxS1E22",
+  number: 22,
+  completed: false,
+  label: {
+    en: "The Shock of the New (Part 22)",
+    es: "El impacto de lo nuevo (Parte 22)"
+  },
+};
