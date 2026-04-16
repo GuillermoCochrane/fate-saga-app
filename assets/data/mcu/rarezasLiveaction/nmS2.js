ce2ef9e1c251ad0910dc26b861287366dce5b402
Guille <cochraneguillermo@gmail.com>
2026-04-16 06:12:43 -0300

    Night Man (1997) (S2) auditada y corregida

--- assets/data/mcu/rarezasLiveaction/nmS2.js
+++ assets/data/mcu/rarezasLiveaction/nmS2.js
@@ -1,9 +1,24 @@
-import { nmS2E1 } from "./nmS2/episodes.js";
+import { nmS2E1, nmS2E2, nmS2E3, nmS2E4, nmS2E5, nmS2E6, nmS2E7, nmS2E8, nmS2E9, nmS2E10, nmS2E11, nmS2E12, nmS2E13, nmS2E14, nmS2E15, nmS2E16, nmS2E17, nmS2E18, nmS2E19, nmS2E20, nmS2E21, nmS2E22 } from "./nmS2/episodes.js";
 
 export const nmS2 = {
   id: "nmS2",
   label: "Night Man (1997) (S2)",
+  format: "Serie",
+  season: 2,
+  release: {
+    year: 1998,
+    month: 10,
+    day: 4
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    nmS2E1
+    nmS2E1, nmS2E2, nmS2E3, nmS2E4, nmS2E5, nmS2E6, nmS2E7, nmS2E8,
+    nmS2E9, nmS2E10, nmS2E11, nmS2E12, nmS2E13, nmS2E14, nmS2E15, nmS2E16,
+    nmS2E17, nmS2E18, nmS2E19, nmS2E20, nmS2E21, nmS2E22
   ],
 };
--- assets/data/mcu/rarezasLiveaction/nmS2/episodes.js
+++ assets/data/mcu/rarezasLiveaction/nmS2/episodes.js
@@ -1,7 +1,219 @@
 export const nmS2E1 = {
   id: "nmS2E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"The Enemy Within"},
+    en: "The Enemy Within",
+    es: "El enemigo interno"
   },
 };
 
+export const nmS2E2 = {
+  id: "nmS2E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "The Enemy Within (Part 2)",
+    es: "El enemigo interno (Parte 2)"
+  },
+};
+
+export const nmS2E3 = {
+  id: "nmS2E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "The Enemy Within (Part 3)",
+    es: "El enemigo interno (Parte 3)"
+  },
+};
+
+export const nmS2E4 = {
+  id: "nmS2E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "The Enemy Within (Part 4)",
+    es: "El enemigo interno (Parte 4)"
+  },
+};
+
+export const nmS2E5 = {
+  id: "nmS2E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "The Enemy Within (Part 5)",
+    es: "El enemigo interno (Parte 5)"
+  },
+};
+
+export const nmS2E6 = {
+  id: "nmS2E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "The Enemy Within (Part 6)",
+    es: "El enemigo interno (Parte 6)"
+  },
+};
+
+export const nmS2E7 = {
+  id: "nmS2E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "The Enemy Within (Part 7)",
+    es: "El enemigo interno (Parte 7)"
+  },
+};
+
+export const nmS2E8 = {
+  id: "nmS2E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "The Enemy Within (Part 8)",
+    es: "El enemigo interno (Parte 8)"
+  },
+};
+
+export const nmS2E9 = {
+  id: "nmS2E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "The Enemy Within (Part 9)",
+    es: "El enemigo interno (Parte 9)"
+  },
+};
+
+export const nmS2E10 = {
+  id: "nmS2E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "The Enemy Within (Part 10)",
+    es: "El enemigo interno (Parte 10)"
+  },
+};
+
+export const nmS2E11 = {
+  id: "nmS2E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "The Enemy Within (Part 11)",
+    es: "El enemigo interno (Parte 11)"
+  },
+};
+
+export const nmS2E12 = {
+  id: "nmS2E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "The Enemy Within (Part 12)",
+    es: "El enemigo interno (Parte 12)"
+  },
+};
+
+export const nmS2E13 = {
+  id: "nmS2E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "The Enemy Within (Part 13)",
+    es: "El enemigo interno (Parte 13)"
+  },
+};
+
+export const nmS2E14 = {
+  id: "nmS2E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "The Enemy Within (Part 14)",
+    es: "El enemigo interno (Parte 14)"
+  },
+};
+
+export const nmS2E15 = {
+  id: "nmS2E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "The Enemy Within (Part 15)",
+    es: "El enemigo interno (Parte 15)"
+  },
+};
+
+export const nmS2E16 = {
+  id: "nmS2E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "The Enemy Within (Part 16)",
+    es: "El enemigo interno (Parte 16)"
+  },
+};
+
+export const nmS2E17 = {
+  id: "nmS2E17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "The Enemy Within (Part 17)",
+    es: "El enemigo interno (Parte 17)"
+  },
+};
+
+export const nmS2E18 = {
+  id: "nmS2E18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "The Enemy Within (Part 18)",
+    es: "El enemigo interno (Parte 18)"
+  },
+};
+
+export const nmS2E19 = {
+  id: "nmS2E19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "The Enemy Within (Part 19)",
+    es: "El enemigo interno (Parte 19)"
+  },
+};
+
+export const nmS2E20 = {
+  id: "nmS2E20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "The Enemy Within (Part 20)",
+    es: "El enemigo interno (Parte 20)"
+  },
+};
+
+export const nmS2E21 = {
+  id: "nmS2E21",
+  number: 21,
+  completed: false,
+  label: {
+    en: "The Enemy Within (Part 21)",
+    es: "El enemigo interno (Parte 21)"
+  },
+};
+
+export const nmS2E22 = {
+  id: "nmS2E22",
+  number: 22,
+  completed: false,
+  label: {
+    en: "The Enemy Within (Part 22)",
+    es: "El enemigo interno (Parte 22)"
+  },
+};
