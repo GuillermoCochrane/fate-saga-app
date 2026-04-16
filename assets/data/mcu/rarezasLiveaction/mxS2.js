ad315f054b407c91e652ab44f7894ebcd256804b
Guille <cochraneguillermo@gmail.com>
2026-04-16 06:15:12 -0300

    Mutant X (2001) (S2) auditada y corregida

--- assets/data/mcu/rarezasLiveaction/mxS2.js
+++ assets/data/mcu/rarezasLiveaction/mxS2.js
@@ -1,9 +1,24 @@
-import { mxS2E1 } from "./mxS2/episodes.js";
+import { mxS2E1, mxS2E2, mxS2E3, mxS2E4, mxS2E5, mxS2E6, mxS2E7, mxS2E8, mxS2E9, mxS2E10, mxS2E11, mxS2E12, mxS2E13, mxS2E14, mxS2E15, mxS2E16, mxS2E17, mxS2E18, mxS2E19, mxS2E20, mxS2E21, mxS2E22 } from "./mxS2/episodes.js";
 
 export const mxS2 = {
   id: "mxS2",
   label: "Mutant X (2001) (S2)",
+  format: "Serie",
+  season: 2,
+  release: {
+    year: 2002,
+    month: 10,
+    day: 5
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    mxS2E1
+    mxS2E1, mxS2E2, mxS2E3, mxS2E4, mxS2E5, mxS2E6, mxS2E7, mxS2E8,
+    mxS2E9, mxS2E10, mxS2E11, mxS2E12, mxS2E13, mxS2E14, mxS2E15, mxS2E16,
+    mxS2E17, mxS2E18, mxS2E19, mxS2E20, mxS2E21, mxS2E22
   ],
 };
--- assets/data/mcu/rarezasLiveaction/mxS2/episodes.js
+++ assets/data/mcu/rarezasLiveaction/mxS2/episodes.js
@@ -1,7 +1,219 @@
 export const mxS2E1 = {
   id: "mxS2E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Past as Prologue"},
+    en: "Past as Prologue",
+    es: "El pasado como prólogo"
   },
 };
 
+export const mxS2E2 = {
+  id: "mxS2E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Past as Prologue (Part 2)",
+    es: "El pasado como prólogo (Parte 2)"
+  },
+};
+
+export const mxS2E3 = {
+  id: "mxS2E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Past as Prologue (Part 3)",
+    es: "El pasado como prólogo (Parte 3)"
+  },
+};
+
+export const mxS2E4 = {
+  id: "mxS2E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Past as Prologue (Part 4)",
+    es: "El pasado como prólogo (Parte 4)"
+  },
+};
+
+export const mxS2E5 = {
+  id: "mxS2E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Past as Prologue (Part 5)",
+    es: "El pasado como prólogo (Parte 5)"
+  },
+};
+
+export const mxS2E6 = {
+  id: "mxS2E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Past as Prologue (Part 6)",
+    es: "El pasado como prólogo (Parte 6)"
+  },
+};
+
+export const mxS2E7 = {
+  id: "mxS2E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Past as Prologue (Part 7)",
+    es: "El pasado como prólogo (Parte 7)"
+  },
+};
+
+export const mxS2E8 = {
+  id: "mxS2E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Past as Prologue (Part 8)",
+    es: "El pasado como prólogo (Parte 8)"
+  },
+};
+
+export const mxS2E9 = {
+  id: "mxS2E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Past as Prologue (Part 9)",
+    es: "El pasado como prólogo (Parte 9)"
+  },
+};
+
+export const mxS2E10 = {
+  id: "mxS2E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "Past as Prologue (Part 10)",
+    es: "El pasado como prólogo (Parte 10)"
+  },
+};
+
+export const mxS2E11 = {
+  id: "mxS2E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "Past as Prologue (Part 11)",
+    es: "El pasado como prólogo (Parte 11)"
+  },
+};
+
+export const mxS2E12 = {
+  id: "mxS2E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "Past as Prologue (Part 12)",
+    es: "El pasado como prólogo (Parte 12)"
+  },
+};
+
+export const mxS2E13 = {
+  id: "mxS2E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "Past as Prologue (Part 13)",
+    es: "El pasado como prólogo (Parte 13)"
+  },
+};
+
+export const mxS2E14 = {
+  id: "mxS2E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "Past as Prologue (Part 14)",
+    es: "El pasado como prólogo (Parte 14)"
+  },
+};
+
+export const mxS2E15 = {
+  id: "mxS2E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "Past as Prologue (Part 15)",
+    es: "El pasado como prólogo (Parte 15)"
+  },
+};
+
+export const mxS2E16 = {
+  id: "mxS2E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "Past as Prologue (Part 16)",
+    es: "El pasado como prólogo (Parte 16)"
+  },
+};
+
+export const mxS2E17 = {
+  id: "mxS2E17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "Past as Prologue (Part 17)",
+    es: "El pasado como prólogo (Parte 17)"
+  },
+};
+
+export const mxS2E18 = {
+  id: "mxS2E18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "Past as Prologue (Part 18)",
+    es: "El pasado como prólogo (Parte 18)"
+  },
+};
+
+export const mxS2E19 = {
+  id: "mxS2E19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "Past as Prologue (Part 19)",
+    es: "El pasado como prólogo (Parte 19)"
+  },
+};
+
+export const mxS2E20 = {
+  id: "mxS2E20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "Past as Prologue (Part 20)",
+    es: "El pasado como prólogo (Parte 20)"
+  },
+};
+
+export const mxS2E21 = {
+  id: "mxS2E21",
+  number: 21,
+  completed: false,
+  label: {
+    en: "Past as Prologue (Part 21)",
+    es: "El pasado como prólogo (Parte 21)"
+  },
+};
+
+export const mxS2E22 = {
+  id: "mxS2E22",
+  number: 22,
+  completed: false,
+  label: {
+    en: "Past as Prologue (Part 22)",
+    es: "El pasado como prólogo (Parte 22)"
+  },
+};
