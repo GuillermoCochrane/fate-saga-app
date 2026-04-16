12869bda95f819299e2634c2dd1fafa124bfbbfd
Guille <cochraneguillermo@gmail.com>
2026-04-15 21:57:09 -0300

    Night Man (1997) (S1) auditada y corregida

--- assets/data/mcu/rarezasLiveaction/nmS1.js
+++ assets/data/mcu/rarezasLiveaction/nmS1.js
@@ -1,9 +1,24 @@
-import { nmS1E1 } from "./nmS1/episodes.js";
+import { nmS1E1, nmS1E2, nmS1E3, nmS1E4, nmS1E5, nmS1E6, nmS1E7, nmS1E8, nmS1E9, nmS1E10, nmS1E11, nmS1E12, nmS1E13, nmS1E14, nmS1E15, nmS1E16, nmS1E17, nmS1E18, nmS1E19, nmS1E20, nmS1E21, nmS1E22 } from "./nmS1/episodes.js";
 
 export const nmS1 = {
   id: "nmS1",
   label: "Night Man (1997) (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 1997,
+    month: 9,
+    day: 21
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    nmS1E1
+    nmS1E1, nmS1E2, nmS1E3, nmS1E4, nmS1E5, nmS1E6, nmS1E7, nmS1E8,
+    nmS1E9, nmS1E10, nmS1E11, nmS1E12, nmS1E13, nmS1E14, nmS1E15, nmS1E16,
+    nmS1E17, nmS1E18, nmS1E19, nmS1E20, nmS1E21, nmS1E22
   ],
 };
--- assets/data/mcu/rarezasLiveaction/nmS1/episodes.js
+++ assets/data/mcu/rarezasLiveaction/nmS1/episodes.js
@@ -1,7 +1,219 @@
 export const nmS1E1 = {
   id: "nmS1E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Pilot"},
+    en: "Pilot",
+    es: "Piloto"
   },
 };
 
+export const nmS1E2 = {
+  id: "nmS1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Pilot (Part 2)",
+    es: "Piloto (Parte 2)"
+  },
+};
+
+export const nmS1E3 = {
+  id: "nmS1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Pilot (Part 3)",
+    es: "Piloto (Parte 3)"
+  },
+};
+
+export const nmS1E4 = {
+  id: "nmS1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Pilot (Part 4)",
+    es: "Piloto (Parte 4)"
+  },
+};
+
+export const nmS1E5 = {
+  id: "nmS1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Pilot (Part 5)",
+    es: "Piloto (Parte 5)"
+  },
+};
+
+export const nmS1E6 = {
+  id: "nmS1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Pilot (Part 6)",
+    es: "Piloto (Parte 6)"
+  },
+};
+
+export const nmS1E7 = {
+  id: "nmS1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Pilot (Part 7)",
+    es: "Piloto (Parte 7)"
+  },
+};
+
+export const nmS1E8 = {
+  id: "nmS1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Pilot (Part 8)",
+    es: "Piloto (Parte 8)"
+  },
+};
+
+export const nmS1E9 = {
+  id: "nmS1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Pilot (Part 9)",
+    es: "Piloto (Parte 9)"
+  },
+};
+
+export const nmS1E10 = {
+  id: "nmS1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "Pilot (Part 10)",
+    es: "Piloto (Parte 10)"
+  },
+};
+
+export const nmS1E11 = {
+  id: "nmS1E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "Pilot (Part 11)",
+    es: "Piloto (Parte 11)"
+  },
+};
+
+export const nmS1E12 = {
+  id: "nmS1E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "Pilot (Part 12)",
+    es: "Piloto (Parte 12)"
+  },
+};
+
+export const nmS1E13 = {
+  id: "nmS1E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "Pilot (Part 13)",
+    es: "Piloto (Parte 13)"
+  },
+};
+
+export const nmS1E14 = {
+  id: "nmS1E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "Pilot (Part 14)",
+    es: "Piloto (Parte 14)"
+  },
+};
+
+export const nmS1E15 = {
+  id: "nmS1E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "Pilot (Part 15)",
+    es: "Piloto (Parte 15)"
+  },
+};
+
+export const nmS1E16 = {
+  id: "nmS1E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "Pilot (Part 16)",
+    es: "Piloto (Parte 16)"
+  },
+};
+
+export const nmS1E17 = {
+  id: "nmS1E17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "Pilot (Part 17)",
+    es: "Piloto (Parte 17)"
+  },
+};
+
+export const nmS1E18 = {
+  id: "nmS1E18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "Pilot (Part 18)",
+    es: "Piloto (Parte 18)"
+  },
+};
+
+export const nmS1E19 = {
+  id: "nmS1E19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "Pilot (Part 19)",
+    es: "Piloto (Parte 19)"
+  },
+};
+
+export const nmS1E20 = {
+  id: "nmS1E20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "Pilot (Part 20)",
+    es: "Piloto (Parte 20)"
+  },
+};
+
+export const nmS1E21 = {
+  id: "nmS1E21",
+  number: 21,
+  completed: false,
+  label: {
+    en: "Pilot (Part 21)",
+    es: "Piloto (Parte 21)"
+  },
+};
+
+export const nmS1E22 = {
+  id: "nmS1E22",
+  number: 22,
+  completed: false,
+  label: {
+    en: "Pilot (Part 22)",
+    es: "Piloto (Parte 22)"
+  },
+};
