dc638c6d3a1b8a197f580f5a867ccc80a6d7992f
Guille <cochraneguillermo@gmail.com>
2026-04-16 06:17:15 -0300

    Mutant X (2001) (S3) auditada y corregida

--- assets/data/mcu/rarezasLiveaction/mxS3.js
+++ assets/data/mcu/rarezasLiveaction/mxS3.js
@@ -1,9 +1,24 @@
-import { mxS3E1 } from "./mxS3/episodes.js";
+import { mxS3E1, mxS3E2, mxS3E3, mxS3E4, mxS3E5, mxS3E6, mxS3E7, mxS3E8, mxS3E9, mxS3E10, mxS3E11, mxS3E12, mxS3E13, mxS3E14, mxS3E15, mxS3E16, mxS3E17, mxS3E18, mxS3E19, mxS3E20, mxS3E21, mxS3E22 } from "./mxS3/episodes.js";
 
 export const mxS3 = {
   id: "mxS3",
   label: "Mutant X (2001) (S3)",
+  format: "Serie",
+  season: 3,
+  release: {
+    year: 2003,
+    month: 10,
+    day: 4
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    mxS3E1
+    mxS3E1, mxS3E2, mxS3E3, mxS3E4, mxS3E5, mxS3E6, mxS3E7, mxS3E8,
+    mxS3E9, mxS3E10, mxS3E11, mxS3E12, mxS3E13, mxS3E14, mxS3E15, mxS3E16,
+    mxS3E17, mxS3E18, mxS3E19, mxS3E20, mxS3E21, mxS3E22
   ],
 };
--- assets/data/mcu/rarezasLiveaction/mxS3/episodes.js
+++ assets/data/mcu/rarezasLiveaction/mxS3/episodes.js
@@ -1,7 +1,219 @@
 export const mxS3E1 = {
   id: "mxS3E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"Into the Moonless Night"},
+    en: "Into the Moonless Night",
+    es: "En la noche sin luna"
   },
 };
 
+export const mxS3E2 = {
+  id: "mxS3E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Into the Moonless Night (Part 2)",
+    es: "En la noche sin luna (Parte 2)"
+  },
+};
+
+export const mxS3E3 = {
+  id: "mxS3E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Into the Moonless Night (Part 3)",
+    es: "En la noche sin luna (Parte 3)"
+  },
+};
+
+export const mxS3E4 = {
+  id: "mxS3E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Into the Moonless Night (Part 4)",
+    es: "En la noche sin luna (Parte 4)"
+  },
+};
+
+export const mxS3E5 = {
+  id: "mxS3E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Into the Moonless Night (Part 5)",
+    es: "En la noche sin luna (Parte 5)"
+  },
+};
+
+export const mxS3E6 = {
+  id: "mxS3E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Into the Moonless Night (Part 6)",
+    es: "En la noche sin luna (Parte 6)"
+  },
+};
+
+export const mxS3E7 = {
+  id: "mxS3E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Into the Moonless Night (Part 7)",
+    es: "En la noche sin luna (Parte 7)"
+  },
+};
+
+export const mxS3E8 = {
+  id: "mxS3E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Into the Moonless Night (Part 8)",
+    es: "En la noche sin luna (Parte 8)"
+  },
+};
+
+export const mxS3E9 = {
+  id: "mxS3E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Into the Moonless Night (Part 9)",
+    es: "En la noche sin luna (Parte 9)"
+  },
+};
+
+export const mxS3E10 = {
+  id: "mxS3E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "Into the Moonless Night (Part 10)",
+    es: "En la noche sin luna (Parte 10)"
+  },
+};
+
+export const mxS3E11 = {
+  id: "mxS3E11",
+  number: 11,
+  completed: false,
+  label: {
+    en: "Into the Moonless Night (Part 11)",
+    es: "En la noche sin luna (Parte 11)"
+  },
+};
+
+export const mxS3E12 = {
+  id: "mxS3E12",
+  number: 12,
+  completed: false,
+  label: {
+    en: "Into the Moonless Night (Part 12)",
+    es: "En la noche sin luna (Parte 12)"
+  },
+};
+
+export const mxS3E13 = {
+  id: "mxS3E13",
+  number: 13,
+  completed: false,
+  label: {
+    en: "Into the Moonless Night (Part 13)",
+    es: "En la noche sin luna (Parte 13)"
+  },
+};
+
+export const mxS3E14 = {
+  id: "mxS3E14",
+  number: 14,
+  completed: false,
+  label: {
+    en: "Into the Moonless Night (Part 14)",
+    es: "En la noche sin luna (Parte 14)"
+  },
+};
+
+export const mxS3E15 = {
+  id: "mxS3E15",
+  number: 15,
+  completed: false,
+  label: {
+    en: "Into the Moonless Night (Part 15)",
+    es: "En la noche sin luna (Parte 15)"
+  },
+};
+
+export const mxS3E16 = {
+  id: "mxS3E16",
+  number: 16,
+  completed: false,
+  label: {
+    en: "Into the Moonless Night (Part 16)",
+    es: "En la noche sin luna (Parte 16)"
+  },
+};
+
+export const mxS3E17 = {
+  id: "mxS3E17",
+  number: 17,
+  completed: false,
+  label: {
+    en: "Into the Moonless Night (Part 17)",
+    es: "En la noche sin luna (Parte 17)"
+  },
+};
+
+export const mxS3E18 = {
+  id: "mxS3E18",
+  number: 18,
+  completed: false,
+  label: {
+    en: "Into the Moonless Night (Part 18)",
+    es: "En la noche sin luna (Parte 18)"
+  },
+};
+
+export const mxS3E19 = {
+  id: "mxS3E19",
+  number: 19,
+  completed: false,
+  label: {
+    en: "Into the Moonless Night (Part 19)",
+    es: "En la noche sin luna (Parte 19)"
+  },
+};
+
+export const mxS3E20 = {
+  id: "mxS3E20",
+  number: 20,
+  completed: false,
+  label: {
+    en: "Into the Moonless Night (Part 20)",
+    es: "En la noche sin luna (Parte 20)"
+  },
+};
+
+export const mxS3E21 = {
+  id: "mxS3E21",
+  number: 21,
+  completed: false,
+  label: {
+    en: "Into the Moonless Night (Part 21)",
+    es: "En la noche sin luna (Parte 21)"
+  },
+};
+
+export const mxS3E22 = {
+  id: "mxS3E22",
+  number: 22,
+  completed: false,
+  label: {
+    en: "Into the Moonless Night (Part 22)",
+    es: "En la noche sin luna (Parte 22)"
+  },
+};
