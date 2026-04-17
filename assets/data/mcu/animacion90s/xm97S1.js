dc1bccdca200c7f9bdb1889623b5ded7259c8acd
Guille <cochraneguillermo@gmail.com>
2026-04-17 05:54:05 -0300

    X-Men '97 (2024) (S1) auditada y corregida

--- assets/data/mcu/animacion90s/xm97S1.js
+++ assets/data/mcu/animacion90s/xm97S1.js
@@ -1,9 +1,23 @@
-import { xm97S1E1 } from "./xm97S1/episodes.js";
+import { xm97S1E1, xm97S1E2, xm97S1E3, xm97S1E4, xm97S1E5, xm97S1E6, xm97S1E7, xm97S1E8, xm97S1E9, xm97S1E10 } from "./xm97S1/episodes.js";
 
 export const xm97S1 = {
   id: "xm97S1",
   label: "X-Men '97 (2024) (S1)",
+  format: "Serie",
+  season: 1,
+  release: {
+    year: 2024,
+    month: 3,
+    day: 20
+  },
+  year_in_timeline: {
+    start: null,
+    end: null
+  },
+  completed: false,
+  opened: false,
   episodes: [
-    xm97S1E1
+    xm97S1E1, xm97S1E2, xm97S1E3, xm97S1E4, xm97S1E5,
+    xm97S1E6, xm97S1E7, xm97S1E8, xm97S1E9, xm97S1E10
   ],
 };
--- assets/data/mcu/animacion90s/xm97S1/episodes.js
+++ assets/data/mcu/animacion90s/xm97S1/episodes.js
@@ -1,7 +1,99 @@
 export const xm97S1E1 = {
   id: "xm97S1E1",
+  number: 1,
+  completed: false,
   label: {
-    en: {"en":"To Me, My X-Men"},
+    en: "To Me, My X-Men",
+    es: "A mí, mis X-Men"
   },
 };
 
+export const xm97S1E2 = {
+  id: "xm97S1E2",
+  number: 2,
+  completed: false,
+  label: {
+    en: "Mutant Liberation Begins",
+    es: "Comienza la liberación mutante"
+  },
+};
+
+export const xm97S1E3 = {
+  id: "xm97S1E3",
+  number: 3,
+  completed: false,
+  label: {
+    en: "Fire Made Flesh",
+    es: "Fuego hecho carne"
+  },
+};
+
+export const xm97S1E4 = {
+  id: "xm97S1E4",
+  number: 4,
+  completed: false,
+  label: {
+    en: "Motendo / Lifedeath (Part 1)",
+    es: "Motendo / Vida muerte (Parte 1)"
+  },
+};
+
+export const xm97S1E5 = {
+  id: "xm97S1E5",
+  number: 5,
+  completed: false,
+  label: {
+    en: "Remember It",
+    es: "Recuérdalo"
+  },
+};
+
+export const xm97S1E6 = {
+  id: "xm97S1E6",
+  number: 6,
+  completed: false,
+  label: {
+    en: "Lifedeath (Part 2)",
+    es: "Vida muerte (Parte 2)"
+  },
+};
+
+export const xm97S1E7 = {
+  id: "xm97S1E7",
+  number: 7,
+  completed: false,
+  label: {
+    en: "Bright Eyes",
+    es: "Ojos brillantes"
+  },
+};
+
+export const xm97S1E8 = {
+  id: "xm97S1E8",
+  number: 8,
+  completed: false,
+  label: {
+    en: "Tolerance is Extinction (Part 1)",
+    es: "La tolerancia es extinción (Parte 1)"
+  },
+};
+
+export const xm97S1E9 = {
+  id: "xm97S1E9",
+  number: 9,
+  completed: false,
+  label: {
+    en: "Tolerance is Extinction (Part 2)",
+    es: "La tolerancia es extinción (Parte 2)"
+  },
+};
+
+export const xm97S1E10 = {
+  id: "xm97S1E10",
+  number: 10,
+  completed: false,
+  label: {
+    en: "Tolerance is Extinction (Part 3)",
+    es: "La tolerancia es extinción (Parte 3)"
+  },
+};
