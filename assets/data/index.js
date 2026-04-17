bf9818548975a0d31b481a6815010d1baeca5ca4
Guille <cochraneguillermo@gmail.com>
2026-04-17 06:06:24 -0300

    Eliminadas entrada de DB en desuso, por la version modularizada

--- assets/data/index.js
+++ assets/data/index.js
@@ -1,11 +1,9 @@
-import { fateChecklistData } from './data.js';
 import { asistenceChecklistData } from "./assistance.js";
 import { fateChecklist } from './fate.js';
 import { mcuChecklist } from './mcu.js';
 
 export const checklistData = {
     mcu: mcuChecklist,
-    fate: fateChecklistData,
+    fate: fateChecklist,
     assistence: asistenceChecklistData,
-    fate2: fateChecklist
 };
