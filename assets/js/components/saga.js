ae48f5b9908b7e568eebdf730a2e2fa5a440b6cc
Guille <cochraneguillermo@gmail.com>
2026-05-26 17:22:44 -0300

    sagaCreator refactorizado, Delegando la creacion del details de saga y eliminando elementos redundantes

--- assets/js/components/saga.js
+++ assets/js/components/saga.js
@@ -2,12 +2,22 @@
 
 import { createElement } from '../utilities/dom.js';
 import { shouldShowItem } from "../handlers/filterHandlers.js";
+import { calculateProgress } from '../handlers/progressHandler.js';
 import { seasonContainerCreator } from './season.js';
 
 
 // Crear las temporadas de la saga
 export function sagaCreator(sagaData, sagaID, handleToggleCheckbox, currentFilter){
-    const $sagaContainer = createElement('div'); //creamos el contendor de saga
+
+    const $details = createElement('details', 'saga');  //Creamos el elemento details, y le ponemos  el atributo open cuando corresponda, asi el details queda abierto
+    $details.dataset.sagaId = sagaData.id;              //le ponemos el atributo data-saga-id con el id de la saga
+    if (sagaData.opened) {
+        $details.setAttribute('open', 'true');
+    }
+
+    const percentage = calculateProgress(sagaData.seasons);       //calculamos el progreso de la saga
+    const $summary = sagaSummaryCreator(sagaData.saga, percentage, sagaData.id);  // Creamos el summary de la saga
+    $details.appendChild($summary);
 
     let hasVisibleItems = false; 
 
@@ -17,16 +27,26 @@ export function sagaCreator(sagaData, sagaID, handleToggleCheckbox, currentFilte
             //si se debe mostrar la season
             hasVisibleItems = true;  //lo ponemos visible ( valor booleano), para no crear el contendor del mensaje de no hay items visibles      
             const $seasonContainer = seasonContainerCreator(season, sagaID, handleToggleCheckbox); //creamos el capitulo
-            $sagaContainer.appendChild($seasonContainer); //lo agregamos al contenedor
+            $details.appendChild($seasonContainer); //lo agregamos al contenedor
         }
     } // devuelve todas las temporadas de la saga, con sus episodios
 
     if (!hasVisibleItems) {
         //si no hay items visibles
         const $noItemsMsg = createElement('p', "empty-list", "No hay elementos que coincidan con el filtro");
-        $sagaContainer.appendChild($noItemsMsg); // agregamos un mensaje de que no hay items visibles
+        $details.appendChild($noItemsMsg); // agregamos un mensaje de que no hay items visibles
     }
-    return $sagaContainer;
+
+    /* 
+        hasta aca tendriamos (2)
+        <details class="saga-details" open>
+            <summary class="saga-summary">
+                --- con todo lo del componente summary ---
+            </summary>
+            --- todas las temporadas de la saga, o el mensaje de no hay items visibles ---
+        </details>
+    */      
+    return $details;
 }
 
 // Crear el Summary de la saga, con el progreso de la misma
